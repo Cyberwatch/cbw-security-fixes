8ed1d324f4f33ee232ded00464ba1bf514d76ec0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-03 07:40:44 +0200

    USN: add 4 fixes and update 2 fixes

---
+++ Ubuntu_12.04_LTS/i386/2015/USN-2759-1.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2759-1
+#
+# Security announcement date: 2015-10-01 00:00:00 UTC
+# Script generation date:     2015-10-03 05:38:41 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - linux-image-3.2.0-91-generic-pae:3.2.0-91.129
+#   - linux-image-3.2.0-91-powerpc64-smp:3.2.0-91.129
+#   - linux-image-3.2.0-91-generic:3.2.0-91.129
+#   - linux-image-3.2.0-91-virtual:3.2.0-91.129
+#   - linux-image-3.2.0-91-omap:3.2.0-91.129
+#   - linux-image-3.2.0-91-powerpc-smp:3.2.0-91.129
+#   - linux-image-3.2.0-91-highbank:3.2.0-91.129
+#
+# Last versions recommanded by security team:
+#   - linux-image-3.2.0-91-generic-pae:3.2.0-91.129
+#   - linux-image-3.2.0-91-powerpc64-smp:3.2.0-91.129
+#   - linux-image-3.2.0-91-generic:3.2.0-91.129
+#   - linux-image-3.2.0-91-virtual:3.2.0-91.129
+#   - linux-image-3.2.0-91-omap:3.2.0-91.129
+#   - linux-image-3.2.0-91-powerpc-smp:3.2.0-91.129
+#   - linux-image-3.2.0-91-highbank:3.2.0-91.129
+#
+# CVE List:
+#   - CVE-2015-5707
+#   - CVE-2015-6252
+#   - CVE-2015-6526
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2759-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-generic-pae=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-powerpc64-smp=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-generic=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-virtual=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-omap=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-powerpc-smp=3.2.0-91.129 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-91-highbank=3.2.0-91.129 -y
---
+++ Ubuntu_12.04_LTS/x86_64/2015/USN-2760-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2760-1
+#
+# Security announcement date: 2015-10-01 00:00:00 UTC
+# Script generation date:     2015-10-03 05:38:41 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-image-3.2.0-1471-omap4:3.2.0-1471.92
+#
+# Last versions recommanded by security team:
+#   - linux-image-3.2.0-1471-omap4:3.2.0-1471.92
+#
+# CVE List:
+#   - CVE-2015-5707
+#   - CVE-2015-6252
+#   - CVE-2015-6526
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2760-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-image-3.2.0-1471-omap4=3.2.0-1471.92 -y
