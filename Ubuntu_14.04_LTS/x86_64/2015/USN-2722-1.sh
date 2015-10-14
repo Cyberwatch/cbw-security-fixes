7e3df9f8bd0e661d9a751bd12193b89e57228723
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-14 08:05:15 +0200

    USN: add 6 fixes and update 6 fixes

--- Ubuntu_14.04_LTS/x86_64/2015/USN-2722-1.sh
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2722-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2722-1
 #
 # Security announcement date: 2015-08-26 00:00:00 UTC
-# Script generation date:     2015-09-10 10:18:24 UTC
+# Script generation date:     2015-10-14 06:02:38 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.1
 #
 # Last versions recommanded by security team:
-#   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.1
+#   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.2
 #
 # CVE List:
 #   - CVE-2015-4491
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/USN-2722-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libgdk-pixbuf2.0-0=2.30.7-0ubuntu1.1 -y
+sudo apt-get install --only-upgrade libgdk-pixbuf2.0-0=2.30.7-0ubuntu1.2 -y
--- Ubuntu_14.04_LTS/x86_64/2015/USN-2767-1.sh
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2767-1.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2015
 #
-# Security fix for USN-2722-1
+# Security fix for USN-2767-1
 #
-# Security announcement date: 2015-08-26 00:00:00 UTC
-# Script generation date:     2015-10-14 06:02:38 UTC
+# Security announcement date: 2015-10-13 00:00:00 UTC
+# Script generation date:     2015-10-14 06:02:44 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
-# Architecture: i386
+# Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.1
+#   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.2
 #
 # Last versions recommanded by security team:
 #   - libgdk-pixbuf2.0-0:2.30.7-0ubuntu1.2
 #
 # CVE List:
-#   - CVE-2015-4491
+#   - CVE-2015-7673
+#   - CVE-2015-7674
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/USN-2722-1
+#   - https://www.cyberwatch.fr/notices/USN-2767-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade libgdk-pixbuf2.0-0=2.30.7-0ubuntu1.2 -y
