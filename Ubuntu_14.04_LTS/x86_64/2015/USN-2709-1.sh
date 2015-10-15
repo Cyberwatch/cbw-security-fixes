9d8ddeb441e974e9012f16a5de4b2549f7643d3a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-15 08:04:49 +0200

    USN: add 4 fixes and update 6 fixes

--- Ubuntu_14.04_LTS/x86_64/2015/USN-2709-1.sh
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2709-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2709-1
 #
 # Security announcement date: 2015-08-14 00:00:00 UTC
-# Script generation date:     2015-09-10 10:18:23 UTC
+# Script generation date:     2015-10-15 06:02:34 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - pollinate:4.7-0ubuntu1.3
 #
 # Last versions recommanded by security team:
-#   - pollinate:4.7-0ubuntu1.3
+#   - pollinate:4.7-0ubuntu1.4
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/notices/USN-2709-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade pollinate=4.7-0ubuntu1.3 -y
+sudo apt-get install --only-upgrade pollinate=4.7-0ubuntu1.4 -y
--- Ubuntu_14.04_LTS/x86_64/2015/USN-2709-2.sh
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2709-2.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2015
 #
-# Security fix for USN-2387-1
+# Security fix for USN-2709-2
 #
-# Security announcement date: 2014-10-22 00:00:00 UTC
-# Script generation date:     2015-10-15 06:01:53 UTC
+# Security announcement date: 2015-10-14 00:00:00 UTC
+# Script generation date:     2015-10-15 06:02:41 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
-# Architecture: i386
+# Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - pollinate:4.7-0ubuntu1.2
+#   - pollinate:4.7-0ubuntu1.4
 #
 # Last versions recommanded by security team:
 #   - pollinate:4.7-0ubuntu1.4
@@ -18,7 +18,7 @@
 # CVE List:
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/USN-2387-1
+#   - https://www.cyberwatch.fr/notices/USN-2709-2
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade pollinate=4.7-0ubuntu1.4 -y
