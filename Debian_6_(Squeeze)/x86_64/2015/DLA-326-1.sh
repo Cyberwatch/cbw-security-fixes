7859a0929f0ec791f730c4168b6c6245b93bfd80
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-15 20:08:45 +0200

    DLA: add 2 fixes and update 6 fixes

--- Debian_6_(Squeeze)/x86_64/2015/DLA-326-1.sh
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-326-1.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2015
 #
-# Security fix for DLA-251-2
+# Security fix for DLA-326-1
 #
-# Security announcement date: 2015-06-22 00:00:00 UTC
-# Script generation date:     2015-10-15 18:07:57 UTC
+# Security announcement date: 2015-10-15 00:00:00 UTC
+# Script generation date:     2015-10-15 18:08:02 UTC
 #
 # Operating System: Debian 6 (Squeeze)
-# Architecture: i386
+# Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - zendframework:1.10.6-1squeeze4
+#   - zendframework:1.10.6-1squeeze6
 #
 # Last versions recommanded by security team:
 #   - zendframework:1.10.6-1squeeze6
 #
 # CVE List:
+#   - CVE-2015-7695
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/DLA-251-2
+#   - https://www.cyberwatch.fr/notices/DLA-326-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade zendframework=1.10.6-1squeeze6 -y
