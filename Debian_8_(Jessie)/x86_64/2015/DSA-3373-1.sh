d361011a36987d5f4826c6f405259e07255c48c5
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-18 20:08:00 +0200

    DSA: add 2 fixes and update 2 fixes

--- Debian_8_(Jessie)/x86_64/2015/DSA-3373-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3373-1.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2015
 #
-# Security fix for DSA-3244-1
+# Security fix for DSA-3373-1
 #
-# Security announcement date: 2015-05-02 00:00:00 UTC
-# Script generation date:     2015-10-18 18:04:18 UTC
+# Security announcement date: 2015-10-18 00:00:00 UTC
+# Script generation date:     2015-10-18 18:04:33 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - owncloud:7.0.4+dfsg-4~deb8u1
+#   - owncloud:7.0.4+dfsg-4~deb8u3
 #
 # Last versions recommanded by security team:
 #   - owncloud:7.0.4+dfsg-4~deb8u3
 #
 # CVE List:
-#   - CVE-2015-3011
-#   - CVE-2015-3012
-#   - CVE-2015-3013
+#   - CVE-2015-4716
+#   - CVE-2015-4717
+#   - CVE-2015-4718
+#   - CVE-2015-5953
+#   - CVE-2015-5954
+#   - CVE-2015-6500
+#   - CVE-2015-6670
+#   - CVE-2015-7699
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/DSA-3244-1
+#   - https://www.cyberwatch.fr/notices/DSA-3373-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade owncloud=7.0.4+dfsg-4~deb8u3 -y
