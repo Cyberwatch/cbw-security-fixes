47f812115da29492b91bf29934f496f4d79f2fea
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-21 08:10:31 +0200

    CESA: add 2 fixes

---
+++ CentOS_7/x86_64/2015/CESA-2015:1917.sh
@@ -0,0 +1,29 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:1917
+#
+# Security announcement date: 2015-10-20 19:06:55 UTC
+# Script generation date:     2015-10-21 06:10:28 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libwmf:0.2.8.4-41.el7_1
+#   - libwmf-devel:0.2.8.4-41.el7_1
+#   - libwmf-lite:0.2.8.4-41.el7_1
+#
+# Last versions recommanded by security team:
+#   - libwmf:0.2.8.4-41.el7_1
+#   - libwmf-devel:0.2.8.4-41.el7_1
+#   - libwmf-lite:0.2.8.4-41.el7_1
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:1917
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libwmf-0.2.8.4 -y 
+sudo yum install libwmf-devel-0.2.8.4 -y 
+sudo yum install libwmf-lite-0.2.8.4 -y 
