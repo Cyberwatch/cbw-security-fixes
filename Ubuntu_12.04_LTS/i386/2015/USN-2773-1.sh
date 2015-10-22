f7296b0c7aab918c29726d0bdfb04816441ea1dd
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-22 20:04:41 +0200

    USN: update 2 fixes

--- Ubuntu_12.04_LTS/i386/2015/USN-2773-1.sh
+++ Ubuntu_12.04_LTS/i386/2015/USN-2773-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2773-1
 #
 # Security announcement date: 2015-10-19 00:00:00 UTC
-# Script generation date:     2015-10-20 18:02:50 UTC
+# Script generation date:     2015-10-22 18:02:33 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -19,12 +19,12 @@
 #   - linux-image-3.2.0-92-highbank:3.2.0-92.130
 #
 # Last versions recommanded by security team:
-#   - linux-image-3.2.0-92-virtual:3.2.0-92.130
-#   - linux-image-3.2.0-92-generic-pae:3.2.0-92.130
+#   - linux-image-3.2.0-92-virtual:3.2.0-92.131
+#   - linux-image-3.2.0-92-generic-pae:3.2.0-92.131
 #   - linux-image-3.2.0-92-powerpc-smp:3.2.0-92.130
 #   - linux-image-3.2.0-92-powerpc64-smp:3.2.0-92.130
 #   - linux-image-3.2.0-92-omap:3.2.0-92.130
-#   - linux-image-3.2.0-92-generic:3.2.0-92.130
+#   - linux-image-3.2.0-92-generic:3.2.0-92.131
 #   - linux-image-3.2.0-92-highbank:3.2.0-92.130
 #
 # CVE List:
@@ -35,10 +35,10 @@
 #   - https://www.cyberwatch.fr/notices/USN-2773-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade linux-image-3.2.0-92-virtual=3.2.0-92.130 -y
-sudo apt-get install --only-upgrade linux-image-3.2.0-92-generic-pae=3.2.0-92.130 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-92-virtual=3.2.0-92.131 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-92-generic-pae=3.2.0-92.131 -y
 sudo apt-get install --only-upgrade linux-image-3.2.0-92-powerpc-smp=3.2.0-92.130 -y
 sudo apt-get install --only-upgrade linux-image-3.2.0-92-powerpc64-smp=3.2.0-92.130 -y
 sudo apt-get install --only-upgrade linux-image-3.2.0-92-omap=3.2.0-92.130 -y
-sudo apt-get install --only-upgrade linux-image-3.2.0-92-generic=3.2.0-92.130 -y
+sudo apt-get install --only-upgrade linux-image-3.2.0-92-generic=3.2.0-92.131 -y
 sudo apt-get install --only-upgrade linux-image-3.2.0-92-highbank=3.2.0-92.130 -y
