7bda6a27ec45f6fbbdd83aedf61a0e9f35f6d98a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-23 20:04:54 +0200

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2015/DLA-333-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-333-1
+#
+# Security announcement date: 2015-10-23 00:00:00 UTC
+# Script generation date:     2015-10-23 18:04:52 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - cakephp:1.3.2-1.1+deb6u11
+#
+# Last versions recommanded by security team:
+#   - cakephp:1.3.2-1.1+deb6u11
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-333-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade cakephp=1.3.2-1.1+deb6u11 -y
