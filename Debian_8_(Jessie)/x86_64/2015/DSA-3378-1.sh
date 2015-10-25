fe5cd18934c1b91b0719871c81dd9b6c7565ca30
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-25 08:04:21 +0100

    DSA: add 5 fixes

---
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3378-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3378-1
+#
+# Security announcement date: 2015-10-24 00:00:00 UTC
+# Script generation date:     2015-10-25 07:04:18 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - gdk-pixbuf:2.31.1-2+deb8u3
+#
+# Last versions recommanded by security team:
+#   - gdk-pixbuf:2.31.1-2+deb8u3
+#
+# CVE List:
+#   - CVE-2015-7673
+#   - CVE-2015-7674
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3378-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade gdk-pixbuf=2.31.1-2+deb8u3 -y
