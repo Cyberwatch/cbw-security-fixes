f5a567a77a6180d2d79fbc70a44c9aea915fe7de
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-25 20:08:50 +0100

    DLA: add 2 fixes and update 8 fixes

--- Debian_6_(Squeeze)/x86_64/2015/DLA-334-1.sh
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-334-1.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2015
 #
-# Security fix for DLA-151-1
+# Security fix for DLA-334-1
 #
-# Security announcement date: 2015-02-07 00:00:00 UTC
-# Script generation date:     2015-10-25 19:07:57 UTC
+# Security announcement date: 2015-10-25 00:00:00 UTC
+# Script generation date:     2015-10-25 19:08:10 UTC
 #
 # Operating System: Debian 6 (Squeeze)
-# Architecture: i386
+# Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - libxml2:2.7.8.dfsg-2+squeeze11
+#   - libxml2:2.7.8.dfsg-2+squeeze13
 #
 # Last versions recommanded by security team:
 #   - libxml2:2.7.8.dfsg-2+squeeze13
 #
 # CVE List:
-#   - CVE-2014-0191
-#   - CVE-2014-3660
+#   - CVE-2015-7942
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/DLA-151-1
+#   - https://www.cyberwatch.fr/notices/DLA-334-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade libxml2=2.7.8.dfsg-2+squeeze13 -y
