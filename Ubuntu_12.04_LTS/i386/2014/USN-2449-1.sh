84d346d12d406d3e513e6d07a5b7123b7ccfdeb0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-28 08:04:35 +0100

    USN: add 6 fixes and update 12 fixes

--- Ubuntu_12.04_LTS/i386/2014/USN-2449-1.sh
+++ Ubuntu_12.04_LTS/i386/2014/USN-2449-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2449-1
 #
 # Security announcement date: 2014-12-22 00:00:00 UTC
-# Script generation date:     2015-09-10 10:17:51 UTC
+# Script generation date:     2015-10-28 07:01:57 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.2
 #
 # Last versions recommanded by security team:
-#   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.4
+#   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.6
 #
 # CVE List:
 #   - CVE-2014-9293
@@ -25,4 +25,4 @@
 #   - https://www.cyberwatch.fr/notices/USN-2449-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ntp=1:4.2.6.p3+dfsg-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade ntp=1:4.2.6.p3+dfsg-1ubuntu3.6 -y
--- Ubuntu_12.04_LTS/i386/2015/USN-2567-1.sh
+++ Ubuntu_12.04_LTS/i386/2015/USN-2567-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2567-1
 #
 # Security announcement date: 2015-04-13 00:00:00 UTC
-# Script generation date:     2015-09-10 10:18:06 UTC
+# Script generation date:     2015-10-28 07:02:12 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.4
 #
 # Last versions recommanded by security team:
-#   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.4
+#   - ntp:1:4.2.6.p3+dfsg-1ubuntu3.6
 #
 # CVE List:
 #   - CVE-2015-1798
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/USN-2567-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ntp=1:4.2.6.p3+dfsg-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade ntp=1:4.2.6.p3+dfsg-1ubuntu3.6 -y
