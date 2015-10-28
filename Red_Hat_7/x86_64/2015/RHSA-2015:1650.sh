f24342838f90a9de77e9401a0712c8673bae02ef
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-28 08:23:15 +0100

    RHSA: add one fix and update 2 fixes

--- Red_Hat_7/x86_64/2015/RHSA-2015:1650.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:1650.sh
@@ -3,7 +3,7 @@
 # Security fix for RHSA-2015:1650
 #
 # Security announcement date: 2015-08-20 19:26:33 UTC
-# Script generation date:     2015-09-10 09:47:51 UTC
+# Script generation date:     2015-10-28 07:20:20 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
@@ -17,12 +17,12 @@
 #   - tuned-profiles-openshift-node:3.0.1.0-1.git.527.f8d5fed.el7ose
 #
 # Last versions recommanded by security team:
-#   - openshift:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-clients:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-master:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-node:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-sdn-ovs:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - tuned-profiles-openshift-node:3.0.1.0-1.git.529.dcab62c.el7ose
+#   - openshift:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-clients:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-master:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-node:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-sdn-ovs:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - tuned-profiles-openshift-node:3.0.2.0-0.git.20.656dc3e.el7ose
 #
 # CVE List:
 #   - CVE-2015-5222
@@ -31,9 +31,9 @@
 #   - https://www.cyberwatch.fr/notices/RHSA-2015:1650
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo yum install openshift-3.0.1.0 -y 
-sudo yum install openshift-clients-3.0.1.0 -y 
-sudo yum install openshift-master-3.0.1.0 -y 
-sudo yum install openshift-node-3.0.1.0 -y 
-sudo yum install openshift-sdn-ovs-3.0.1.0 -y 
-sudo yum install tuned-profiles-openshift-node-3.0.1.0 -y 
+sudo yum install openshift-3.0.2.0 -y 
+sudo yum install openshift-clients-3.0.2.0 -y 
+sudo yum install openshift-master-3.0.2.0 -y 
+sudo yum install openshift-node-3.0.2.0 -y 
+sudo yum install openshift-sdn-ovs-3.0.2.0 -y 
+sudo yum install tuned-profiles-openshift-node-3.0.2.0 -y 
--- Red_Hat_7/x86_64/2015/RHSA-2015:1945.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:1945.sh
@@ -1,20 +1,20 @@
 # CYBERWATCH SAS - 2015
 #
-# Security fix for RHSA-2015:1736
+# Security fix for RHSA-2015:1945
 #
-# Security announcement date: 2015-09-04 03:05:02 UTC
-# Script generation date:     2015-10-28 07:20:27 UTC
+# Security announcement date: 2015-10-27 20:24:53 UTC
+# Script generation date:     2015-10-28 07:20:38 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - openshift:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-clients:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-master:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-node:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - openshift-sdn-ovs:3.0.1.0-1.git.529.dcab62c.el7ose
-#   - tuned-profiles-openshift-node:3.0.1.0-1.git.529.dcab62c.el7ose
+#   - openshift:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-clients:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-master:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-node:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - openshift-sdn-ovs:3.0.2.0-0.git.20.656dc3e.el7ose
+#   - tuned-profiles-openshift-node:3.0.2.0-0.git.20.656dc3e.el7ose
 #
 # Last versions recommanded by security team:
 #   - openshift:3.0.2.0-0.git.20.656dc3e.el7ose
@@ -25,10 +25,10 @@
 #   - tuned-profiles-openshift-node:3.0.2.0-0.git.20.656dc3e.el7ose
 #
 # CVE List:
-#   - CVE-2015-5250
+#   - CVE-2015-5305
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/RHSA-2015:1736
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:1945
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo yum install openshift-3.0.2.0 -y 
