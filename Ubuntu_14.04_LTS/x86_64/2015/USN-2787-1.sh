9fddbb18b03cc68a7f2241976bf5e0613e96a33d
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-29 08:02:37 +0100

    USN: add 6 fixes

---
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2787-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2787-1
+#
+# Security announcement date: 2015-10-28 00:00:00 UTC
+# Script generation date:     2015-10-29 07:02:34 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libaudiofile1:0.3.6-2ubuntu0.14.04.1
+#
+# Last versions recommanded by security team:
+#   - libaudiofile1:0.3.6-2ubuntu0.14.04.1
+#
+# CVE List:
+#   - CVE-2015-7747
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2787-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libaudiofile1=0.3.6-2ubuntu0.14.04.1 -y
