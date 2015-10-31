161165ad16c6666b535be4e89c165c71bb231176
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-10-31 08:04:01 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-337-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-337-1
+#
+# Security announcement date: 2015-10-31 00:00:00 UTC
+# Script generation date:     2015-10-31 07:03:59 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - busybox:1:1.17.1-8+deb6u11
+#
+# Last versions recommanded by security team:
+#   - busybox:1:1.17.1-8+deb6u11
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-337-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade busybox=1:1.17.1-8+deb6u11 -y
