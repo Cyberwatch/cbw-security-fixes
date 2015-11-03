4cce7e0a9430630403c8d03484449f6130a73830
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-03 20:04:01 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2015/DLA-339-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-339-1
+#
+# Security announcement date: 2015-11-03 00:00:00 UTC
+# Script generation date:     2015-11-03 19:03:59 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libhtml-scrubber-perl:0.08-4+deb6u1
+#
+# Last versions recommanded by security team:
+#   - libhtml-scrubber-perl:0.08-4+deb6u1
+#
+# CVE List:
+#   - CVE-2015-5667
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-339-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libhtml-scrubber-perl=0.08-4+deb6u1 -y
