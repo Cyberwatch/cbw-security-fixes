b38ea344cfe8a4e30708d87d0e619fc63b1fb875
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-04 20:10:37 +0100

    CESA: update one fix

--- CentOS_7/x86_64/2015/CESA-2015:1979.sh
+++ CentOS_7/x86_64/2015/CESA-2015:1979.sh
@@ -3,7 +3,7 @@
 # Security fix for CESA-2015:1979
 #
 # Security announcement date: 2015-11-03 21:50:51 UTC
-# Script generation date:     2015-11-04 07:07:20 UTC
+# Script generation date:     2015-11-04 19:08:45 UTC
 #
 # Operating System: CentOS 7
 # Architecture: x86_64
@@ -15,6 +15,7 @@
 #   - libreswan:3.15-5.el7_1
 #
 # CVE List:
+#   - CVE-2015-3240
 #
 # More details:
 #   - https://www.cyberwatch.fr/notices/CESA-2015:1979
