28944e0f6f7a501286e65b5bca5e6edc93cc7f83
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-04 08:09:11 +0100

    CESA: add one fix and update one fix

--- CentOS_7/x86_64/2015/CESA-2015:1979.sh
+++ CentOS_7/x86_64/2015/CESA-2015:1979.sh
@@ -1,24 +1,23 @@
 # CYBERWATCH SAS - 2015
 #
-# Security fix for CESA-2015:1154
+# Security fix for CESA-2015:1979
 #
-# Security announcement date: 2015-06-24 03:33:57 UTC
-# Script generation date:     2015-11-04 07:06:47 UTC
+# Security announcement date: 2015-11-03 21:50:51 UTC
+# Script generation date:     2015-11-04 07:07:20 UTC
 #
 # Operating System: CentOS 7
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - libreswan:3.12-10.1.el7_1
+#   - libreswan:3.15-5.el7_1
 #
 # Last versions recommanded by security team:
 #   - libreswan:3.15-5.el7_1
 #
 # CVE List:
-#   - CVE-2015-3204
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/CESA-2015:1154
+#   - https://www.cyberwatch.fr/notices/CESA-2015:1979
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo yum install libreswan-3.15 -y 
