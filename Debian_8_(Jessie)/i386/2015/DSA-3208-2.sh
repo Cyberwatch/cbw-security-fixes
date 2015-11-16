0f0735a3a0f0f518c41691a4a10493d8923c363e
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-16 08:03:10 +0100

    DSA: add 5 fixes

---
+++ Debian_8_(Jessie)/i386/2015/DSA-3208-2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3208-2
+#
+# Security announcement date: 2015-11-14 00:00:00 UTC
+# Script generation date:     2015-11-16 07:03:07 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - freexl:1.0.0g-1+deb8u3
+#
+# Last versions recommanded by security team:
+#   - freexl:1.0.0g-1+deb8u3
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3208-2
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade freexl=1.0.0g-1+deb8u3 -y
