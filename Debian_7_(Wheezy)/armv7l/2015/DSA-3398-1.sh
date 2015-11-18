9572f190c687b1db8f50c142b02691ae97e78df4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-18 08:08:09 +0100

    DSA: add 5 fixes and update 8 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2015/DSA-3398-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3398-1
+#
+# Security announcement date: 2015-11-16 00:00:00 UTC
+# Script generation date:     2015-11-18 07:05:01 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - strongswan:4.5.2-1.5+deb7u8
+#
+# Last versions recommanded by security team:
+#   - strongswan:4.5.2-1.5+deb7u7
+#
+# CVE List:
+#   - CVE-2015-8023
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3398-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade strongswan=4.5.2-1.5+deb7u7 -y
---
+++ Debian_8_(Jessie)/i386/2015/DSA-3398-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3398-1
+#
+# Security announcement date: 2015-11-16 00:00:00 UTC
+# Script generation date:     2015-11-18 07:05:01 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - strongswan:5.2.1-6+deb8u2
+#
+# Last versions recommanded by security team:
+#   - strongswan:5.2.1-6+deb8u2
+#
+# CVE List:
+#   - CVE-2015-8023
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3398-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade strongswan=5.2.1-6+deb8u2 -y
