364c1cb4034d59efe6fe269b745fb400f7d4110b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-19 20:08:15 +0100

    DSA: update 14 fixes

--- Debian_6_(Squeeze)/x86_64/2013/DSA-2820-1.sh
+++ Debian_6_(Squeeze)/x86_64/2013/DSA-2820-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2820-1
 #
 # Security announcement date: 2013-12-17 00:00:00 UTC
-# Script generation date:     2015-09-10 10:01:49 UTC
+# Script generation date:     2015-11-19 19:04:30 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - nspr:4.8.6-1+squeeze1
 #
 # Last versions recommanded by security team:
-#   - nspr:4.8.6-1+squeeze2
+#   - nspr:4.8.6-1+squeeze3
 #
 # CVE List:
 #   - CVE-2013-5607
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2820-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade nspr=4.8.6-1+squeeze2 -y
+sudo apt-get install --only-upgrade nspr=4.8.6-1+squeeze3 -y
--- Debian_8_(Jessie)/x86_64/2015/DSA-3391-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3391-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3391-1
 #
 # Security announcement date: 2015-11-03 00:00:00 UTC
-# Script generation date:     2015-11-04 07:04:14 UTC
+# Script generation date:     2015-11-19 19:05:16 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -16,6 +16,7 @@
 #   - php-horde:5.2.1+debian0-2+deb8u2
 #
 # CVE List:
+#   - CVE-2015-7984
 #
 # More details:
 #   - https://www.cyberwatch.fr/notices/DSA-3391-1
