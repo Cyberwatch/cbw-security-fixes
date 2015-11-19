cb45a4b0aa21b775d1aa4b9b5b058c55e5f1b0a4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-19 20:09:13 +0100

    DLA: update 6 fixes

--- Debian_6_(Squeeze)/i386/2014/DLA-32-1.sh
+++ Debian_6_(Squeeze)/i386/2014/DLA-32-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-32-1
 #
 # Security announcement date: 2014-08-07 00:00:00 UTC
-# Script generation date:     2015-09-10 10:03:00 UTC
+# Script generation date:     2015-11-19 19:08:20 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - nspr:4.8.6-1+squeeze2
 #
 # Last versions recommanded by security team:
-#   - nspr:4.8.6-1+squeeze2
+#   - nspr:4.8.6-1+squeeze3
 #
 # CVE List:
 #   - CVE-2014-1545
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-32-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade nspr=4.8.6-1+squeeze2 -y
+sudo apt-get install --only-upgrade nspr=4.8.6-1+squeeze3 -y
