f267a66c8215fd072e42c4ec4c4a6cb5843adb66
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-19 08:01:52 +0100

    USN: add 4 fixes

---
+++ Ubuntu_15.04/i686/2015/USN-2813-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2813-1
+#
+# Security announcement date: 2015-11-17 00:00:00 UTC
+# Script generation date:     2015-11-19 07:01:49 UTC
+#
+# Operating System: Ubuntu 15.04
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - lxcfs:0.7-0ubuntu4.1
+#
+# Last versions recommanded by security team:
+#   - lxcfs:0.7-0ubuntu4.1
+#
+# CVE List:
+#   - CVE-2015-1342
+#   - CVE-2015-1344
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2813-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade lxcfs=0.7-0ubuntu4.1 -y
