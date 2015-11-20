230ead2356e76bdad990aef72ec9438b5723673b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-20 08:01:51 +0100

    USN: add 8 fixes

---
+++ Ubuntu_15.04/i686/2015/USN-2814-1.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2814-1
+#
+# Security announcement date: 2015-11-18 00:00:00 UTC
+# Script generation date:     2015-11-20 07:01:47 UTC
+#
+# Operating System: Ubuntu 15.04
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 304.131-0ubuntu0.15.04.1:304.131-0ubuntu0.15.04.1
+#   - 304.131-0ubuntu0.15.04.1:304.131-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#
+# Last versions recommanded by security team:
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 340.96-0ubuntu0.15.04.1:340.96-0ubuntu0.15.04.1
+#   - 304.131-0ubuntu0.15.04.1:304.131-0ubuntu0.15.04.1
+#   - 304.131-0ubuntu0.15.04.1:304.131-0ubuntu0.15.04.1
+#   - 352.63-0ubuntu0.15.04.1:352.63-0ubuntu0.15.04.1
+#
+# CVE List:
+#   - CVE-2015-7869
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2814-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade 340.96-0ubuntu0.15.04.1=340.96-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 352.63-0ubuntu0.15.04.1=352.63-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 352.63-0ubuntu0.15.04.1=352.63-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 352.63-0ubuntu0.15.04.1=352.63-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 340.96-0ubuntu0.15.04.1=340.96-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 340.96-0ubuntu0.15.04.1=340.96-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 340.96-0ubuntu0.15.04.1=340.96-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 304.131-0ubuntu0.15.04.1=304.131-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 304.131-0ubuntu0.15.04.1=304.131-0ubuntu0.15.04.1 -y
+sudo apt-get install --only-upgrade 352.63-0ubuntu0.15.04.1=352.63-0ubuntu0.15.04.1 -y
---
+++ Ubuntu_15.10/x86_64/2015/USN-2814-1.sh
@@ -0,0 +1,41 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2814-1
+#
+# Security announcement date: 2015-11-18 00:00:00 UTC
+# Script generation date:     2015-11-20 07:01:48 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 304.131-0ubuntu0.15.10.1:304.131-0ubuntu0.15.10.1
+#   - 304.131-0ubuntu0.15.10.1:304.131-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#
+# Last versions recommanded by security team:
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 340.96-0ubuntu0.15.10.1:340.96-0ubuntu0.15.10.1
+#   - 304.131-0ubuntu0.15.10.1:304.131-0ubuntu0.15.10.1
+#   - 304.131-0ubuntu0.15.10.1:304.131-0ubuntu0.15.10.1
+#   - 352.63-0ubuntu0.15.10.1:352.63-0ubuntu0.15.10.1
+#
+# CVE List:
+#   - CVE-2015-7869
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2814-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
