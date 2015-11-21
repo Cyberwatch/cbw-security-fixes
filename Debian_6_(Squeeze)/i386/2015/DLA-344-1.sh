785aca9cc7fe15dca1c8082455173bf399ebed10
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-21 08:03:36 +0100

    DLA: add 4 fixes

---
+++ Debian_6_(Squeeze)/i386/2015/DLA-344-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-344-1
+#
+# Security announcement date: 2015-11-19 00:00:00 UTC
+# Script generation date:     2015-11-21 07:03:34 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - nspr:4.8.6-1+squeeze3
+#
+# Last versions recommanded by security team:
+#   - nspr:4.8.6-1+squeeze3
+#
+# CVE List:
+#   - CVE-2015-7183
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-344-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade nspr=4.8.6-1+squeeze3 -y
---
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-345-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-345-1
+#
+# Security announcement date: 2015-11-19 00:00:00 UTC
+# Script generation date:     2015-11-21 07:03:34 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - strongswan:4.4.1-5.8
+#
+# Last versions recommanded by security team:
+#   - strongswan:4.4.1-5.8
+#
+# CVE List:
+#   - CVE-2015-8023
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-345-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade strongswan=4.4.1-5.8 -y
