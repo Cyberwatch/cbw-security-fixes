5dbcec97d66755f66630a2b943b53e0569fcf4c4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-21 20:08:36 +0100

    RHSA: add one fix

---
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2078.sh
@@ -0,0 +1,58 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2078
+#
+# Security announcement date: 2015-11-19 09:04:11 UTC
+# Script generation date:     2015-11-21 19:08:34 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - postgresql:9.2.14-1.el7_1
+#   - postgresql-contrib:9.2.14-1.el7_1
+#   - postgresql-debuginfo:9.2.14-1.el7_1
+#   - postgresql-devel:9.2.14-1.el7_1
+#   - postgresql-docs:9.2.14-1.el7_1
+#   - postgresql-libs:9.2.14-1.el7_1
+#   - postgresql-plperl:9.2.14-1.el7_1
+#   - postgresql-plpython:9.2.14-1.el7_1
+#   - postgresql-pltcl:9.2.14-1.el7_1
+#   - postgresql-server:9.2.14-1.el7_1
+#   - postgresql-test:9.2.14-1.el7_1
+#   - postgresql-upgrade:9.2.14-1.el7_1
+#
+# Last versions recommanded by security team:
+#   - postgresql:9.2.14-1.el7_1
+#   - postgresql-contrib:9.2.14-1.el7_1
+#   - postgresql-debuginfo:9.2.14-1.el7_1
+#   - postgresql-devel:9.2.14-1.el7_1
+#   - postgresql-docs:9.2.14-1.el7_1
+#   - postgresql-libs:9.2.14-1.el7_1
+#   - postgresql-plperl:9.2.14-1.el7_1
+#   - postgresql-plpython:9.2.14-1.el7_1
+#   - postgresql-pltcl:9.2.14-1.el7_1
+#   - postgresql-server:9.2.14-1.el7_1
+#   - postgresql-test:9.2.14-1.el7_1
+#   - postgresql-upgrade:9.2.14-1.el7_1
+#
+# CVE List:
+#   - CVE-2015-5288
+#   - CVE-2015-5289
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2078
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install postgresql-9.2.14 -y 
+sudo yum install postgresql-contrib-9.2.14 -y 
+sudo yum install postgresql-debuginfo-9.2.14 -y 
+sudo yum install postgresql-devel-9.2.14 -y 
+sudo yum install postgresql-docs-9.2.14 -y 
+sudo yum install postgresql-libs-9.2.14 -y 
+sudo yum install postgresql-plperl-9.2.14 -y 
+sudo yum install postgresql-plpython-9.2.14 -y 
+sudo yum install postgresql-pltcl-9.2.14 -y 
+sudo yum install postgresql-server-9.2.14 -y 
+sudo yum install postgresql-test-9.2.14 -y 
+sudo yum install postgresql-upgrade-9.2.14 -y 
