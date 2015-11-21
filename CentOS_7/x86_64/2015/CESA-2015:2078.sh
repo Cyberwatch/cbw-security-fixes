bf2cf282ff2635c2053db11c9b519bd483d2efbd
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-21 08:06:12 +0100

    CESA: add 5 fixes

---
+++ CentOS_7/x86_64/2015/CESA-2015:2078.sh
@@ -0,0 +1,55 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:2078
+#
+# Security announcement date: 2015-11-18 20:39:01 UTC
+# Script generation date:     2015-11-21 07:06:09 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - postgresql:9.2.14-1.el7_1
+#   - postgresql-contrib:9.2.14-1.el7_1
+#   - postgresql-devel:9.2.14-1.el7_1
+#   - postgresql-docs:9.2.14-1.el7_1
+#   - postgresql-libs:9.2.14-1.el7_1
+#   - postgresql-plperl:9.2.14-1.el7_1
+#   - postgresql-plpython:9.2.14-1.el7_1
+#   - postgresql-pltcl:9.2.14-1.el7_1
+#   - postgresql-server:9.2.14-1.el7_1
+#   - postgresql-test:9.2.14-1.el7_1
+#   - postgresql-upgrade:9.2.14-1.el7_1
+#
+# Last versions recommanded by security team:
+#   - postgresql:9.2.14-1.el7_1
+#   - postgresql-contrib:9.2.14-1.el7_1
+#   - postgresql-devel:9.2.14-1.el7_1
+#   - postgresql-docs:9.2.14-1.el7_1
+#   - postgresql-libs:9.2.14-1.el7_1
+#   - postgresql-plperl:9.2.14-1.el7_1
+#   - postgresql-plpython:9.2.14-1.el7_1
+#   - postgresql-pltcl:9.2.14-1.el7_1
+#   - postgresql-server:9.2.14-1.el7_1
+#   - postgresql-test:9.2.14-1.el7_1
+#   - postgresql-upgrade:9.2.14-1.el7_1
+#
+# CVE List:
+#   - CVE-2015-5288
+#   - CVE-2015-5289
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:2078
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install postgresql-9.2.14 -y 
+sudo yum install postgresql-contrib-9.2.14 -y 
+sudo yum install postgresql-devel-9.2.14 -y 
+sudo yum install postgresql-docs-9.2.14 -y 
+sudo yum install postgresql-libs-9.2.14 -y 
+sudo yum install postgresql-plperl-9.2.14 -y 
+sudo yum install postgresql-plpython-9.2.14 -y 
+sudo yum install postgresql-pltcl-9.2.14 -y 
+sudo yum install postgresql-server-9.2.14 -y 
+sudo yum install postgresql-test-9.2.14 -y 
+sudo yum install postgresql-upgrade-9.2.14 -y 
