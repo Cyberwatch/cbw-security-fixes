c76d59bcbdbf2f53c42f572a5d2abf70582a2341
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-23 08:08:46 +0100

    RHSA: add 3 fixes

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2500.sh
@@ -0,0 +1,24 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2500
+#
+# Security announcement date: 2015-11-20 19:20:32 UTC
+# Script generation date:     2015-11-23 07:08:44 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - apache-commons-collections-eap6:3.2.1-16.redhat_5.1.ep6.el6
+#
+# Last versions recommanded by security team:
+#   - apache-commons-collections-eap6:3.2.1-16.redhat_5.1.ep6.el6
+#
+# CVE List:
+#   - CVE-2015-7501
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2500
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install apache-commons-collections-eap6-3.2.1 -y 
