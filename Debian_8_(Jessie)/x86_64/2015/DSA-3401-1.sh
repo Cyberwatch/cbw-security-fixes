6abadc8341985d4a064814bbb4d3f52b6b7939ba
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-24 08:06:47 +0100

    DSA: add 5 fixes and update 2 fixes

---
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3401-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3401-1
+#
+# Security announcement date: 2015-11-22 00:00:00 UTC
+# Script generation date:     2015-11-24 07:03:14 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - openjdk-7:7u91-2.6.3-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - openjdk-7:7u91-2.6.3-1~deb8u1
+#
+# CVE List:
+#   - CVE-2015-4871
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3401-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openjdk-7=7u91-2.6.3-1~deb8u1 -y
