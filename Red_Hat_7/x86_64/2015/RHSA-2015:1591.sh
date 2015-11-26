9d13c1008ad6a84565baf0cf62c04da0b675cfc9
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-26 20:12:48 +0100

    RHSA: update one fix

--- Red_Hat_7/x86_64/2015/RHSA-2015:1591.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:1591.sh
@@ -3,34 +3,34 @@
 # Security fix for RHSA-2015:1591
 #
 # Security announcement date: 2015-08-12 15:09:50 UTC
-# Script generation date:     2015-11-24 07:13:05 UTC
+# Script generation date:     2015-11-26 19:12:27 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
 #   - hiera:1.3.1-2.el7
-#   - ruby193-rubygem-deep_cloneable:2.0.0-4.el7
-#   - ruby193-rubygem-friendly_id:4.0.10.1-1.el7
-#   - ruby193-rubygem-ldap_fluff:0.3.2-1.el7
-#   - ruby193-rubygem-secure_headers:1.3.3-1.el7
 #   - rubygem-apipie-bindings:0.0.11-1.el7
 #   - ruby-augeas:0.5.0-1.el7
 #   - ruby-augeas-debuginfo:0.5.0-1.el7
 #   - ruby-shadow:1.4.1-21.el7
 #   - ruby-shadow-debuginfo:1.4.1-21.el7
+#   - ruby193-rubygem-deep_cloneable:2.0.0-4.el7
+#   - ruby193-rubygem-friendly_id:4.0.10.1-1.el7
+#   - ruby193-rubygem-ldap_fluff:0.3.2-1.el7
+#   - ruby193-rubygem-secure_headers:1.3.3-1.el7
 #
 # Last versions recommanded by security team:
 #   - hiera:1.3.1-2.el7
-#   - ruby193-rubygem-deep_cloneable:2.0.0-4.el7
-#   - ruby193-rubygem-friendly_id:4.0.10.1-1.el7
-#   - ruby193-rubygem-ldap_fluff:0.3.2-1.el7
-#   - ruby193-rubygem-secure_headers:1.3.3-1.el7
 #   - rubygem-apipie-bindings:0.0.11-1.el7
 #   - ruby-augeas:0.5.0-1.el7
 #   - ruby-augeas-debuginfo:0.5.0-1.el7
 #   - ruby-shadow:1.4.1-21.el7
 #   - ruby-shadow-debuginfo:1.4.1-21.el7
+#   - ruby193-rubygem-deep_cloneable:2.0.0-4.el7
+#   - ruby193-rubygem-friendly_id:4.0.10.1-1.el7
+#   - ruby193-rubygem-ldap_fluff:0.3.2-1.el7
+#   - ruby193-rubygem-secure_headers:1.3.3-1.el7
 #
 # CVE List:
 #   - CVE-2013-4346
@@ -46,12 +46,12 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo yum install hiera-1.3.1 -y 
-sudo yum install ruby193-rubygem-deep_cloneable-2.0.0 -y 
-sudo yum install ruby193-rubygem-friendly_id-4.0.10.1 -y 
-sudo yum install ruby193-rubygem-ldap_fluff-0.3.2 -y 
-sudo yum install ruby193-rubygem-secure_headers-1.3.3 -y 
 sudo yum install rubygem-apipie-bindings-0.0.11 -y 
 sudo yum install ruby-augeas-0.5.0 -y 
 sudo yum install ruby-augeas-debuginfo-0.5.0 -y 
 sudo yum install ruby-shadow-1.4.1 -y 
 sudo yum install ruby-shadow-debuginfo-1.4.1 -y 
+sudo yum install ruby193-rubygem-deep_cloneable-2.0.0 -y 
+sudo yum install ruby193-rubygem-friendly_id-4.0.10.1 -y 
+sudo yum install ruby193-rubygem-ldap_fluff-0.3.2 -y 
+sudo yum install ruby193-rubygem-secure_headers-1.3.3 -y 
