cdcfd41eeb1a88fcfad9e421033042ddf486224b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-27 20:13:04 +0100

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2515.sh
@@ -0,0 +1,62 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2515
+#
+# Security announcement date: 2015-11-25 18:28:18 UTC
+# Script generation date:     2015-11-27 19:13:02 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - git19-emacs-git:1.9.4-3.el6.1
+#   - git19-emacs-git-el:1.9.4-3.el6.1
+#   - git19-git-all:1.9.4-3.el6.1
+#   - git19-git-cvs:1.9.4-3.el6.1
+#   - git19-git-email:1.9.4-3.el6.1
+#   - git19-git-gui:1.9.4-3.el6.1
+#   - git19-gitk:1.9.4-3.el6.1
+#   - git19-gitweb:1.9.4-3.el6.1
+#   - git19-perl-Git:1.9.4-3.el6.1
+#   - git19-perl-Git-SVN:1.9.4-3.el6.1
+#   - git19-git:1.9.4-3.el6.1
+#   - git19-git-daemon:1.9.4-3.el6.1
+#   - git19-git-debuginfo:1.9.4-3.el6.1
+#   - git19-git-svn:1.9.4-3.el6.1
+#
+# Last versions recommanded by security team:
+#   - git19-emacs-git:1.9.4-3.el6.1
+#   - git19-emacs-git-el:1.9.4-3.el6.1
+#   - git19-git-all:1.9.4-3.el6.1
+#   - git19-git-cvs:1.9.4-3.el6.1
+#   - git19-git-email:1.9.4-3.el6.1
+#   - git19-git-gui:1.9.4-3.el6.1
+#   - git19-gitk:1.9.4-3.el6.1
+#   - git19-gitweb:1.9.4-3.el6.1
+#   - git19-perl-Git:1.9.4-3.el6.1
+#   - git19-perl-Git-SVN:1.9.4-3.el6.1
+#   - git19-git:1.9.4-3.el6.1
+#   - git19-git-daemon:1.9.4-3.el6.1
+#   - git19-git-debuginfo:1.9.4-3.el6.1
+#   - git19-git-svn:1.9.4-3.el6.1
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2515
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install git19-emacs-git-1.9.4 -y 
+sudo yum install git19-emacs-git-el-1.9.4 -y 
+sudo yum install git19-git-all-1.9.4 -y 
+sudo yum install git19-git-cvs-1.9.4 -y 
+sudo yum install git19-git-email-1.9.4 -y 
+sudo yum install git19-git-gui-1.9.4 -y 
+sudo yum install git19-gitk-1.9.4 -y 
+sudo yum install git19-gitweb-1.9.4 -y 
+sudo yum install git19-perl-Git-1.9.4 -y 
+sudo yum install git19-perl-Git-SVN-1.9.4 -y 
+sudo yum install git19-git-1.9.4 -y 
+sudo yum install git19-git-daemon-1.9.4 -y 
+sudo yum install git19-git-debuginfo-1.9.4 -y 
+sudo yum install git19-git-svn-1.9.4 -y 
