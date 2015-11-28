670aca269ef4bf1f0a0f6e1f39f446f9e5e3cf3d
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-28 08:02:02 +0100

    USN: add 8 fixes

---
+++ Ubuntu_15.04/x86_64/2015/USN-2820-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2820-1
+#
+# Security announcement date: 2015-11-26 00:00:00 UTC
+# Script generation date:     2015-11-28 07:01:59 UTC
+#
+# Operating System: Ubuntu 15.04
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - dpkg:1.17.25ubuntu1.1
+#
+# Last versions recommanded by security team:
+#   - dpkg:1.17.25ubuntu1.1
+#
+# CVE List:
+#   - CVE-2015-0860
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2820-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade dpkg=1.17.25ubuntu1.1 -y
---
+++ Ubuntu_15.10/i686/2015/USN-2820-1.sh
@@ -0,0 +1,23 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2820-1
+#
+# Security announcement date: 2015-11-26 00:00:00 UTC
+# Script generation date:     2015-11-28 07:01:59 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - dpkg:1.18.2ubuntu5.1
+#
+# Last versions recommanded by security team:
+#   - dpkg:1.18.2ubuntu5.1
+#
+# CVE List:
+#   - CVE-2015-0860
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2820-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
