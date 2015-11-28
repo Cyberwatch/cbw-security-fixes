c068909436f250f6cb4f47cb35b125d88faaa373
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-28 08:11:11 +0100

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2518.sh
@@ -0,0 +1,44 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2518
+#
+# Security announcement date: 2015-11-25 21:38:00 UTC
+# Script generation date:     2015-11-28 07:11:09 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.5.0-ibm:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-demo:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-devel:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-javacomm:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-src:1.5.0.16.14-1jpp.1.el6_7
+#
+# Last versions recommanded by security team:
+#   - java-1.5.0-ibm:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-demo:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-devel:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-javacomm:1.5.0.16.14-1jpp.1.el6_7
+#   - java-1.5.0-ibm-src:1.5.0.16.14-1jpp.1.el6_7
+#
+# CVE List:
+#   - CVE-2015-4805
+#   - CVE-2015-4806
+#   - CVE-2015-4843
+#   - CVE-2015-4844
+#   - CVE-2015-4860
+#   - CVE-2015-4872
+#   - CVE-2015-4883
+#   - CVE-2015-4902
+#   - CVE-2015-4903
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2518
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.5.0-ibm-1.5.0.16.14 -y 
+sudo yum install java-1.5.0-ibm-demo-1.5.0.16.14 -y 
+sudo yum install java-1.5.0-ibm-devel-1.5.0.16.14 -y 
+sudo yum install java-1.5.0-ibm-javacomm-1.5.0.16.14 -y 
+sudo yum install java-1.5.0-ibm-src-1.5.0.16.14 -y 
