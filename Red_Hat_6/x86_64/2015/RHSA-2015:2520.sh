c3919660de40547be79fb272660ed8f92fda97bd
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-28 20:11:53 +0100

    RHSA: add 4 fixes

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2520.sh
@@ -0,0 +1,51 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2520
+#
+# Security announcement date: 2015-11-26 14:16:17 UTC
+# Script generation date:     2015-11-28 19:11:51 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - ntp:4.2.6p5-2.el6_5.1
+#   - ntp-debuginfo:4.2.6p5-2.el6_5.1
+#   - ntpdate:4.2.6p5-2.el6_5.1
+#   - ntp:4.2.6p5-3.el6_6.1
+#   - ntp-debuginfo:4.2.6p5-3.el6_6.1
+#   - ntpdate:4.2.6p5-3.el6_6.1
+#   - ntp-doc:4.2.6p5-2.el6_5.1
+#   - ntp-perl:4.2.6p5-2.el6_5.1
+#   - ntp-doc:4.2.6p5-3.el6_6.1
+#   - ntp-perl:4.2.6p5-3.el6_6.1
+#
+# Last versions recommanded by security team:
+#   - ntp:4.2.6p5-3.el6_6.1
+#   - ntp-debuginfo:4.2.6p5-3.el6_6.1
+#   - ntpdate:4.2.6p5-3.el6_6.1
+#   - ntp:4.2.6p5-3.el6_6.1
+#   - ntp-debuginfo:4.2.6p5-3.el6_6.1
+#   - ntpdate:4.2.6p5-3.el6_6.1
+#   - ntp-doc:4.2.6p5-3.el6_6.1
+#   - ntp-perl:4.2.6p5-3.el6_6.1
+#   - ntp-doc:4.2.6p5-3.el6_6.1
+#   - ntp-perl:4.2.6p5-3.el6_6.1
+#
+# CVE List:
+#   - CVE-2015-7704
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2520
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install ntp-4.2.6p5 -y 
+sudo yum install ntp-debuginfo-4.2.6p5 -y 
+sudo yum install ntpdate-4.2.6p5 -y 
+sudo yum install ntp-4.2.6p5 -y 
+sudo yum install ntp-debuginfo-4.2.6p5 -y 
+sudo yum install ntpdate-4.2.6p5 -y 
+sudo yum install ntp-doc-4.2.6p5 -y 
+sudo yum install ntp-perl-4.2.6p5 -y 
+sudo yum install ntp-doc-4.2.6p5 -y 
+sudo yum install ntp-perl-4.2.6p5 -y 
