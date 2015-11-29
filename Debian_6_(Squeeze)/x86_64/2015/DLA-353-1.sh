4fc21f5d633a73a759d54e7e487b842d5558fed0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-29 08:03:45 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-353-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-353-1
+#
+# Security announcement date: 2015-11-27 00:00:00 UTC
+# Script generation date:     2015-11-29 07:03:43 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - imagemagick:8:6.6.0.4-3+squeeze7
+#
+# Last versions recommanded by security team:
+#   - imagemagick:8:6.6.0.4-3+squeeze4
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-353-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade imagemagick=8:6.6.0.4-3+squeeze4 -y
