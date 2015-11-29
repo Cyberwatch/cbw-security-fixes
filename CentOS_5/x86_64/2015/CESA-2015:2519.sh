c27723705ff35987dc63b4007bddabcc92fa2133
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-11-29 08:06:34 +0100

    CESA: add 2 fixes

---
+++ CentOS_5/x86_64/2015/CESA-2015:2519.sh
@@ -0,0 +1,30 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:2519
+#
+# Security announcement date: 2015-11-27 06:41:12 UTC
+# Script generation date:     2015-11-29 07:06:31 UTC
+#
+# Operating System: CentOS 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird:38.4.0-1.el5.centos
+#
+# Last versions recommanded by security team:
+#   - thunderbird:38.4.0-1.el5.centos
+#
+# CVE List:
+#   - CVE-2015-4513
+#   - CVE-2015-7189
+#   - CVE-2015-7193
+#   - CVE-2015-7197
+#   - CVE-2015-7198
+#   - CVE-2015-7199
+#   - CVE-2015-7200
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:2519
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird-38.4.0 -y 
