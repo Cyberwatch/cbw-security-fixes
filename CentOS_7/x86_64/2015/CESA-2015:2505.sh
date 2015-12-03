4c312b12245de356d9949b00c897ebe161598cc3
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-03 20:07:51 +0100

    CESA: add one fix

---
+++ CentOS_7/x86_64/2015/CESA-2015:2505.sh
@@ -0,0 +1,95 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:2505
+#
+# Security announcement date: 2015-12-01 18:46:37 UTC
+# Script generation date:     2015-12-03 19:07:47 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libreport:2.1.11-32.el7.centos
+#   - libreport-anaconda:2.1.11-32.el7.centos
+#   - libreport-centos:2.1.11-32.el7.centos
+#   - libreport-cli:2.1.11-32.el7.centos
+#   - libreport-compat:2.1.11-32.el7.centos
+#   - libreport-devel:2.1.11-32.el7.centos
+#   - libreport-filesystem:2.1.11-32.el7.centos
+#   - libreport-gtk:2.1.11-32.el7.centos
+#   - libreport-gtk-devel:2.1.11-32.el7.centos
+#   - libreport-newt:2.1.11-32.el7.centos
+#   - libreport-plugin-bugzilla:2.1.11-32.el7.centos
+#   - libreport-plugin-kerneloops:2.1.11-32.el7.centos
+#   - libreport-plugin-logger:2.1.11-32.el7.centos
+#   - libreport-plugin-mailx:2.1.11-32.el7.centos
+#   - libreport-plugin-mantisbt:2.1.11-32.el7.centos
+#   - libreport-plugin-reportuploader:2.1.11-32.el7.centos
+#   - libreport-plugin-rhtsupport:2.1.11-32.el7.centos
+#   - libreport-plugin-ureport:2.1.11-32.el7.centos
+#   - libreport-python:2.1.11-32.el7.centos
+#   - libreport-rhel:2.1.11-32.el7.centos
+#   - libreport-rhel-anaconda-bugzilla:2.1.11-32.el7.centos
+#   - libreport-rhel-bugzilla:2.1.11-32.el7.centos
+#   - libreport-web:2.1.11-32.el7.centos
+#   - libreport-web-devel:2.1.11-32.el7.centos
+#
+# Last versions recommanded by security team:
+#   - libreport:2.1.11-32.el7.centos
+#   - libreport-anaconda:2.1.11-32.el7.centos
+#   - libreport-centos:2.1.11-32.el7.centos
+#   - libreport-cli:2.1.11-32.el7.centos
+#   - libreport-compat:2.1.11-32.el7.centos
+#   - libreport-devel:2.1.11-32.el7.centos
+#   - libreport-filesystem:2.1.11-32.el7.centos
+#   - libreport-gtk:2.1.11-32.el7.centos
+#   - libreport-gtk-devel:2.1.11-32.el7.centos
+#   - libreport-newt:2.1.11-32.el7.centos
+#   - libreport-plugin-bugzilla:2.1.11-32.el7.centos
+#   - libreport-plugin-kerneloops:2.1.11-32.el7.centos
+#   - libreport-plugin-logger:2.1.11-32.el7.centos
+#   - libreport-plugin-mailx:2.1.11-32.el7.centos
+#   - libreport-plugin-mantisbt:2.1.11-32.el7.centos
+#   - libreport-plugin-reportuploader:2.1.11-32.el7.centos
+#   - libreport-plugin-rhtsupport:2.1.11-32.el7.centos
+#   - libreport-plugin-ureport:2.1.11-32.el7.centos
+#   - libreport-python:2.1.11-32.el7.centos
+#   - libreport-rhel:2.1.11-32.el7.centos
+#   - libreport-rhel-anaconda-bugzilla:2.1.11-32.el7.centos
+#   - libreport-rhel-bugzilla:2.1.11-32.el7.centos
+#   - libreport-web:2.1.11-32.el7.centos
+#   - libreport-web-devel:2.1.11-32.el7.centos
+#
+# CVE List:
+#   - CVE-2015-5302
+#   - CVE-2015-5273
+#   - CVE-2015-5287
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:2505
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libreport-2.1.11 -y 
+sudo yum install libreport-anaconda-2.1.11 -y 
+sudo yum install libreport-centos-2.1.11 -y 
+sudo yum install libreport-cli-2.1.11 -y 
+sudo yum install libreport-compat-2.1.11 -y 
+sudo yum install libreport-devel-2.1.11 -y 
+sudo yum install libreport-filesystem-2.1.11 -y 
+sudo yum install libreport-gtk-2.1.11 -y 
+sudo yum install libreport-gtk-devel-2.1.11 -y 
+sudo yum install libreport-newt-2.1.11 -y 
+sudo yum install libreport-plugin-bugzilla-2.1.11 -y 
+sudo yum install libreport-plugin-kerneloops-2.1.11 -y 
+sudo yum install libreport-plugin-logger-2.1.11 -y 
+sudo yum install libreport-plugin-mailx-2.1.11 -y 
+sudo yum install libreport-plugin-mantisbt-2.1.11 -y 
+sudo yum install libreport-plugin-reportuploader-2.1.11 -y 
+sudo yum install libreport-plugin-rhtsupport-2.1.11 -y 
+sudo yum install libreport-plugin-ureport-2.1.11 -y 
+sudo yum install libreport-python-2.1.11 -y 
+sudo yum install libreport-rhel-2.1.11 -y 
+sudo yum install libreport-rhel-anaconda-bugzilla-2.1.11 -y 
+sudo yum install libreport-rhel-bugzilla-2.1.11 -y 
+sudo yum install libreport-web-2.1.11 -y 
+sudo yum install libreport-web-devel-2.1.11 -y 
