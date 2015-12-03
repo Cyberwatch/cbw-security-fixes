1f0c03354f2bb20fb7b2ab8cbf26a038e796db37
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-03 20:10:50 +0100

    RHSA: add one fix

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2525.sh
@@ -0,0 +1,23 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2525
+#
+# Security announcement date: 2015-12-01 13:25:10 UTC
+# Script generation date:     2015-12-03 19:10:48 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - redhat-release-server:6Server-6.5.0.3.el6_5.2
+#
+# Last versions recommanded by security team:
+#   - redhat-release-server:6Server-6.5.0.3.el6_5.2
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2525
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install redhat-release-server-6Server -y 
