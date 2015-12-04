ebf772f24811954a50e3e9ebfdc19b7b66059d59
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-04 08:12:56 +0100

    RHSA: add 5 fixes

---
+++ Red_Hat_5/x86_64/2015/RHSA-2015:2535.sh
@@ -0,0 +1,27 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2535
+#
+# Security announcement date: 2015-12-01 20:46:13 UTC
+# Script generation date:     2015-12-04 07:12:53 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - jakarta-commons-collections:3.2.1-5.ep5.el5
+#   - jakarta-commons-collections-tomcat5:3.2.1-5.ep5.el5
+#
+# Last versions recommanded by security team:
+#   - jakarta-commons-collections:3.2.1-5.ep5.el5
+#   - jakarta-commons-collections-tomcat5:3.2.1-5.ep5.el5
+#
+# CVE List:
+#   - CVE-2015-7501
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2535
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install jakarta-commons-collections-3.2.1 -y 
+sudo yum install jakarta-commons-collections-tomcat5-3.2.1 -y 
---
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2536.sh
@@ -0,0 +1,24 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2536
+#
+# Security announcement date: 2015-12-01 20:46:25 UTC
+# Script generation date:     2015-12-04 07:12:53 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - apache-commons-collections-eap6:3.2.1-16.redhat_5.1.ep6.el7
+#
+# Last versions recommanded by security team:
+#   - apache-commons-collections-eap6:3.2.1-18.redhat_7.1.ep6.el7
+#
+# CVE List:
+#   - CVE-2015-7501
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2536
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install apache-commons-collections-eap6-3.2.1 -y 
