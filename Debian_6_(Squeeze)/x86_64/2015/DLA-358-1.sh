2e8b2e4f90d6f3f2087220d922921d45a8c1b326
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-05 08:07:17 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-358-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-358-1
+#
+# Security announcement date: 2015-12-03 00:00:00 UTC
+# Script generation date:     2015-12-05 07:07:15 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - openssl:0.9.8o-4squeeze22
+#
+# Last versions recommanded by security team:
+#   - openssl:0.9.8o-4squeeze22
+#
+# CVE List:
+#   - CVE-2015-3195
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-358-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openssl=0.9.8o-4squeeze22 -y
