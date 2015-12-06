b5c5684259439ec2a0b3bc498a06941fdedac6b8
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-06 08:13:23 +0100

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2015/RHSA-2015:2545.sh
@@ -0,0 +1,49 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2545
+#
+# Security announcement date: 2015-12-03 19:45:31 UTC
+# Script generation date:     2015-12-06 07:13:21 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser:47.0.2526.73-1.el6
+#   - chromium-browser-debuginfo:47.0.2526.73-1.el6
+#
+# Last versions recommanded by security team:
+#   - chromium-browser:47.0.2526.73-1.el6
+#   - chromium-browser-debuginfo:47.0.2526.73-1.el6
+#
+# CVE List:
+#   - CVE-2015-6764
+#   - CVE-2015-6765
+#   - CVE-2015-6766
+#   - CVE-2015-6767
+#   - CVE-2015-6768
+#   - CVE-2015-6769
+#   - CVE-2015-6770
+#   - CVE-2015-6771
+#   - CVE-2015-6772
+#   - CVE-2015-6773
+#   - CVE-2015-6774
+#   - CVE-2015-6775
+#   - CVE-2015-6776
+#   - CVE-2015-6777
+#   - CVE-2015-6778
+#   - CVE-2015-6779
+#   - CVE-2015-6780
+#   - CVE-2015-6781
+#   - CVE-2015-6782
+#   - CVE-2015-6784
+#   - CVE-2015-6785
+#   - CVE-2015-6786
+#   - CVE-2015-6787
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2545
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install chromium-browser-47.0.2526.73 -y 
+sudo yum install chromium-browser-debuginfo-47.0.2526.73 -y 
---
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2544.sh
@@ -0,0 +1,69 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2544
+#
+# Security announcement date: 2015-12-03 19:22:48 UTC
+# Script generation date:     2015-12-06 07:13:21 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - openshift:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-clients:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-master:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-node:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-sdn-ovs:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - tuned-profiles-openshift-node:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - atomic-openshift:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-clients:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-clients-redistributable:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-dockerregistry:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-master:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-node:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-pod:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-recycle:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-sdn-ovs:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - tuned-profiles-atomic-openshift-node:3.1.0.4-1.git.15.5e061c3.el7aos
+#
+# Last versions recommanded by security team:
+#   - openshift:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-clients:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-master:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-node:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - openshift-sdn-ovs:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - tuned-profiles-openshift-node:3.0.2.0-0.git.38.7576bc5.el7ose
+#   - atomic-openshift:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-clients:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-clients-redistributable:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-dockerregistry:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-master:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-node:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-pod:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-recycle:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - atomic-openshift-sdn-ovs:3.1.0.4-1.git.15.5e061c3.el7aos
+#   - tuned-profiles-atomic-openshift-node:3.1.0.4-1.git.15.5e061c3.el7aos
+#
+# CVE List:
+#   - CVE-2015-7528
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2544
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install openshift-3.0.2.0 -y 
+sudo yum install openshift-clients-3.0.2.0 -y 
+sudo yum install openshift-master-3.0.2.0 -y 
+sudo yum install openshift-node-3.0.2.0 -y 
+sudo yum install openshift-sdn-ovs-3.0.2.0 -y 
+sudo yum install tuned-profiles-openshift-node-3.0.2.0 -y 
+sudo yum install atomic-openshift-3.1.0.4 -y 
+sudo yum install atomic-openshift-clients-3.1.0.4 -y 
+sudo yum install atomic-openshift-clients-redistributable-3.1.0.4 -y 
+sudo yum install atomic-openshift-dockerregistry-3.1.0.4 -y 
+sudo yum install atomic-openshift-master-3.1.0.4 -y 
+sudo yum install atomic-openshift-node-3.1.0.4 -y 
+sudo yum install atomic-openshift-pod-3.1.0.4 -y 
+sudo yum install atomic-openshift-recycle-3.1.0.4 -y 
+sudo yum install atomic-openshift-sdn-ovs-3.1.0.4 -y 
+sudo yum install tuned-profiles-atomic-openshift-node-3.1.0.4 -y 
