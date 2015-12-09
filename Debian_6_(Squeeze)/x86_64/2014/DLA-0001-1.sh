a1c649870e6bb11bd5287d786a3e533042c1a328
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-09 08:08:11 +0100

    DLA: update 4 fixes

--- Debian_6_(Squeeze)/x86_64/2014/DLA-0001-1.sh
+++ Debian_6_(Squeeze)/x86_64/2014/DLA-0001-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-0001-1
 #
 # Security announcement date: 2014-06-02 00:00:00 UTC
-# Script generation date:     2015-09-10 10:02:57 UTC
+# Script generation date:     2015-12-09 07:07:10 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - gnutls26:2.8.6-1+squeeze4
 #
 # Last versions recommanded by security team:
-#   - gnutls26:2.8.6-1+squeeze5
+#   - gnutls26:2.8.6-1+squeeze6
 #
 # CVE List:
 #   - CVE-2014-3466
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-0001-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze5 -y
+sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze6 -y
