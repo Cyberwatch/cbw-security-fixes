378a4f2f5b30b1a26c1b25535be1ea105580d2bd
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-09 08:07:08 +0100

    DSA: update 4 fixes

--- Debian_6_(Squeeze)/x86_64/2012/DSA-2441-1.sh
+++ Debian_6_(Squeeze)/x86_64/2012/DSA-2441-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2441-1
 #
 # Security announcement date: 2012-03-25 00:00:00 UTC
-# Script generation date:     2015-09-10 09:59:56 UTC
+# Script generation date:     2015-12-09 07:02:49 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - gnutls26:2.8.6-1+squeeze2
 #
 # Last versions recommanded by security team:
-#   - gnutls26:2.8.6-1+squeeze5
+#   - gnutls26:2.8.6-1+squeeze6
 #
 # CVE List:
 #   - CVE-2012-1573
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2441-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze5 -y
+sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze6 -y
--- Debian_6_(Squeeze)/x86_64/2014/DSA-2869-1.sh
+++ Debian_6_(Squeeze)/x86_64/2014/DSA-2869-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2869-1
 #
 # Security announcement date: 2014-03-03 00:00:00 UTC
-# Script generation date:     2015-09-10 10:01:55 UTC
+# Script generation date:     2015-12-09 07:03:18 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - gnutls26:2.8.6-1+squeeze3
 #
 # Last versions recommanded by security team:
-#   - gnutls26:2.8.6-1+squeeze5
+#   - gnutls26:2.8.6-1+squeeze6
 #
 # CVE List:
 #   - CVE-2014-0092
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2869-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze5 -y
+sudo apt-get install --only-upgrade gnutls26=2.8.6-1+squeeze6 -y
