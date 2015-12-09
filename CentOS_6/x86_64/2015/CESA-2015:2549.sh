e6e1d1c0e5fb81ca4e58d11199ace3590612bcab
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-09 20:09:44 +0100

    CESA: add one fix

---
+++ CentOS_6/x86_64/2015/CESA-2015:2549.sh
@@ -0,0 +1,42 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:2549
+#
+# Security announcement date: 2015-12-07 13:26:33 UTC
+# Script generation date:     2015-12-09 19:09:41 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libxml2:2.7.6-20.el6_7.1
+#   - libxml2-devel:2.7.6-20.el6_7.1
+#   - libxml2-python:2.7.6-20.el6_7.1
+#   - libxml2-static:2.7.6-20.el6_7.1
+#
+# Last versions recommanded by security team:
+#   - libxml2:2.7.6-20.el6_7.1
+#   - libxml2-devel:2.7.6-20.el6_7.1
+#   - libxml2-python:2.7.6-20.el6_7.1
+#   - libxml2-static:2.7.6-20.el6_7.1
+#
+# CVE List:
+#   - CVE-2015-7941
+#   - CVE-2015-7942
+#   - CVE-2015-8241
+#   - CVE-2015-8317
+#   - CVE-2015-5312
+#   - CVE-2015-7497
+#   - CVE-2015-7498
+#   - CVE-2015-7499
+#   - CVE-2015-7500
+#   - CVE-2015-8242
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:2549
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libxml2-2.7.6 -y 
+sudo yum install libxml2-devel-2.7.6 -y 
+sudo yum install libxml2-python-2.7.6 -y 
+sudo yum install libxml2-static-2.7.6 -y 
