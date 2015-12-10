0b7cfcba23e4d45762862d5935949cbe8863bf18
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-10 20:12:44 +0100

    RHSA: add 3 fixes

---
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2551.sh
@@ -0,0 +1,138 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2551
+#
+# Security announcement date: 2015-12-08 15:52:31 UTC
+# Script generation date:     2015-12-10 19:12:42 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - rh-ruby22-rubygem-redhat_access_cfme:0.0.7-1.el7cf
+#   - rh-ruby22-rubygem-redhat_access_lib:0.0.6-1.el7cf
+#   - rh-ruby22-rubygem-unf_ext-doc:0.0.7.1-3.el7cf
+#   - cfme:5.5.0.13-2.el7cf
+#   - cfme-appliance:5.5.0.13-1.el7cf
+#   - cfme-appliance-debuginfo:5.5.0.13-1.el7cf
+#   - cfme-debuginfo:5.5.0.13-2.el7cf
+#   - cfme-gemset:5.5.0.13-1.el7cf
+#   - prince:9.0r2-10.el7cf
+#   - rh-ruby22-rubygem-bcrypt:3.1.10-3.el7cf
+#   - rh-ruby22-rubygem-bcrypt-debuginfo:3.1.10-3.el7cf
+#   - rh-ruby22-rubygem-escape_utils:1.1.0-2.el7cf
+#   - rh-ruby22-rubygem-escape_utils-debuginfo:1.1.0-2.el7cf
+#   - rh-ruby22-rubygem-eventmachine:1.0.7-6.el7cf
+#   - rh-ruby22-rubygem-eventmachine-debuginfo:1.0.7-6.el7cf
+#   - rh-ruby22-rubygem-ffi:1.9.8-4.el7cf
+#   - rh-ruby22-rubygem-ffi-debuginfo:1.9.8-4.el7cf
+#   - rh-ruby22-rubygem-json:1.8.2-9.el7cf
+#   - rh-ruby22-rubygem-json-debuginfo:1.8.2-9.el7cf
+#   - rh-ruby22-rubygem-linux_block_device:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-linux_block_device-debuginfo:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-memory_buffer:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-memory_buffer-debuginfo:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-net_app_manageability:0.1.0-3.el7cf
+#   - rh-ruby22-rubygem-net_app_manageability-debuginfo:0.1.0-3.el7cf
+#   - rh-ruby22-rubygem-nokogiri:1.6.6.2-3.el7cf
+#   - rh-ruby22-rubygem-nokogiri-debuginfo:1.6.6.2-3.el7cf
+#   - rh-ruby22-rubygem-pg:0.18.2-2.el7cf
+#   - rh-ruby22-rubygem-pg-debuginfo:0.18.2-2.el7cf
+#   - rh-ruby22-rubygem-psych:2.0.13-4.el7cf
+#   - rh-ruby22-rubygem-psych-debuginfo:2.0.13-4.el7cf
+#   - rh-ruby22-rubygem-puma:2.13.4-2.el7cf
+#   - rh-ruby22-rubygem-puma-debuginfo:2.13.4-2.el7cf
+#   - rh-ruby22-rubygem-thin:1.6.3-2.el7cf
+#   - rh-ruby22-rubygem-thin-debuginfo:1.6.3-2.el7cf
+#   - rh-ruby22-rubygem-unf_ext:0.0.7.1-3.el7cf
+#   - rh-ruby22-rubygem-unf_ext-debuginfo:0.0.7.1-3.el7cf
+#   - wmi:1.3.14-6.el7cf
+#   - wmi-debuginfo:1.3.14-6.el7cf
+#
+# Last versions recommanded by security team:
+#   - rh-ruby22-rubygem-redhat_access_cfme:0.0.7-1.el7cf
+#   - rh-ruby22-rubygem-redhat_access_lib:0.0.6-1.el7cf
+#   - rh-ruby22-rubygem-unf_ext-doc:0.0.7.1-3.el7cf
+#   - cfme:5.5.0.13-2.el7cf
+#   - cfme-appliance:5.5.0.13-1.el7cf
+#   - cfme-appliance-debuginfo:5.5.0.13-1.el7cf
+#   - cfme-debuginfo:5.5.0.13-2.el7cf
+#   - cfme-gemset:5.5.0.13-1.el7cf
+#   - prince:9.0r2-10.el7cf
+#   - rh-ruby22-rubygem-bcrypt:3.1.10-3.el7cf
+#   - rh-ruby22-rubygem-bcrypt-debuginfo:3.1.10-3.el7cf
+#   - rh-ruby22-rubygem-escape_utils:1.1.0-2.el7cf
+#   - rh-ruby22-rubygem-escape_utils-debuginfo:1.1.0-2.el7cf
+#   - rh-ruby22-rubygem-eventmachine:1.0.7-6.el7cf
+#   - rh-ruby22-rubygem-eventmachine-debuginfo:1.0.7-6.el7cf
+#   - rh-ruby22-rubygem-ffi:1.9.8-4.el7cf
+#   - rh-ruby22-rubygem-ffi-debuginfo:1.9.8-4.el7cf
+#   - rh-ruby22-rubygem-json:1.8.2-9.el7cf
+#   - rh-ruby22-rubygem-json-debuginfo:1.8.2-9.el7cf
+#   - rh-ruby22-rubygem-linux_block_device:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-linux_block_device-debuginfo:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-memory_buffer:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-memory_buffer-debuginfo:0.1.0-2.el7cf
+#   - rh-ruby22-rubygem-net_app_manageability:0.1.0-3.el7cf
+#   - rh-ruby22-rubygem-net_app_manageability-debuginfo:0.1.0-3.el7cf
+#   - rh-ruby22-rubygem-nokogiri:1.6.6.2-3.el7cf
+#   - rh-ruby22-rubygem-nokogiri-debuginfo:1.6.6.2-3.el7cf
+#   - rh-ruby22-rubygem-pg:0.18.2-2.el7cf
+#   - rh-ruby22-rubygem-pg-debuginfo:0.18.2-2.el7cf
+#   - rh-ruby22-rubygem-psych:2.0.13-4.el7cf
+#   - rh-ruby22-rubygem-psych-debuginfo:2.0.13-4.el7cf
+#   - rh-ruby22-rubygem-puma:2.13.4-2.el7cf
+#   - rh-ruby22-rubygem-puma-debuginfo:2.13.4-2.el7cf
+#   - rh-ruby22-rubygem-thin:1.6.3-2.el7cf
+#   - rh-ruby22-rubygem-thin-debuginfo:1.6.3-2.el7cf
+#   - rh-ruby22-rubygem-unf_ext:0.0.7.1-3.el7cf
+#   - rh-ruby22-rubygem-unf_ext-debuginfo:0.0.7.1-3.el7cf
+#   - wmi:1.3.14-6.el7cf
+#   - wmi-debuginfo:1.3.14-6.el7cf
+#
+# CVE List:
+#   - CVE-2015-7502
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2551
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install rh-ruby22-rubygem-redhat_access_cfme-0.0.7 -y 
+sudo yum install rh-ruby22-rubygem-redhat_access_lib-0.0.6 -y 
+sudo yum install rh-ruby22-rubygem-unf_ext-doc-0.0.7.1 -y 
+sudo yum install cfme-5.5.0.13 -y 
+sudo yum install cfme-appliance-5.5.0.13 -y 
+sudo yum install cfme-appliance-debuginfo-5.5.0.13 -y 
+sudo yum install cfme-debuginfo-5.5.0.13 -y 
+sudo yum install cfme-gemset-5.5.0.13 -y 
+sudo yum install prince-9.0r2 -y 
+sudo yum install rh-ruby22-rubygem-bcrypt-3.1.10 -y 
+sudo yum install rh-ruby22-rubygem-bcrypt-debuginfo-3.1.10 -y 
+sudo yum install rh-ruby22-rubygem-escape_utils-1.1.0 -y 
+sudo yum install rh-ruby22-rubygem-escape_utils-debuginfo-1.1.0 -y 
+sudo yum install rh-ruby22-rubygem-eventmachine-1.0.7 -y 
+sudo yum install rh-ruby22-rubygem-eventmachine-debuginfo-1.0.7 -y 
+sudo yum install rh-ruby22-rubygem-ffi-1.9.8 -y 
+sudo yum install rh-ruby22-rubygem-ffi-debuginfo-1.9.8 -y 
+sudo yum install rh-ruby22-rubygem-json-1.8.2 -y 
+sudo yum install rh-ruby22-rubygem-json-debuginfo-1.8.2 -y 
+sudo yum install rh-ruby22-rubygem-linux_block_device-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-linux_block_device-debuginfo-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-memory_buffer-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-memory_buffer-debuginfo-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-net_app_manageability-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-net_app_manageability-debuginfo-0.1.0 -y 
+sudo yum install rh-ruby22-rubygem-nokogiri-1.6.6.2 -y 
+sudo yum install rh-ruby22-rubygem-nokogiri-debuginfo-1.6.6.2 -y 
+sudo yum install rh-ruby22-rubygem-pg-0.18.2 -y 
+sudo yum install rh-ruby22-rubygem-pg-debuginfo-0.18.2 -y 
+sudo yum install rh-ruby22-rubygem-psych-2.0.13 -y 
+sudo yum install rh-ruby22-rubygem-psych-debuginfo-2.0.13 -y 
+sudo yum install rh-ruby22-rubygem-puma-2.13.4 -y 
+sudo yum install rh-ruby22-rubygem-puma-debuginfo-2.13.4 -y 
+sudo yum install rh-ruby22-rubygem-thin-1.6.3 -y 
+sudo yum install rh-ruby22-rubygem-thin-debuginfo-1.6.3 -y 
+sudo yum install rh-ruby22-rubygem-unf_ext-0.0.7.1 -y 
+sudo yum install rh-ruby22-rubygem-unf_ext-debuginfo-0.0.7.1 -y 
+sudo yum install wmi-1.3.14 -y 
+sudo yum install wmi-debuginfo-1.3.14 -y 
---
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2561.sh
@@ -0,0 +1,71 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2561
+#
+# Security announcement date: 2015-12-08 14:22:39 UTC
+# Script generation date:     2015-12-10 19:12:42 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - emacs-git:1.8.3.1-6.el7
+#   - emacs-git-el:1.8.3.1-6.el7
+#   - git-all:1.8.3.1-6.el7
+#   - git-bzr:1.8.3.1-6.el7
+#   - git-cvs:1.8.3.1-6.el7
+#   - git-email:1.8.3.1-6.el7
+#   - git-gui:1.8.3.1-6.el7
+#   - git-hg:1.8.3.1-6.el7
+#   - git-p4:1.8.3.1-6.el7
+#   - gitk:1.8.3.1-6.el7
+#   - gitweb:1.8.3.1-6.el7
+#   - perl-Git:1.8.3.1-6.el7
+#   - perl-Git-SVN:1.8.3.1-6.el7
+#   - git:1.8.3.1-6.el7
+#   - git-daemon:1.8.3.1-6.el7
+#   - git-debuginfo:1.8.3.1-6.el7
+#   - git-svn:1.8.3.1-6.el7
+#
+# Last versions recommanded by security team:
+#   - emacs-git:1.8.3.1-6.el7
+#   - emacs-git-el:1.8.3.1-6.el7
+#   - git-all:1.8.3.1-6.el7
+#   - git-bzr:1.8.3.1-6.el7
+#   - git-cvs:1.8.3.1-6.el7
+#   - git-email:1.8.3.1-6.el7
+#   - git-gui:1.8.3.1-6.el7
+#   - git-hg:1.8.3.1-6.el7
+#   - git-p4:1.8.3.1-6.el7
+#   - gitk:1.8.3.1-6.el7
+#   - gitweb:1.8.3.1-6.el7
+#   - perl-Git:1.8.3.1-6.el7
+#   - perl-Git-SVN:1.8.3.1-6.el7
+#   - git:1.8.3.1-6.el7
+#   - git-daemon:1.8.3.1-6.el7
+#   - git-debuginfo:1.8.3.1-6.el7
+#   - git-svn:1.8.3.1-6.el7
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2561
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install emacs-git-1.8.3.1 -y 
+sudo yum install emacs-git-el-1.8.3.1 -y 
+sudo yum install git-all-1.8.3.1 -y 
+sudo yum install git-bzr-1.8.3.1 -y 
+sudo yum install git-cvs-1.8.3.1 -y 
+sudo yum install git-email-1.8.3.1 -y 
+sudo yum install git-gui-1.8.3.1 -y 
+sudo yum install git-hg-1.8.3.1 -y 
+sudo yum install git-p4-1.8.3.1 -y 
+sudo yum install gitk-1.8.3.1 -y 
+sudo yum install gitweb-1.8.3.1 -y 
+sudo yum install perl-Git-1.8.3.1 -y 
+sudo yum install perl-Git-SVN-1.8.3.1 -y 
+sudo yum install git-1.8.3.1 -y 
+sudo yum install git-daemon-1.8.3.1 -y 
+sudo yum install git-debuginfo-1.8.3.1 -y 
+sudo yum install git-svn-1.8.3.1 -y 
