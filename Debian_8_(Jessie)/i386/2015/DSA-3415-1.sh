889ccd9c58a0d8edcd07ff0644bd00c97dfce5e6
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-11 08:04:03 +0100

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/i386/2015/DSA-3415-1.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3415-1
+#
+# Security announcement date: 2015-12-09 00:00:00 UTC
+# Script generation date:     2015-12-11 07:03:59 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser:47.0.2526.73-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - chromium-browser:47.0.2526.73-1~deb8u1
+#
+# CVE List:
+#   - CVE-2015-1302
+#   - CVE-2015-6764
+#   - CVE-2015-6765
+#   - CVE-2015-6766
+#   - CVE-2015-6767
+#   - CVE-2015-6768
+#   - CVE-2015-6769
+#   - CVE-2015-6770
+#   - CVE-2015-6771
+#   - CVE-2015-6772
+#   - CVE-2015-6773
+#   - CVE-2015-6774
+#   - CVE-2015-6775
+#   - CVE-2015-6776
+#   - CVE-2015-6777
+#   - CVE-2015-6778
+#   - CVE-2015-6779
+#   - CVE-2015-6780
+#   - CVE-2015-6781
+#   - CVE-2015-6782
+#   - CVE-2015-6784
+#   - CVE-2015-6785
+#   - CVE-2015-6786
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3415-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade chromium-browser=47.0.2526.73-1~deb8u1 -y
---
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3414-1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3414-1
+#
+# Security announcement date: 2015-12-09 00:00:00 UTC
+# Script generation date:     2015-12-11 07:03:59 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - xen:4.4.1-9+deb8u3
+#
+# Last versions recommanded by security team:
+#   - xen:4.4.1-9+deb8u3
+#
+# CVE List:
+#   - CVE-2015-3259
+#   - CVE-2015-3340
+#   - CVE-2015-5307
+#   - CVE-2015-6654
+#   - CVE-2015-7311
+#   - CVE-2015-7812
+#   - CVE-2015-7813
+#   - CVE-2015-7814
+#   - CVE-2015-7969
+#   - CVE-2015-7970
+#   - CVE-2015-7971
+#   - CVE-2015-7972
+#   - CVE-2015-8104
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3414-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade xen=4.4.1-9+deb8u3 -y
