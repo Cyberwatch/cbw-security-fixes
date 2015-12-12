3b9f09cbd6dddb6e0c22b70c7ae68197592f200e
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-12 08:02:35 +0100

    USN: add 6 fixes

---
+++ Ubuntu_14.04_LTS/i686/2015/USN-2825-1.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2825-1
+#
+# Security announcement date: 2015-12-10 00:00:00 UTC
+# Script generation date:     2015-12-12 07:02:31 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - liboxideqtcore0:1.11.3-0ubuntu0.14.04.1
+#
+# Last versions recommanded by security team:
+#   - liboxideqtcore0:1.11.3-0ubuntu0.14.04.1
+#
+# CVE List:
+#   - CVE-2015-6765
+#   - CVE-2015-6766
+#   - CVE-2015-6767
+#   - CVE-2015-6768
+#   - CVE-2015-6770
+#   - CVE-2015-6769
+#   - CVE-2015-6771
+#   - CVE-2015-6772
+#   - CVE-2015-6773
+#   - CVE-2015-6777
+#   - CVE-2015-6782
+#   - CVE-2015-6784
+#   - CVE-2015-6785
+#   - CVE-2015-6786
+#   - CVE-2015-6787
+#   - CVE-2015-8478
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2825-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade liboxideqtcore0=1.11.3-0ubuntu0.14.04.1 -y
---
+++ Ubuntu_15.10/i686/2015/USN-2825-1.sh
@@ -0,0 +1,38 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2825-1
+#
+# Security announcement date: 2015-12-10 00:00:00 UTC
+# Script generation date:     2015-12-12 07:02:32 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - liboxideqtcore0:1.11.3-0ubuntu0.15.10.1
+#
+# Last versions recommanded by security team:
+#   - liboxideqtcore0:1.11.3-0ubuntu0.15.10.1
+#
+# CVE List:
+#   - CVE-2015-6765
+#   - CVE-2015-6766
+#   - CVE-2015-6767
+#   - CVE-2015-6768
+#   - CVE-2015-6770
+#   - CVE-2015-6769
+#   - CVE-2015-6771
+#   - CVE-2015-6772
+#   - CVE-2015-6773
+#   - CVE-2015-6777
+#   - CVE-2015-6782
+#   - CVE-2015-6784
+#   - CVE-2015-6785
+#   - CVE-2015-6786
+#   - CVE-2015-6787
+#   - CVE-2015-8478
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2825-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
