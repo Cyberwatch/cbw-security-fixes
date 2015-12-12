b729ce8a2c109c2586321d75640a67119a4fd86a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-12 08:07:13 +0100

    CESA: add 4 fixes

---
+++ CentOS_7/x86_64/2015/CESA-2015:2595.sh
@@ -0,0 +1,29 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for CESA-2015:2595
+#
+# Security announcement date: 2015-12-09 19:21:50 UTC
+# Script generation date:     2015-12-12 07:07:10 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libpng12:1.2.50-7.el7_2
+#   - libpng12-devel:1.2.50-7.el7_2
+#
+# Last versions recommanded by security team:
+#   - libpng12:1.2.50-7.el7_2
+#   - libpng12-devel:1.2.50-7.el7_2
+#
+# CVE List:
+#   - CVE-2015-8126
+#   - CVE-2015-7981
+#   - CVE-2015-8472
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2015:2595
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libpng12-1.2.50 -y 
+sudo yum install libpng12-devel-1.2.50 -y 
