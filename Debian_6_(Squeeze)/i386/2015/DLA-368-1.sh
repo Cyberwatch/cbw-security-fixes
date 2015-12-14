cdffc7f5d358390e36c158e5bf0fa0696e313c60
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-14 08:04:19 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2015/DLA-368-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-368-1
+#
+# Security announcement date: 2015-12-12 00:00:00 UTC
+# Script generation date:     2015-12-14 07:04:17 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - grub2:1.98+20100804-14+squeeze2
+#
+# Last versions recommanded by security team:
+#   - grub2:1.98+20100804-14+squeeze2
+#
+# CVE List:
+#   - CVE-2015-8370
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-368-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade grub2=1.98+20100804-14+squeeze2 -y
