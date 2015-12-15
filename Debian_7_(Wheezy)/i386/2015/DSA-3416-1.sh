4f81b3db5a1ab3121f0710845714c63ad9790cab
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-15 08:04:05 +0100

    DSA: add 5 fixes

---
+++ Debian_7_(Wheezy)/i386/2015/DSA-3416-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3416-1
+#
+# Security announcement date: 2015-12-13 00:00:00 UTC
+# Script generation date:     2015-12-15 07:04:01 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libphp-phpmailer:5.1-1.1
+#
+# Last versions recommanded by security team:
+#   - libphp-phpmailer:5.1-1.1
+#
+# CVE List:
+#   - CVE-2015-8476
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3416-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libphp-phpmailer=5.1-1.1 -y
---
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3416-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3416-1
+#
+# Security announcement date: 2015-12-13 00:00:00 UTC
+# Script generation date:     2015-12-15 07:04:01 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libphp-phpmailer:5.2.9+dfsg-2+deb8u1
+#
+# Last versions recommanded by security team:
+#   - libphp-phpmailer:5.2.9+dfsg-2+deb8u1
+#
+# CVE List:
+#   - CVE-2015-8476
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3416-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libphp-phpmailer=5.2.9+dfsg-2+deb8u1 -y
