66664de70547edb6d36e151847c090d4d238cd4c
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-15 20:08:10 +0100

    DSA: update 4 fixes

--- Debian_8_(Jessie)/i386/2015/DSA-3303-1.sh
+++ Debian_8_(Jessie)/i386/2015/DSA-3303-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3303-1
 #
 # Security announcement date: 2015-07-07 00:00:00 UTC
-# Script generation date:     2015-12-03 07:03:17 UTC
+# Script generation date:     2015-12-15 19:03:44 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - cups-filters:1.0.61-5+deb8u1
 #
 # Last versions recommanded by security team:
-#   - cups-filters:1.0.61-5+deb8u2
+#   - cups-filters:1.0.61-5+deb8u3
 #
 # CVE List:
 #   - CVE-2015-3258
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3303-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade cups-filters=1.0.61-5+deb8u2 -y
+sudo apt-get install --only-upgrade cups-filters=1.0.61-5+deb8u3 -y
--- Debian_8_(Jessie)/x86_64/2015/DSA-3411-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3411-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3411-1
 #
 # Security announcement date: 2015-12-02 00:00:00 UTC
-# Script generation date:     2015-12-04 07:03:21 UTC
+# Script generation date:     2015-12-15 19:03:54 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - cups-filters:1.0.61-5+deb8u2
 #
 # Last versions recommanded by security team:
-#   - cups-filters:1.0.61-5+deb8u2
+#   - cups-filters:1.0.61-5+deb8u3
 #
 # CVE List:
 #   - CVE-2015-8327
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3411-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade cups-filters=1.0.61-5+deb8u2 -y
+sudo apt-get install --only-upgrade cups-filters=1.0.61-5+deb8u3 -y
