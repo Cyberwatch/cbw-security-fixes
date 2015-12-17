7429b0800b9b0c92005770efb6fd100b4541697b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-17 08:27:22 +0100

    RHSA: update 3 fixes

--- Red_Hat_5/x86_64/2015/RHSA-2015:1982.sh
+++ Red_Hat_5/x86_64/2015/RHSA-2015:1982.sh
@@ -3,7 +3,7 @@
 # Security fix for RHSA-2015:1982
 #
 # Security announcement date: 2015-11-04 13:29:22 UTC
-# Script generation date:     2015-11-04 19:16:38 UTC
+# Script generation date:     2015-12-17 07:27:02 UTC
 #
 # Operating System: Red Hat 5
 # Architecture: x86_64
@@ -13,8 +13,8 @@
 #   - firefox-debuginfo:38.4.0-1.el5_11
 #
 # Last versions recommanded by security team:
-#   - firefox:38.4.0-1.el5_11
-#   - firefox-debuginfo:38.4.0-1.el5_11
+#   - firefox:38.5.0-2.el5_11
+#   - firefox-debuginfo:38.5.0-2.el5_11
 #
 # CVE List:
 #   - CVE-2015-4513
@@ -30,5 +30,5 @@
 #   - https://www.cyberwatch.fr/notices/RHSA-2015:1982
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo yum install firefox-38.4.0 -y 
-sudo yum install firefox-debuginfo-38.4.0 -y 
+sudo yum install firefox-38.5.0 -y 
+sudo yum install firefox-debuginfo-38.5.0 -y 
