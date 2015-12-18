9382103b991c0221faebd707caa77b23a44cfab6
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-18 20:09:40 +0100

    DSA: update 6 fixes

--- Debian_6_(Squeeze)/i386/2014/DSA-2904-1.sh
+++ Debian_6_(Squeeze)/i386/2014/DSA-2904-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2904-1
 #
 # Security announcement date: 2014-04-15 00:00:00 UTC
-# Script generation date:     2015-12-08 07:03:19 UTC
+# Script generation date:     2015-12-18 19:06:08 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - virtualbox-ose:3.2.10-dfsg-1+squeeze3
 #
 # Last versions recommanded by security team:
-#   - virtualbox-ose:3.2.28-dfsg-1+squeeze1
+#   - virtualbox-ose:<end-of-life>
 #
 # CVE List:
 #   - CVE-2014-0981
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2904-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade virtualbox-ose=3.2.28-dfsg-1+squeeze1 -y
+sudo apt-get install --only-upgrade virtualbox-ose=<end-of-life> -y
