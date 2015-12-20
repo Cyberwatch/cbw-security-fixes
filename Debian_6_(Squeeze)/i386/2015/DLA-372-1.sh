3e91f83c41dd7255a7d3c3f5642a4c970785fdb0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-20 08:07:52 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2015/DLA-372-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-372-1
+#
+# Security announcement date: 2015-12-18 00:00:00 UTC
+# Script generation date:     2015-12-20 07:07:50 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - virtualbox-ose:<end-of-life>
+#
+# Last versions recommanded by security team:
+#   - virtualbox-ose:<end-of-life>
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-372-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade virtualbox-ose=<end-of-life> -y
