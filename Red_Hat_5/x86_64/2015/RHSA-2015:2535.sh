0a3a0034f82cfb91df4cf91369d7249128bce480
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-21 20:14:46 +0100

    RHSA: update one fix

--- Red_Hat_5/x86_64/2015/RHSA-2015:2535.sh
+++ Red_Hat_5/x86_64/2015/RHSA-2015:2535.sh
@@ -3,7 +3,7 @@
 # Security fix for RHSA-2015:2535
 #
 # Security announcement date: 2015-12-01 20:46:13 UTC
-# Script generation date:     2015-12-04 07:12:53 UTC
+# Script generation date:     2015-12-21 19:14:25 UTC
 #
 # Operating System: Red Hat 5
 # Architecture: x86_64
@@ -13,8 +13,8 @@
 #   - jakarta-commons-collections-tomcat5:3.2.1-5.ep5.el5
 #
 # Last versions recommanded by security team:
-#   - jakarta-commons-collections:3.2.1-5.ep5.el5
-#   - jakarta-commons-collections-tomcat5:3.2.1-5.ep5.el5
+#   - jakarta-commons-collections:3.2-2jpp.4
+#   - jakarta-commons-collections-tomcat5:3.2-2jpp.4
 #
 # CVE List:
 #   - CVE-2015-7501
@@ -23,5 +23,5 @@
 #   - https://www.cyberwatch.fr/notices/RHSA-2015:2535
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo yum install jakarta-commons-collections-3.2.1 -y 
-sudo yum install jakarta-commons-collections-tomcat5-3.2.1 -y 
+sudo yum install jakarta-commons-collections-3.2 -y 
+sudo yum install jakarta-commons-collections-tomcat5-3.2 -y 
