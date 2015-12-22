befedbb4405e0a3b4ca71da370defab2fa51b39b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-22 08:02:29 +0100

    USN: add 4 fixes

---
+++ Ubuntu_14.04_LTS/i686/2015/USN-2853-1.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2853-1
+#
+# Security announcement date: 2015-12-20 00:00:00 UTC
+# Script generation date:     2015-12-22 07:02:25 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - linux-image-4.2.0-22-powerpc64-smp:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-lowlatency:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-generic:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc-e500mc:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc-smp:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc64-emb:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-generic-lpae:4.2.0-22.27~14.04.1
+#
+# Last versions recommanded by security team:
+#   - linux-image-4.2.0-22-powerpc64-smp:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-lowlatency:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-generic:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc-e500mc:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc-smp:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-powerpc64-emb:4.2.0-22.27~14.04.1
+#   - linux-image-4.2.0-22-generic-lpae:4.2.0-22.27~14.04.1
+#
+# CVE List:
+#   - CVE-2015-8550
+#   - CVE-2015-8551
+#   - CVE-2015-8552
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2853-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-powerpc64-smp=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-lowlatency=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-generic=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-powerpc-e500mc=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-powerpc-smp=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-powerpc64-emb=4.2.0-22.27~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-4.2.0-22-generic-lpae=4.2.0-22.27~14.04.1 -y
---
+++ Ubuntu_14.04_LTS/x86_64/2015/USN-2854-1.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for USN-2854-1
+#
+# Security announcement date: 2015-12-20 00:00:00 UTC
+# Script generation date:     2015-12-22 07:02:25 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-image-3.19.0-42-generic-lpae:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc64-smp:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc64-emb:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-generic:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-lowlatency:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc-smp:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc-e500mc:3.19.0-42.48~14.04.1
+#
+# Last versions recommanded by security team:
+#   - linux-image-3.19.0-42-generic-lpae:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc64-smp:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc64-emb:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-generic:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-lowlatency:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc-smp:3.19.0-42.48~14.04.1
+#   - linux-image-3.19.0-42-powerpc-e500mc:3.19.0-42.48~14.04.1
+#
+# CVE List:
+#   - CVE-2015-8550
+#   - CVE-2015-8551
+#   - CVE-2015-8552
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2854-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-generic-lpae=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-powerpc64-smp=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-powerpc64-emb=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-generic=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-lowlatency=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-powerpc-smp=3.19.0-42.48~14.04.1 -y
+sudo apt-get install --only-upgrade linux-image-3.19.0-42-powerpc-e500mc=3.19.0-42.48~14.04.1 -y
