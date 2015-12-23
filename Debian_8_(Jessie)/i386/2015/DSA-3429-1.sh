6a0a15aaf6fb8e1095a8bea8963aa6359f00cc88
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-23 08:03:41 +0100

    DSA: add 5 fixes

---
+++ Debian_8_(Jessie)/i386/2015/DSA-3429-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3429-1
+#
+# Security announcement date: 2015-12-21 00:00:00 UTC
+# Script generation date:     2015-12-23 07:03:38 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - foomatic-filters:4.0.17-5+deb8u1
+#
+# Last versions recommanded by security team:
+#   - foomatic-filters:4.0.17-5+deb8u1
+#
+# CVE List:
+#   - CVE-2015-8327
+#   - CVE-2015-8560
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3429-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade foomatic-filters=4.0.17-5+deb8u1 -y
