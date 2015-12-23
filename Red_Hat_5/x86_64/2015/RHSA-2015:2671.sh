65bdac0670267bca334d4322f561898f95c8db53
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-23 08:11:06 +0100

    RHSA: add one fix

---
+++ Red_Hat_5/x86_64/2015/RHSA-2015:2671.sh
@@ -0,0 +1,39 @@
+# CYBERWATCH SAS - 2015
+#
+# Security fix for RHSA-2015:2671
+#
+# Security announcement date: 2015-12-21 06:13:45 UTC
+# Script generation date:     2015-12-23 07:11:04 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - jakarta-commons-collections-debuginfo:3.2-2jpp.4
+#   - jakarta-commons-collections-tomcat5:3.2-2jpp.4
+#   - jakarta-commons-collections:3.2-2jpp.4
+#   - jakarta-commons-collections-javadoc:3.2-2jpp.4
+#   - jakarta-commons-collections-testframework:3.2-2jpp.4
+#   - jakarta-commons-collections-testframework-javadoc:3.2-2jpp.4
+#
+# Last versions recommanded by security team:
+#   - jakarta-commons-collections-debuginfo:3.2-2jpp.4
+#   - jakarta-commons-collections-tomcat5:3.2-2jpp.4
+#   - jakarta-commons-collections:3.2-2jpp.4
+#   - jakarta-commons-collections-javadoc:3.2-2jpp.4
+#   - jakarta-commons-collections-testframework:3.2-2jpp.4
+#   - jakarta-commons-collections-testframework-javadoc:3.2-2jpp.4
+#
+# CVE List:
+#   - CVE-2015-7501
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2015:2671
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install jakarta-commons-collections-debuginfo-3.2 -y 
+sudo yum install jakarta-commons-collections-tomcat5-3.2 -y 
+sudo yum install jakarta-commons-collections-3.2 -y 
+sudo yum install jakarta-commons-collections-javadoc-3.2 -y 
+sudo yum install jakarta-commons-collections-testframework-3.2 -y 
+sudo yum install jakarta-commons-collections-testframework-javadoc-3.2 -y 
