d40cd203fc7c5934ff84470d7e34ac76a69a52f0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-25 08:03:59 +0100

    DSA: add 5 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2015/DSA-3430-1.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DSA-3430-1
+#
+# Security announcement date: 2015-12-23 00:00:00 UTC
+# Script generation date:     2015-12-25 07:03:56 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libxml2:2.8.0+dfsg1-7+wheezy5
+#
+# Last versions recommanded by security team:
+#   - libxml2:2.8.0+dfsg1-7+wheezy5
+#
+# CVE List:
+#   - CVE-2015-1819
+#   - CVE-2015-5312
+#   - CVE-2015-7497
+#   - CVE-2015-7498
+#   - CVE-2015-7499
+#   - CVE-2015-7500
+#   - CVE-2015-7941
+#   - CVE-2015-7942
+#   - CVE-2015-8035
+#   - CVE-2015-8241
+#   - CVE-2015-8317
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3430-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libxml2=2.8.0+dfsg1-7+wheezy5 -y
