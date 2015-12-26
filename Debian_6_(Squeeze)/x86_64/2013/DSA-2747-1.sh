d544c1439b4149641d0727a63900aad392d551a3
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-26 20:07:39 +0100

    DSA: update 22 fixes

--- Debian_6_(Squeeze)/x86_64/2013/DSA-2747-1.sh
+++ Debian_6_(Squeeze)/x86_64/2013/DSA-2747-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2747-1
 #
 # Security announcement date: 2013-08-31 00:00:00 UTC
-# Script generation date:     2015-12-08 07:03:05 UTC
+# Script generation date:     2015-12-26 19:02:59 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - cacti:0.8.7g-1+squeeze3
 #
 # Last versions recommanded by security team:
-#   - cacti:0.8.7g-1+squeeze8
+#   - cacti:0.8.7g-1+squeeze9+deb6u11
 #
 # CVE List:
 #   - CVE-2013-5588
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2747-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade cacti=0.8.7g-1+squeeze8 -y
+sudo apt-get install --only-upgrade cacti=0.8.7g-1+squeeze9+deb6u11 -y
