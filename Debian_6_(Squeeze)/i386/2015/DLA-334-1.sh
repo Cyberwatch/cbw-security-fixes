e1b531a71fac69f141cf16403debcdcf6591cec6
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-26 20:08:48 +0100

    DLA: update 22 fixes

--- Debian_6_(Squeeze)/i386/2015/DLA-334-1.sh
+++ Debian_6_(Squeeze)/i386/2015/DLA-334-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-334-1
 #
 # Security announcement date: 2015-10-25 00:00:00 UTC
-# Script generation date:     2015-12-08 07:07:17 UTC
+# Script generation date:     2015-12-26 19:08:00 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - libxml2:2.7.8.dfsg-2+squeeze13
 #
 # Last versions recommanded by security team:
-#   - libxml2:2.7.8.dfsg-2+squeeze15
+#   - libxml2:2.7.8.dfsg-2+squeeze16
 #
 # CVE List:
 #   - CVE-2015-7942
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-334-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libxml2=2.7.8.dfsg-2+squeeze15 -y
+sudo apt-get install --only-upgrade libxml2=2.7.8.dfsg-2+squeeze16 -y
--- Debian_6_(Squeeze)/x86_64/2015/DLA-334-2.sh
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-334-2.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-334-2
 #
 # Security announcement date: 2015-10-26 00:00:00 UTC
-# Script generation date:     2015-12-08 07:07:17 UTC
+# Script generation date:     2015-12-26 19:08:00 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - libxml2:2.7.8.dfsg-2+squeeze14
 #
 # Last versions recommanded by security team:
-#   - libxml2:2.7.8.dfsg-2+squeeze15
+#   - libxml2:2.7.8.dfsg-2+squeeze16
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-334-2
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libxml2=2.7.8.dfsg-2+squeeze15 -y
+sudo apt-get install --only-upgrade libxml2=2.7.8.dfsg-2+squeeze16 -y
