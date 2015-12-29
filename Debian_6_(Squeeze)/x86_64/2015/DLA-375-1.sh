f5efc41707a7c2131338287dac96789736cb7ff6
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-29 08:04:14 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-375-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2015
+#
+# Security fix for DLA-375-1
+#
+# Security announcement date: 2015-12-27 00:00:00 UTC
+# Script generation date:     2015-12-29 07:04:11 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libpng:1.2.44-1+squeeze6
+#
+# Last versions recommanded by security team:
+#   - libpng:1.2.44-1+squeeze6
+#
+# CVE List:
+#   - CVE-2012-3425
+#   - CVE-2015-8472
+#   - CVE-2015-8540
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-375-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libpng=1.2.44-1+squeeze6 -y
