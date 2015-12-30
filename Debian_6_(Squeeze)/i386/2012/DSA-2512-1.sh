ca0c5a3725a5f5fed956183a70f285eabaa6b765
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-30 20:11:19 +0100

    DSA: update 2 fixes

--- Debian_6_(Squeeze)/i386/2012/DSA-2512-1.sh
+++ Debian_6_(Squeeze)/i386/2012/DSA-2512-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2512-1
 #
 # Security announcement date: 2012-07-12 00:00:00 UTC
-# Script generation date:     2015-09-10 09:59:59 UTC
+# Script generation date:     2015-12-30 19:06:16 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - mono:2.6.7-5.1
 #
 # Last versions recommanded by security team:
-#   - mono:2.6.7-5.1+deb6u1
+#   - mono:2.6.7-5.1+deb6u2
 #
 # CVE List:
 #   - CVE-2012-3382
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2512-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade mono=2.6.7-5.1+deb6u1 -y
+sudo apt-get install --only-upgrade mono=2.6.7-5.1+deb6u2 -y
