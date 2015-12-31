9b45815a634b14a0be7c62f17c41cbc400ea4668
Cyberwatch Security Team <csirt@cyberwatch.fr>
2015-12-31 20:04:58 +0100

    DLA: update 8 fixes

--- Debian_6_(Squeeze)/i386/2014/DLA-52-1.sh
+++ Debian_6_(Squeeze)/i386/2014/DLA-52-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-52-1
 #
 # Security announcement date: 2014-09-11 00:00:00 UTC
-# Script generation date:     2015-09-10 10:03:01 UTC
+# Script generation date:     2015-12-31 19:03:54 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -14,7 +14,7 @@
 #   - ia32-libs-gtk:20140911
 #
 # Last versions recommanded by security team:
-#   - ia32-libs:20150804
+#   - ia32-libs:20151231
 #   - ia32-libs-gtk:20150804
 #
 # CVE List:
@@ -23,5 +23,5 @@
 #   - https://www.cyberwatch.fr/notices/DLA-52-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ia32-libs=20150804 -y
+sudo apt-get install --only-upgrade ia32-libs=20151231 -y
 sudo apt-get install --only-upgrade ia32-libs-gtk=20150804 -y
--- Debian_6_(Squeeze)/i386/2015/DLA-135-1.sh
+++ Debian_6_(Squeeze)/i386/2015/DLA-135-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-135-1
 #
 # Security announcement date: 2015-01-16 00:00:00 UTC
-# Script generation date:     2015-09-10 10:21:40 UTC
+# Script generation date:     2015-12-31 19:03:58 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -14,7 +14,7 @@
 #   - ia32-libs-gtk:20150116
 #
 # Last versions recommanded by security team:
-#   - ia32-libs:20150804
+#   - ia32-libs:20151231
 #   - ia32-libs-gtk:20150804
 #
 # CVE List:
@@ -23,5 +23,5 @@
 #   - https://www.cyberwatch.fr/notices/DLA-135-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ia32-libs=20150804 -y
+sudo apt-get install --only-upgrade ia32-libs=20151231 -y
 sudo apt-get install --only-upgrade ia32-libs-gtk=20150804 -y
