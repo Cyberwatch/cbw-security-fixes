680af36ce63417b5cfb236920aba460ce63e5357
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-04 08:08:26 +0100

    DLA: add 4 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-378-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-378-1
+#
+# Security announcement date: 2016-01-02 00:00:00 UTC
+# Script generation date:     2016-01-04 07:08:23 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-2.6:2.6.32-48squeeze18
+#
+# Last versions recommanded by security team:
+#   - linux-2.6:2.6.32-48squeeze18
+#
+# CVE List:
+#   - CVE-2015-7550
+#   - CVE-2015-8543
+#   - CVE-2015-8575
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-378-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-2.6=2.6.32-48squeeze18 -y
---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-379-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-379-1
+#
+# Security announcement date: 2016-01-02 00:00:00 UTC
+# Script generation date:     2016-01-04 07:08:23 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - samba:2:3.5.6~dfsg-3squeeze13
+#
+# Last versions recommanded by security team:
+#   - samba:2:3.5.6~dfsg-3squeeze13
+#
+# CVE List:
+#   - CVE-2015-5252
+#   - CVE-2015-5296
+#   - CVE-2015-5299
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-379-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade samba=2:3.5.6~dfsg-3squeeze13 -y
