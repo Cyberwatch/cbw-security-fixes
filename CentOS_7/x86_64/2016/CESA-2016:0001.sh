796b49eb830f80827ed9291b9f84e300a9b42f24
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-09 08:09:40 +0100

    CESA: add 3 fixes

---
+++ CentOS_7/x86_64/2016/CESA-2016:0001.sh
@@ -0,0 +1,28 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0001
+#
+# Security announcement date: 2016-01-06 21:44:11 UTC
+# Script generation date:     2016-01-09 07:09:38 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird:38.5.0-1.el7.centos.x86_64
+#
+# Last versions recommanded by security team:
+#   - thunderbird:38.5.0-1.el7.centos.x86_64
+#
+# CVE List:
+#   - CVE-2015-7201
+#   - CVE-2015-7205
+#   - CVE-2015-7212
+#   - CVE-2015-7213
+#   - CVE-2015-7214
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0001
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird-38.5.0 -y 
