bde81bd77e4ead59d9c79116b1750c8f33657f1a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-10 08:05:24 +0100

    USN: add 14 fixes and update 6 fixes

---
+++ Ubuntu_12.04_LTS/x86_64/2016/USN-2866-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2866-1
+#
+# Security announcement date: 2016-01-08 00:00:00 UTC
+# Script generation date:     2016-01-10 07:02:29 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - firefox:43.0.4+build3-0ubuntu0.12.04.1
+#
+# Last versions recommanded by security team:
+#   - firefox:43.0.4+build3-0ubuntu0.12.04.1
+#
+# CVE List:
+#   - CVE-2015-7575
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2866-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade firefox=43.0.4+build3-0ubuntu0.12.04.1 -y
---
+++ Ubuntu_15.04/x86_64/2016/USN-2865-1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2865-1
+#
+# Security announcement date: 2016-01-08 00:00:00 UTC
+# Script generation date:     2016-01-10 07:02:29 UTC
+#
+# Operating System: Ubuntu 15.04
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libgnutls-openssl27:3.3.8-3ubuntu3.2
+#   - libgnutls-deb0-28:3.3.8-3ubuntu3.2
+#   - libgnutlsxx28:3.3.8-3ubuntu3.2
+#
+# Last versions recommanded by security team:
+#   - libgnutls-openssl27:3.3.8-3ubuntu3.1
+#   - libgnutls-deb0-28:3.3.8-3ubuntu3.1
+#   - libgnutlsxx28:3.3.8-3ubuntu3.1
+#
+# CVE List:
+#   - CVE-2015-7575
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2865-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libgnutls-openssl27=3.3.8-3ubuntu3.1 -y
+sudo apt-get install --only-upgrade libgnutls-deb0-28=3.3.8-3ubuntu3.1 -y
+sudo apt-get install --only-upgrade libgnutlsxx28=3.3.8-3ubuntu3.1 -y
