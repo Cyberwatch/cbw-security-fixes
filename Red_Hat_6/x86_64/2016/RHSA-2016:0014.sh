f4d909cb0d0bdabdf5fe805bfa24fa66f817b7f0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-10 20:16:27 +0100

    RHSA: add 4 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0014.sh
@@ -0,0 +1,40 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0014
+#
+# Security announcement date: 2016-01-08 11:39:40 UTC
+# Script generation date:     2016-01-10 19:16:23 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - ldb-tools:1.1.20-3.el6rhs.x86_64
+#   - libldb:1.1.20-3.el6rhs.x86_64
+#   - libldb-debuginfo:1.1.20-3.el6rhs.x86_64
+#   - libldb-devel:1.1.20-3.el6rhs.x86_64
+#   - pyldb:1.1.20-3.el6rhs.x86_64
+#   - pyldb-devel:1.1.20-3.el6rhs.x86_64
+#
+# Last versions recommanded by security team:
+#   - ldb-tools:1.1.20-3.el6rhs.x86_64
+#   - libldb:1.1.20-3.el6rhs.x86_64
+#   - libldb-debuginfo:1.1.20-3.el6rhs.x86_64
+#   - libldb-devel:1.1.20-3.el6rhs.x86_64
+#   - pyldb:1.1.20-3.el6rhs.x86_64
+#   - pyldb-devel:1.1.20-3.el6rhs.x86_64
+#
+# CVE List:
+#   - CVE-2015-3223
+#   - CVE-2015-5330
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0014
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install ldb-tools-1.1.20 -y 
+sudo yum install libldb-1.1.20 -y 
+sudo yum install libldb-debuginfo-1.1.20 -y 
+sudo yum install libldb-devel-1.1.20 -y 
+sudo yum install pyldb-1.1.20 -y 
+sudo yum install pyldb-devel-1.1.20 -y 
---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0015.sh
@@ -0,0 +1,91 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0015
+#
+# Security announcement date: 2016-01-08 11:40:33 UTC
+# Script generation date:     2016-01-10 19:16:23 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - samba-pidl:4.1.17-16.el6rhs.noarch
+#   - libsmbclient:4.1.17-16.el6rhs.x86_64
+#   - libsmbclient-devel:4.1.17-16.el6rhs.x86_64
+#   - libwbclient:4.1.17-16.el6rhs.x86_64
+#   - libwbclient-devel:4.1.17-16.el6rhs.x86_64
+#   - samba:4.1.17-16.el6rhs.x86_64
+#   - samba-client:4.1.17-16.el6rhs.x86_64
+#   - samba-common:4.1.17-16.el6rhs.x86_64
+#   - samba-dc:4.1.17-16.el6rhs.x86_64
+#   - samba-dc-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-debuginfo:4.1.17-16.el6rhs.x86_64
+#   - samba-devel:4.1.17-16.el6rhs.x86_64
+#   - samba-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-python:4.1.17-16.el6rhs.x86_64
+#   - samba-test:4.1.17-16.el6rhs.x86_64
+#   - samba-test-devel:4.1.17-16.el6rhs.x86_64
+#   - samba-test-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-vfs-glusterfs:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-clients:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-krb5-locator:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-modules:4.1.17-16.el6rhs.x86_64
+#
+# Last versions recommanded by security team:
+#   - samba-pidl:4.1.17-16.el6rhs.noarch
+#   - libsmbclient:4.1.17-16.el6rhs.x86_64
+#   - libsmbclient-devel:4.1.17-16.el6rhs.x86_64
+#   - libwbclient:4.1.17-16.el6rhs.x86_64
+#   - libwbclient-devel:4.1.17-16.el6rhs.x86_64
+#   - samba:4.1.17-16.el6rhs.x86_64
+#   - samba-client:4.1.17-16.el6rhs.x86_64
+#   - samba-common:4.1.17-16.el6rhs.x86_64
+#   - samba-dc:4.1.17-16.el6rhs.x86_64
+#   - samba-dc-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-debuginfo:4.1.17-16.el6rhs.x86_64
+#   - samba-devel:4.1.17-16.el6rhs.x86_64
+#   - samba-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-python:4.1.17-16.el6rhs.x86_64
+#   - samba-test:4.1.17-16.el6rhs.x86_64
+#   - samba-test-devel:4.1.17-16.el6rhs.x86_64
+#   - samba-test-libs:4.1.17-16.el6rhs.x86_64
+#   - samba-vfs-glusterfs:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-clients:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-krb5-locator:4.1.17-16.el6rhs.x86_64
+#   - samba-winbind-modules:4.1.17-16.el6rhs.x86_64
+#
+# CVE List:
+#   - CVE-2015-5252
+#   - CVE-2015-5296
+#   - CVE-2015-5299
+#   - CVE-2015-5330
+#   - CVE-2015-7540
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0015
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install samba-pidl-4.1.17 -y 
+sudo yum install libsmbclient-4.1.17 -y 
+sudo yum install libsmbclient-devel-4.1.17 -y 
+sudo yum install libwbclient-4.1.17 -y 
+sudo yum install libwbclient-devel-4.1.17 -y 
+sudo yum install samba-4.1.17 -y 
+sudo yum install samba-client-4.1.17 -y 
+sudo yum install samba-common-4.1.17 -y 
+sudo yum install samba-dc-4.1.17 -y 
+sudo yum install samba-dc-libs-4.1.17 -y 
+sudo yum install samba-debuginfo-4.1.17 -y 
+sudo yum install samba-devel-4.1.17 -y 
+sudo yum install samba-libs-4.1.17 -y 
+sudo yum install samba-python-4.1.17 -y 
+sudo yum install samba-test-4.1.17 -y 
+sudo yum install samba-test-devel-4.1.17 -y 
+sudo yum install samba-test-libs-4.1.17 -y 
+sudo yum install samba-vfs-glusterfs-4.1.17 -y 
+sudo yum install samba-winbind-4.1.17 -y 
+sudo yum install samba-winbind-clients-4.1.17 -y 
+sudo yum install samba-winbind-krb5-locator-4.1.17 -y 
+sudo yum install samba-winbind-modules-4.1.17 -y 
