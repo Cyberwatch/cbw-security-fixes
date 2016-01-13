a81f5de473f8e3184b374d68532ccd7730a88789
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-13 08:19:40 +0100

    RHSA: add one fix

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0017.sh
@@ -0,0 +1,67 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0017
+#
+# Security announcement date: 2016-01-11 00:22:19 UTC
+# Script generation date:     2016-01-13 07:19:37 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - openstack-nova:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-api:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-cells:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-cert:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-common:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-compute:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-conductor:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-console:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-doc:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-network:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-novncproxy:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-objectstore:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-scheduler:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-serialproxy:2014.1.5-16.el6ost.noarch
+#   - python-nova:2014.1.5-16.el6ost.noarch
+#
+# Last versions recommanded by security team:
+#   - openstack-nova:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-api:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-cells:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-cert:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-common:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-compute:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-conductor:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-console:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-doc:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-network:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-novncproxy:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-objectstore:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-scheduler:2014.1.5-16.el6ost.noarch
+#   - openstack-nova-serialproxy:2014.1.5-16.el6ost.noarch
+#   - python-nova:2014.1.5-16.el6ost.noarch
+#
+# CVE List:
+#   - CVE-2015-7548
+#   - CVE-2015-7713
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0017
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install openstack-nova-2014.1.5 -y 
+sudo yum install openstack-nova-api-2014.1.5 -y 
+sudo yum install openstack-nova-cells-2014.1.5 -y 
+sudo yum install openstack-nova-cert-2014.1.5 -y 
+sudo yum install openstack-nova-common-2014.1.5 -y 
+sudo yum install openstack-nova-compute-2014.1.5 -y 
+sudo yum install openstack-nova-conductor-2014.1.5 -y 
+sudo yum install openstack-nova-console-2014.1.5 -y 
+sudo yum install openstack-nova-doc-2014.1.5 -y 
+sudo yum install openstack-nova-network-2014.1.5 -y 
+sudo yum install openstack-nova-novncproxy-2014.1.5 -y 
+sudo yum install openstack-nova-objectstore-2014.1.5 -y 
+sudo yum install openstack-nova-scheduler-2014.1.5 -y 
+sudo yum install openstack-nova-serialproxy-2014.1.5 -y 
+sudo yum install python-nova-2014.1.5 -y 
