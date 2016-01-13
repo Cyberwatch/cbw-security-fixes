04f5c799ba134defcb1b87676a6d79bd88334008
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-13 20:05:34 +0100

    USN: update 4 fixes

--- Ubuntu_12.04_LTS/x86_64/2012/USN-1571-1.sh
+++ Ubuntu_12.04_LTS/x86_64/2012/USN-1571-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-1571-1
 #
 # Security announcement date: 2012-09-18 00:00:00 UTC
-# Script generation date:     2016-01-01 07:00:38 UTC
+# Script generation date:     2016-01-13 19:00:36 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: x86_64
@@ -14,8 +14,8 @@
 #   - isc-dhcp-client:4.1.ESV-R4-0ubuntu5.5
 #
 # Last versions recommanded by security team:
-#   - isc-dhcp-server:4.1.ESV-R4-0ubuntu5.5
-#   - isc-dhcp-client:4.1.ESV-R4-0ubuntu5.5
+#   - isc-dhcp-server:4.1.ESV-R4-0ubuntu5.10
+#   - isc-dhcp-client:4.1.ESV-R4-0ubuntu5.10
 #
 # CVE List:
 #   - CVE-2012-3955
@@ -24,5 +24,5 @@
 #   - https://www.cyberwatch.fr/notices/USN-1571-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade isc-dhcp-server=4.1.ESV-R4-0ubuntu5.5 -y
-sudo apt-get install --only-upgrade isc-dhcp-client=4.1.ESV-R4-0ubuntu5.5 -y
+sudo apt-get install --only-upgrade isc-dhcp-server=4.1.ESV-R4-0ubuntu5.10 -y
+sudo apt-get install --only-upgrade isc-dhcp-client=4.1.ESV-R4-0ubuntu5.10 -y
