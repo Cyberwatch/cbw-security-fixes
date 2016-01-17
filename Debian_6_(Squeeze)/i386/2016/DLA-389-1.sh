5b9bb302c2f25d1ac435ddfc2145857ae9bf3d85
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-17 08:04:37 +0100

    DLA: add 6 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-389-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-389-1
+#
+# Security announcement date: 2016-01-15 00:00:00 UTC
+# Script generation date:     2016-01-17 07:04:35 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - giflib:4.1.6-9+deb6u1
+#
+# Last versions recommanded by security team:
+#   - giflib:4.1.6-9+deb6u1
+#
+# CVE List:
+#   - CVE-2015-7555
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-389-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade giflib=4.1.6-9+deb6u1 -y
---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-390-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-390-1
+#
+# Security announcement date: 2016-01-15 00:00:00 UTC
+# Script generation date:     2016-01-17 07:04:35 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - dbconfig-common:1.8.46+squeeze.1
+#
+# Last versions recommanded by security team:
+#   - dbconfig-common:1.8.46+squeeze.1
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-390-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade dbconfig-common=1.8.46+squeeze.1 -y
