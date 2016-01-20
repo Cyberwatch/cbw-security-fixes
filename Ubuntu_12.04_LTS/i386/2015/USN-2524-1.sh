7e309e38bc8dad4264f97e1333e316aeb27d16d5
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-20 20:06:10 +0100

    USN: update 4 fixes

--- Ubuntu_12.04_LTS/i386/2015/USN-2524-1.sh
+++ Ubuntu_12.04_LTS/i386/2015/USN-2524-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2524-1
 #
 # Security announcement date: 2015-03-10 00:00:00 UTC
-# Script generation date:     2016-01-01 07:01:44 UTC
+# Script generation date:     2016-01-20 19:01:48 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -14,8 +14,8 @@
 #   - libecryptfs0:96-0ubuntu3.4
 #
 # Last versions recommanded by security team:
-#   - ecryptfs-utils:96-0ubuntu3.4
-#   - libecryptfs0:96-0ubuntu3.4
+#   - ecryptfs-utils:96-0ubuntu3.5
+#   - libecryptfs0:96-0ubuntu3.5
 #
 # CVE List:
 #   - CVE-2014-9687
@@ -24,5 +24,5 @@
 #   - https://www.cyberwatch.fr/notices/USN-2524-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ecryptfs-utils=96-0ubuntu3.4 -y
-sudo apt-get install --only-upgrade libecryptfs0=96-0ubuntu3.4 -y
+sudo apt-get install --only-upgrade ecryptfs-utils=96-0ubuntu3.5 -y
+sudo apt-get install --only-upgrade libecryptfs0=96-0ubuntu3.5 -y
--- Ubuntu_14.04_LTS/i686/2015/USN-2524-1.sh
+++ Ubuntu_14.04_LTS/i686/2015/USN-2524-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2524-1
 #
 # Security announcement date: 2015-03-10 00:00:00 UTC
-# Script generation date:     2016-01-01 07:01:44 UTC
+# Script generation date:     2016-01-20 19:01:48 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
 # Architecture: i686
@@ -14,8 +14,8 @@
 #   - libecryptfs0:104-0ubuntu1.14.04.3
 #
 # Last versions recommanded by security team:
-#   - ecryptfs-utils:104-0ubuntu1.14.04.3
-#   - libecryptfs0:104-0ubuntu1.14.04.3
+#   - ecryptfs-utils:104-0ubuntu1.14.04.4
+#   - libecryptfs0:104-0ubuntu1.14.04.4
 #
 # CVE List:
 #   - CVE-2014-9687
@@ -24,5 +24,5 @@
 #   - https://www.cyberwatch.fr/notices/USN-2524-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ecryptfs-utils=104-0ubuntu1.14.04.3 -y
-sudo apt-get install --only-upgrade libecryptfs0=104-0ubuntu1.14.04.3 -y
+sudo apt-get install --only-upgrade ecryptfs-utils=104-0ubuntu1.14.04.4 -y
+sudo apt-get install --only-upgrade libecryptfs0=104-0ubuntu1.14.04.4 -y
