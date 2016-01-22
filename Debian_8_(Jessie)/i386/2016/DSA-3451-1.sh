e8e62cbb34204a51def334efa5a745b8984ce0dc
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-22 08:11:49 +0100

    DSA: add 7 fixes and update 2 fixes

--- Debian_8_(Jessie)/i386/2016/DSA-3451-1.sh
+++ Debian_8_(Jessie)/i386/2016/DSA-3451-1.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DSA-3266-1
+# Security fix for DSA-3451-1
 #
-# Security announcement date: 2015-05-21 00:00:00 UTC
-# Script generation date:     2016-01-22 07:07:27 UTC
+# Security announcement date: 2016-01-20 00:00:00 UTC
+# Script generation date:     2016-01-22 07:07:44 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
 #
 # Vulnerable packages fix on version:
-#   - fuse:2.9.3-15+deb8u1
+#   - fuse:2.9.3-15+deb8u2
 #
 # Last versions recommanded by security team:
 #   - fuse:2.9.3-15+deb8u2
 #
 # CVE List:
-#   - CVE-2015-3202
+#   - CVE-2016-1233
 #
 # More details:
-#   - https://www.cyberwatch.fr/notices/DSA-3266-1
+#   - https://www.cyberwatch.fr/notices/DSA-3451-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade fuse=2.9.3-15+deb8u2 -y
--- Debian_8_(Jessie)/x86_64/2015/DSA-3266-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3266-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3266-1
 #
 # Security announcement date: 2015-05-21 00:00:00 UTC
-# Script generation date:     2016-01-01 07:03:53 UTC
+# Script generation date:     2016-01-22 07:07:27 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - fuse:2.9.3-15+deb8u1
 #
 # Last versions recommanded by security team:
-#   - fuse:2.9.3-15+deb8u1
+#   - fuse:2.9.3-15+deb8u2
 #
 # CVE List:
 #   - CVE-2015-3202
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3266-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade fuse=2.9.3-15+deb8u1 -y
+sudo apt-get install --only-upgrade fuse=2.9.3-15+deb8u2 -y
