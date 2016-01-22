c1f257083f55c4d659228f3bbbf646c380a9dba5
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-22 08:12:16 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-397-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-397-1
+#
+# Security announcement date: 2016-01-20 00:00:00 UTC
+# Script generation date:     2016-01-22 07:12:14 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - ecryptfs-utils:83-4+squeeze2
+#
+# Last versions recommanded by security team:
+#   - ecryptfs-utils:83-4+squeeze2
+#
+# CVE List:
+#   - CVE-2016-1572
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-397-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade ecryptfs-utils=83-4+squeeze2 -y
