c5577b02b38adaf3a1c73743d8ee31aed639e6a3
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-23 20:11:17 +0100

    CESA: add 4 fixes

---
+++ CentOS_7/x86_64/2016/CESA-2016:0049.sh
@@ -0,0 +1,69 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0049
+#
+# Security announcement date: 2016-01-21 17:19:22 UTC
+# Script generation date:     2016-01-23 19:11:14 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.8.0-openjdk:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-accessibility:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-accessibility-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-demo:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-demo-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-devel:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-devel-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-headless:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-headless-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-javadoc:1.8.0.71-2.b15.el7_2.noarch
+#   - java-1.8.0-openjdk-javadoc-debug:1.8.0.71-2.b15.el7_2.noarch
+#   - java-1.8.0-openjdk-src:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-src-debug:1.8.0.71-2.b15.el7_2.x86_64
+#
+# Last versions recommanded by security team:
+#   - java-1.8.0-openjdk:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-accessibility:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-accessibility-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-demo:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-demo-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-devel:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-devel-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-headless:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-headless-debug:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-javadoc:1.8.0.71-2.b15.el7_2.noarch
+#   - java-1.8.0-openjdk-javadoc-debug:1.8.0.71-2.b15.el7_2.noarch
+#   - java-1.8.0-openjdk-src:1.8.0.71-2.b15.el7_2.x86_64
+#   - java-1.8.0-openjdk-src-debug:1.8.0.71-2.b15.el7_2.x86_64
+#
+# CVE List:
+#   - CVE-2015-7575
+#   - CVE-2016-0402
+#   - CVE-2016-0448
+#   - CVE-2016-0466
+#   - CVE-2016-0475
+#   - CVE-2016-0483
+#   - CVE-2016-0494
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0049
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.8.0-openjdk-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-accessibility-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-accessibility-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-demo-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-demo-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-devel-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-headless-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-debug-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-src-1.8.0.71 -y 
+sudo yum install java-1.8.0-openjdk-src-debug-1.8.0.71 -y 
---
+++ CentOS_7/x86_64/2016/CESA-2016:0054.sh
@@ -0,0 +1,48 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0054
+#
+# Security announcement date: 2016-01-21 17:19:02 UTC
+# Script generation date:     2016-01-23 19:11:14 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.7.0-openjdk:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-accessibility:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-demo:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-devel:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-headless:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-javadoc:1.7.0.95-2.6.4.0.el7_2.noarch
+#   - java-1.7.0-openjdk-src:1.7.0.95-2.6.4.0.el7_2.x86_64
+#
+# Last versions recommanded by security team:
+#   - java-1.7.0-openjdk:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-accessibility:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-demo:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-devel:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-headless:1.7.0.95-2.6.4.0.el7_2.x86_64
+#   - java-1.7.0-openjdk-javadoc:1.7.0.95-2.6.4.0.el7_2.noarch
+#   - java-1.7.0-openjdk-src:1.7.0.95-2.6.4.0.el7_2.x86_64
+#
+# CVE List:
+#   - CVE-2015-4871
+#   - CVE-2015-7575
+#   - CVE-2016-0402
+#   - CVE-2016-0448
+#   - CVE-2016-0466
+#   - CVE-2016-0483
+#   - CVE-2016-0494
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0054
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.7.0-openjdk-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-accessibility-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-demo-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-devel-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-headless-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-javadoc-1.7.0.95 -y 
+sudo yum install java-1.7.0-openjdk-src-1.7.0.95 -y 
