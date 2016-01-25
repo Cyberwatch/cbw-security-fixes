70012d09107ebebdd3218903525dd78a29be806c
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-25 08:08:10 +0100

    DLA: add 4 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-399-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-399-1
+#
+# Security announcement date: 2016-01-23 00:00:00 UTC
+# Script generation date:     2016-01-25 07:08:08 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - foomatic-filters:4.0.5-6+squeeze2+deb6u13
+#
+# Last versions recommanded by security team:
+#   - foomatic-filters:4.0.5-6+squeeze2+deb6u13
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-399-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade foomatic-filters=4.0.5-6+squeeze2+deb6u13 -y
---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-398-1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-398-1
+#
+# Security announcement date: 2016-01-23 00:00:00 UTC
+# Script generation date:     2016-01-25 07:08:08 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - privoxy:3.0.16-1+deb6u1
+#   - privoxy:3.0.16-1+deb6u2
+#
+# Last versions recommanded by security team:
+#   - privoxy:3.0.16-1+deb6u2
+#   - privoxy:3.0.16-1+deb6u2
+#
+# CVE List:
+#   - CVE-2016-1982
+#   - CVE-2016-1983
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-398-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade privoxy=3.0.16-1+deb6u2 -y
+sudo apt-get install --only-upgrade privoxy=3.0.16-1+deb6u2 -y
