319879de58bb48410c004f51ba9b93283da6fefe
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-27 20:11:22 +0100

    DSA: update 8 fixes

--- Debian_8_(Jessie)/i386/2015/DSA-3232-1.sh
+++ Debian_8_(Jessie)/i386/2015/DSA-3232-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3232-1
 #
 # Security announcement date: 2015-04-22 00:00:00 UTC
-# Script generation date:     2016-01-01 07:03:50 UTC
+# Script generation date:     2016-01-27 19:07:11 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - curl:7.38.0-4+deb8u1
 #
 # Last versions recommanded by security team:
-#   - curl:7.38.0-4+deb8u2
+#   - curl:7.38.0-4+deb8u3
 #
 # CVE List:
 #   - CVE-2015-3143
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3232-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade curl=7.38.0-4+deb8u2 -y
+sudo apt-get install --only-upgrade curl=7.38.0-4+deb8u3 -y
--- Debian_8_(Jessie)/x86_64/2015/DSA-3240-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3240-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3240-1
 #
 # Security announcement date: 2015-04-29 00:00:00 UTC
-# Script generation date:     2016-01-01 07:03:51 UTC
+# Script generation date:     2016-01-27 19:07:12 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - curl:7.38.0-4+deb8u2
 #
 # Last versions recommanded by security team:
-#   - curl:7.38.0-4+deb8u2
+#   - curl:7.38.0-4+deb8u3
 #
 # CVE List:
 #   - CVE-2015-3153
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3240-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade curl=7.38.0-4+deb8u2 -y
+sudo apt-get install --only-upgrade curl=7.38.0-4+deb8u3 -y
