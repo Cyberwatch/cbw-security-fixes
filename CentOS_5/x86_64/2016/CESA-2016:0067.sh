3aed9b0e8655a4f780c6b02847fdbd7b3b7f319d
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-28 20:11:21 +0100

    CESA: add 3 fixes

---
+++ CentOS_5/x86_64/2016/CESA-2016:0067.sh
@@ -0,0 +1,40 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0067
+#
+# Security announcement date: 2016-01-26 13:50:52 UTC
+# Script generation date:     2016-01-28 19:11:16 UTC
+#
+# Operating System: CentOS 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.6.0-openjdk:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-demo:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-devel:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-javadoc:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-src:1.6.0.38-1.13.10.0.el5_11.x86_64
+#
+# Last versions recommanded by security team:
+#   - java-1.6.0-openjdk:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-demo:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-devel:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-javadoc:1.6.0.38-1.13.10.0.el5_11.x86_64
+#   - java-1.6.0-openjdk-src:1.6.0.38-1.13.10.0.el5_11.x86_64
+#
+# CVE List:
+#   - CVE-2016-0402
+#   - CVE-2016-0448
+#   - CVE-2016-0466
+#   - CVE-2016-0483
+#   - CVE-2016-0494
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0067
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.6.0-openjdk-1.6.0.38 -y 
+sudo yum install java-1.6.0-openjdk-demo-1.6.0.38 -y 
+sudo yum install java-1.6.0-openjdk-devel-1.6.0.38 -y 
+sudo yum install java-1.6.0-openjdk-javadoc-1.6.0.38 -y 
+sudo yum install java-1.6.0-openjdk-src-1.6.0.38 -y 
