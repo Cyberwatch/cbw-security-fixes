16695c71de7dcaefa463b329b9eae73c74ec48ad
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-28 08:11:34 +0100

    DLA: add 4 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-403-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-403-1
+#
+# Security announcement date: 2016-01-26 00:00:00 UTC
+# Script generation date:     2016-01-28 07:11:31 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - radicale:0.3-2+deb6u1
+#
+# Last versions recommanded by security team:
+#   - radicale:0.3-2+deb6u1
+#
+# CVE List:
+#   - CVE-2015-8747
+#   - CVE-2015-8748
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-403-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade radicale=0.3-2+deb6u1 -y
---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-402-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-402-1
+#
+# Security announcement date: 2016-01-26 00:00:00 UTC
+# Script generation date:     2016-01-28 07:11:31 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - tiff:3.9.4-5+squeeze13
+#
+# Last versions recommanded by security team:
+#   - tiff:3.9.4-5+squeeze13
+#
+# CVE List:
+#   - CVE-2015-8665
+#   - CVE-2015-8683
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-402-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tiff=3.9.4-5+squeeze13 -y
