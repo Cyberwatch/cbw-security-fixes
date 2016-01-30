65cedb6acbc94c87a23d4a99cd67c48bc9657cb3
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-30 08:02:40 +0100

    USN: add 2 fixes

---
+++ Ubuntu_15.10/x86_64/2016/USN-2883-1.sh
@@ -0,0 +1,23 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2883-1
+#
+# Security announcement date: 2016-01-28 00:00:00 UTC
+# Script generation date:     2016-01-30 07:02:37 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libssl1.0.0:1.0.2d-0ubuntu1.3
+#
+# Last versions recommanded by security team:
+#   - libssl1.0.0:1.0.2d-0ubuntu1.3
+#
+# CVE List:
+#   - CVE-2016-0701
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2883-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
