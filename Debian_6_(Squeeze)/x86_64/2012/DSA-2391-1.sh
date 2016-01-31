488a7e14801480c477bcb138c9b3b6df8182f5d2
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-31 08:07:29 +0100

    DSA: update 6 fixes

--- Debian_6_(Squeeze)/x86_64/2012/DSA-2391-1.sh
+++ Debian_6_(Squeeze)/x86_64/2012/DSA-2391-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2391-1
 #
 # Security announcement date: 2012-01-22 00:00:00 UTC
-# Script generation date:     2016-01-21 07:06:36 UTC
+# Script generation date:     2016-01-31 07:02:54 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - phpmyadmin:4:3.3.7-7
 #
 # Last versions recommanded by security team:
-#   - phpmyadmin:4:3.3.7-9
+#   - phpmyadmin:4:3.3.7-11
 #
 # CVE List:
 #   - CVE-2011-1940
@@ -24,4 +24,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2391-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade phpmyadmin=4:3.3.7-9 -y
+sudo apt-get install --only-upgrade phpmyadmin=4:3.3.7-11 -y
