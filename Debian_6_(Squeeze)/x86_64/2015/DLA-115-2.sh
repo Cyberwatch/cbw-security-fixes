c7b21d8b9b0777908cfdb210fc16a5072e3b8c75
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-01-31 20:08:33 +0100

    DLA: update 4 fixes

--- Debian_6_(Squeeze)/x86_64/2015/DLA-115-2.sh
+++ Debian_6_(Squeeze)/x86_64/2015/DLA-115-2.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-115-2
 #
 # Security announcement date: 2015-03-11 00:00:00 UTC
-# Script generation date:     2016-01-01 07:04:24 UTC
+# Script generation date:     2016-01-31 19:07:36 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - gosa:2.6.11-3+squeeze4
 #
 # Last versions recommanded by security team:
-#   - gosa:2.6.11-3+squeeze4
+#   - gosa:2.6.11-3+squeeze5
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-115-2
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade gosa=2.6.11-3+squeeze4 -y
+sudo apt-get install --only-upgrade gosa=2.6.11-3+squeeze5 -y
