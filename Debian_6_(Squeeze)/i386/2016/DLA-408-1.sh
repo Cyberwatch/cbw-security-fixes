998d615f171ffcb16fc7d2b132b7df992751765f
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-02 08:11:31 +0100

    DLA: add 4 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-408-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-408-1
+#
+# Security announcement date: 2016-01-31 00:00:00 UTC
+# Script generation date:     2016-02-02 07:11:29 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - gosa:2.6.11-3+squeeze5
+#
+# Last versions recommanded by security team:
+#   - gosa:2.6.11-3+squeeze5
+#
+# CVE List:
+#   - CVE-2015-8771
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-408-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade gosa=2.6.11-3+squeeze5 -y
---
+++ Debian_6_(Squeeze)/i386/2016/DLA-409-1.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-409-1
+#
+# Security announcement date: 2016-01-31 00:00:00 UTC
+# Script generation date:     2016-02-02 07:11:29 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - mysql-5.5:5.5.47-0+deb6u1
+#
+# Last versions recommanded by security team:
+#   - mysql-5.5:5.5.47-0+deb6u1
+#
+# CVE List:
+#   - CVE-2016-0505
+#   - CVE-2016-0546
+#   - CVE-2016-0596
+#   - CVE-2016-0597
+#   - CVE-2016-0598
+#   - CVE-2016-0600
+#   - CVE-2016-0606
+#   - CVE-2016-0608
+#   - CVE-2016-0609
+#   - CVE-2016-0616
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-409-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade mysql-5.5=5.5.47-0+deb6u1 -y
