e8b89cd9f5e324a85f30f7a94568a4e4642b359b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-02 08:25:26 +0100

    RHSA: update one fix

--- Red_Hat_7/x86_64/2015/RHSA-2015:1676.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:1676.sh
@@ -3,7 +3,7 @@
 # Security fix for RHSA-2015:1676
 #
 # Security announcement date: 2015-08-24 20:21:56 UTC
-# Script generation date:     2016-01-06 19:14:16 UTC
+# Script generation date:     2016-02-02 07:21:43 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
@@ -13,8 +13,8 @@
 #   - redis-debuginfo:2.8.21-1.el7ost.x86_64
 #
 # Last versions recommanded by security team:
-#   - redis:2.8.21-1.el7ost.x86_64
-#   - redis-debuginfo:2.8.21-1.el7ost.x86_64
+#   - redis:2.8.24-1.el7ost.x86_64
+#   - redis-debuginfo:2.8.24-1.el7ost.x86_64
 #
 # CVE List:
 #   - CVE-2015-4335
@@ -23,5 +23,5 @@
 #   - https://www.cyberwatch.fr/notices/RHSA-2015:1676
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo yum install redis-2.8.21 -y 
-sudo yum install redis-debuginfo-2.8.21 -y 
+sudo yum install redis-2.8.24 -y 
+sudo yum install redis-debuginfo-2.8.24 -y 
