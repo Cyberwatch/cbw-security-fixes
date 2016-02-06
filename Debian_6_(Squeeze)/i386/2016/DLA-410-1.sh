31b9f354d58aa8f8408c8ea42f9f7084acabc672
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-06 08:04:51 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-410-1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-410-1
+#
+# Security announcement date: 2016-02-04 00:00:00 UTC
+# Script generation date:     2016-02-06 07:04:49 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - openjdk-6:6b38-1.13.10-1~deb6u1
+#
+# Last versions recommanded by security team:
+#   - openjdk-6:6b38-1.13.10-1~deb6u1
+#
+# CVE List:
+#   - CVE-2015-7575
+#   - CVE-2015-8126
+#   - CVE-2015-8472
+#   - CVE-2016-0402
+#   - CVE-2016-0448
+#   - CVE-2016-0466
+#   - CVE-2016-0483
+#   - CVE-2016-0494
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-410-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openjdk-6=6b38-1.13.10-1~deb6u1 -y
