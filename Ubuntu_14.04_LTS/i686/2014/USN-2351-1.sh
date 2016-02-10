c503c72b2fd43d55a4606fda8f79ef80b32b99b4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-10 08:06:52 +0100

    USN: add 6 fixes and update 2 fixes

--- Ubuntu_14.04_LTS/i686/2014/USN-2351-1.sh
+++ Ubuntu_14.04_LTS/i686/2014/USN-2351-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2351-1
 #
 # Security announcement date: 2014-09-22 00:00:00 UTC
-# Script generation date:     2016-01-01 07:01:20 UTC
+# Script generation date:     2016-02-10 07:01:33 UTC
 #
 # Operating System: Ubuntu 14.04 LTS
 # Architecture: i686
@@ -19,7 +19,7 @@
 # Last versions recommanded by security team:
 #   - nginx-extras:1.4.6-1ubuntu3.1
 #   - nginx-full:1.4.6-1ubuntu3.1
-#   - nginx-core:1.4.6-1ubuntu3.1
+#   - nginx-core:1.4.6-1ubuntu3.4
 #   - nginx-light:1.4.6-1ubuntu3.1
 #   - nginx-naxsi:1.4.6-1ubuntu3.1
 #
@@ -32,6 +32,6 @@
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade nginx-extras=1.4.6-1ubuntu3.1 -y
 sudo apt-get install --only-upgrade nginx-full=1.4.6-1ubuntu3.1 -y
-sudo apt-get install --only-upgrade nginx-core=1.4.6-1ubuntu3.1 -y
+sudo apt-get install --only-upgrade nginx-core=1.4.6-1ubuntu3.4 -y
 sudo apt-get install --only-upgrade nginx-light=1.4.6-1ubuntu3.1 -y
 sudo apt-get install --only-upgrade nginx-naxsi=1.4.6-1ubuntu3.1 -y
