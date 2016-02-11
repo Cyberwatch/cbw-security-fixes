c679d270832612e1eae62378958f6a4b3e157c54
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-11 08:08:46 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-413-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-413-1
+#
+# Security announcement date: 2016-02-09 00:00:00 UTC
+# Script generation date:     2016-02-11 07:08:43 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - gajim:0.13.4-3+squeeze4
+#
+# Last versions recommanded by security team:
+#   - gajim:0.13.4-3+squeeze4
+#
+# CVE List:
+#   - CVE-2015-8688
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-413-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade gajim=0.13.4-3+squeeze4 -y
