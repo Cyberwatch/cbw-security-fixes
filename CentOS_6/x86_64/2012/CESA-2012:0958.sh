c7d3ddb25b3934234307e7bb16d5cd89c164bab4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-11 08:14:02 +0100

    CESA: update one fix

--- CentOS_6/x86_64/2012/CESA-2012:0958.sh
+++ CentOS_6/x86_64/2012/CESA-2012:0958.sh
@@ -3,7 +3,7 @@
 # Security fix for CESA-2012:0958
 #
 # Security announcement date: 2012-07-10 17:27:19 UTC
-# Script generation date:     2016-02-04 19:11:40 UTC
+# Script generation date:     2016-02-11 07:10:43 UTC
 #
 # Operating System: CentOS 6
 # Architecture: x86_64
@@ -12,7 +12,7 @@
 #   - sos.noarch:2.2-29.el6
 #
 # Last versions recommanded by security team:
-#   - sos.noarch:2.2-29.el6
+#   - sos.noarch:3.2-28.el6.centos.2
 #
 # CVE List:
 #   - CVE-2012-2664
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/notices/CESA-2012:0958
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo yum install sos.noarch-2.2 -y 
+sudo yum install sos.noarch-3.2 -y 
