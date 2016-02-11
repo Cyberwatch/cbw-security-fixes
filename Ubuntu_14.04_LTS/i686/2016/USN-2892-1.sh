4d3b96400fbe32182b336faed5de1a74fbee46be
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-11 08:06:42 +0100

    USN: add 4 fixes and update 6 fixes

---
+++ Ubuntu_14.04_LTS/i686/2016/USN-2892-1.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2892-1
+#
+# Security announcement date: 2016-02-09 00:00:00 UTC
+# Script generation date:     2016-02-11 07:02:53 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - nginx-extras:1.4.6-1ubuntu3.4
+#   - nginx-full:1.4.6-1ubuntu3.4
+#   - nginx-core:1.4.6-1ubuntu3.4
+#   - nginx-light:1.4.6-1ubuntu3.4
+#   - nginx-naxsi:1.4.6-1ubuntu3.4
+#
+# Last versions recommanded by security team:
+#   - nginx-extras:1.4.6-1ubuntu3.4
+#   - nginx-full:1.4.6-1ubuntu3.4
+#   - nginx-core:1.4.6-1ubuntu3.4
+#   - nginx-light:1.4.6-1ubuntu3.4
+#   - nginx-naxsi:1.4.6-1ubuntu3.4
+#
+# CVE List:
+#   - CVE-2016-0742
+#   - CVE-2016-0746
+#   - CVE-2016-0747
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2892-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade nginx-extras=1.4.6-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade nginx-full=1.4.6-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade nginx-core=1.4.6-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade nginx-light=1.4.6-1ubuntu3.4 -y
+sudo apt-get install --only-upgrade nginx-naxsi=1.4.6-1ubuntu3.4 -y
---
+++ Ubuntu_15.10/i686/2016/USN-2892-1.sh
@@ -0,0 +1,31 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2892-1
+#
+# Security announcement date: 2016-02-09 00:00:00 UTC
+# Script generation date:     2016-02-11 07:02:53 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - nginx-extras:1.9.3-1ubuntu1.1
+#   - nginx-full:1.9.3-1ubuntu1.1
+#   - nginx-core:1.9.3-1ubuntu1.1
+#   - nginx-light:1.9.3-1ubuntu1.1
+#
+# Last versions recommanded by security team:
+#   - nginx-extras:1.9.3-1ubuntu1.1
+#   - nginx-full:1.9.3-1ubuntu1.1
+#   - nginx-core:1.9.3-1ubuntu1.1
+#   - nginx-light:1.9.3-1ubuntu1.1
+#
+# CVE List:
+#   - CVE-2016-0742
+#   - CVE-2016-0746
+#   - CVE-2016-0747
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2892-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
