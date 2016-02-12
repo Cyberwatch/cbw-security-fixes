bad6adc68d46cec39b6136fe4d8c0d5c4b045d4e
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-12 20:12:31 +0100

    CESA: add one fix

---
+++ CentOS_6/x86_64/2016/CESA-2016:0152.sh
@@ -0,0 +1,24 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0152
+#
+# Security announcement date: 2016-02-10 13:41:55 UTC
+# Script generation date:     2016-02-12 19:12:28 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - sos.noarch:3.2-28.el6.centos.2
+#
+# Last versions recommanded by security team:
+#   - sos.noarch:3.2-28.el6.centos.2
+#
+# CVE List:
+#   - CVE-2015-7529
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0152
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install sos.noarch-3.2 -y 
