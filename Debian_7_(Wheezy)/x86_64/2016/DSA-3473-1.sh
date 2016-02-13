7613875374688a437db441e6680a6d8c0648c1d7
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-13 08:08:52 +0100

    DSA: add 5 fixes and update 2 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3473-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3473-1
+#
+# Security announcement date: 2016-02-11 00:00:00 UTC
+# Script generation date:     2016-02-13 07:04:29 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - nginx:1.2.1-2.2+wheezy4
+#
+# Last versions recommanded by security team:
+#   - nginx:1.2.1-2.2+wheezy4
+#
+# CVE List:
+#   - CVE-2016-0742
+#   - CVE-2016-0746
+#   - CVE-2016-0747
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3473-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade nginx=1.2.1-2.2+wheezy4 -y
