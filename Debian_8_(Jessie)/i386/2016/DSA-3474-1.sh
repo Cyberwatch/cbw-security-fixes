730047eaaa8f3e4a7ee3b8756d6edd31f0665c23
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-14 08:08:19 +0100

    DSA: add 2 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3474-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3474-1
+#
+# Security announcement date: 2016-02-12 00:00:00 UTC
+# Script generation date:     2016-02-14 07:08:15 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libgcrypt20:1.6.3-2+deb8u1
+#
+# Last versions recommanded by security team:
+#   - libgcrypt20:1.6.3-2+deb8u1
+#
+# CVE List:
+#   - CVE-2015-7511
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3474-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libgcrypt20=1.6.3-2+deb8u1 -y
