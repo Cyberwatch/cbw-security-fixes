36fac351d805c38fc7156a5431d1334eea84b22c
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-14 08:08:47 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-414-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-414-1
+#
+# Security announcement date: 2016-02-12 00:00:00 UTC
+# Script generation date:     2016-02-14 07:08:45 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - chrony:1.24-3+squeeze3
+#
+# Last versions recommanded by security team:
+#   - chrony:1.24-3+squeeze3
+#
+# CVE List:
+#   - CVE-2016-1567
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-414-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade chrony=1.24-3+squeeze3 -y
