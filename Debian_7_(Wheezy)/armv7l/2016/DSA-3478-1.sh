0d91877a63ccde92a6fa3b5a64af40e9b464b0d8
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-17 08:08:43 +0100

    DSA: add 8 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2016/DSA-3478-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3478-1
+#
+# Security announcement date: 2016-02-15 00:00:00 UTC
+# Script generation date:     2016-02-17 07:08:40 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - libgcrypt11:1.5.0-5+deb7u4
+#
+# Last versions recommanded by security team:
+#   - libgcrypt11:1.5.0-5+deb7u3
+#
+# CVE List:
+#   - CVE-2015-7511
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3478-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libgcrypt11=1.5.0-5+deb7u3 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3479-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3479-1
+#
+# Security announcement date: 2016-02-15 00:00:00 UTC
+# Script generation date:     2016-02-17 07:08:40 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - graphite2:1.3.5-1~deb7u1
+#
+# Last versions recommanded by security team:
+#   - graphite2:1.3.5-1~deb7u1
+#
+# CVE List:
+#   - CVE-2016-1521
+#   - CVE-2016-1522
+#   - CVE-2016-1523
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3479-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade graphite2=1.3.5-1~deb7u1 -y
