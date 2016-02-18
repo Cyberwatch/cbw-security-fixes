96fcae7e2d815cbd111b43917280cdefd92277bb
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-18 08:08:37 +0100

    DSA: add 5 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2016/DSA-3480-1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3480-1
+#
+# Security announcement date: 2016-02-16 00:00:00 UTC
+# Script generation date:     2016-02-18 07:08:34 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - eglibc:2.13-38+deb7u10
+#
+# Last versions recommanded by security team:
+#   - eglibc:2.13-38+deb7u10
+#
+# CVE List:
+#   - CVE-2014-8121
+#   - CVE-2015-1781
+#   - CVE-2015-7547
+#   - CVE-2015-8776
+#   - CVE-2015-8777
+#   - CVE-2015-8778
+#   - CVE-2015-8779
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3480-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade eglibc=2.13-38+deb7u10 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3481-1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3481-1
+#
+# Security announcement date: 2016-02-16 00:00:00 UTC
+# Script generation date:     2016-02-18 07:08:34 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - glibc:2.19-18+deb8u3
+#
+# Last versions recommanded by security team:
+#   - glibc:2.19-18+deb8u3
+#
+# CVE List:
+#   - CVE-2015-7547
+#   - CVE-2015-8776
+#   - CVE-2015-8778
+#   - CVE-2015-8779
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3481-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade glibc=2.19-18+deb8u3 -y
