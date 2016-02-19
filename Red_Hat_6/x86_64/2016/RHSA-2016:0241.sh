8fc5079d3c7828c95b68e284a4504989db69960a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-19 08:22:23 +0100

    RHSA: add one fix

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0241.sh
@@ -0,0 +1,32 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0241
+#
+# Security announcement date: 2016-02-17 05:04:01 UTC
+# Script generation date:     2016-02-19 07:22:20 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser.x86_64:48.0.2564.109-1.el6
+#   - chromium-browser-debuginfo.x86_64:48.0.2564.109-1.el6
+#
+# Last versions recommanded by security team:
+#   - chromium-browser.x86_64:48.0.2564.109-1.el6
+#   - chromium-browser-debuginfo.x86_64:48.0.2564.109-1.el6
+#
+# CVE List:
+#   - CVE-2016-1622
+#   - CVE-2016-1623
+#   - CVE-2016-1624
+#   - CVE-2016-1625
+#   - CVE-2016-1626
+#   - CVE-2016-1627
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0241
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install chromium-browser.x86_64-48.0.2564.109 -y 
+sudo yum install chromium-browser-debuginfo.x86_64-48.0.2564.109 -y 
