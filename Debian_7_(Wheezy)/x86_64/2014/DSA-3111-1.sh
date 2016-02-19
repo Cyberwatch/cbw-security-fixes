7e640bd984ed043e9d24ddbee1bd715ddc441ef1
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-19 20:08:44 +0100

    DSA: update 2 fixes

--- Debian_7_(Wheezy)/x86_64/2014/DSA-3111-1.sh
+++ Debian_7_(Wheezy)/x86_64/2014/DSA-3111-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3111-1
 #
 # Security announcement date: 2014-12-22 00:00:00 UTC
-# Script generation date:     2016-01-01 07:03:42 UTC
+# Script generation date:     2016-02-19 19:04:05 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - cpio:2.11+dfsg-0.1+deb7u1
 #
 # Last versions recommanded by security team:
-#   - cpio:2.11+dfsg-0.1+deb7u1
+#   - cpio:2.11+dfsg-0.1+deb7u2
 #
 # CVE List:
 #   - CVE-2014-9112
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3111-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade cpio=2.11+dfsg-0.1+deb7u1 -y
+sudo apt-get install --only-upgrade cpio=2.11+dfsg-0.1+deb7u2 -y
