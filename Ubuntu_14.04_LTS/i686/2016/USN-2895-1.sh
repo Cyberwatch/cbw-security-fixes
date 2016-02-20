b4be3875b80453c86b716a5adc1614bd2956763c
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-20 08:06:55 +0100

    USN: add 4 fixes and update 6 fixes

---
+++ Ubuntu_14.04_LTS/i686/2016/USN-2895-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2895-1
+#
+# Security announcement date: 2016-02-18 00:00:00 UTC
+# Script generation date:     2016-02-20 07:03:01 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - liboxideqtcore0:1.12.6-0ubuntu0.14.04.1
+#
+# Last versions recommanded by security team:
+#   - liboxideqtcore0:1.12.6-0ubuntu0.14.04.1
+#
+# CVE List:
+#   - CVE-2016-1623
+#   - CVE-2016-1624
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2895-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade liboxideqtcore0=1.12.6-0ubuntu0.14.04.1 -y
---
+++ Ubuntu_15.10/i686/2016/USN-2895-1.sh
@@ -0,0 +1,24 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2895-1
+#
+# Security announcement date: 2016-02-18 00:00:00 UTC
+# Script generation date:     2016-02-20 07:03:01 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - liboxideqtcore0:1.12.6-0ubuntu0.15.10.1
+#
+# Last versions recommanded by security team:
+#   - liboxideqtcore0:1.12.6-0ubuntu0.15.10.1
+#
+# CVE List:
+#   - CVE-2016-1623
+#   - CVE-2016-1624
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2895-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
