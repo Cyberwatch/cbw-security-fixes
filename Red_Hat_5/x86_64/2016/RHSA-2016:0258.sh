0866405640ff567d21260565c975b472466e65f4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-20 20:22:17 +0100

    RHSA: add 3 fixes

---
+++ Red_Hat_5/x86_64/2016/RHSA-2016:0258.sh
@@ -0,0 +1,28 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0258
+#
+# Security announcement date: 2016-02-18 15:26:26 UTC
+# Script generation date:     2016-02-20 19:22:14 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird.x86_64:38.6.0-1.el5_11
+#   - thunderbird-debuginfo.x86_64:38.6.0-1.el5_11
+#
+# Last versions recommanded by security team:
+#   - thunderbird.x86_64:38.6.0-1.el5_11
+#   - thunderbird-debuginfo.x86_64:38.6.0-1.el5_11
+#
+# CVE List:
+#   - CVE-2016-1930
+#   - CVE-2016-1935
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0258
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird.x86_64-38.6.0 -y 
+sudo yum install thunderbird-debuginfo.x86_64-38.6.0 -y 
