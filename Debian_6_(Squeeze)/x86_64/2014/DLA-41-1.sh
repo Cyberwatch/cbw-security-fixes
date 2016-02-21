7a5fdbc32401eaf77af9748e30e292a1e116d75b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-21 20:09:50 +0100

    DLA: update 2 fixes

--- Debian_6_(Squeeze)/x86_64/2014/DLA-41-1.sh
+++ Debian_6_(Squeeze)/x86_64/2014/DLA-41-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-41-1
 #
 # Security announcement date: 2014-08-24 00:00:00 UTC
-# Script generation date:     2016-01-01 07:04:19 UTC
+# Script generation date:     2016-02-21 19:08:34 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - python-imaging:1.1.7-2+deb6u1
 #
 # Last versions recommanded by security team:
-#   - python-imaging:1.1.7-2+deb6u1
+#   - python-imaging:1.1.7-2+deb6u2
 #
 # CVE List:
 #   - CVE-2014-3589
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-41-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade python-imaging=1.1.7-2+deb6u1 -y
+sudo apt-get install --only-upgrade python-imaging=1.1.7-2+deb6u2 -y
