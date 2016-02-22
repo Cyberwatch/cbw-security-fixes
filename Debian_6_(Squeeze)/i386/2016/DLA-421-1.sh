c2dcdc1c93ece11330ec734078d2319aa38e7b01
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-22 08:04:59 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/i386/2016/DLA-421-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-421-1
+#
+# Security announcement date: 2016-02-20 00:00:00 UTC
+# Script generation date:     2016-02-22 07:04:57 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - openssl:0.9.8o-4squeeze23
+#
+# Last versions recommanded by security team:
+#   - openssl:0.9.8o-4squeeze23
+#
+# CVE List:
+#   - CVE-2015-3197
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-421-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openssl=0.9.8o-4squeeze23 -y
