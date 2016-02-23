8a2142ae63f04f124f4f07ce83f23293a8435d56
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-23 08:07:18 +0100

    USN: update 10 fixes

--- Ubuntu_12.04_LTS/i386/2015/USN-2456-1.sh
+++ Ubuntu_12.04_LTS/i386/2015/USN-2456-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2456-1
 #
 # Security announcement date: 2015-01-08 00:00:00 UTC
-# Script generation date:     2016-01-01 07:01:31 UTC
+# Script generation date:     2016-02-23 07:01:59 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - cpio:2.11-7ubuntu3.1
 #
 # Last versions recommanded by security team:
-#   - cpio:2.11-7ubuntu3.1
+#   - cpio:2.11-7ubuntu3.2
 #
 # CVE List:
 #   - CVE-2014-9112
@@ -23,4 +23,4 @@
 #   - https://www.cyberwatch.fr/notices/USN-2456-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade cpio=2.11-7ubuntu3.1 -y
+sudo apt-get install --only-upgrade cpio=2.11-7ubuntu3.2 -y
