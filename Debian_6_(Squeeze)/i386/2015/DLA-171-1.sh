203007f87c2b9dce1bbf2d0762d8df87c8260904
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-23 20:14:24 +0100

    DLA: update 2 fixes

--- Debian_6_(Squeeze)/i386/2015/DLA-171-1.sh
+++ Debian_6_(Squeeze)/i386/2015/DLA-171-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-171-1
 #
 # Security announcement date: 2015-03-14 00:00:00 UTC
-# Script generation date:     2016-01-01 07:04:25 UTC
+# Script generation date:     2016-02-23 19:13:17 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - libssh2:1.2.6-1+deb6u1
 #
 # Last versions recommanded by security team:
-#   - libssh2:1.2.6-1+deb6u1
+#   - libssh2:1.2.6-1+deb6u2
 #
 # CVE List:
 #   - CVE-2015-1782
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-171-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libssh2=1.2.6-1+deb6u1 -y
+sudo apt-get install --only-upgrade libssh2=1.2.6-1+deb6u2 -y
