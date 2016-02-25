a552924e735edf2d05124a8280002a4be34c5121
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-25 20:09:19 +0100

    DSA: update 18 fixes

--- Debian_6_(Squeeze)/i386/2011/DSA-2327-1.sh
+++ Debian_6_(Squeeze)/i386/2011/DSA-2327-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2327-1
 #
 # Security announcement date: 2011-10-24 00:00:00 UTC
-# Script generation date:     2016-01-01 07:02:49 UTC
+# Script generation date:     2016-02-25 19:05:37 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - libfcgi-perl:0.71-1+squeeze1
 #
 # Last versions recommanded by security team:
-#   - libfcgi-perl:0.71-1+squeeze1
+#   - libfcgi-perl:0.71-1+squeeze1+deb6u1
 #
 # CVE List:
 #   - CVE-2011-2766
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2327-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libfcgi-perl=0.71-1+squeeze1 -y
+sudo apt-get install --only-upgrade libfcgi-perl=0.71-1+squeeze1+deb6u1 -y
--- Debian_6_(Squeeze)/x86_64/2014/DSA-2864-1.sh
+++ Debian_6_(Squeeze)/x86_64/2014/DSA-2864-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2864-1
 #
 # Security announcement date: 2014-02-20 00:00:00 UTC
-# Script generation date:     2016-01-21 07:07:08 UTC
+# Script generation date:     2016-02-25 19:06:12 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - postgresql-8.4:8.4.20-0squeeze1
 #
 # Last versions recommanded by security team:
-#   - postgresql-8.4:8.4.22lts5-0+deb6u1
+#   - postgresql-8.4:8.4.22lts6-0+deb6u1
 #
 # CVE List:
 #   - CVE-2014-0060
@@ -29,4 +29,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2864-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade postgresql-8.4=8.4.22lts5-0+deb6u1 -y
+sudo apt-get install --only-upgrade postgresql-8.4=8.4.22lts6-0+deb6u1 -y
