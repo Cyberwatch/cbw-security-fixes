f747a962d7e5b165508382a22948569ee309710d
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-26 08:09:37 +0100

    DLA: add 4 fixes and update 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-428-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-428-1
+#
+# Security announcement date: 2016-02-24 00:00:00 UTC
+# Script generation date:     2016-02-26 07:08:49 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - websvn:2.3.1-1+deb6u2
+#
+# Last versions recommanded by security team:
+#   - websvn:2.3.1-1+deb6u2
+#
+# CVE List:
+#   - CVE-2016-2511
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-428-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade websvn=2.3.1-1+deb6u2 -y
---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-429-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-429-1
+#
+# Security announcement date: 2016-02-24 00:00:00 UTC
+# Script generation date:     2016-02-26 07:08:49 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - pixman:0.16.4-1+deb6u2
+#
+# Last versions recommanded by security team:
+#   - pixman:0.16.4-1+deb6u2
+#
+# CVE List:
+#   - CVE-2014-9766
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-429-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade pixman=0.16.4-1+deb6u2 -y
