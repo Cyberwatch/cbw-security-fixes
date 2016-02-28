426fb5c4c8ca1aa53e59a0fcd117e145c5fb31b4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-28 08:03:10 +0100

    USN: add 2 fixes

---
+++ Ubuntu_15.10/x86_64/2016/USN-2908-4.sh
@@ -0,0 +1,39 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2908-4
+#
+# Security announcement date: 2016-02-26 00:00:00 UTC
+# Script generation date:     2016-02-28 07:03:06 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-image-4.2.0-30-powerpc-smp:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc64-smp:4.2.0-30.36
+#   - linux-image-4.2.0-30-lowlatency:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc-e500mc:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc64-emb:4.2.0-30.36
+#   - linux-image-4.2.0-30-generic-lpae:4.2.0-30.36
+#   - linux-image-4.2.0-30-generic:4.2.0-30.36
+#
+# Last versions recommanded by security team:
+#   - linux-image-4.2.0-30-powerpc-smp:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc64-smp:4.2.0-30.36
+#   - linux-image-4.2.0-30-lowlatency:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc-e500mc:4.2.0-30.36
+#   - linux-image-4.2.0-30-powerpc64-emb:4.2.0-30.36
+#   - linux-image-4.2.0-30-generic-lpae:4.2.0-30.36
+#   - linux-image-4.2.0-30-generic:4.2.0-30.36
+#
+# CVE List:
+#   - CVE-2016-1576
+#   - CVE-2016-1575
+#   - CVE-2013-4312
+#   - CVE-2015-8785
+#   - CVE-2016-2069
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2908-4
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
