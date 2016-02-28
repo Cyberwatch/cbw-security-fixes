7fd98663eb384b946879e2718e93e3a6af03a5ef
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-02-28 08:11:05 +0100

    DLA: update 6 fixes

--- Debian_6_(Squeeze)/i386/2015/DLA-232-1.sh
+++ Debian_6_(Squeeze)/i386/2015/DLA-232-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-232-1
 #
 # Security announcement date: 2015-05-28 00:00:00 UTC
-# Script generation date:     2016-01-21 07:11:39 UTC
+# Script generation date:     2016-02-28 07:09:12 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - tomcat6:6.0.41-2+squeeze7
 #
 # Last versions recommanded by security team:
-#   - tomcat6:6.0.41-2+squeeze7
+#   - tomcat6:6.0.45-1~deb6u1
 #
 # CVE List:
 #   - CVE-2014-0227
@@ -24,4 +24,4 @@
 #   - https://www.cyberwatch.fr/notices/DLA-232-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tomcat6=6.0.41-2+squeeze7 -y
+sudo apt-get install --only-upgrade tomcat6=6.0.45-1~deb6u1 -y
