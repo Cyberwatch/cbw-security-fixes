6714a6936841d69ab3df6643464620a885a8ce83
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-04 20:13:07 +0100

    CESA: add 2 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:0347.sh
@@ -0,0 +1,60 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0347
+#
+# Security announcement date: 2016-03-02 17:36:01 UTC
+# Script generation date:     2016-03-04 19:13:04 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - postgresql.i686:8.4.20-5.el6_7
+#   - postgresql.x86_64:8.4.20-5.el6_7
+#   - postgresql-contrib.x86_64:8.4.20-5.el6_7
+#   - postgresql-devel.i686:8.4.20-5.el6_7
+#   - postgresql-devel.x86_64:8.4.20-5.el6_7
+#   - postgresql-docs.x86_64:8.4.20-5.el6_7
+#   - postgresql-libs.i686:8.4.20-5.el6_7
+#   - postgresql-libs.x86_64:8.4.20-5.el6_7
+#   - postgresql-plperl.x86_64:8.4.20-5.el6_7
+#   - postgresql-plpython.x86_64:8.4.20-5.el6_7
+#   - postgresql-pltcl.x86_64:8.4.20-5.el6_7
+#   - postgresql-server.x86_64:8.4.20-5.el6_7
+#   - postgresql-test.x86_64:8.4.20-5.el6_7
+#
+# Last versions recommanded by security team:
+#   - postgresql.i686:8.4.20-5.el6_7
+#   - postgresql.x86_64:8.4.20-5.el6_7
+#   - postgresql-contrib.x86_64:8.4.20-5.el6_7
+#   - postgresql-devel.i686:8.4.20-5.el6_7
+#   - postgresql-devel.x86_64:8.4.20-5.el6_7
+#   - postgresql-docs.x86_64:8.4.20-5.el6_7
+#   - postgresql-libs.i686:8.4.20-5.el6_7
+#   - postgresql-libs.x86_64:8.4.20-5.el6_7
+#   - postgresql-plperl.x86_64:8.4.20-5.el6_7
+#   - postgresql-plpython.x86_64:8.4.20-5.el6_7
+#   - postgresql-pltcl.x86_64:8.4.20-5.el6_7
+#   - postgresql-server.x86_64:8.4.20-5.el6_7
+#   - postgresql-test.x86_64:8.4.20-5.el6_7
+#
+# CVE List:
+#   - CVE-2016-0773
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0347
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install postgresql.i686-8.4.20 -y 
+sudo yum install postgresql.x86_64-8.4.20 -y 
+sudo yum install postgresql-contrib.x86_64-8.4.20 -y 
+sudo yum install postgresql-devel.i686-8.4.20 -y 
+sudo yum install postgresql-devel.x86_64-8.4.20 -y 
+sudo yum install postgresql-docs.x86_64-8.4.20 -y 
+sudo yum install postgresql-libs.i686-8.4.20 -y 
+sudo yum install postgresql-libs.x86_64-8.4.20 -y 
+sudo yum install postgresql-plperl.x86_64-8.4.20 -y 
+sudo yum install postgresql-plpython.x86_64-8.4.20 -y 
+sudo yum install postgresql-pltcl.x86_64-8.4.20 -y 
+sudo yum install postgresql-server.x86_64-8.4.20 -y 
+sudo yum install postgresql-test.x86_64-8.4.20 -y 
---
+++ CentOS_7/x86_64/2016/CESA-2016:0346.sh
@@ -0,0 +1,63 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0346
+#
+# Security announcement date: 2016-03-02 18:17:48 UTC
+# Script generation date:     2016-03-04 19:13:04 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - postgresql.i686:9.2.15-1.el7_2
+#   - postgresql.x86_64:9.2.15-1.el7_2
+#   - postgresql-contrib.x86_64:9.2.15-1.el7_2
+#   - postgresql-devel.i686:9.2.15-1.el7_2
+#   - postgresql-devel.x86_64:9.2.15-1.el7_2
+#   - postgresql-docs.x86_64:9.2.15-1.el7_2
+#   - postgresql-libs.i686:9.2.15-1.el7_2
+#   - postgresql-libs.x86_64:9.2.15-1.el7_2
+#   - postgresql-plperl.x86_64:9.2.15-1.el7_2
+#   - postgresql-plpython.x86_64:9.2.15-1.el7_2
+#   - postgresql-pltcl.x86_64:9.2.15-1.el7_2
+#   - postgresql-server.x86_64:9.2.15-1.el7_2
+#   - postgresql-test.x86_64:9.2.15-1.el7_2
+#   - postgresql-upgrade.x86_64:9.2.15-1.el7_2
+#
+# Last versions recommanded by security team:
+#   - postgresql.i686:9.2.15-1.el7_2
+#   - postgresql.x86_64:9.2.15-1.el7_2
+#   - postgresql-contrib.x86_64:9.2.15-1.el7_2
+#   - postgresql-devel.i686:9.2.15-1.el7_2
+#   - postgresql-devel.x86_64:9.2.15-1.el7_2
+#   - postgresql-docs.x86_64:9.2.15-1.el7_2
+#   - postgresql-libs.i686:9.2.15-1.el7_2
+#   - postgresql-libs.x86_64:9.2.15-1.el7_2
+#   - postgresql-plperl.x86_64:9.2.15-1.el7_2
+#   - postgresql-plpython.x86_64:9.2.15-1.el7_2
+#   - postgresql-pltcl.x86_64:9.2.15-1.el7_2
+#   - postgresql-server.x86_64:9.2.15-1.el7_2
+#   - postgresql-test.x86_64:9.2.15-1.el7_2
+#   - postgresql-upgrade.x86_64:9.2.15-1.el7_2
+#
+# CVE List:
+#   - CVE-2016-0773
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0346
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install postgresql.i686-9.2.15 -y 
+sudo yum install postgresql.x86_64-9.2.15 -y 
+sudo yum install postgresql-contrib.x86_64-9.2.15 -y 
+sudo yum install postgresql-devel.i686-9.2.15 -y 
+sudo yum install postgresql-devel.x86_64-9.2.15 -y 
+sudo yum install postgresql-docs.x86_64-9.2.15 -y 
+sudo yum install postgresql-libs.i686-9.2.15 -y 
+sudo yum install postgresql-libs.x86_64-9.2.15 -y 
+sudo yum install postgresql-plperl.x86_64-9.2.15 -y 
+sudo yum install postgresql-plpython.x86_64-9.2.15 -y 
+sudo yum install postgresql-pltcl.x86_64-9.2.15 -y 
+sudo yum install postgresql-server.x86_64-9.2.15 -y 
+sudo yum install postgresql-test.x86_64-9.2.15 -y 
+sudo yum install postgresql-upgrade.x86_64-9.2.15 -y 
