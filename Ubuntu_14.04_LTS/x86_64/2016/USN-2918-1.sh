419a81505868dffe901866aca0765c17586b984a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-05 08:03:07 +0100

    USN: add 10 fixes

---
+++ Ubuntu_14.04_LTS/x86_64/2016/USN-2918-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2918-1
+#
+# Security announcement date: 2016-03-03 00:00:00 UTC
+# Script generation date:     2016-03-05 07:03:03 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libpixman-1-0:0.30.2-2ubuntu1.1
+#
+# Last versions recommanded by security team:
+#   - libpixman-1-0:0.30.2-2ubuntu1.1
+#
+# CVE List:
+#   - CVE-2014-9766
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2918-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libpixman-1-0=0.30.2-2ubuntu1.1 -y
---
+++ Ubuntu_15.10/x86_64/2016/USN-2919-1.sh
@@ -0,0 +1,24 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2919-1
+#
+# Security announcement date: 2016-03-03 00:00:00 UTC
+# Script generation date:     2016-03-05 07:03:03 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libjasper1:1.900.1-debian1-2.4ubuntu0.15.10.1
+#
+# Last versions recommanded by security team:
+#   - libjasper1:1.900.1-debian1-2.4ubuntu0.15.10.1
+#
+# CVE List:
+#   - CVE-2016-1577
+#   - CVE-2016-2116
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2919-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
