7b2eb9dc50678c87fdf47b731fb22a07d5263cf3
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-05 08:09:08 +0100

    DLA: add 2 fixes

---
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-445-2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-445-2
+#
+# Security announcement date: 2016-03-03 00:00:00 UTC
+# Script generation date:     2016-03-05 07:09:06 UTC
+#
+# Operating System: Debian 6 (Squeeze)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - squid3:3.1.6-1.2+squeeze7
+#
+# Last versions recommanded by security team:
+#   - squid3:3.1.6-1.2+squeeze7
+#
+# CVE List:
+#   - CVE-2016-2569
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DLA-445-2
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade squid3=3.1.6-1.2+squeeze7 -y
