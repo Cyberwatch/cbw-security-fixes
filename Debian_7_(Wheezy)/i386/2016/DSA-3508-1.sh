d33ed07c5dfdce8c84b2a8204d3936c6485e4255
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-08 08:08:43 +0100

    DSA: add 5 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DSA-3508-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3508-1
+#
+# Security announcement date: 2016-03-06 00:00:00 UTC
+# Script generation date:     2016-03-08 07:08:39 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - jasper:1.900.1-13+deb7u4
+#
+# Last versions recommanded by security team:
+#   - jasper:1.900.1-13+deb7u4
+#
+# CVE List:
+#   - CVE-2016-1577
+#   - CVE-2016-2089
+#   - CVE-2016-2116
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3508-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade jasper=1.900.1-13+deb7u4 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3508-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3508-1
+#
+# Security announcement date: 2016-03-06 00:00:00 UTC
+# Script generation date:     2016-03-08 07:08:39 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - jasper:1.900.1-debian1-2.4+deb8u1
+#
+# Last versions recommanded by security team:
+#   - jasper:1.900.1-debian1-2.4+deb8u1
+#
+# CVE List:
+#   - CVE-2016-1577
+#   - CVE-2016-2089
+#   - CVE-2016-2116
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3508-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade jasper=1.900.1-debian1-2.4+deb8u1 -y
