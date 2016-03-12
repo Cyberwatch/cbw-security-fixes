04cfe2d64923b1ea369ce57ec5252c3cc3860923
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-12 08:07:49 +0100

    DSA: add 2 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3513-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3513-1
+#
+# Security announcement date: 2016-03-10 00:00:00 UTC
+# Script generation date:     2016-03-12 07:07:46 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser:49.0.2623.87-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - chromium-browser:49.0.2623.87-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-1643
+#   - CVE-2016-1644
+#   - CVE-2016-1645
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3513-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade chromium-browser=49.0.2623.87-1~deb8u1 -y
