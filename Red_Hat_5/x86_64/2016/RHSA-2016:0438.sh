2c5cb3d38ef6ea5ab91e641aff469e2fbd0c5cb0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-13 20:15:37 +0100

    RHSA: add 2 fixes

---
+++ Red_Hat_5/x86_64/2016/RHSA-2016:0438.sh
@@ -0,0 +1,46 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0438
+#
+# Security announcement date: 2016-03-11 18:38:00 UTC
+# Script generation date:     2016-03-13 19:15:33 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - flash-plugin.i386:11.2.202.577-1.el5
+#
+# Last versions recommanded by security team:
+#   - flash-plugin.i386:11.2.202.577-1.el5
+#
+# CVE List:
+#   - CVE-2016-0960
+#   - CVE-2016-0961
+#   - CVE-2016-0962
+#   - CVE-2016-0963
+#   - CVE-2016-0986
+#   - CVE-2016-0987
+#   - CVE-2016-0988
+#   - CVE-2016-0989
+#   - CVE-2016-0990
+#   - CVE-2016-0991
+#   - CVE-2016-0992
+#   - CVE-2016-0993
+#   - CVE-2016-0994
+#   - CVE-2016-0995
+#   - CVE-2016-0996
+#   - CVE-2016-0997
+#   - CVE-2016-0998
+#   - CVE-2016-0999
+#   - CVE-2016-1000
+#   - CVE-2016-1001
+#   - CVE-2016-1002
+#   - CVE-2016-1005
+#   - CVE-2016-1010
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0438
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install flash-plugin.i386-11.2.202.577 -y 
