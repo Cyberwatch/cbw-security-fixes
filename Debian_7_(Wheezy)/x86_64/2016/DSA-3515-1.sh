b9499a536a46e2aa994ad482ed9fbb57ff5cc834
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-15 08:04:23 +0100

    DSA: add 10 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3515-1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3515-1
+#
+# Security announcement date: 2016-03-13 00:00:00 UTC
+# Script generation date:     2016-03-15 07:04:19 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - graphite2:1.3.6-1~deb7u1
+#
+# Last versions recommanded by security team:
+#   - graphite2:1.3.6-1~deb7u1
+#
+# CVE List:
+#   - CVE-2016-1977
+#   - CVE-2016-2790
+#   - CVE-2016-2791
+#   - CVE-2016-2792
+#   - CVE-2016-2793
+#   - CVE-2016-2794
+#   - CVE-2016-2795
+#   - CVE-2016-2796
+#   - CVE-2016-2797
+#   - CVE-2016-2798
+#   - CVE-2016-2799
+#   - CVE-2016-2800
+#   - CVE-2016-2801
+#   - CVE-2016-2802
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3515-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade graphite2=1.3.6-1~deb7u1 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3516-1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3516-1
+#
+# Security announcement date: 2016-03-13 00:00:00 UTC
+# Script generation date:     2016-03-15 07:04:19 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u5
+#
+# Last versions recommanded by security team:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u5
+#
+# CVE List:
+#   - CVE-2015-8731
+#   - CVE-2016-2523
+#   - CVE-2016-2530
+#   - CVE-2016-2531
+#   - CVE-2016-2532
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3516-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade wireshark=1.12.1+g01b65bf-4+deb8u5 -y
