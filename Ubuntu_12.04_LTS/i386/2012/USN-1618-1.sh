1c5fa2b4944c010ccb67f771e1bc35edbd85d4d5
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-15 20:06:25 +0100

    USN: update 8 fixes

--- Ubuntu_12.04_LTS/i386/2012/USN-1618-1.sh
+++ Ubuntu_12.04_LTS/i386/2012/USN-1618-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-1618-1
 #
 # Security announcement date: 2012-10-26 00:00:00 UTC
-# Script generation date:     2016-01-01 07:00:40 UTC
+# Script generation date:     2016-03-15 19:00:47 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -15,9 +15,9 @@
 #   - exim4-daemon-light:4.76-3ubuntu3.1
 #
 # Last versions recommanded by security team:
-#   - exim4-daemon-heavy:4.76-3ubuntu3.1
+#   - exim4-daemon-heavy:4.76-3ubuntu3.3
 #   - exim4-daemon-custom:4.76-3ubuntu3.1
-#   - exim4-daemon-light:4.76-3ubuntu3.1
+#   - exim4-daemon-light:4.76-3ubuntu3.3
 #
 # CVE List:
 #   - CVE-2012-5671
@@ -26,6 +26,6 @@
 #   - https://www.cyberwatch.fr/notices/USN-1618-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.1 -y
+sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.3 -y
 sudo apt-get install --only-upgrade exim4-daemon-custom=4.76-3ubuntu3.1 -y
-sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.1 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.3 -y
