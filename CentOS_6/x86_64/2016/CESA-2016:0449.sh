eb468dd06070484f60a536bcaf4bcbe198d74a22
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-18 08:08:32 +0100

    CESA: add 3 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:0449.sh
@@ -0,0 +1,63 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0449
+#
+# Security announcement date: 2016-03-15 23:36:02 UTC
+# Script generation date:     2016-03-18 07:08:28 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - samba4.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-client.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-common.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-dc.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-dc-libs.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-devel.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-libs.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-pidl.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-python.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-swat.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-test.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind-clients.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind-krb5-locator.x86_64:4.0.0-68.el6_7.rc4
+#
+# Last versions recommanded by security team:
+#   - samba4.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-client.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-common.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-dc.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-dc-libs.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-devel.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-libs.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-pidl.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-python.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-swat.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-test.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind-clients.x86_64:4.0.0-68.el6_7.rc4
+#   - samba4-winbind-krb5-locator.x86_64:4.0.0-68.el6_7.rc4
+#
+# CVE List:
+#   - CVE-2015-7560
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0449
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install samba4.x86_64-4.0.0 -y 
+sudo yum install samba4-client.x86_64-4.0.0 -y 
+sudo yum install samba4-common.x86_64-4.0.0 -y 
+sudo yum install samba4-dc.x86_64-4.0.0 -y 
+sudo yum install samba4-dc-libs.x86_64-4.0.0 -y 
+sudo yum install samba4-devel.x86_64-4.0.0 -y 
+sudo yum install samba4-libs.x86_64-4.0.0 -y 
+sudo yum install samba4-pidl.x86_64-4.0.0 -y 
+sudo yum install samba4-python.x86_64-4.0.0 -y 
+sudo yum install samba4-swat.x86_64-4.0.0 -y 
+sudo yum install samba4-test.x86_64-4.0.0 -y 
+sudo yum install samba4-winbind.x86_64-4.0.0 -y 
+sudo yum install samba4-winbind-clients.x86_64-4.0.0 -y 
+sudo yum install samba4-winbind-krb5-locator.x86_64-4.0.0 -y 
