cce79dd71c752341487950b99c345991811800b4
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-18 08:05:28 +0100

    DLA: update 2 fixes

--- Debian_6_(Squeeze)/x86_64/2016/DLA-441-1.sh
+++ Debian_6_(Squeeze)/x86_64/2016/DLA-441-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-441-1
 #
 # Security announcement date: 2016-02-29 00:00:00 UTC
-# Script generation date:     2016-03-02 07:09:10 UTC
+# Script generation date:     2016-03-18 07:04:44 UTC
 #
 # Operating System: Debian 6 (Squeeze)
 # Architecture: x86_64
@@ -16,6 +16,7 @@
 #   - pcre3:8.02-1.1+deb6u1
 #
 # CVE List:
+#   - CVE-2016-3191
 #
 # More details:
 #   - https://www.cyberwatch.fr/notices/DLA-441-1
