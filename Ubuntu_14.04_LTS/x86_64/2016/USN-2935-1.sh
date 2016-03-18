f6e8627fc25abd74722fcb22dfd34c806ac4e8fa
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-18 08:02:49 +0100

    USN: add 14 fixes

---
+++ Ubuntu_14.04_LTS/x86_64/2016/USN-2935-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2935-1
+#
+# Security announcement date: 2016-03-16 00:00:00 UTC
+# Script generation date:     2016-03-18 07:02:45 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libpam-modules:1.1.8-1ubuntu2.1
+#
+# Last versions recommanded by security team:
+#   - libpam-modules:1.1.8-1ubuntu2.2
+#
+# CVE List:
+#   - CVE-2013-7041
+#   - CVE-2014-2583
+#   - CVE-2015-3238
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2935-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libpam-modules=1.1.8-1ubuntu2.2 -y
---
+++ Ubuntu_15.10/x86_64/2016/USN-2930-3.sh
@@ -0,0 +1,29 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2930-3
+#
+# Security announcement date: 2016-03-16 00:00:00 UTC
+# Script generation date:     2016-03-18 07:02:45 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-image-4.2.0-1027-raspi2:4.2.0-1027.35
+#
+# Last versions recommanded by security team:
+#   - linux-image-4.2.0-1027-raspi2:4.2.0-1027.35
+#
+# CVE List:
+#   - CVE-2016-3134
+#   - CVE-2016-3135
+#   - CVE-2015-7566
+#   - CVE-2015-8767
+#   - CVE-2016-0723
+#   - CVE-2016-2384
+#   - CVE-2016-2782
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2930-3
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
