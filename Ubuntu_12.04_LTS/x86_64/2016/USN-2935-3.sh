f84f8a1f70a63c4ce7bde45016ad3f995cde5fc6
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-19 08:02:46 +0100

    USN: add 2 fixes

---
+++ Ubuntu_12.04_LTS/x86_64/2016/USN-2935-3.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2935-3
+#
+# Security announcement date: 2016-03-17 00:00:00 UTC
+# Script generation date:     2016-03-19 07:02:43 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libpam-modules:1.1.3-7ubuntu2.3
+#
+# Last versions recommanded by security team:
+#   - libpam-modules:1.1.3-7ubuntu2.3
+#
+# CVE List:
+#   - CVE-2013-7041
+#   - CVE-2014-2583
+#   - CVE-2015-3238
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2935-3
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libpam-modules=1.1.3-7ubuntu2.3 -y
