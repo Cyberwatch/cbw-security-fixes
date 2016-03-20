84c7c1a30a83101c232f3431c5d1db036cadf683
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-20 08:07:30 +0100

    DSA: add 5 fixes

---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3520-1.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3520-1
+#
+# Security announcement date: 2016-03-18 00:00:00 UTC
+# Script generation date:     2016-03-20 07:07:27 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - icedove:38.7.0-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - icedove:38.7.0-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-1950
+#   - CVE-2016-1954
+#   - CVE-2016-1957
+#   - CVE-2016-1960
+#   - CVE-2016-1961
+#   - CVE-2016-1962
+#   - CVE-2016-1964
+#   - CVE-2016-1966
+#   - CVE-2016-1974
+#   - CVE-2016-1977
+#   - CVE-2016-2790
+#   - CVE-2016-2791
+#   - CVE-2016-2792
+#   - CVE-2016-2793
+#   - CVE-2016-2794
+#   - CVE-2016-2795
+#   - CVE-2016-2796
+#   - CVE-2016-2797
+#   - CVE-2016-2798
+#   - CVE-2016-2799
+#   - CVE-2016-2800
+#   - CVE-2016-2801
+#   - CVE-2016-2802
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3520-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade icedove=38.7.0-1~deb8u1 -y
