260f16f63baa051bb7ffbd85928cac549ee0d862
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-24 08:10:18 +0100

    DSA: add 2 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DSA-3525-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3525-1
+#
+# Security announcement date: 2016-03-22 00:00:00 UTC
+# Script generation date:     2016-03-24 07:10:07 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - pixman:0.26.0-4+deb7u2
+#
+# Last versions recommanded by security team:
+#   - pixman:0.26.0-4+deb7u2
+#
+# CVE List:
+#   - CVE-2014-9766
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3525-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade pixman=0.26.0-4+deb7u2 -y
