c1bf94316ed9d947700b0dc9ba28e10784e1c082
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-26 08:04:27 +0100

    USN: add 10 fixes

---
+++ Ubuntu_14.04_LTS/i686/2016/USN-2941-1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2941-1
+#
+# Security announcement date: 2016-03-24 00:00:00 UTC
+# Script generation date:     2016-03-26 07:04:22 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - quagga:0.99.22.4-3ubuntu1.1
+#   - quagga-dbg:0.99.22.4-3ubuntu1.1
+#   - quagga-doc:0.99.22.4-3ubuntu1.1
+#
+# Last versions recommanded by security team:
+#   - quagga:0.99.22.4-3ubuntu1.1
+#   - quagga-dbg:0.99.22.4-3ubuntu1.1
+#   - quagga-doc:0.99.22.4-3ubuntu1.1
+#
+# CVE List:
+#   - CVE-2016-2342
+#   - CVE-2013-2236
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2941-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade quagga=0.99.22.4-3ubuntu1.1 -y
+sudo apt-get install --only-upgrade quagga-dbg=0.99.22.4-3ubuntu1.1 -y
+sudo apt-get install --only-upgrade quagga-doc=0.99.22.4-3ubuntu1.1 -y
---
+++ Ubuntu_14.04_LTS/i686/2016/USN-2942-1.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2942-1
+#
+# Security announcement date: 2016-03-24 00:00:00 UTC
+# Script generation date:     2016-03-26 07:04:23 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: i686
+#
+# Vulnerable packages fix on version:
+#   - openjdk-7-jre-zero:7u95-2.6.4-0ubuntu0.14.04.2
+#   - icedtea-7-jre-jamvm:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jdk:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-headless:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-demo:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-source:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-doc:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-dbg:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-lib:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jdk:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-headless:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre:7u95-2.6.4-0ubuntu0.14.04.2
+#
+# Last versions recommanded by security team:
+#   - openjdk-7-jre-zero:7u95-2.6.4-0ubuntu0.14.04.2
+#   - icedtea-7-jre-jamvm:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jdk:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-headless:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-demo:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-source:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-doc:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-dbg:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-lib:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jdk:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre-headless:7u95-2.6.4-0ubuntu0.14.04.2
+#   - openjdk-7-jre:7u95-2.6.4-0ubuntu0.14.04.2
+#
+# CVE List:
+#   - CVE-2016-0636
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/USN-2942-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openjdk-7-jre-zero=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade icedtea-7-jre-jamvm=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jdk=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-headless=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jre=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-demo=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-source=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-doc=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-dbg=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-lib=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jdk=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-headless=7u95-2.6.4-0ubuntu0.14.04.2 -y
+sudo apt-get install --only-upgrade openjdk-7-jre=7u95-2.6.4-0ubuntu0.14.04.2 -y
