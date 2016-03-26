233c27e3ab4fb2f5700af4ce57f118a617911a4f
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-26 08:10:35 +0100

    DSA: update 2 fixes

--- Debian_7_(Wheezy)/x86_64/2013/DSA-2725-1.sh
+++ Debian_7_(Wheezy)/x86_64/2013/DSA-2725-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2725-1
 #
 # Security announcement date: 2013-07-18 00:00:00 UTC
-# Script generation date:     2016-03-21 19:06:03 UTC
+# Script generation date:     2016-03-26 07:04:57 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -23,17 +23,17 @@
 #   - tomcat6-extras:6.0.35-6+deb7u1
 #
 # Last versions recommanded by security team:
-#   - tomcat6:6.0.35-6+deb7u1
-#   - tomcat6-common:6.0.35-6+deb7u1
-#   - tomcat6-user:6.0.35-6+deb7u1
-#   - libtomcat6-java:6.0.35-6+deb7u1
-#   - libservlet2.4-java:6.0.35-6+deb7u1
-#   - libservlet2.5-java:6.0.35-6+deb7u1
-#   - libservlet2.5-java-doc:6.0.35-6+deb7u1
-#   - tomcat6-admin:6.0.35-6+deb7u1
-#   - tomcat6-examples:6.0.35-6+deb7u1
-#   - tomcat6-docs:6.0.35-6+deb7u1
-#   - tomcat6-extras:6.0.35-6+deb7u1
+#   - tomcat6:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-common:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-user:6.0.45+dfsg-1~deb7u1
+#   - libtomcat6-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.4-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java-doc:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-admin:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-examples:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-docs:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-extras:6.0.45+dfsg-1~deb7u1
 #
 # CVE List:
 #   - CVE-2012-3544
@@ -43,14 +43,14 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2725-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tomcat6=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-common=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-user=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade libtomcat6-java=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade libservlet2.4-java=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade libservlet2.5-java=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade libservlet2.5-java-doc=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-admin=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-examples=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-docs=6.0.35-6+deb7u1 -y
-sudo apt-get install --only-upgrade tomcat6-extras=6.0.35-6+deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-common=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-user=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libtomcat6-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.4-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.5-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.5-java-doc=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-admin=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-examples=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-docs=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-extras=6.0.45+dfsg-1~deb7u1 -y
