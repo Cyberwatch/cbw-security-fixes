4b3fd5898a7c9b718740d31f42aaf33c82c17121
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-27 08:06:26 +0200

    DSA: add 4 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3530-1.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3530-1
+#
+# Security announcement date: 2016-03-25 00:00:00 UTC
+# Script generation date:     2016-03-27 06:06:22 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - tomcat6:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-common:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-user:6.0.45+dfsg-1~deb7u1
+#   - libtomcat6-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.4-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java-doc:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-admin:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-examples:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-docs:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-extras:6.0.45+dfsg-1~deb7u1
+#
+# Last versions recommanded by security team:
+#   - tomcat6:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-common:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-user:6.0.45+dfsg-1~deb7u1
+#   - libtomcat6-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.4-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java:6.0.45+dfsg-1~deb7u1
+#   - libservlet2.5-java-doc:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-admin:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-examples:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-docs:6.0.45+dfsg-1~deb7u1
+#   - tomcat6-extras:6.0.45+dfsg-1~deb7u1
+#
+# CVE List:
+#   - CVE-2013-4286
+#   - CVE-2013-4322
+#   - CVE-2013-4590
+#   - CVE-2014-0033
+#   - CVE-2014-0075
+#   - CVE-2014-0096
+#   - CVE-2014-0099
+#   - CVE-2014-0119
+#   - CVE-2014-0227
+#   - CVE-2014-0230
+#   - CVE-2014-7810
+#   - CVE-2015-5174
+#   - CVE-2015-5345
+#   - CVE-2015-5346
+#   - CVE-2015-5351
+#   - CVE-2016-0706
+#   - CVE-2016-0714
+#   - CVE-2016-0763
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3530-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tomcat6=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-common=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-user=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libtomcat6-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.4-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.5-java=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade libservlet2.5-java-doc=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-admin=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-examples=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-docs=6.0.45+dfsg-1~deb7u1 -y
+sudo apt-get install --only-upgrade tomcat6-extras=6.0.45+dfsg-1~deb7u1 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3531-1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3531-1
+#
+# Security announcement date: 2016-03-25 00:00:00 UTC
+# Script generation date:     2016-03-27 06:06:22 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser:49.0.2623.108-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - chromium-browser:49.0.2623.108-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-1646
+#   - CVE-2016-1647
+#   - CVE-2016-1648
+#   - CVE-2016-1649
+#   - CVE-2016-1650
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3531-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade chromium-browser=49.0.2623.108-1~deb8u1 -y
