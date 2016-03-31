b7f06ee662adf4dd1457d76a2da4e66d51bf6504
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-03-31 20:09:19 +0200

    DSA: update 2 fixes

--- Debian_7_(Wheezy)/i386/2014/DSA-2940-1.sh
+++ Debian_7_(Wheezy)/i386/2014/DSA-2940-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2940-1
 #
 # Security announcement date: 2014-06-01 00:00:00 UTC
-# Script generation date:     2016-01-01 07:03:31 UTC
+# Script generation date:     2016-03-31 18:05:21 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - libstruts1.2-java:1.2.9-5+deb7u1
 #
 # Last versions recommanded by security team:
-#   - libstruts1.2-java:1.2.9-5+deb7u1
+#   - libstruts1.2-java:1.2.9-5+deb7u2
 #
 # CVE List:
 #   - CVE-2014-0114
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-2940-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libstruts1.2-java=1.2.9-5+deb7u1 -y
+sudo apt-get install --only-upgrade libstruts1.2-java=1.2.9-5+deb7u2 -y
