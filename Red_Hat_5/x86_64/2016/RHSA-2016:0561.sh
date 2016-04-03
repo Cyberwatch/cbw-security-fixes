b4a97a59a0be80c109c0c3d8471bb9ce9984c9e2
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-03 08:18:36 +0200

    RHSA: add 2 fixes

---
+++ Red_Hat_5/x86_64/2016/RHSA-2016:0561.sh
@@ -0,0 +1,26 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0561
+#
+# Security announcement date: 2016-03-31 19:31:29 UTC
+# Script generation date:     2016-04-03 06:18:32 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - redhat-release.x86_64:5Client-5.11.0.3
+#   - redhat-release.x86_64:5Server-5.11.0.3
+#
+# Last versions recommanded by security team:
+#   - redhat-release.x86_64:5Server-5.11.0.3
+#   - redhat-release.x86_64:5Server-5.11.0.3
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0561
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install redhat-release.x86_64-5Server -y 
+sudo yum install redhat-release.x86_64-5Server -y 
---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0562.sh
@@ -0,0 +1,112 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0562
+#
+# Security announcement date: 2016-03-31 19:34:27 UTC
+# Script generation date:     2016-04-03 06:18:32 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - bind-debuginfo.i686:9.8.2-0.30.rc1.el6_6.5
+#   - bind-debuginfo.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind-libs.i686:9.8.2-0.30.rc1.el6_6.5
+#   - bind-libs.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind-utils.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind-chroot.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind-devel.i686:9.8.2-0.30.rc1.el6_6.5
+#   - bind-devel.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind-sdb.x86_64:9.8.2-0.30.rc1.el6_6.5
+#   - bind.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-chroot.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-debuginfo.i686:9.8.2-0.17.rc1.el6_4.8
+#   - bind-debuginfo.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-libs.i686:9.8.2-0.17.rc1.el6_4.8
+#   - bind-libs.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-utils.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-chroot.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-utils.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.i686:9.8.2-0.17.rc1.el6_4.8
+#   - bind-devel.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-sdb.x86_64:9.8.2-0.17.rc1.el6_4.8
+#   - bind-devel.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-sdb.x86_64:9.8.2-0.23.rc1.el6_5.3
+#
+# Last versions recommanded by security team:
+#   - bind-debuginfo.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-utils.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-chroot.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-sdb.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-chroot.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-utils.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-chroot.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-debuginfo.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-libs.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-utils.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-sdb.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.i686:9.8.2-0.23.rc1.el6_5.3
+#   - bind-devel.x86_64:9.8.2-0.23.rc1.el6_5.3
+#   - bind-sdb.x86_64:9.8.2-0.23.rc1.el6_5.3
+#
+# CVE List:
+#   - CVE-2016-1285
+#   - CVE-2016-1286
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0562
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install bind-debuginfo.i686-9.8.2 -y 
+sudo yum install bind-debuginfo.x86_64-9.8.2 -y 
+sudo yum install bind-libs.i686-9.8.2 -y 
+sudo yum install bind-libs.x86_64-9.8.2 -y 
+sudo yum install bind-utils.x86_64-9.8.2 -y 
+sudo yum install bind.x86_64-9.8.2 -y 
+sudo yum install bind-chroot.x86_64-9.8.2 -y 
+sudo yum install bind-devel.i686-9.8.2 -y 
+sudo yum install bind-devel.x86_64-9.8.2 -y 
+sudo yum install bind-sdb.x86_64-9.8.2 -y 
+sudo yum install bind.x86_64-9.8.2 -y 
+sudo yum install bind-chroot.x86_64-9.8.2 -y 
+sudo yum install bind-debuginfo.i686-9.8.2 -y 
+sudo yum install bind-debuginfo.x86_64-9.8.2 -y 
+sudo yum install bind-libs.i686-9.8.2 -y 
+sudo yum install bind-libs.x86_64-9.8.2 -y 
+sudo yum install bind-utils.x86_64-9.8.2 -y 
+sudo yum install bind.x86_64-9.8.2 -y 
+sudo yum install bind-chroot.x86_64-9.8.2 -y 
+sudo yum install bind-debuginfo.i686-9.8.2 -y 
+sudo yum install bind-debuginfo.x86_64-9.8.2 -y 
+sudo yum install bind-libs.i686-9.8.2 -y 
+sudo yum install bind-libs.x86_64-9.8.2 -y 
+sudo yum install bind-utils.x86_64-9.8.2 -y 
+sudo yum install bind-devel.i686-9.8.2 -y 
+sudo yum install bind-devel.x86_64-9.8.2 -y 
+sudo yum install bind-sdb.x86_64-9.8.2 -y 
+sudo yum install bind-devel.i686-9.8.2 -y 
+sudo yum install bind-devel.x86_64-9.8.2 -y 
+sudo yum install bind-sdb.x86_64-9.8.2 -y 
