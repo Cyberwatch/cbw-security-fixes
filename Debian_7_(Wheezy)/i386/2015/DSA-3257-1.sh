d9194000741fea8261e66e4991d37a491b128b34
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-05 20:13:54 +0200

    DSA: update 6 fixes

--- Debian_7_(Wheezy)/i386/2015/DSA-3257-1.sh
+++ Debian_7_(Wheezy)/i386/2015/DSA-3257-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3257-1
 #
 # Security announcement date: 2015-05-11 00:00:00 UTC
-# Script generation date:     2016-03-21 19:06:56 UTC
+# Script generation date:     2016-04-05 18:09:25 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -14,8 +14,8 @@
 #   - mercurial-common:2.2.2-4+deb7u1
 #
 # Last versions recommanded by security team:
-#   - mercurial:2.2.2-4+deb7u1
-#   - mercurial-common:2.2.2-4+deb7u1
+#   - mercurial:2.2.2-4+deb7u2
+#   - mercurial-common:2.2.2-4+deb7u2
 #
 # CVE List:
 #   - CVE-2014-9462
@@ -24,5 +24,5 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3257-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade mercurial=2.2.2-4+deb7u1 -y
-sudo apt-get install --only-upgrade mercurial-common=2.2.2-4+deb7u1 -y
+sudo apt-get install --only-upgrade mercurial=2.2.2-4+deb7u2 -y
+sudo apt-get install --only-upgrade mercurial-common=2.2.2-4+deb7u2 -y
