3b1089adaed29d1fcbe005777a3a212cc07733fb
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-08 08:14:34 +0200

    CESA: add one fix

---
+++ CentOS_7/x86_64/2016/CESA-2016:0594.sh
@@ -0,0 +1,36 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0594
+#
+# Security announcement date: 2016-04-05 20:02:28 UTC
+# Script generation date:     2016-04-08 06:14:31 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - graphite2.i686:1.3.6-1.el7_2
+#   - graphite2.x86_64:1.3.6-1.el7_2
+#   - graphite2-devel.i686:1.3.6-1.el7_2
+#   - graphite2-devel.x86_64:1.3.6-1.el7_2
+#
+# Last versions recommanded by security team:
+#   - graphite2.i686:1.3.6-1.el7_2
+#   - graphite2.x86_64:1.3.6-1.el7_2
+#   - graphite2-devel.i686:1.3.6-1.el7_2
+#   - graphite2-devel.x86_64:1.3.6-1.el7_2
+#
+# CVE List:
+#   - CVE-2016-1523
+#   - CVE-2016-1521
+#   - CVE-2016-1522
+#   - CVE-2016-1526
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0594
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install graphite2.i686-1.3.6 -y 
+sudo yum install graphite2.x86_64-1.3.6 -y 
+sudo yum install graphite2-devel.i686-1.3.6 -y 
+sudo yum install graphite2-devel.x86_64-1.3.6 -y 
