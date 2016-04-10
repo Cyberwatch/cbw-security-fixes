b30a3c93574d2bf262a049c7012282ca3d93b77b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-10 20:18:23 +0200

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0610.sh
@@ -0,0 +1,47 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0610
+#
+# Security announcement date: 2016-04-08 12:59:08 UTC
+# Script generation date:     2016-04-10 18:18:20 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - flash-plugin.i686:11.2.202.616-1.el6_7
+#
+# Last versions recommanded by security team:
+#   - flash-plugin.i686:11.2.202.616-1.el6_7
+#
+# CVE List:
+#   - CVE-2016-1006
+#   - CVE-2016-1011
+#   - CVE-2016-1012
+#   - CVE-2016-1013
+#   - CVE-2016-1014
+#   - CVE-2016-1015
+#   - CVE-2016-1016
+#   - CVE-2016-1017
+#   - CVE-2016-1018
+#   - CVE-2016-1019
+#   - CVE-2016-1020
+#   - CVE-2016-1021
+#   - CVE-2016-1022
+#   - CVE-2016-1023
+#   - CVE-2016-1024
+#   - CVE-2016-1025
+#   - CVE-2016-1026
+#   - CVE-2016-1027
+#   - CVE-2016-1028
+#   - CVE-2016-1029
+#   - CVE-2016-1030
+#   - CVE-2016-1031
+#   - CVE-2016-1032
+#   - CVE-2016-1033
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0610
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install flash-plugin.i686-11.2.202.616 -y 
