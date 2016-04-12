5a3f09d7d93525da211cf0c125bb2febb18a6899
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-12 20:11:16 +0200

    DSA: update 4 fixes

--- Debian_7_(Wheezy)/x86_64/2016/DSA-3485-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3485-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3485-1
 #
 # Security announcement date: 2016-02-20 00:00:00 UTC
-# Script generation date:     2016-02-22 07:04:28 UTC
+# Script generation date:     2016-04-12 18:06:48 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - didiwiki:0.5-11+deb7u1
 #
 # Last versions recommanded by security team:
-#   - didiwiki:0.5-11+deb7u1
+#   - didiwiki:0.5-11+deb7u2
 #
 # CVE List:
 #   - CVE-2013-7448
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/notices/DSA-3485-1
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade didiwiki=0.5-11+deb7u1 -y
+sudo apt-get install --only-upgrade didiwiki=0.5-11+deb7u2 -y
