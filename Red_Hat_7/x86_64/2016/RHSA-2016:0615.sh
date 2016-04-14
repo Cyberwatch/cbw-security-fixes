bae332844b99129914e3fdd79e7184277b351e2c
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-14 20:21:07 +0200

    RHSA: add 2 fixes

---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:0615.sh
@@ -0,0 +1,30 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0615
+#
+# Security announcement date: 2016-04-12 07:25:27 UTC
+# Script generation date:     2016-04-14 18:21:02 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - openvswitch.x86_64:2.4.0-2.el7_2
+#   - openvswitch-debuginfo.x86_64:2.4.0-2.el7_2
+#   - openvswitch-devel.x86_64:2.4.0-2.el7_2
+#
+# Last versions recommanded by security team:
+#   - openvswitch.x86_64:2.4.0-2.el7_2
+#   - openvswitch-debuginfo.x86_64:2.4.0-2.el7_2
+#   - openvswitch-devel.x86_64:2.4.0-2.el7_2
+#
+# CVE List:
+#   - CVE-2016-2074
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0615
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install openvswitch.x86_64-2.4.0 -y 
+sudo yum install openvswitch-debuginfo.x86_64-2.4.0 -y 
+sudo yum install openvswitch-devel.x86_64-2.4.0 -y 
