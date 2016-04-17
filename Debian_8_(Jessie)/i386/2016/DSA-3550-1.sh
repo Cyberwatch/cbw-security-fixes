9884dc3f34437d4fa7862647d6343f9c83669b21
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-17 08:07:14 +0200

    DSA: add 6 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3550-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3550-1
+#
+# Security announcement date: 2016-04-15 00:00:00 UTC
+# Script generation date:     2016-04-17 06:07:11 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - openssh:1:6.7p1-5+deb8u2
+#
+# Last versions recommanded by security team:
+#   - openssh:1:6.7p1-5+deb8u2
+#
+# CVE List:
+#   - CVE-2015-8325
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3550-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openssh=1:6.7p1-5+deb8u2 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3549-1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3549-1
+#
+# Security announcement date: 2016-04-15 00:00:00 UTC
+# Script generation date:     2016-04-17 06:07:10 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser:50.0.2661.75-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - chromium-browser:50.0.2661.75-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-1651
+#   - CVE-2016-1652
+#   - CVE-2016-1653
+#   - CVE-2016-1654
+#   - CVE-2016-1655
+#   - CVE-2016-1657
+#   - CVE-2016-1658
+#   - CVE-2016-1659
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3549-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade chromium-browser=50.0.2661.75-1~deb8u1 -y
