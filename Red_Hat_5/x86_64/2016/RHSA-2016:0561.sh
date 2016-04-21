dbd95d56ec70acce015c59ee95a79c9530f5f106
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-21 20:22:05 +0200

    RHSA: update one fix

--- Red_Hat_5/x86_64/2016/RHSA-2016:0561.sh
+++ Red_Hat_5/x86_64/2016/RHSA-2016:0561.sh
@@ -2,15 +2,15 @@
 #
 # Security fix for RHSA-2016:0561
 #
-# Security announcement date: 2016-03-31 19:31:29 UTC
-# Script generation date:     2016-04-19 18:20:33 UTC
+# Security announcement date: 2016-04-19 12:44:54 UTC
+# Script generation date:     2016-04-21 18:18:29 UTC
 #
 # Operating System: Red Hat 5
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - redhat-release.x86_64:5Client-5.11.0.3
-#   - redhat-release.x86_64:5Server-5.11.0.3
+#   - redhat-release.x86_64:5Client-5.11.0.4
+#   - redhat-release.x86_64:5Server-5.11.0.4
 #
 # Last versions recommanded by security team:
 #   - redhat-release.x86_64:5Server-5.11.0.4
