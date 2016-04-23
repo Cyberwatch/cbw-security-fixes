4d1798a20654c6e9bb143461bf2e14b486eec464
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-23 08:19:06 +0200

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:0651.sh
@@ -0,0 +1,65 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0651
+#
+# Security announcement date: 2016-04-20 20:04:59 UTC
+# Script generation date:     2016-04-23 06:19:02 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debuginfo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el6_7
+#
+# Last versions recommanded by security team:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debuginfo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el6_7
+#
+# CVE List:
+#   - CVE-2016-0686
+#   - CVE-2016-0687
+#   - CVE-2016-0695
+#   - CVE-2016-3425
+#   - CVE-2016-3426
+#   - CVE-2016-3427
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0651
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.8.0-openjdk.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-debuginfo.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc.noarch-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-debug.noarch-1.8.0.91 -y 
---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:0650.sh
@@ -0,0 +1,71 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:0650
+#
+# Security announcement date: 2016-04-20 20:04:02 UTC
+# Script generation date:     2016-04-23 06:19:01 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-debuginfo.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-accessibility.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-accessibility-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el7_2
+#
+# Last versions recommanded by security team:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-debuginfo.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-accessibility.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-accessibility-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el7_2
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el7_2
+#
+# CVE List:
+#   - CVE-2016-0686
+#   - CVE-2016-0687
+#   - CVE-2016-0695
+#   - CVE-2016-3425
+#   - CVE-2016-3426
+#   - CVE-2016-3427
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/RHSA-2016:0650
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.8.0-openjdk.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-debuginfo.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc.noarch-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-debug.noarch-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-accessibility.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-accessibility-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src-debug.x86_64-1.8.0.91 -y 
