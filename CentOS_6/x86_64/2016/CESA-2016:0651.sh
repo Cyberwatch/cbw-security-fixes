be6808c4e3fb5dc26cc909e3400bc0a021fb286e
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-23 20:11:36 +0200

    CESA: add 5 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:0651.sh
@@ -0,0 +1,62 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0651
+#
+# Security announcement date: 2016-04-21 14:19:29 UTC
+# Script generation date:     2016-04-23 18:11:33 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el6_7
+#
+# Last versions recommanded by security team:
+#   - java-1.8.0-openjdk.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.91-0.b14.el6_7
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.91-0.b14.el6_7
+#
+# CVE List:
+#   - CVE-2016-0686
+#   - CVE-2016-0687
+#   - CVE-2016-0695
+#   - CVE-2016-3425
+#   - CVE-2016-3426
+#   - CVE-2016-3427
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0651
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.8.0-openjdk.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-demo-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc.noarch-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-debug.noarch-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src.x86_64-1.8.0.91 -y 
+sudo yum install java-1.8.0-openjdk-src-debug.x86_64-1.8.0.91 -y 
---
+++ CentOS_7/x86_64/2016/CESA-2016:0676.sh
@@ -0,0 +1,46 @@
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:0676
+#
+# Security announcement date: 2016-04-21 16:45:32 UTC
+# Script generation date:     2016-04-23 18:11:34 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.7.0-openjdk.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-accessibility.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-demo.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-devel.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-headless.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-javadoc.noarch:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-src.x86_64:1.7.0.101-2.6.6.1.el7_2
+#
+# Last versions recommanded by security team:
+#   - java-1.7.0-openjdk.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-accessibility.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-demo.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-devel.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-headless.x86_64:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-javadoc.noarch:1.7.0.101-2.6.6.1.el7_2
+#   - java-1.7.0-openjdk-src.x86_64:1.7.0.101-2.6.6.1.el7_2
+#
+# CVE List:
+#   - CVE-2016-0686
+#   - CVE-2016-0687
+#   - CVE-2016-0695
+#   - CVE-2016-3425
+#   - CVE-2016-3427
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/CESA-2016:0676
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.7.0-openjdk.x86_64-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-accessibility.x86_64-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-demo.x86_64-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-devel.x86_64-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-headless.x86_64-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-javadoc.noarch-1.7.0.101 -y 
+sudo yum install java-1.7.0-openjdk-src.x86_64-1.7.0.101 -y 
