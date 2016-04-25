6f34209a5c6c336b12bd675ffa1bd6a9381ee14d
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-04-25 08:07:19 +0200

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3555-1.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3555-1
+#
+# Security announcement date: 2016-04-23 00:00:00 UTC
+# Script generation date:     2016-04-25 06:07:16 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - imlib2:1.4.6-2+deb8u2
+#
+# Last versions recommanded by security team:
+#   - imlib2:1.4.6-2+deb8u2
+#
+# CVE List:
+#   - CVE-2011-5326
+#   - CVE-2014-9771
+#   - CVE-2016-3993
+#   - CVE-2016-3994
+#   - CVE-2016-4024
+#
+# More details:
+#   - https://www.cyberwatch.fr/notices/DSA-3555-1
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade imlib2=1.4.6-2+deb8u2 -y
