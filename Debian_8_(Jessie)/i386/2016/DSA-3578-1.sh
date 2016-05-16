f956b16eb98406075bf9333408ac4fad88db3442
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-16 08:06:22 +0200

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3578-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3578-1
+#
+# Security announcement date: 2016-05-14 00:00:00 UTC
+# Script generation date:     2016-05-16 06:06:19 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libidn:1.29-1+deb8u1
+#
+# Last versions recommanded by security team:
+#   - libidn:1.29-1+deb8u1
+#
+# CVE List:
+#   - CVE-2015-2059
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libidn=1.29-1+deb8u1 -y
