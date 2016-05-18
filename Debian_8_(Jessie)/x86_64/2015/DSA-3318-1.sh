9bfc08067623625f9d765727e9c17721701b5db5
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-18 08:09:46 +0200

    DSA: add 4 fixes and update 2 fixes

--- Debian_8_(Jessie)/x86_64/2015/DSA-3318-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3318-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3318-1
 #
 # Security announcement date: 2015-07-26 00:00:00 UTC
-# Script generation date:     2016-05-12 18:05:35 UTC
+# Script generation date:     2016-05-18 06:05:37 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -15,9 +15,9 @@
 #   - libexpat1:2.1.0-6+deb8u1
 #
 # Last versions recommanded by security team:
-#   - expat:2.1.0-6+deb8u1
-#   - libexpat1-dev:2.1.0-6+deb8u1
-#   - libexpat1:2.1.0-6+deb8u1
+#   - expat:2.1.0-6+deb8u2
+#   - libexpat1-dev:2.1.0-6+deb8u2
+#   - libexpat1:2.1.0-6+deb8u2
 #
 # CVE List:
 #   - CVE-2015-1283
@@ -26,6 +26,6 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade expat=2.1.0-6+deb8u1 -y
-sudo apt-get install --only-upgrade libexpat1-dev=2.1.0-6+deb8u1 -y
-sudo apt-get install --only-upgrade libexpat1=2.1.0-6+deb8u1 -y
+sudo apt-get install --only-upgrade expat=2.1.0-6+deb8u2 -y
+sudo apt-get install --only-upgrade libexpat1-dev=2.1.0-6+deb8u2 -y
+sudo apt-get install --only-upgrade libexpat1=2.1.0-6+deb8u2 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3579-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3579-1
+#
+# Security announcement date: 2016-05-16 00:00:00 UTC
+# Script generation date:     2016-05-18 06:06:16 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - xerces-c:3.1.1-5.1+deb8u2
+#
+# Last versions recommanded by security team:
+#   - xerces-c:3.1.1-5.1+deb8u2
+#
+# CVE List:
+#   - CVE-2016-2099
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade xerces-c=3.1.1-5.1+deb8u2 -y
