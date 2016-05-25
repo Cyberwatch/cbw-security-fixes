37a5cd864db3affa0617de9c177daf7793114859
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-25 08:10:58 +0200

    DSA: add 2 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3586-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3586-1
+#
+# Security announcement date: 2016-05-23 00:00:00 UTC
+# Script generation date:     2016-05-25 06:10:54 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - atheme-services:6.0.11-2+deb8u1
+#
+# Last versions recommanded by security team:
+#   - atheme-services:6.0.11-2+deb8u1
+#
+# CVE List:
+#   - CVE-2016-4478
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade atheme-services=6.0.11-2+deb8u1 -y
