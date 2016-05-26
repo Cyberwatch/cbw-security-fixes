08805634842e19939dbeb2e6196ac1fcd6cba31e
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-26 16:09:07 +0200

    RHSA: add 3 fixes and update 2 fixes

---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1098.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1098
+#
+# Security announcement date: 2016-05-24 00:26:30 UTC
+# Script generation date:     2016-05-26 14:05:29 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - jq.x86_64:1.3-3.el7ost
+#   - jq-debuginfo.x86_64:1.3-3.el7ost
+#
+# Last versions recommanded by security team:
+#   - jq.x86_64:1.3-3.el7ost
+#   - jq-debuginfo.x86_64:1.3-3.el7ost
+#
+# CVE List:
+#   - CVE-2015-8863
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install jq.x86_64-1.3 -y 
+sudo yum install jq-debuginfo.x86_64-1.3 -y 
