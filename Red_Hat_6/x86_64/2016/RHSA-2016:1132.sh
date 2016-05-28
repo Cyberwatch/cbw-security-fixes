3674203a2d063a33bdff98179158eed9bf4faca0
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-28 20:12:28 +0200

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1132.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1132
+#
+# Security announcement date: 2016-05-26 09:53:32 UTC
+# Script generation date:     2016-05-28 18:12:24 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - rh-mariadb100-mariadb.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-bench.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-common.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-config.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-debuginfo.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-devel.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-errmsg.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-oqgraph-engine.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-server.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-test.x86_64:10.0.25-4.el6
+#
+# Last versions recommanded by security team:
+#   - rh-mariadb100-mariadb.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-bench.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-common.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-config.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-debuginfo.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-devel.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-errmsg.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-oqgraph-engine.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-server.x86_64:10.0.25-4.el6
+#   - rh-mariadb100-mariadb-test.x86_64:10.0.25-4.el6
+#
+# CVE List:
+#   - CVE-2015-3210
+#   - CVE-2015-3217
+#   - CVE-2015-4792
+#   - CVE-2015-4802
+#   - CVE-2015-4815
+#   - CVE-2015-4816
+#   - CVE-2015-4819
+#   - CVE-2015-4826
+#   - CVE-2015-4830
+#   - CVE-2015-4836
+#   - CVE-2015-4858
+#   - CVE-2015-4861
+#   - CVE-2015-4870
+#   - CVE-2015-4879
+#   - CVE-2015-4895
+#   - CVE-2015-4913
+#   - CVE-2015-5073
+#   - CVE-2015-8381
+#   - CVE-2015-8383
+#   - CVE-2015-8384
+#   - CVE-2015-8385
+#   - CVE-2015-8386
+#   - CVE-2015-8388
+#   - CVE-2015-8391
+#   - CVE-2015-8392
+#   - CVE-2015-8395
+#   - CVE-2016-0505
+#   - CVE-2016-0546
+#   - CVE-2016-0596
+#   - CVE-2016-0597
+#   - CVE-2016-0598
+#   - CVE-2016-0600
+#   - CVE-2016-0606
+#   - CVE-2016-0608
+#   - CVE-2016-0609
+#   - CVE-2016-0610
+#   - CVE-2016-0616
+#   - CVE-2016-0640
+#   - CVE-2016-0641
+#   - CVE-2016-0642
+#   - CVE-2016-0643
+#   - CVE-2016-0644
+#   - CVE-2016-0646
+#   - CVE-2016-0647
+#   - CVE-2016-0648
+#   - CVE-2016-0649
+#   - CVE-2016-0650
+#   - CVE-2016-0651
+#   - CVE-2016-0655
+#   - CVE-2016-0666
+#   - CVE-2016-0668
+#   - CVE-2016-1283
+#   - CVE-2016-2047
+#   - CVE-2016-3191
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install rh-mariadb100-mariadb.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-bench.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-common.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-config.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-debuginfo.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-devel.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-errmsg.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-oqgraph-engine.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-server.x86_64-10.0.25 -y 
+sudo yum install rh-mariadb100-mariadb-test.x86_64-10.0.25 -y 
