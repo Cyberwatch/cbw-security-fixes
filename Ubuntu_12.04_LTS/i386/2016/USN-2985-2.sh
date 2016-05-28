b22fa5333feeccc247680e1c91663a98a54f65f2
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-28 08:03:45 +0200

    USN: add 6 fixes

---
+++ Ubuntu_12.04_LTS/i386/2016/USN-2985-2.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-2985-2
+#
+# Security announcement date: 2016-05-26 00:00:00 UTC
+# Script generation date:     2016-05-28 06:03:41 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libc-bin:2.15-0ubuntu10.15
+#   - libc6-dev:2.15-0ubuntu10.15
+#   - libc6:2.15-0ubuntu10.15
+#
+# Last versions recommanded by security team:
+#   - libc-bin:2.15-0ubuntu10.15
+#   - libc6-dev:2.15-0ubuntu10.15
+#   - libc6:2.15-0ubuntu10.15
+#
+# CVE List:
+#   - CVE-2014-9761
+#   - CVE-2013-2207
+#   - CVE-2016-2856
+#   - CVE-2014-8121
+#   - CVE-2015-1781
+#   - CVE-2015-5277
+#   - CVE-2015-8776
+#   - CVE-2015-8777
+#   - CVE-2015-8778
+#   - CVE-2015-8779
+#   - CVE-2016-3075
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libc-bin=2.15-0ubuntu10.15 -y
+sudo apt-get install --only-upgrade libc6-dev=2.15-0ubuntu10.15 -y
+sudo apt-get install --only-upgrade libc6=2.15-0ubuntu10.15 -y
