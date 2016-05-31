36411c85cad8634b8521e2165f50178d053f7c6b
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-05-31 09:24:25 +0200

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3587-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3587-1
+#
+# Security announcement date: 2016-05-27 00:00:00 UTC
+# Script generation date:     2016-05-31 07:24:16 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libgd2:2.1.0-5+deb8u3
+#
+# Last versions recommanded by security team:
+#   - libgd2:2.1.0-5+deb8u3
+#
+# CVE List:
+#   - CVE-2013-7456
+#   - CVE-2015-8874
+#   - CVE-2015-8877
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libgd2=2.1.0-5+deb8u3 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3588-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3588-1
+#
+# Security announcement date: 2016-05-29 00:00:00 UTC
+# Script generation date:     2016-05-31 07:24:16 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - symfony:2.3.21+dfsg-4+deb8u3
+#
+# Last versions recommanded by security team:
+#   - symfony:2.3.21+dfsg-4+deb8u3
+#
+# CVE List:
+#   - CVE-2016-1902
+#   - CVE-2016-4423
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade symfony=2.3.21+dfsg-4+deb8u3 -y
