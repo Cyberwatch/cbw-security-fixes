c87c410ef0ea3b7016538b0d948e4a61f26d2cd2
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-06-03 20:13:15 +0200

    RHSA: add one fix

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1190.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1190
+#
+# Security announcement date: 2016-06-01 10:52:45 UTC
+# Script generation date:     2016-06-03 18:13:11 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser.x86_64:51.0.2704.63-1.el6
+#   - chromium-browser-debuginfo.x86_64:51.0.2704.63-1.el6
+#
+# Last versions recommanded by security team:
+#   - chromium-browser.x86_64:51.0.2704.79-1.el6
+#   - chromium-browser-debuginfo.x86_64:51.0.2704.79-1.el6
+#
+# CVE List:
+#   - CVE-2016-1672
+#   - CVE-2016-1673
+#   - CVE-2016-1674
+#   - CVE-2016-1675
+#   - CVE-2016-1676
+#   - CVE-2016-1677
+#   - CVE-2016-1678
+#   - CVE-2016-1679
+#   - CVE-2016-1680
+#   - CVE-2016-1681
+#   - CVE-2016-1682
+#   - CVE-2016-1683
+#   - CVE-2016-1684
+#   - CVE-2016-1685
+#   - CVE-2016-1686
+#   - CVE-2016-1687
+#   - CVE-2016-1688
+#   - CVE-2016-1689
+#   - CVE-2016-1690
+#   - CVE-2016-1691
+#   - CVE-2016-1692
+#   - CVE-2016-1693
+#   - CVE-2016-1694
+#   - CVE-2016-1695
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install chromium-browser.x86_64-51.0.2704.79 -y 
+sudo yum install chromium-browser-debuginfo.x86_64-51.0.2704.79 -y 
