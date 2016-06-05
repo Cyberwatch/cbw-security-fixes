9176ffa8e84f8feb3494f933c2e886984873c23f
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-06-05 20:12:09 +0200

    DSA: update 10 fixes

--- Debian_8_(Jessie)/x86_64/2016/DSA-3426-2.sh
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3426-2.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3426-2
 #
 # Security announcement date: 2016-03-03 00:00:00 UTC
-# Script generation date:     2016-05-12 18:06:03 UTC
+# Script generation date:     2016-06-05 18:06:10 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -16,7 +16,7 @@
 #   - ctdb-pcp-pmda:2.5.4+debian0-4+deb8u1
 #
 # Last versions recommanded by security team:
-#   - ctdb:2:4.2.10+dfsg-0+deb8u2
+#   - ctdb:2:4.2.10+dfsg-0+deb8u3
 #   - libctdb-dev:2.5.4+debian0-4+deb8u1
 #   - ctdb-dbg:2.5.4+debian0-4+deb8u1
 #   - ctdb-pcp-pmda:2.5.4+debian0-4+deb8u1
@@ -27,7 +27,7 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ctdb=2:4.2.10+dfsg-0+deb8u2 -y
+sudo apt-get install --only-upgrade ctdb=2:4.2.10+dfsg-0+deb8u3 -y
 sudo apt-get install --only-upgrade libctdb-dev=2.5.4+debian0-4+deb8u1 -y
 sudo apt-get install --only-upgrade ctdb-dbg=2.5.4+debian0-4+deb8u1 -y
 sudo apt-get install --only-upgrade ctdb-pcp-pmda=2.5.4+debian0-4+deb8u1 -y
--- Debian_8_(Jessie)/x86_64/2016/DSA-3514-1.sh
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3514-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3514-1
 #
 # Security announcement date: 2016-03-12 00:00:00 UTC
-# Script generation date:     2016-05-12 18:06:05 UTC
+# Script generation date:     2016-06-05 18:06:12 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -36,30 +36,30 @@
 #   - libwbclient-dev:2:4.1.17+dfsg-2+deb8u2
 #
 # Last versions recommanded by security team:
-#   - samba:2:4.2.10+dfsg-0+deb8u2
-#   - samba-libs:2:4.2.10+dfsg-0+deb8u2
-#   - samba-common:2:4.2.10+dfsg-0+deb8u2
-#   - samba-common-bin:2:4.2.10+dfsg-0+deb8u2
-#   - smbclient:2:4.2.10+dfsg-0+deb8u2
-#   - samba-testsuite:2:4.2.10+dfsg-0+deb8u2
-#   - registry-tools:2:4.2.10+dfsg-0+deb8u2
-#   - libparse-pidl-perl:2:4.2.10+dfsg-0+deb8u2
-#   - samba-dev:2:4.2.10+dfsg-0+deb8u2
-#   - samba-doc:2:4.2.10+dfsg-0+deb8u2
-#   - python-samba:2:4.2.10+dfsg-0+deb8u2
-#   - samba-dsdb-modules:2:4.2.10+dfsg-0+deb8u2
-#   - samba-vfs-modules:2:4.2.10+dfsg-0+deb8u2
-#   - libpam-smbpass:2:4.2.10+dfsg-0+deb8u2
-#   - libsmbclient:2:4.2.10+dfsg-0+deb8u2
-#   - libsmbclient-dev:2:4.2.10+dfsg-0+deb8u2
+#   - samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-libs:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common-bin:2:4.2.10+dfsg-0+deb8u3
+#   - smbclient:2:4.2.10+dfsg-0+deb8u3
+#   - samba-testsuite:2:4.2.10+dfsg-0+deb8u3
+#   - registry-tools:2:4.2.10+dfsg-0+deb8u3
+#   - libparse-pidl-perl:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dev:2:4.2.10+dfsg-0+deb8u3
+#   - samba-doc:2:4.2.10+dfsg-0+deb8u3
+#   - python-samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dsdb-modules:2:4.2.10+dfsg-0+deb8u3
+#   - samba-vfs-modules:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-smbpass:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient-dev:2:4.2.10+dfsg-0+deb8u3
 #   - libsmbsharemodes0:2:4.1.17+dfsg-2+deb8u2
 #   - libsmbsharemodes-dev:2:4.1.17+dfsg-2+deb8u2
-#   - winbind:2:4.2.10+dfsg-0+deb8u2
-#   - libpam-winbind:2:4.2.10+dfsg-0+deb8u2
-#   - libnss-winbind:2:4.2.10+dfsg-0+deb8u2
-#   - samba-dbg:2:4.2.10+dfsg-0+deb8u2
-#   - libwbclient0:2:4.2.10+dfsg-0+deb8u2
-#   - libwbclient-dev:2:4.2.10+dfsg-0+deb8u2
+#   - winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libnss-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dbg:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient0:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient-dev:2:4.2.10+dfsg-0+deb8u3
 #
 # CVE List:
 #   - CVE-2015-7560
@@ -69,27 +69,27 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade samba=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-libs=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-common=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-common-bin=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade smbclient=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-testsuite=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade registry-tools=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libparse-pidl-perl=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-dev=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-doc=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade python-samba=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-dsdb-modules=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-vfs-modules=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libpam-smbpass=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libsmbclient=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libsmbclient-dev=2:4.2.10+dfsg-0+deb8u2 -y
+sudo apt-get install --only-upgrade samba=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-libs=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-common=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-common-bin=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade smbclient=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-testsuite=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade registry-tools=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libparse-pidl-perl=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dev=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-doc=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade python-samba=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dsdb-modules=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-vfs-modules=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libpam-smbpass=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libsmbclient=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libsmbclient-dev=2:4.2.10+dfsg-0+deb8u3 -y
 sudo apt-get install --only-upgrade libsmbsharemodes0=2:4.1.17+dfsg-2+deb8u2 -y
 sudo apt-get install --only-upgrade libsmbsharemodes-dev=2:4.1.17+dfsg-2+deb8u2 -y
-sudo apt-get install --only-upgrade winbind=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libpam-winbind=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libnss-winbind=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade samba-dbg=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libwbclient0=2:4.2.10+dfsg-0+deb8u2 -y
-sudo apt-get install --only-upgrade libwbclient-dev=2:4.2.10+dfsg-0+deb8u2 -y
+sudo apt-get install --only-upgrade winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libpam-winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libnss-winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dbg=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libwbclient0=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libwbclient-dev=2:4.2.10+dfsg-0+deb8u3 -y
