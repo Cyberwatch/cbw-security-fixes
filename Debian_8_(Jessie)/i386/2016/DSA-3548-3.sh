478f68473a90dc9eac809b91bdbb3bfdc6b21744
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-06-07 08:12:42 +0200

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3548-3.sh
@@ -0,0 +1,90 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3548-3
+#
+# Security announcement date: 2016-06-05 00:00:00 UTC
+# Script generation date:     2016-06-07 06:12:38 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-libs:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common-bin:2:4.2.10+dfsg-0+deb8u3
+#   - smbclient:2:4.2.10+dfsg-0+deb8u3
+#   - samba-testsuite:2:4.2.10+dfsg-0+deb8u3
+#   - registry-tools:2:4.2.10+dfsg-0+deb8u3
+#   - libparse-pidl-perl:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dev:2:4.2.10+dfsg-0+deb8u3
+#   - samba-doc:2:4.2.10+dfsg-0+deb8u3
+#   - python-samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dsdb-modules:2:4.2.10+dfsg-0+deb8u3
+#   - samba-vfs-modules:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-smbpass:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient-dev:2:4.2.10+dfsg-0+deb8u3
+#   - winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libnss-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dbg:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient0:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient-dev:2:4.2.10+dfsg-0+deb8u3
+#   - ctdb:2:4.2.10+dfsg-0+deb8u3
+#
+# Last versions recommanded by security team:
+#   - samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-libs:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common:2:4.2.10+dfsg-0+deb8u3
+#   - samba-common-bin:2:4.2.10+dfsg-0+deb8u3
+#   - smbclient:2:4.2.10+dfsg-0+deb8u3
+#   - samba-testsuite:2:4.2.10+dfsg-0+deb8u3
+#   - registry-tools:2:4.2.10+dfsg-0+deb8u3
+#   - libparse-pidl-perl:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dev:2:4.2.10+dfsg-0+deb8u3
+#   - samba-doc:2:4.2.10+dfsg-0+deb8u3
+#   - python-samba:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dsdb-modules:2:4.2.10+dfsg-0+deb8u3
+#   - samba-vfs-modules:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-smbpass:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient:2:4.2.10+dfsg-0+deb8u3
+#   - libsmbclient-dev:2:4.2.10+dfsg-0+deb8u3
+#   - winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libpam-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - libnss-winbind:2:4.2.10+dfsg-0+deb8u3
+#   - samba-dbg:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient0:2:4.2.10+dfsg-0+deb8u3
+#   - libwbclient-dev:2:4.2.10+dfsg-0+deb8u3
+#   - ctdb:2:4.2.10+dfsg-0+deb8u3
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade samba=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-libs=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-common=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-common-bin=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade smbclient=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-testsuite=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade registry-tools=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libparse-pidl-perl=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dev=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-doc=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade python-samba=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dsdb-modules=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-vfs-modules=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libpam-smbpass=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libsmbclient=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libsmbclient-dev=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libpam-winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libnss-winbind=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade samba-dbg=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libwbclient0=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade libwbclient-dev=2:4.2.10+dfsg-0+deb8u3 -y
+sudo apt-get install --only-upgrade ctdb=2:4.2.10+dfsg-0+deb8u3 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3595-1.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3595-1
+#
+# Security announcement date: 2016-06-05 00:00:00 UTC
+# Script generation date:     2016-06-07 06:12:38 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - mariadb-10.0:10.0.25-0+deb8u1
+#
+# Last versions recommanded by security team:
+#   - mariadb-10.0:10.0.25-0+deb8u1
+#
+# CVE List:
+#   - CVE-2016-0640
+#   - CVE-2016-0641
+#   - CVE-2016-0643
+#   - CVE-2016-0644
+#   - CVE-2016-0646
+#   - CVE-2016-0647
+#   - CVE-2016-0648
+#   - CVE-2016-0649
+#   - CVE-2016-0650
+#   - CVE-2016-0655
+#   - CVE-2016-0666
+#   - CVE-2016-0668
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade mariadb-10.0=10.0.25-0+deb8u1 -y
