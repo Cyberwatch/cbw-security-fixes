f439b8b38561a55c0c02db8102bc87e1a81a268a
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-06-09 08:06:16 +0200

    DSA: add 4 fixes

---
+++ Debian_8_(Jessie)/i386/2016/DSA-3597-1.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3597-1
+#
+# Security announcement date: 2016-06-07 00:00:00 UTC
+# Script generation date:     2016-06-09 06:06:12 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - expat:2.1.0-6+deb8u3
+#   - lib64expat1-dev:2.1.0-6+deb8u3
+#   - lib64expat1:2.1.0-6+deb8u3
+#   - libexpat1-dev:2.1.0-6+deb8u3
+#   - libexpat1:2.1.0-6+deb8u3
+#
+# Last versions recommanded by security team:
+#   - expat:2.1.0-6+deb8u3
+#   - lib64expat1-dev:2.1.0-6+deb8u3
+#   - lib64expat1:2.1.0-6+deb8u3
+#   - libexpat1-dev:2.1.0-6+deb8u3
+#   - libexpat1:2.1.0-6+deb8u3
+#
+# CVE List:
+#   - CVE-2012-6702
+#   - CVE-2016-5300
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade expat=2.1.0-6+deb8u3 -y
+sudo apt-get install --only-upgrade lib64expat1-dev=2.1.0-6+deb8u3 -y
+sudo apt-get install --only-upgrade lib64expat1=2.1.0-6+deb8u3 -y
+sudo apt-get install --only-upgrade libexpat1-dev=2.1.0-6+deb8u3 -y
+sudo apt-get install --only-upgrade libexpat1=2.1.0-6+deb8u3 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3598-1.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3598-1
+#
+# Security announcement date: 2016-06-07 00:00:00 UTC
+# Script generation date:     2016-06-09 06:06:12 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - vlc:2.2.4-1~deb8u1
+#   - libvlc-dev:2.2.4-1~deb8u1
+#   - libvlc5:2.2.4-1~deb8u1
+#   - libvlccore-dev:2.2.4-1~deb8u1
+#   - libvlccore8:2.2.4-1~deb8u1
+#   - vlc-data:2.2.4-1~deb8u1
+#   - vlc-dbg:2.2.4-1~deb8u1
+#   - vlc-nox:2.2.4-1~deb8u1
+#   - vlc-plugin-fluidsynth:2.2.4-1~deb8u1
+#   - vlc-plugin-jack:2.2.4-1~deb8u1
+#   - vlc-plugin-notify:2.2.4-1~deb8u1
+#   - vlc-plugin-sdl:2.2.4-1~deb8u1
+#   - vlc-plugin-svg:2.2.4-1~deb8u1
+#   - vlc-plugin-zvbi:2.2.4-1~deb8u1
+#   - vlc-plugin-samba:2.2.4-1~deb8u1
+#   - vlc-plugin-pulse:2.2.4-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - vlc:2.2.4-1~deb8u1
+#   - libvlc-dev:2.2.4-1~deb8u1
+#   - libvlc5:2.2.4-1~deb8u1
+#   - libvlccore-dev:2.2.4-1~deb8u1
+#   - libvlccore8:2.2.4-1~deb8u1
+#   - vlc-data:2.2.4-1~deb8u1
+#   - vlc-dbg:2.2.4-1~deb8u1
+#   - vlc-nox:2.2.4-1~deb8u1
+#   - vlc-plugin-fluidsynth:2.2.4-1~deb8u1
+#   - vlc-plugin-jack:2.2.4-1~deb8u1
+#   - vlc-plugin-notify:2.2.4-1~deb8u1
+#   - vlc-plugin-sdl:2.2.4-1~deb8u1
+#   - vlc-plugin-svg:2.2.4-1~deb8u1
+#   - vlc-plugin-zvbi:2.2.4-1~deb8u1
+#   - vlc-plugin-samba:2.2.4-1~deb8u1
+#   - vlc-plugin-pulse:2.2.4-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-5108
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade vlc=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade libvlc-dev=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade libvlc5=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade libvlccore-dev=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade libvlccore8=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-data=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-dbg=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-nox=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-fluidsynth=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-jack=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-notify=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-sdl=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-svg=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-zvbi=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-samba=2.2.4-1~deb8u1 -y
+sudo apt-get install --only-upgrade vlc-plugin-pulse=2.2.4-1~deb8u1 -y
