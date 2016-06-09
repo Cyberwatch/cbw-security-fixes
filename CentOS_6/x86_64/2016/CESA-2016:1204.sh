a1804320ccb34a2a83e8d2f6f66365c68f1553ca
Cyberwatch Security Team <csirt@cyberwatch.fr>
2016-06-09 20:21:09 +0200

    CESA: add 2 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:1204.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1204
+#
+# Security announcement date: 2016-06-07 14:48:31 UTC
+# Script generation date:     2016-06-09 18:21:04 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - spice-server.x86_64:0.12.4-13.el6.1
+#   - spice-server-devel.x86_64:0.12.4-13.el6.1
+#
+# Last versions recommanded by security team:
+#   - spice-server.x86_64:0.12.4-13.el6.1
+#   - spice-server-devel.x86_64:0.12.4-13.el6.1
+#
+# CVE List:
+#   - CVE-2016-0749
+#   - CVE-2016-2150
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install spice-server.x86_64-0.12.4 -y 
+sudo yum install spice-server-devel.x86_64-0.12.4 -y 
---
+++ CentOS_7/x86_64/2016/CESA-2016:1205.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1205
+#
+# Security announcement date: 2016-06-07 15:09:48 UTC
+# Script generation date:     2016-06-09 18:21:04 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - spice-server.x86_64:0.12.4-15.el7_2.1
+#   - spice-server-devel.x86_64:0.12.4-15.el7_2.1
+#
+# Last versions recommanded by security team:
+#   - spice-server.x86_64:0.12.4-15.el7_2.1
+#   - spice-server-devel.x86_64:0.12.4-15.el7_2.1
+#
+# CVE List:
+#   - CVE-2016-0749
+#   - CVE-2016-2150
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install spice-server.x86_64-0.12.4 -y 
+sudo yum install spice-server-devel.x86_64-0.12.4 -y 
