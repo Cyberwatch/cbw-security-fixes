fa0dff6cf3ec5b7eb142bb41b50ad5c77ed7c244
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-06-23 01:58:41 +0000

    USN: add 6 fixes

---
+++ Ubuntu_15.10/x86_64/2016/USN-3014-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3014-1
+#
+# Security announcement date: 2016-06-21 00:00:00 UTC
+# Script generation date:     2016-06-23 01:58:38 UTC
+#
+# Operating System: Ubuntu 15.10
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libspice-server1:0.12.5-1.1ubuntu2.1
+#
+# Last versions recommanded by security team:
+#   - libspice-server1:0.12.5-1.1ubuntu2.1
+#
+# CVE List:
+#   - CVE-2016-0749
+#   - CVE-2016-2150
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libspice-server1=0.12.5-1.1ubuntu2.1 -y
