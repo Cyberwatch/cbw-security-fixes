132a25220aba5566e950101f7cef6d1acbf3f0c8
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-06-23 02:01:28 +0000

    DLA: add 2 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-522-1.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-522-1
+#
+# Security announcement date: 2016-06-21 00:00:00 UTC
+# Script generation date:     2016-06-23 02:01:26 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - python2.7:2.7.3-6+deb7u3
+#
+# Last versions recommanded by security team:
+#   - python2.7:2.7.3-6+deb7u3
+#
+# CVE List:
+#   - CVE-2016-0772
+#   - CVE-2016-5636
+#   - CVE-2016-5699
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade python2.7=2.7.3-6+deb7u3 -y
