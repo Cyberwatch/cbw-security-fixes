6229f82e9c63943592ec94f1587b76a820975a27
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-06-25 21:40:01 +0000

    RHSA: add 6 fixes

---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1293.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1293
+#
+# Security announcement date: 2016-06-23 11:00:09 UTC
+# Script generation date:     2016-06-25 21:39:55 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - setroubleshoot.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-debuginfo.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-server.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-plugins.noarch:3.0.59-2.el7_2
+#
+# Last versions recommanded by security team:
+#   - setroubleshoot.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-debuginfo.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-server.x86_64:3.2.24-4.el7_2
+#   - setroubleshoot-plugins.noarch:3.0.59-2.el7_2
+#
+# CVE List:
+#   - CVE-2016-4444
+#   - CVE-2016-4446
+#   - CVE-2016-4989
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install setroubleshoot.x86_64-3.2.24 -y 
+sudo yum install setroubleshoot-debuginfo.x86_64-3.2.24 -y 
+sudo yum install setroubleshoot-server.x86_64-3.2.24 -y 
+sudo yum install setroubleshoot-plugins.noarch-3.0.59 -y 
---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1301.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1301
+#
+# Security announcement date: 2016-06-23 20:08:45 UTC
+# Script generation date:     2016-06-25 21:39:58 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - kernel-rt.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debuginfo-common-x86_64.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-doc.noarch:3.10.0-327.22.2.rt56.230.el7_2
+#
+# Last versions recommanded by security team:
+#   - kernel-rt.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debug-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-debuginfo-common-x86_64.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-devel.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-kvm.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-trace-kvm-debuginfo.x86_64:3.10.0-327.22.2.rt56.230.el7_2
+#   - kernel-rt-doc.noarch:3.10.0-327.22.2.rt56.230.el7_2
+#
+# CVE List:
+#   - CVE-2015-8767
+#   - CVE-2016-3707
+#   - CVE-2016-4565
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install kernel-rt.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debug.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debug-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debug-devel.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debug-kvm.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debug-kvm-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-debuginfo-common-x86_64.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-devel.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-kvm.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-kvm-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-trace.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-trace-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-trace-devel.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-trace-kvm.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-trace-kvm-debuginfo.x86_64-3.10.0 -y 
+sudo yum install kernel-rt-doc.noarch-3.10.0 -y 
