553ebc3e4718921eafb0ea55102c5dfbf47a3e9b
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-06-29 21:17:14 +0000

    DLA: add 4 fixes and update 4 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-531-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-531-1.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-482-1
+# Security fix for DLA-531-1
 #
-# Security announcement date: 2016-05-19 00:00:00 UTC
-# Script generation date:     2016-06-29 21:16:26 UTC
+# Security announcement date: 2016-06-27 00:00:00 UTC
+# Script generation date:     2016-06-29 21:16:28 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
 #
 # Vulnerable packages fix on version:
-#   - libgd2:2.0.36~rc1~dfsg-6.1+deb7u3
+#   - spice:0.11.0-1+deb7u3
 #
 # Last versions recommanded by security team:
-#   - libgd2:2.0.36~rc1~dfsg-6.1+deb7u4
+#   - spice:0.11.0-1+deb7u3
 #
 # CVE List:
-#   - CVE-2015-8874
+#   - CVE-2016-2150
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libgd2=2.0.36~rc1~dfsg-6.1+deb7u4 -y
+sudo apt-get install --only-upgrade spice=0.11.0-1+deb7u3 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-532-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-532-1
+#
+# Security announcement date: 2016-06-27 00:00:00 UTC
+# Script generation date:     2016-06-29 21:16:28 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - movabletype-opensource:5.1.4+dfsg-4+deb7u4
+#
+# Last versions recommanded by security team:
+#   - movabletype-opensource:5.1.4+dfsg-4+deb7u4
+#
+# CVE List:
+#   - CVE-2016-5742
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade movabletype-opensource=5.1.4+dfsg-4+deb7u4 -y
