77af3726d00a134181183fae1e45a1a20f95a471
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-01 21:13:34 +0000

    DLA: add 6 fixes and update 2 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-533-1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-533-1
+#
+# Security announcement date: 2016-06-29 00:00:00 UTC
+# Script generation date:     2016-07-01 21:12:57 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - php5:5.4.45-0+deb7u4
+#
+# Last versions recommanded by security team:
+#   - php5:5.4.45-0+deb7u4
+#
+# CVE List:
+#   - CVE-2016-5093
+#   - CVE-2016-5094
+#   - CVE-2016-5095
+#   - CVE-2016-5096
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade php5=5.4.45-0+deb7u4 -y
---
+++ Debian_7_(Wheezy)/i386/2016/DLA-534-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-534-1
+#
+# Security announcement date: 2016-06-29 00:00:00 UTC
+# Script generation date:     2016-07-01 21:12:57 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libgd2:2.0.36~rc1~dfsg-6.1+deb7u4
+#
+# Last versions recommanded by security team:
+#   - libgd2:2.0.36~rc1~dfsg-6.1+deb7u4
+#
+# CVE List:
+#   - CVE-2016-5766
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libgd2=2.0.36~rc1~dfsg-6.1+deb7u4 -y
