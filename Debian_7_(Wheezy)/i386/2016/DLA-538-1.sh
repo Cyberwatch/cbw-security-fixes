b5d7aa8e2b2db558adbe2d8447b431e030334b66
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-02 21:14:34 +0000

    DLA: add 6 fixes and update 8 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-538-1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-538-1
+#
+# Security announcement date: 2016-06-30 00:00:00 UTC
+# Script generation date:     2016-07-02 21:14:00 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u6~deb7u2
+#
+# Last versions recommanded by security team:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u6~deb7u2
+#
+# CVE List:
+#   - CVE-2016-5350
+#   - CVE-2016-5351
+#   - CVE-2016-5353
+#   - CVE-2016-5354
+#   - CVE-2016-5355
+#   - CVE-2016-5356
+#   - CVE-2016-5357
+#   - CVE-2016-5359
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade wireshark=1.12.1+g01b65bf-4+deb8u6~deb7u2 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-537-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-537-1.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-472-1
+# Security fix for DLA-537-1
 #
-# Security announcement date: 2016-05-14 00:00:00 UTC
-# Script generation date:     2016-07-02 21:13:57 UTC
+# Security announcement date: 2016-06-30 00:00:00 UTC
+# Script generation date:     2016-07-02 21:14:00 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - icedove:38.8.0-1~deb7u1
+#   - roundcube:0.7.2-9+deb7u3
 #
 # Last versions recommanded by security team:
-#   - icedove:1:45.1.0-1~deb7u1
+#   - roundcube:0.7.2-9+deb7u3
 #
 # CVE List:
-#   - CVE-2016-1979
-#   - CVE-2016-2805
-#   - CVE-2016-2807
+#   - CVE-2015-8864
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=1:45.1.0-1~deb7u1 -y
+sudo apt-get install --only-upgrade roundcube=0.7.2-9+deb7u3 -y
