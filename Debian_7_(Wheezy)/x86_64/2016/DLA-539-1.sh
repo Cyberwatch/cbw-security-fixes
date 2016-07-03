a0de39d5cecebe96fff7a18934fdbdc3cce34823
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-03 21:11:10 +0000

    DLA: add 6 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-539-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-539-1
+#
+# Security announcement date: 2016-07-01 00:00:00 UTC
+# Script generation date:     2016-07-03 21:11:08 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - qemu-kvm:1.1.2+dfsg-6+deb7u13
+#
+# Last versions recommanded by security team:
+#   - qemu-kvm:1.1.2+dfsg-6+deb7u13
+#
+# CVE List:
+#   - CVE-2016-3710
+#   - CVE-2016-3712
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade qemu-kvm=1.1.2+dfsg-6+deb7u13 -y
