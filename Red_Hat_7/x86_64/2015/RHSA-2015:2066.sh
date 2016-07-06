76620f49084b810cfc277a5dd66978a3e20dec8b
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-06 21:41:34 +0000

    RHSA: update one fix

--- Red_Hat_7/x86_64/2015/RHSA-2015:2066.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2066.sh
@@ -4,7 +4,7 @@
 # Security fix for RHSA-2015:2066
 #
 # Security announcement date: 2015-11-23 21:49:36 UTC
-# Script generation date:     2016-05-12 18:13:28 UTC
+# Script generation date:     2016-07-06 21:38:08 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
@@ -51,20 +51,20 @@
 #   - radosgw-agent.noarch:1.2.3-1.el7cp
 #   - babeltrace.x86_64:1.2.4-3.el7cp
 #   - babeltrace-debuginfo.x86_64:1.2.4-3.el7cp
-#   - ceph-common.x86_64:0.94.3-3.el7cp
-#   - ceph-debuginfo.x86_64:0.94.3-3.el7cp
-#   - ceph-radosgw.x86_64:0.94.3-3.el7cp
+#   - ceph-common.x86_64:0.94.5-14.el7cp
+#   - ceph-debuginfo.x86_64:0.94.5-14.el7cp
+#   - ceph-radosgw.x86_64:0.94.5-14.el7cp
 #   - libbabeltrace.x86_64:1.2.4-3.el7cp
-#   - librados2.x86_64:0.94.3-3.el7cp
-#   - librados2-devel.x86_64:0.94.3-3.el7cp
-#   - librbd1.x86_64:0.94.3-3.el7cp
-#   - librbd1-devel.x86_64:0.94.3-3.el7cp
+#   - librados2.x86_64:0.94.5-14.el7cp
+#   - librados2-devel.x86_64:0.94.5-14.el7cp
+#   - librbd1.x86_64:0.94.5-14.el7cp
+#   - librbd1-devel.x86_64:0.94.5-14.el7cp
 #   - lttng-tools.x86_64:2.4.1-1.el7cp
 #   - lttng-tools-debuginfo.x86_64:2.4.1-1.el7cp
 #   - lttng-ust.x86_64:2.4.1-1.el7cp
 #   - lttng-ust-debuginfo.x86_64:2.4.1-1.el7cp
-#   - python-rados.x86_64:0.94.3-3.el7cp
-#   - python-rbd.x86_64:0.94.3-3.el7cp
+#   - python-rados.x86_64:0.94.5-14.el7cp
+#   - python-rbd.x86_64:0.94.5-14.el7cp
 #   - calamari-server.x86_64:1.3-11.el7cp
 #   - ceph-deploy.noarch:1.5.27.3-1.el7cp
 #   - ceph-puppet-modules.noarch:0.1.1-1.el7cp
@@ -80,10 +80,10 @@
 #   - ruby-augeas-debuginfo.x86_64:0.5.0-1.el7
 #   - ruby-shadow.x86_64:1.4.1-21.el7
 #   - ruby-shadow-debuginfo.x86_64:1.4.1-21.el7
-#   - ceph.x86_64:0.94.3-3.el7cp
-#   - ceph-mon.x86_64:0.94.3-3.el7cp
-#   - ceph-test.x86_64:0.94.3-3.el7cp
-#   - ceph-osd.x86_64:0.94.3-3.el7cp
+#   - ceph.x86_64:0.94.5-14.el7cp
+#   - ceph-mon.x86_64:0.94.5-14.el7cp
+#   - ceph-test.x86_64:0.94.5-14.el7cp
+#   - ceph-osd.x86_64:0.94.5-14.el7cp
 #
 # CVE List:
 #   - CVE-2015-5245
@@ -95,20 +95,20 @@
 sudo yum install radosgw-agent.noarch-1.2.3 -y 
 sudo yum install babeltrace.x86_64-1.2.4 -y 
 sudo yum install babeltrace-debuginfo.x86_64-1.2.4 -y 
-sudo yum install ceph-common.x86_64-0.94.3 -y 
-sudo yum install ceph-debuginfo.x86_64-0.94.3 -y 
-sudo yum install ceph-radosgw.x86_64-0.94.3 -y 
+sudo yum install ceph-common.x86_64-0.94.5 -y 
+sudo yum install ceph-debuginfo.x86_64-0.94.5 -y 
+sudo yum install ceph-radosgw.x86_64-0.94.5 -y 
 sudo yum install libbabeltrace.x86_64-1.2.4 -y 
-sudo yum install librados2.x86_64-0.94.3 -y 
-sudo yum install librados2-devel.x86_64-0.94.3 -y 
-sudo yum install librbd1.x86_64-0.94.3 -y 
-sudo yum install librbd1-devel.x86_64-0.94.3 -y 
+sudo yum install librados2.x86_64-0.94.5 -y 
+sudo yum install librados2-devel.x86_64-0.94.5 -y 
+sudo yum install librbd1.x86_64-0.94.5 -y 
+sudo yum install librbd1-devel.x86_64-0.94.5 -y 
 sudo yum install lttng-tools.x86_64-2.4.1 -y 
 sudo yum install lttng-tools-debuginfo.x86_64-2.4.1 -y 
 sudo yum install lttng-ust.x86_64-2.4.1 -y 
 sudo yum install lttng-ust-debuginfo.x86_64-2.4.1 -y 
-sudo yum install python-rados.x86_64-0.94.3 -y 
-sudo yum install python-rbd.x86_64-0.94.3 -y 
+sudo yum install python-rados.x86_64-0.94.5 -y 
+sudo yum install python-rbd.x86_64-0.94.5 -y 
 sudo yum install calamari-server.x86_64-1.3 -y 
 sudo yum install ceph-deploy.noarch-1.5.27.3 -y 
 sudo yum install ceph-puppet-modules.noarch-0.1.1 -y 
@@ -124,7 +124,7 @@ sudo yum install ruby-augeas.x86_64-0.5.0 -y
 sudo yum install ruby-augeas-debuginfo.x86_64-0.5.0 -y 
 sudo yum install ruby-shadow.x86_64-1.4.1 -y 
 sudo yum install ruby-shadow-debuginfo.x86_64-1.4.1 -y 
-sudo yum install ceph.x86_64-0.94.3 -y 
-sudo yum install ceph-mon.x86_64-0.94.3 -y 
-sudo yum install ceph-test.x86_64-0.94.3 -y 
-sudo yum install ceph-osd.x86_64-0.94.3 -y 
+sudo yum install ceph.x86_64-0.94.5 -y 
+sudo yum install ceph-mon.x86_64-0.94.5 -y 
+sudo yum install ceph-test.x86_64-0.94.5 -y 
+sudo yum install ceph-osd.x86_64-0.94.5 -y 
