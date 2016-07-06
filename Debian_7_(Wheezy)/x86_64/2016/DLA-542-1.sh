8706304f77fbf03c0dc2d112995b2b2184d62784
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-06 21:12:34 +0000

    DLA: add 2 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-542-1.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-542-1
+#
+# Security announcement date: 2016-07-04 00:00:00 UTC
+# Script generation date:     2016-07-06 21:12:31 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - pidgin:2.10.10-1~deb7u2
+#
+# Last versions recommanded by security team:
+#   - pidgin:2.10.10-1~deb7u2
+#
+# CVE List:
+#   - CVE-2016-2365
+#   - CVE-2016-2366
+#   - CVE-2016-2367
+#   - CVE-2016-2368
+#   - CVE-2016-2369
+#   - CVE-2016-2370
+#   - CVE-2016-2371
+#   - CVE-2016-2372
+#   - CVE-2016-2373
+#   - CVE-2016-2374
+#   - CVE-2016-2375
+#   - CVE-2016-2376
+#   - CVE-2016-2377
+#   - CVE-2016-2378
+#   - CVE-2016-2380
+#   - CVE-2016-4323
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade pidgin=2.10.10-1~deb7u2 -y
