bce85800a07b3a66602fd3db4e5f8f63ee43a459
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-07 21:13:53 +0000

    DLA: add 2 fixes and update 4 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-519-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-519-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-519-1
 #
 # Security announcement date: 2016-06-18 00:00:00 UTC
-# Script generation date:     2016-07-04 21:13:27 UTC
+# Script generation date:     2016-07-07 21:13:13 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - icedove:45.1.0-1~deb7u1
 #
 # Last versions recommanded by security team:
-#   - icedove:45.1.0-1~deb7u1
+#   - icedove:1:45.1.0-1~deb7u1
 #
 # CVE List:
 #   - CVE-2016-2806
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=45.1.0-1~deb7u1 -y
+sudo apt-get install --only-upgrade icedove=1:45.1.0-1~deb7u1 -y
--- Debian_7_(Wheezy)/i386/2016/DLA-543-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-543-1.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-523-1
+# Security fix for DLA-543-1
 #
-# Security announcement date: 2016-06-22 00:00:00 UTC
-# Script generation date:     2016-07-07 21:13:13 UTC
+# Security announcement date: 2016-07-05 00:00:00 UTC
+# Script generation date:     2016-07-07 21:13:14 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
 #
 # Vulnerable packages fix on version:
-#   - enigmail:1.8.2-4~deb7u2
+#   - sqlite3:3.7.13-1+deb7u3
 #
 # Last versions recommanded by security team:
-#   - enigmail:2:1.8.2-4~deb7u2
+#   - sqlite3:3.7.13-1+deb7u3
 #
 # CVE List:
+#   - CVE-2016-6153
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade enigmail=2:1.8.2-4~deb7u2 -y
+sudo apt-get install --only-upgrade sqlite3=3.7.13-1+deb7u3 -y
