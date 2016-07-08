c48f8d156aa7011598cc57e21f38954a3f4e1192
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-08 21:41:16 +0000

    RHSA: add one fix

---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1384.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1384
+#
+# Security announcement date: 2016-07-05 21:53:36 UTC
+# Script generation date:     2016-07-08 21:41:13 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - ceph-common.x86_64:0.94.5-14.el7cp
+#   - ceph-debuginfo.x86_64:0.94.5-14.el7cp
+#   - ceph-radosgw.x86_64:0.94.5-14.el7cp
+#   - ceph-selinux.x86_64:0.94.5-14.el7cp
+#   - librados2.x86_64:0.94.5-14.el7cp
+#   - librados2-devel.x86_64:0.94.5-14.el7cp
+#   - librbd1.x86_64:0.94.5-14.el7cp
+#   - librbd1-devel.x86_64:0.94.5-14.el7cp
+#   - python-rados.x86_64:0.94.5-14.el7cp
+#   - python-rbd.x86_64:0.94.5-14.el7cp
+#   - ceph.x86_64:0.94.5-14.el7cp
+#   - ceph-mon.x86_64:0.94.5-14.el7cp
+#   - ceph-test.x86_64:0.94.5-14.el7cp
+#   - ceph-osd.x86_64:0.94.5-14.el7cp
+#
+# Last versions recommanded by security team:
+#   - ceph-common.x86_64:0.94.5-14.el7cp
+#   - ceph-debuginfo.x86_64:0.94.5-14.el7cp
+#   - ceph-radosgw.x86_64:0.94.5-14.el7cp
+#   - ceph-selinux.x86_64:0.94.5-14.el7cp
+#   - librados2.x86_64:0.94.5-14.el7cp
+#   - librados2-devel.x86_64:0.94.5-14.el7cp
+#   - librbd1.x86_64:0.94.5-14.el7cp
+#   - librbd1-devel.x86_64:0.94.5-14.el7cp
+#   - python-rados.x86_64:0.94.5-14.el7cp
+#   - python-rbd.x86_64:0.94.5-14.el7cp
+#   - ceph.x86_64:0.94.5-14.el7cp
+#   - ceph-mon.x86_64:0.94.5-14.el7cp
+#   - ceph-test.x86_64:0.94.5-14.el7cp
+#   - ceph-osd.x86_64:0.94.5-14.el7cp
+#
+# CVE List:
+#   - CVE-2016-5009
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install ceph-common.x86_64-0.94.5 -y 
+sudo yum install ceph-debuginfo.x86_64-0.94.5 -y 
+sudo yum install ceph-radosgw.x86_64-0.94.5 -y 
+sudo yum install ceph-selinux.x86_64-0.94.5 -y 
+sudo yum install librados2.x86_64-0.94.5 -y 
+sudo yum install librados2-devel.x86_64-0.94.5 -y 
+sudo yum install librbd1.x86_64-0.94.5 -y 
+sudo yum install librbd1-devel.x86_64-0.94.5 -y 
+sudo yum install python-rados.x86_64-0.94.5 -y 
+sudo yum install python-rbd.x86_64-0.94.5 -y 
+sudo yum install ceph.x86_64-0.94.5 -y 
+sudo yum install ceph-mon.x86_64-0.94.5 -y 
+sudo yum install ceph-test.x86_64-0.94.5 -y 
+sudo yum install ceph-osd.x86_64-0.94.5 -y 
