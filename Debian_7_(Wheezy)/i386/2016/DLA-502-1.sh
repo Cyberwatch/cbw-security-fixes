90ca4570283282a10f2c37330d7861e5b9992f15
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-11 21:16:53 +0000

    DLA: update 4 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-502-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-502-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-502-1
 #
 # Security announcement date: 2016-06-02 00:00:00 UTC
-# Script generation date:     2016-06-22 12:20:34 UTC
+# Script generation date:     2016-07-11 21:16:16 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - graphicsmagick:1.3.16-1.1+deb7u2
 #
 # Last versions recommanded by security team:
-#   - graphicsmagick:1.3.16-1.1+deb7u2
+#   - graphicsmagick:1.3.16-1.1+deb7u3
 #
 # CVE List:
 #   - CVE-2016-5118
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade graphicsmagick=1.3.16-1.1+deb7u2 -y
+sudo apt-get install --only-upgrade graphicsmagick=1.3.16-1.1+deb7u3 -y
