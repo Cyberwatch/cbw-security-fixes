86e4eb8d4af243006d217932cd8350d1a5df026a
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-12 21:46:44 +0000

    RHSA: update 24 fixes

--- Red_Hat_7/x86_64/2015/RHSA-2015:2552.sh
+++ Red_Hat_7/x86_64/2015/RHSA-2015:2552.sh
@@ -4,7 +4,7 @@
 # Security fix for RHSA-2015:2552
 #
 # Security announcement date: 2015-12-08 14:20:13 UTC
-# Script generation date:     2016-06-24 21:41:12 UTC
+# Script generation date:     2016-07-12 21:43:12 UTC
 #
 # Operating System: Red Hat 7
 # Architecture: x86_64
@@ -30,24 +30,24 @@
 #   - kernel-tools-libs-devel.x86_64:3.10.0-327.3.1.el7
 #
 # Last versions recommanded by security team:
-#   - kernel-abi-whitelists.noarch:3.10.0-327.22.2.el7
-#   - kernel-doc.noarch:3.10.0-327.22.2.el7
-#   - kernel.x86_64:3.10.0-327.22.2.el7
-#   - kernel-debug.x86_64:3.10.0-327.22.2.el7
-#   - kernel-debug-debuginfo.x86_64:3.10.0-327.22.2.el7
-#   - kernel-debug-devel.x86_64:3.10.0-327.22.2.el7
-#   - kernel-debuginfo.x86_64:3.10.0-327.22.2.el7
-#   - kernel-debuginfo-common-x86_64.x86_64:3.10.0-327.22.2.el7
-#   - kernel-devel.x86_64:3.10.0-327.22.2.el7
-#   - kernel-headers.x86_64:3.10.0-327.22.2.el7
-#   - kernel-tools.x86_64:3.10.0-327.22.2.el7
-#   - kernel-tools-debuginfo.x86_64:3.10.0-327.22.2.el7
-#   - kernel-tools-libs.x86_64:3.10.0-327.22.2.el7
-#   - perf.x86_64:3.10.0-327.22.2.el7
-#   - perf-debuginfo.x86_64:3.10.0-327.22.2.el7
-#   - python-perf.x86_64:3.10.0-327.22.2.el7
-#   - python-perf-debuginfo.x86_64:3.10.0-327.22.2.el7
-#   - kernel-tools-libs-devel.x86_64:3.10.0-327.22.2.el7
+#   - kernel-abi-whitelists.noarch:3.10.0-229.38.1.ael7b
+#   - kernel-doc.noarch:3.10.0-229.38.1.ael7b
+#   - kernel.x86_64:3.10.0-229.38.1.el7
+#   - kernel-debug.x86_64:3.10.0-229.38.1.el7
+#   - kernel-debug-debuginfo.x86_64:3.10.0-229.38.1.el7
+#   - kernel-debug-devel.x86_64:3.10.0-229.38.1.el7
+#   - kernel-debuginfo.x86_64:3.10.0-229.38.1.el7
+#   - kernel-debuginfo-common-x86_64.x86_64:3.10.0-229.38.1.el7
+#   - kernel-devel.x86_64:3.10.0-229.38.1.el7
+#   - kernel-headers.x86_64:3.10.0-229.38.1.el7
+#   - kernel-tools.x86_64:3.10.0-229.38.1.el7
+#   - kernel-tools-debuginfo.x86_64:3.10.0-229.38.1.el7
+#   - kernel-tools-libs.x86_64:3.10.0-229.38.1.el7
+#   - perf.x86_64:3.10.0-229.38.1.el7
+#   - perf-debuginfo.x86_64:3.10.0-229.38.1.el7
+#   - python-perf.x86_64:3.10.0-229.38.1.el7
+#   - python-perf-debuginfo.x86_64:3.10.0-229.38.1.el7
+#   - kernel-tools-libs-devel.x86_64:3.10.0-229.38.1.el7
 #
 # CVE List:
 #   - CVE-2015-5307
