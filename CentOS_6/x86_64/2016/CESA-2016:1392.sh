503e0e745182e10837146f8ec61502b5e25c5ab2
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-14 21:25:18 +0000

    CESA: add 4 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:1392.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1392
+#
+# Security announcement date: 2016-07-11 21:49:16 UTC
+# Script generation date:     2016-07-14 21:25:13 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird.x86_64:45.2-1.el6.centos
+#
+# Last versions recommanded by security team:
+#   - thunderbird.x86_64:45.2-1.el6.centos
+#
+# CVE List:
+#   - CVE-2016-2818
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird.x86_64-45.2 -y 
---
+++ CentOS_7/x86_64/2016/CESA-2016:1392.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1392
+#
+# Security announcement date: 2016-07-11 23:09:28 UTC
+# Script generation date:     2016-07-14 21:25:13 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird.x86_64:45.2-1.el7.centos
+#
+# Last versions recommanded by security team:
+#   - thunderbird.x86_64:45.2-1.el7.centos
+#
+# CVE List:
+#   - CVE-2016-2818
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird.x86_64-45.2 -y 
