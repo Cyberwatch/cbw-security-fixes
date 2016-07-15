1622866964618a9f5b67fc5c27e696ae356a7372
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-15 21:41:15 +0000

    RHSA: add 3 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1423.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1423
+#
+# Security announcement date: 2016-07-13 18:55:52 UTC
+# Script generation date:     2016-07-15 21:41:12 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - flash-plugin.i686:11.2.202.632-1.el6_8
+#
+# Last versions recommanded by security team:
+#   - flash-plugin.i686:11.2.202.632-1.el6_8
+#
+# CVE List:
+#   - CVE-2016-4172
+#   - CVE-2016-4173
+#   - CVE-2016-4174
+#   - CVE-2016-4175
+#   - CVE-2016-4176
+#   - CVE-2016-4177
+#   - CVE-2016-4178
+#   - CVE-2016-4179
+#   - CVE-2016-4180
+#   - CVE-2016-4181
+#   - CVE-2016-4182
+#   - CVE-2016-4183
+#   - CVE-2016-4184
+#   - CVE-2016-4185
+#   - CVE-2016-4186
+#   - CVE-2016-4187
+#   - CVE-2016-4188
+#   - CVE-2016-4189
+#   - CVE-2016-4190
+#   - CVE-2016-4217
+#   - CVE-2016-4218
+#   - CVE-2016-4219
+#   - CVE-2016-4220
+#   - CVE-2016-4221
+#   - CVE-2016-4222
+#   - CVE-2016-4223
+#   - CVE-2016-4224
+#   - CVE-2016-4225
+#   - CVE-2016-4226
+#   - CVE-2016-4227
+#   - CVE-2016-4228
+#   - CVE-2016-4229
+#   - CVE-2016-4230
+#   - CVE-2016-4231
+#   - CVE-2016-4232
+#   - CVE-2016-4233
+#   - CVE-2016-4234
+#   - CVE-2016-4235
+#   - CVE-2016-4236
+#   - CVE-2016-4237
+#   - CVE-2016-4238
+#   - CVE-2016-4239
+#   - CVE-2016-4240
+#   - CVE-2016-4241
+#   - CVE-2016-4242
+#   - CVE-2016-4243
+#   - CVE-2016-4244
+#   - CVE-2016-4245
+#   - CVE-2016-4246
+#   - CVE-2016-4247
+#   - CVE-2016-4248
+#   - CVE-2016-4249
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install flash-plugin.i686-11.2.202.632 -y 
