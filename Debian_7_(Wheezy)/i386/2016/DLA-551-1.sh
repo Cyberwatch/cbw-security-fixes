19666dec04434b8e38d4e15f584ccf8d5879fcf0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-19 21:14:18 +0000

    DLA: add 2 fixes and update 4 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-551-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-551-1.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-523-1
+# Security fix for DLA-551-1
 #
-# Security announcement date: 2016-06-22 00:00:00 UTC
+# Security announcement date: 2016-07-17 00:00:00 UTC
 # Script generation date:     2016-07-19 21:13:42 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
 #
 # Vulnerable packages fix on version:
-#   - enigmail:1.8.2-4~deb7u2
+#   - phpmyadmin:4:3.4.11.1-2+deb7u5
 #
 # Last versions recommanded by security team:
-#   - enigmail:2:1.8.2-4~deb7u2
+#   - phpmyadmin:4:3.4.11.1-2+deb7u5
 #
 # CVE List:
+#   - CVE-2016-5731
+#   - CVE-2016-5733
+#   - CVE-2016-5739
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade enigmail=2:1.8.2-4~deb7u2 -y
+sudo apt-get install --only-upgrade phpmyadmin=4:3.4.11.1-2+deb7u5 -y
