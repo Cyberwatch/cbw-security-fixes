5407bcdf35e9eb2ea1cafee445190b29d49f95cc
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-23 21:12:36 +0000

    DSA: add 2 fixes and update 42 fixes

--- Debian_7_(Wheezy)/x86_64/2013/DSA-2797-1.sh
+++ Debian_7_(Wheezy)/x86_64/2013/DSA-2797-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2797-1
 #
 # Security announcement date: 2013-11-13 00:00:00 UTC
-# Script generation date:     2016-07-14 21:11:02 UTC
+# Script generation date:     2016-07-23 21:08:44 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - icedove:17.0.10-1~deb7u1
 #
 # Last versions recommanded by security team:
-#   - icedove:45.1.0-1~deb7u1
+#   - icedove:1:45.1.0-1~deb7u1
 #
 # CVE List:
 #   - CVE-2013-5590
@@ -29,4 +29,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=45.1.0-1~deb7u1 -y
+sudo apt-get install --only-upgrade icedove=1:45.1.0-1~deb7u1 -y
