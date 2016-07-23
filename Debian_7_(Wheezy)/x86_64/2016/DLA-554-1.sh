f3ddea0804449cb95bfbee485d1570ac5c9b55ab
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-23 21:13:40 +0000

    DLA: add 4 fixes and update 7 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-554-1.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-554-1
+#
+# Security announcement date: 2016-07-21 00:00:00 UTC
+# Script generation date:     2016-07-23 21:13:03 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libarchive:3.0.4-3+wheezy2
+#
+# Last versions recommanded by security team:
+#   - libarchive:3.0.4-3+wheezy2
+#
+# CVE List:
+#   - CVE-2015-8917
+#   - CVE-2015-8919
+#   - CVE-2015-8920
+#   - CVE-2015-8921
+#   - CVE-2015-8922
+#   - CVE-2015-8923
+#   - CVE-2015-8924
+#   - CVE-2015-8925
+#   - CVE-2015-8926
+#   - CVE-2015-8930
+#   - CVE-2015-8931
+#   - CVE-2015-8932
+#   - CVE-2015-8933
+#   - CVE-2015-8934
+#   - CVE-2016-4300
+#   - CVE-2016-4302
+#   - CVE-2016-4809
+#   - CVE-2016-5844
+#   - CVE-2016-6250
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libarchive=3.0.4-3+wheezy2 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-555-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-555-1.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-472-1
+# Security fix for DLA-555-1
 #
-# Security announcement date: 2016-05-14 00:00:00 UTC
-# Script generation date:     2016-07-23 21:13:00 UTC
+# Security announcement date: 2016-07-21 00:00:00 UTC
+# Script generation date:     2016-07-23 21:13:03 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - icedove:38.8.0-1~deb7u1
+#   - python-django:1.4.5-1+deb7u17
 #
 # Last versions recommanded by security team:
-#   - icedove:1:45.1.0-1~deb7u1
+#   - python-django:1.4.5-1+deb7u17
 #
 # CVE List:
-#   - CVE-2016-1979
-#   - CVE-2016-2805
-#   - CVE-2016-2807
+#   - CVE-2016-6186
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=1:45.1.0-1~deb7u1 -y
+sudo apt-get install --only-upgrade python-django=1.4.5-1+deb7u17 -y
