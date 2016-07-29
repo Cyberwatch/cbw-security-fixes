d037801a01799e21dcca92dfc4517fa0fc927a41
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-07-29 21:24:14 +0000

    CESA: add 4 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:1504.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1504
+#
+# Security announcement date: 2016-07-27 10:40:30 UTC
+# Script generation date:     2016-07-29 21:24:11 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.7.0-openjdk.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-demo.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-devel.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-javadoc.noarch:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-src.x86_64:1.7.0.111-2.6.7.2.el6_8
+#
+# Last versions recommanded by security team:
+#   - java-1.7.0-openjdk.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-demo.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-devel.x86_64:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-javadoc.noarch:1.7.0.111-2.6.7.2.el6_8
+#   - java-1.7.0-openjdk-src.x86_64:1.7.0.111-2.6.7.2.el6_8
+#
+# CVE List:
+#   - CVE-2016-3458
+#   - CVE-2016-3500
+#   - CVE-2016-3508
+#   - CVE-2016-3550
+#   - CVE-2016-3598
+#   - CVE-2016-3606
+#   - CVE-2016-3610
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.7.0-openjdk.x86_64-1.7.0.111 -y 
+sudo yum install java-1.7.0-openjdk-demo.x86_64-1.7.0.111 -y 
+sudo yum install java-1.7.0-openjdk-devel.x86_64-1.7.0.111 -y 
+sudo yum install java-1.7.0-openjdk-javadoc.noarch-1.7.0.111 -y 
+sudo yum install java-1.7.0-openjdk-src.x86_64-1.7.0.111 -y 
