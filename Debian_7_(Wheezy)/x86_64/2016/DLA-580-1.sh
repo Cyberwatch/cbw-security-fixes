97ce0386c402569ffea85df348e19b4c943731a2
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-03 21:13:47 +0000

    DLA: add 2 fixes

---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-580-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-580-1
+#
+# Security announcement date: 2016-08-01 00:00:00 UTC
+# Script generation date:     2016-08-03 21:13:45 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - graphite2:1.3.6-1~deb7u2
+#
+# Last versions recommanded by security team:
+#   - graphite2:1.3.6-1~deb7u2
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade graphite2=1.3.6-1~deb7u2 -y
