94b2c8a02e8795efd8bd468f8226df60b349ddc8
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-06 21:13:35 +0000

    DSA: add 2 fixes and update 2 fixes

--- Debian_7_(Wheezy)/i386/2013/DSA-2768-1.sh
+++ Debian_7_(Wheezy)/i386/2013/DSA-2768-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2768-1
 #
 # Security announcement date: 2013-10-04 00:00:00 UTC
-# Script generation date:     2016-07-28 21:09:23 UTC
+# Script generation date:     2016-08-06 21:08:54 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -19,11 +19,11 @@
 #
 # Last versions recommanded by security team:
 #   - icedtea-web:1.4-3~deb7u2
-#   - icedtea-netx:1.4-3~deb7u2
-#   - icedtea-plugin:1.4-3~deb7u2
-#   - icedtea-netx-common:1.4-3~deb7u2
-#   - icedtea-6-plugin:1.4-3~deb7u2
-#   - icedtea-7-plugin:1.4-3~deb7u2
+#   - icedtea-netx:1.4-3~deb7u3
+#   - icedtea-plugin:1.4-3~deb7u3
+#   - icedtea-netx-common:1.4-3~deb7u3
+#   - icedtea-6-plugin:1.4-3~deb7u3
+#   - icedtea-7-plugin:1.4-3~deb7u3
 #
 # CVE List:
 #   - CVE-2013-4349
@@ -34,8 +34,8 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade icedtea-web=1.4-3~deb7u2 -y
-sudo apt-get install --only-upgrade icedtea-netx=1.4-3~deb7u2 -y
-sudo apt-get install --only-upgrade icedtea-plugin=1.4-3~deb7u2 -y
-sudo apt-get install --only-upgrade icedtea-netx-common=1.4-3~deb7u2 -y
-sudo apt-get install --only-upgrade icedtea-6-plugin=1.4-3~deb7u2 -y
-sudo apt-get install --only-upgrade icedtea-7-plugin=1.4-3~deb7u2 -y
+sudo apt-get install --only-upgrade icedtea-netx=1.4-3~deb7u3 -y
+sudo apt-get install --only-upgrade icedtea-plugin=1.4-3~deb7u3 -y
+sudo apt-get install --only-upgrade icedtea-netx-common=1.4-3~deb7u3 -y
+sudo apt-get install --only-upgrade icedtea-6-plugin=1.4-3~deb7u3 -y
+sudo apt-get install --only-upgrade icedtea-7-plugin=1.4-3~deb7u3 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3641-1.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3641-1
+#
+# Security announcement date: 2016-08-04 00:00:00 UTC
+# Script generation date:     2016-08-06 21:11:05 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - openjdk-7:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jdk:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-headless:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-lib:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-demo:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-source:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-doc:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-dbg:7u111-2.6.7-1~deb8u1
+#   - icedtea-7-jre-jamvm:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-zero:7u111-2.6.7-1~deb8u1
+#
+# Last versions recommanded by security team:
+#   - openjdk-7:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jdk:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-headless:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-lib:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-demo:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-source:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-doc:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-dbg:7u111-2.6.7-1~deb8u1
+#   - icedtea-7-jre-jamvm:7u111-2.6.7-1~deb8u1
+#   - openjdk-7-jre-zero:7u111-2.6.7-1~deb8u1
+#
+# CVE List:
+#   - CVE-2016-3458
+#   - CVE-2016-3500
+#   - CVE-2016-3508
+#   - CVE-2016-3550
+#   - CVE-2016-3606
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade openjdk-7=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jdk=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-headless=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-lib=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-demo=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-source=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-doc=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-dbg=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade icedtea-7-jre-jamvm=7u111-2.6.7-1~deb8u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-zero=7u111-2.6.7-1~deb8u1 -y
