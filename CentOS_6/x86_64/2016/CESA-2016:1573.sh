391edc04c1e0c86f7c161bc768b4143d7a88ae7c
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-06 21:24:20 +0000

    CESA: add one fix

---
+++ CentOS_6/x86_64/2016/CESA-2016:1573.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1573
+#
+# Security announcement date: 2016-08-04 12:51:39 UTC
+# Script generation date:     2016-08-06 21:24:16 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - squid.x86_64:3.1.23-16.el6_8.6
+#
+# Last versions recommanded by security team:
+#   - squid.x86_64:3.1.23-16.el6_8.6
+#
+# CVE List:
+#   - CVE-2016-4051
+#   - CVE-2016-5408
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install squid.x86_64-3.1.23 -y 
