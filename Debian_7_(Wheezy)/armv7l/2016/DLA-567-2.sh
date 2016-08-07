08446da35add5d479d5b773ad6d69660ff3d7f8e
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-07 21:12:52 +0000

    DLA: add 3 fixes and update one fix

--- Debian_7_(Wheezy)/armv7l/2016/DLA-567-2.sh
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-567-2.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-523-1
+# Security fix for DLA-567-2
 #
-# Security announcement date: 2016-06-22 00:00:00 UTC
-# Script generation date:     2016-08-07 21:12:03 UTC
+# Security announcement date: 2016-08-05 00:00:00 UTC
+# Script generation date:     2016-08-07 21:12:06 UTC
 #
 # Operating System: Debian 7 (Wheezy)
-# Architecture: i386
+# Architecture: armv7l
 #
 # Vulnerable packages fix on version:
-#   - enigmail:1.8.2-4~deb7u2
+#   - mysql-5.5:5.5.50-0+deb7u2
 #
 # Last versions recommanded by security team:
-#   - enigmail:1.8.2-4~deb7u2
+#   - mysql-5.5:5.5.50-0+deb7u2
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade enigmail=1.8.2-4~deb7u2 -y
+sudo apt-get install --only-upgrade mysql-5.5=5.5.50-0+deb7u2 -y
