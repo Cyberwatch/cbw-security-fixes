b2ae28581d9681dc58626b8f47d8c656449a7817
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-09 21:15:52 +0000

    DLA: update 6 fixes

--- Debian_7_(Wheezy)/armv7l/2016/DLA-581-1.sh
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-581-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-581-1
 #
 # Security announcement date: 2016-08-02 00:00:00 UTC
-# Script generation date:     2016-08-04 21:16:54 UTC
+# Script generation date:     2016-08-09 21:15:04 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: armv7l
@@ -13,7 +13,7 @@
 #   - libreoffice:1:3.5.4+dfsg2-0+deb7u7
 #
 # Last versions recommanded by security team:
-#   - libreoffice:1:3.5.4+dfsg2-0+deb7u7
+#   - libreoffice:1:3.5.4+dfsg2-0+deb7u8
 #
 # CVE List:
 #   - CVE-2016-4324
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libreoffice=1:3.5.4+dfsg2-0+deb7u7 -y
+sudo apt-get install --only-upgrade libreoffice=1:3.5.4+dfsg2-0+deb7u8 -y
