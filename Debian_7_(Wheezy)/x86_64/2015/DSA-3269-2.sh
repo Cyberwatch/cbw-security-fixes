1328ba78368d54d54df6e25019e3d772b378f695
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-10 21:16:34 +0000

    DSA: add 2 fixes and update 10 fixes

--- Debian_7_(Wheezy)/x86_64/2015/DSA-3269-2.sh
+++ Debian_7_(Wheezy)/x86_64/2015/DSA-3269-2.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3269-2
 #
 # Security announcement date: 2015-05-31 00:00:00 UTC
-# Script generation date:     2016-06-22 12:17:13 UTC
+# Script generation date:     2016-08-10 21:12:55 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -29,21 +29,21 @@
 #
 # Last versions recommanded by security team:
 #   - postgresql-9.1:9.1.22-0+deb7u1
-#   - libpq-dev:9.1.22-0+deb7u1
-#   - libpq5:9.1.22-0+deb7u1
-#   - libecpg6:9.1.22-0+deb7u1
-#   - libecpg-dev:9.1.22-0+deb7u1
-#   - libecpg-compat3:9.1.22-0+deb7u1
-#   - libpgtypes3:9.1.22-0+deb7u1
-#   - postgresql-9.1-dbg:9.1.22-0+deb7u1
-#   - postgresql-client-9.1:9.1.22-0+deb7u1
-#   - postgresql-server-dev-9.1:9.1.22-0+deb7u1
-#   - postgresql-doc-9.1:9.1.22-0+deb7u1
-#   - postgresql-contrib-9.1:9.1.22-0+deb7u1
-#   - postgresql-plperl-9.1:9.1.22-0+deb7u1
-#   - postgresql-plpython-9.1:9.1.22-0+deb7u1
-#   - postgresql-plpython3-9.1:9.1.22-0+deb7u1
-#   - postgresql-pltcl-9.1:9.1.22-0+deb7u1
+#   - libpq-dev:9.1.23-0+deb7u1
+#   - libpq5:9.1.23-0+deb7u1
+#   - libecpg6:9.1.23-0+deb7u1
+#   - libecpg-dev:9.1.23-0+deb7u1
+#   - libecpg-compat3:9.1.23-0+deb7u1
+#   - libpgtypes3:9.1.23-0+deb7u1
+#   - postgresql-9.1-dbg:9.1.23-0+deb7u1
+#   - postgresql-client-9.1:9.1.23-0+deb7u1
+#   - postgresql-server-dev-9.1:9.1.23-0+deb7u1
+#   - postgresql-doc-9.1:9.1.23-0+deb7u1
+#   - postgresql-contrib-9.1:9.1.23-0+deb7u1
+#   - postgresql-plperl-9.1:9.1.23-0+deb7u1
+#   - postgresql-plpython-9.1:9.1.23-0+deb7u1
+#   - postgresql-plpython3-9.1:9.1.23-0+deb7u1
+#   - postgresql-pltcl-9.1:9.1.23-0+deb7u1
 #
 # CVE List:
 #
@@ -52,18 +52,18 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade postgresql-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libpq-dev=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libpq5=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libecpg6=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libecpg-dev=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libecpg-compat3=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade libpgtypes3=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-9.1-dbg=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-client-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-server-dev-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-doc-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-contrib-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-plperl-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-plpython-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-plpython3-9.1=9.1.22-0+deb7u1 -y
-sudo apt-get install --only-upgrade postgresql-pltcl-9.1=9.1.22-0+deb7u1 -y
+sudo apt-get install --only-upgrade libpq-dev=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libpq5=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libecpg6=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libecpg-dev=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libecpg-compat3=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libpgtypes3=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-9.1-dbg=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-client-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-server-dev-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-doc-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-contrib-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-plperl-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-plpython-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-plpython3-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade postgresql-pltcl-9.1=9.1.23-0+deb7u1 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3644-1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3644-1
+#
+# Security announcement date: 2016-08-08 00:00:00 UTC
+# Script generation date:     2016-08-10 21:14:02 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - fontconfig:2.11.0-6.3+deb8u1
+#   - fontconfig-config:2.11.0-6.3+deb8u1
+#   - libfontconfig1-dev:2.11.0-6.3+deb8u1
+#   - libfontconfig1:2.11.0-6.3+deb8u1
+#   - libfontconfig1-dbg:2.11.0-6.3+deb8u1
+#
+# Last versions recommanded by security team:
+#   - fontconfig:2.11.0-6.3+deb8u1
+#   - fontconfig-config:2.11.0-6.3+deb8u1
+#   - libfontconfig1-dev:2.11.0-6.3+deb8u1
+#   - libfontconfig1:2.11.0-6.3+deb8u1
+#   - libfontconfig1-dbg:2.11.0-6.3+deb8u1
+#
+# CVE List:
+#   - CVE-2016-5384
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade fontconfig=2.11.0-6.3+deb8u1 -y
+sudo apt-get install --only-upgrade fontconfig-config=2.11.0-6.3+deb8u1 -y
+sudo apt-get install --only-upgrade libfontconfig1-dev=2.11.0-6.3+deb8u1 -y
+sudo apt-get install --only-upgrade libfontconfig1=2.11.0-6.3+deb8u1 -y
+sudo apt-get install --only-upgrade libfontconfig1-dbg=2.11.0-6.3+deb8u1 -y
