7c435e4afb09a761c705e12fb497e28d5cd7f08f
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-10 21:11:15 +0000

    USN: add 4 fixes and update 4 fixes

---
+++ Ubuntu_12.04_LTS/i386/2016/USN-3048-1.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3048-1
+#
+# Security announcement date: 2016-08-08 00:00:00 UTC
+# Script generation date:     2016-08-10 21:08:38 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libcurl3-nss:7.22.0-3ubuntu4.16
+#   - libcurl3-gnutls:7.22.0-3ubuntu4.16
+#   - libcurl3:7.22.0-3ubuntu4.16
+#
+# Last versions recommanded by security team:
+#   - libcurl3-nss:7.22.0-3ubuntu4.16
+#   - libcurl3-gnutls:7.22.0-3ubuntu4.16
+#   - libcurl3:7.22.0-3ubuntu4.16
+#
+# CVE List:
+#   - CVE-2016-5419
+#   - CVE-2016-5420
+#   - CVE-2016-5421
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libcurl3-nss=7.22.0-3ubuntu4.16 -y
+sudo apt-get install --only-upgrade libcurl3-gnutls=7.22.0-3ubuntu4.16 -y
+sudo apt-get install --only-upgrade libcurl3=7.22.0-3ubuntu4.16 -y
---
+++ Ubuntu_16.04_LTS/i386/2016/USN-3048-1.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3048-1
+#
+# Security announcement date: 2016-08-08 00:00:00 UTC
+# Script generation date:     2016-08-10 21:08:38 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - libcurl3-nss:7.47.0-1ubuntu2.1
+#   - libcurl3-gnutls:7.47.0-1ubuntu2.1
+#   - libcurl3:7.47.0-1ubuntu2.1
+#
+# Last versions recommanded by security team:
+#   - libcurl3-nss:7.47.0-1ubuntu2.1
+#   - libcurl3-gnutls:7.47.0-1ubuntu2.1
+#   - libcurl3:7.47.0-1ubuntu2.1
+#
+# CVE List:
+#   - CVE-2016-5419
+#   - CVE-2016-5420
+#   - CVE-2016-5421
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libcurl3-nss=7.47.0-1ubuntu2.1 -y
+sudo apt-get install --only-upgrade libcurl3-gnutls=7.47.0-1ubuntu2.1 -y
+sudo apt-get install --only-upgrade libcurl3=7.47.0-1ubuntu2.1 -y
