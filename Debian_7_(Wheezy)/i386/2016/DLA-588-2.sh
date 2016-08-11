58927d15bea2ea619cc4f457b51031e0c167e43c
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-11 21:15:10 +0000

    DLA: add 9 fixes and update 3 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-588-2.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-588-2.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-491-1
+# Security fix for DLA-588-2
 #
-# Security announcement date: 2016-05-27 00:00:00 UTC
-# Script generation date:     2016-08-11 21:14:16 UTC
+# Security announcement date: 2016-08-09 00:00:00 UTC
+# Script generation date:     2016-08-11 21:14:20 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
 #
 # Vulnerable packages fix on version:
-#   - postgresql-9.1:9.1.22-0+deb7u1
+#   - mongodb:2.0.6-1.1+deb7u1
 #
 # Last versions recommanded by security team:
-#   - postgresql-9.1:9.1.23-0+deb7u1
+#   - mongodb:2.0.6-1.1+deb7u1
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade postgresql-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade mongodb=2.0.6-1.1+deb7u1 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-591-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-591-1.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-491-1
+# Security fix for DLA-591-1
 #
-# Security announcement date: 2016-05-27 00:00:00 UTC
-# Script generation date:     2016-08-11 21:14:16 UTC
+# Security announcement date: 2016-08-09 00:00:00 UTC
+# Script generation date:     2016-08-11 21:14:20 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
-#   - postgresql-9.1:9.1.22-0+deb7u1
+#   - libreoffice:1:3.5.4+dfsg2-0+deb7u8
 #
 # Last versions recommanded by security team:
-#   - postgresql-9.1:9.1.23-0+deb7u1
+#   - libreoffice:1:3.5.4+dfsg2-0+deb7u8
 #
 # CVE List:
+#   - CVE-2016-1513
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade postgresql-9.1=9.1.23-0+deb7u1 -y
+sudo apt-get install --only-upgrade libreoffice=1:3.5.4+dfsg2-0+deb7u8 -y
