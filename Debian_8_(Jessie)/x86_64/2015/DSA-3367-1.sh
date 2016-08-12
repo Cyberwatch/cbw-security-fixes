38abeb641435eb226bb0b51ca0bae8e1828c9726
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-12 21:16:15 +0000

    DSA: update 14 fixes

--- Debian_8_(Jessie)/x86_64/2015/DSA-3367-1.sh
+++ Debian_8_(Jessie)/x86_64/2015/DSA-3367-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3367-1
 #
 # Security announcement date: 2015-09-24 00:00:00 UTC
-# Script generation date:     2016-07-02 21:10:32 UTC
+# Script generation date:     2016-08-12 21:13:05 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -26,20 +26,20 @@
 #   - libwiretap-dev:1.12.1+g01b65bf-4+deb8u3
 #
 # Last versions recommanded by security team:
-#   - wireshark:1.12.1+g01b65bf-4+deb8u7
-#   - wireshark-common:1.12.1+g01b65bf-4+deb8u7
-#   - wireshark-qt:1.12.1+g01b65bf-4+deb8u7
-#   - tshark:1.12.1+g01b65bf-4+deb8u7
-#   - wireshark-dev:1.12.1+g01b65bf-4+deb8u7
-#   - wireshark-dbg:1.12.1+g01b65bf-4+deb8u7
-#   - wireshark-doc:1.12.1+g01b65bf-4+deb8u7
-#   - libwireshark5:1.12.1+g01b65bf-4+deb8u7
-#   - libwsutil4:1.12.1+g01b65bf-4+deb8u7
-#   - libwsutil-dev:1.12.1+g01b65bf-4+deb8u7
-#   - libwireshark-data:1.12.1+g01b65bf-4+deb8u7
-#   - libwireshark-dev:1.12.1+g01b65bf-4+deb8u7
-#   - libwiretap4:1.12.1+g01b65bf-4+deb8u7
-#   - libwiretap-dev:1.12.1+g01b65bf-4+deb8u7
+#   - wireshark:1.12.1+g01b65bf-4+deb8u8
+#   - wireshark-common:1.12.1+g01b65bf-4+deb8u8
+#   - wireshark-qt:1.12.1+g01b65bf-4+deb8u8
+#   - tshark:1.12.1+g01b65bf-4+deb8u8
+#   - wireshark-dev:1.12.1+g01b65bf-4+deb8u8
+#   - wireshark-dbg:1.12.1+g01b65bf-4+deb8u8
+#   - wireshark-doc:1.12.1+g01b65bf-4+deb8u8
+#   - libwireshark5:1.12.1+g01b65bf-4+deb8u8
+#   - libwsutil4:1.12.1+g01b65bf-4+deb8u8
+#   - libwsutil-dev:1.12.1+g01b65bf-4+deb8u8
+#   - libwireshark-data:1.12.1+g01b65bf-4+deb8u8
+#   - libwireshark-dev:1.12.1+g01b65bf-4+deb8u8
+#   - libwiretap4:1.12.1+g01b65bf-4+deb8u8
+#   - libwiretap-dev:1.12.1+g01b65bf-4+deb8u8
 #
 # CVE List:
 #   - CVE-2015-6241
@@ -56,17 +56,17 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade wireshark=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade wireshark-common=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade wireshark-qt=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade tshark=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade wireshark-dev=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade wireshark-dbg=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade wireshark-doc=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwireshark5=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwsutil4=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwsutil-dev=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwireshark-data=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwireshark-dev=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwiretap4=1.12.1+g01b65bf-4+deb8u7 -y
-sudo apt-get install --only-upgrade libwiretap-dev=1.12.1+g01b65bf-4+deb8u7 -y
+sudo apt-get install --only-upgrade wireshark=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade wireshark-common=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade wireshark-qt=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade tshark=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade wireshark-dev=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade wireshark-dbg=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade wireshark-doc=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwireshark5=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwsutil4=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwsutil-dev=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwireshark-data=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwireshark-dev=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwiretap4=1.12.1+g01b65bf-4+deb8u8 -y
+sudo apt-get install --only-upgrade libwiretap-dev=1.12.1+g01b65bf-4+deb8u8 -y
