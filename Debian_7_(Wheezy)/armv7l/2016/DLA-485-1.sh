587502c2af8893e13fe9ef1eb39730a0749fcc23
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-16 21:15:12 +0000

    DLA: update 6 fixes

--- Debian_7_(Wheezy)/armv7l/2016/DLA-485-1.sh
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-485-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-485-1
 #
 # Security announcement date: 2016-05-22 00:00:00 UTC
-# Script generation date:     2016-08-04 21:16:50 UTC
+# Script generation date:     2016-08-16 21:14:21 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: armv7l
@@ -13,7 +13,7 @@
 #   - extplorer:2.1.0b6+dfsg.3-4+deb7u3
 #
 # Last versions recommanded by security team:
-#   - extplorer:2.1.0b6+dfsg.3-4+deb7u3
+#   - extplorer:2.1.0b6+dfsg.3-4+deb7u4
 #
 # CVE List:
 #   - CVE-2015-5660
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade extplorer=2.1.0b6+dfsg.3-4+deb7u3 -y
+sudo apt-get install --only-upgrade extplorer=2.1.0b6+dfsg.3-4+deb7u4 -y
