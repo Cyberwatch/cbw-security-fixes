65698877ab9fd4c77b4a69e099cf67fbbf84ab86
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-17 21:12:09 +0000

    DLA: add 6 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-595-1.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-595-1
+#
+# Security announcement date: 2016-08-15 00:00:00 UTC
+# Script generation date:     2016-08-17 21:12:06 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u6~deb7u3
+#
+# Last versions recommanded by security team:
+#   - wireshark:1.12.1+g01b65bf-4+deb8u6~deb7u3
+#
+# CVE List:
+#   - CVE-2016-6504
+#   - CVE-2016-6505
+#   - CVE-2016-6506
+#   - CVE-2016-6507
+#   - CVE-2016-6508
+#   - CVE-2016-6509
+#   - CVE-2016-6510
+#   - CVE-2016-6511
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade wireshark=1.12.1+g01b65bf-4+deb8u6~deb7u3 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-596-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-596-1
+#
+# Security announcement date: 2016-08-15 00:00:00 UTC
+# Script generation date:     2016-08-17 21:12:06 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - extplorer:2.1.0b6+dfsg.3-4+deb7u4
+#
+# Last versions recommanded by security team:
+#   - extplorer:2.1.0b6+dfsg.3-4+deb7u4
+#
+# CVE List:
+#   - CVE-2016-4313
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade extplorer=2.1.0b6+dfsg.3-4+deb7u4 -y
