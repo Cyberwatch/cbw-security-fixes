fbd05675c709a15289f926ac07f8e751da2df3be
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-18 21:16:01 +0000

    DSA: update 2 fixes

--- Debian_8_(Jessie)/x86_64/2016/DSA-3474-1.sh
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3474-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3474-1
 #
 # Security announcement date: 2016-02-12 00:00:00 UTC
-# Script generation date:     2016-05-12 18:05:58 UTC
+# Script generation date:     2016-08-18 21:13:14 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -16,10 +16,10 @@
 #   - libgcrypt20-dbg:1.6.3-2+deb8u1
 #
 # Last versions recommanded by security team:
-#   - libgcrypt20:1.6.3-2+deb8u1
-#   - libgcrypt20-doc:1.6.3-2+deb8u1
-#   - libgcrypt20-dev:1.6.3-2+deb8u1
-#   - libgcrypt20-dbg:1.6.3-2+deb8u1
+#   - libgcrypt20:1.6.3-2+deb8u2
+#   - libgcrypt20-doc:1.6.3-2+deb8u2
+#   - libgcrypt20-dev:1.6.3-2+deb8u2
+#   - libgcrypt20-dbg:1.6.3-2+deb8u2
 #
 # CVE List:
 #   - CVE-2015-7511
@@ -28,7 +28,7 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libgcrypt20=1.6.3-2+deb8u1 -y
-sudo apt-get install --only-upgrade libgcrypt20-doc=1.6.3-2+deb8u1 -y
-sudo apt-get install --only-upgrade libgcrypt20-dev=1.6.3-2+deb8u1 -y
-sudo apt-get install --only-upgrade libgcrypt20-dbg=1.6.3-2+deb8u1 -y
+sudo apt-get install --only-upgrade libgcrypt20=1.6.3-2+deb8u2 -y
+sudo apt-get install --only-upgrade libgcrypt20-doc=1.6.3-2+deb8u2 -y
+sudo apt-get install --only-upgrade libgcrypt20-dev=1.6.3-2+deb8u2 -y
+sudo apt-get install --only-upgrade libgcrypt20-dbg=1.6.3-2+deb8u2 -y
