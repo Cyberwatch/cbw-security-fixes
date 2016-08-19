563395b0a48ed6792e819443cc4eb6941ae421f4
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-19 21:08:44 +0000

    USN: add 4 fixes

---
+++ Ubuntu_12.04_LTS/i386/2016/USN-3063-1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3063-1
+#
+# Security announcement date: 2016-08-17 00:00:00 UTC
+# Script generation date:     2016-08-19 21:08:40 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - fontconfig:2.8.0-3ubuntu9.2
+#   - libfontconfig1:2.8.0-3ubuntu9.2
+#
+# Last versions recommanded by security team:
+#   - fontconfig:2.8.0-3ubuntu9.2
+#   - libfontconfig1:2.8.0-3ubuntu9.2
+#
+# CVE List:
+#   - CVE-2016-5384
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade fontconfig=2.8.0-3ubuntu9.2 -y
+sudo apt-get install --only-upgrade libfontconfig1=2.8.0-3ubuntu9.2 -y
