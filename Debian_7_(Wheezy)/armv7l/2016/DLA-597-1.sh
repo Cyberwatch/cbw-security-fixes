738b416bc5a18efe5cc13d886edde554f14d1d98
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-20 21:12:28 +0000

    DLA: add 3 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-597-1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-597-1
+#
+# Security announcement date: 2016-08-18 00:00:00 UTC
+# Script generation date:     2016-08-20 21:12:26 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - libupnp:1.6.17-1.2+deb7u1
+#   - libupnp:1:1.6.17-1.2+deb7u1
+#
+# Last versions recommanded by security team:
+#   - libupnp:1:1.6.17-1.2+deb7u1
+#   - libupnp:1:1.6.17-1.2+deb7u1
+#
+# CVE List:
+#   - CVE-2016-6255
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libupnp=1:1.6.17-1.2+deb7u1 -y
+sudo apt-get install --only-upgrade libupnp=1:1.6.17-1.2+deb7u1 -y
