317d943541b5869675b58c9455186afbd2b4aa53
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-25 21:15:32 +0000

    RHSA: add 6 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1652.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1652
+#
+# Security announcement date: 2016-08-23 06:19:07 UTC
+# Script generation date:     2016-08-25 21:15:28 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - qemu-img-rhev.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev-debuginfo.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev-tools.x86_64:0.12.1.2-2.491.el6_8.3
+#
+# Last versions recommanded by security team:
+#   - qemu-img-rhev.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev-debuginfo.x86_64:0.12.1.2-2.491.el6_8.3
+#   - qemu-kvm-rhev-tools.x86_64:0.12.1.2-2.491.el6_8.3
+#
+# CVE List:
+#   - CVE-2016-5403
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install qemu-img-rhev.x86_64-0.12.1.2 -y 
+sudo yum install qemu-kvm-rhev.x86_64-0.12.1.2 -y 
+sudo yum install qemu-kvm-rhev-debuginfo.x86_64-0.12.1.2 -y 
+sudo yum install qemu-kvm-rhev-tools.x86_64-0.12.1.2 -y 
---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1655.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1655
+#
+# Security announcement date: 2016-08-23 06:21:28 UTC
+# Script generation date:     2016-08-25 21:15:28 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libcacard-devel-rhev.x86_64:2.3.0-31.el7_2.21
+#   - libcacard-rhev.x86_64:2.3.0-31.el7_2.21
+#   - libcacard-tools-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-img-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-common-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-rhev-debuginfo.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-tools-rhev.x86_64:2.3.0-31.el7_2.21
+#
+# Last versions recommanded by security team:
+#   - libcacard-devel-rhev.x86_64:2.3.0-31.el7_2.21
+#   - libcacard-rhev.x86_64:2.3.0-31.el7_2.21
+#   - libcacard-tools-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-img-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-common-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-rhev.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-rhev-debuginfo.x86_64:2.3.0-31.el7_2.21
+#   - qemu-kvm-tools-rhev.x86_64:2.3.0-31.el7_2.21
+#
+# CVE List:
+#   - CVE-2016-5126
+#   - CVE-2016-5403
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libcacard-devel-rhev.x86_64-2.3.0 -y 
+sudo yum install libcacard-rhev.x86_64-2.3.0 -y 
+sudo yum install libcacard-tools-rhev.x86_64-2.3.0 -y 
+sudo yum install qemu-img-rhev.x86_64-2.3.0 -y 
+sudo yum install qemu-kvm-common-rhev.x86_64-2.3.0 -y 
+sudo yum install qemu-kvm-rhev.x86_64-2.3.0 -y 
+sudo yum install qemu-kvm-rhev-debuginfo.x86_64-2.3.0 -y 
+sudo yum install qemu-kvm-tools-rhev.x86_64-2.3.0 -y 
