7802d4c30ca0dca7f5a48a16c9c1d614e7a073e9
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-27 21:04:44 +0000

    USN: add 4 fixes

---
+++ Ubuntu_12.04_LTS/i386/2016/USN-3069-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3069-1
+#
+# Security announcement date: 2016-08-25 00:00:00 UTC
+# Script generation date:     2016-08-27 21:04:36 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - eog:3.4.2-0ubuntu1.3
+#
+# Last versions recommanded by security team:
+#   - eog:3.4.2-0ubuntu1.3
+#
+# CVE List:
+#   - CVE-2016-6855
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade eog=3.4.2-0ubuntu1.3 -y
