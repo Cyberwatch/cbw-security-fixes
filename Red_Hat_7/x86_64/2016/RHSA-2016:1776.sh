893cea48fc0aba177efddd4910df489d3b8b743f
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-08-28 21:18:44 +0000

    RHSA: add 3 fixes

---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:1776.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1776
+#
+# Security announcement date: 2016-08-26 15:11:38 UTC
+# Script generation date:     2016-08-28 21:18:40 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.6.0-openjdk.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-debuginfo.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-devel.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-demo.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-javadoc.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-src.x86_64:1.6.0.40-1.13.12.5.el7_2
+#
+# Last versions recommanded by security team:
+#   - java-1.6.0-openjdk.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-debuginfo.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-devel.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-demo.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-javadoc.x86_64:1.6.0.40-1.13.12.5.el7_2
+#   - java-1.6.0-openjdk-src.x86_64:1.6.0.40-1.13.12.5.el7_2
+#
+# CVE List:
+#   - CVE-2016-3458
+#   - CVE-2016-3500
+#   - CVE-2016-3508
+#   - CVE-2016-3550
+#   - CVE-2016-3606
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.6.0-openjdk.x86_64-1.6.0.40 -y 
+sudo yum install java-1.6.0-openjdk-debuginfo.x86_64-1.6.0.40 -y 
+sudo yum install java-1.6.0-openjdk-devel.x86_64-1.6.0.40 -y 
+sudo yum install java-1.6.0-openjdk-demo.x86_64-1.6.0.40 -y 
+sudo yum install java-1.6.0-openjdk-javadoc.x86_64-1.6.0.40 -y 
+sudo yum install java-1.6.0-openjdk-src.x86_64-1.6.0.40 -y 
