8ad33dd6721caf8dc51775aba6820664461ebc77
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-01 21:06:03 +0000

    USN: add 4 fixes

---
+++ Ubuntu_16.04_LTS/x86_64/2016/USN-3070-3.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3070-3
+#
+# Security announcement date: 2016-08-30 00:00:00 UTC
+# Script generation date:     2016-09-01 21:05:58 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - linux-image-4.4.0-1024-snapdragon:4.4.0-1024.27
+#
+# Last versions recommanded by security team:
+#   - linux-image-4.4.0-1024-snapdragon:4.4.0-1024.27
+#
+# CVE List:
+#   - CVE-2016-1237
+#   - CVE-2016-5244
+#   - CVE-2016-5400
+#   - CVE-2016-5696
+#   - CVE-2016-5728
+#   - CVE-2016-5828
+#   - CVE-2016-5829
+#   - CVE-2016-6197
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade linux-image-4.4.0-1024-snapdragon=4.4.0-1024.27 -y
