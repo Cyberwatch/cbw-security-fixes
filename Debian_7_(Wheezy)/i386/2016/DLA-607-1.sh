d145ad257c9a8b9bf59f58f22c9d9f97e0c6f5f9
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-02 21:10:48 +0000

    DLA: add 6 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-607-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-607-1
+#
+# Security announcement date: 2016-08-31 00:00:00 UTC
+# Script generation date:     2016-09-02 21:10:45 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - tryton-server:2.2.4-1+deb7u3
+#
+# Last versions recommanded by security team:
+#   - tryton-server:2.2.4-1+deb7u3
+#
+# CVE List:
+#   - CVE-2016-1242
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tryton-server=2.2.4-1+deb7u3 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-560-2.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-560-2
+#
+# Security announcement date: 2016-08-31 00:00:00 UTC
+# Script generation date:     2016-09-02 21:10:45 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - cacti:0.8.8a+dfsg-5+deb7u10
+#
+# Last versions recommanded by security team:
+#   - cacti:0.8.8a+dfsg-5+deb7u10
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade cacti=0.8.8a+dfsg-5+deb7u10 -y
