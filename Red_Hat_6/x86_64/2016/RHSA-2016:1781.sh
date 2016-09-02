318295726dbe97c1aebabb7eb2c74631a4396900
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-02 21:17:54 +0000

    RHSA: add 2 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1781.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1781
+#
+# Security announcement date: 2016-08-31 06:12:36 UTC
+# Script generation date:     2016-09-02 21:17:49 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - rh-postgresql94-postgresql.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-contrib.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-debuginfo.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-devel.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-docs.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-libs.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-plperl.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-plpython.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-pltcl.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-server.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-static.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-test.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-upgrade.x86_64:9.4.9-1.el6
+#
+# Last versions recommanded by security team:
+#   - rh-postgresql94-postgresql.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-contrib.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-debuginfo.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-devel.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-docs.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-libs.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-plperl.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-plpython.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-pltcl.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-server.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-static.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-test.x86_64:9.4.9-1.el6
+#   - rh-postgresql94-postgresql-upgrade.x86_64:9.4.9-1.el6
+#
+# CVE List:
+#   - CVE-2016-5423
+#   - CVE-2016-5424
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install rh-postgresql94-postgresql.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-contrib.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-debuginfo.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-devel.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-docs.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-libs.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-plperl.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-plpython.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-pltcl.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-server.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-static.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-test.x86_64-9.4.9 -y 
+sudo yum install rh-postgresql94-postgresql-upgrade.x86_64-9.4.9 -y 
