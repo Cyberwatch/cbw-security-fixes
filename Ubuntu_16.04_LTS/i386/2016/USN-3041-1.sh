10a1f7a23b14359cc3a15db961aadc6eb1894334
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-14 21:08:34 +0000

    USN: add 2 fixes and update 10 fixes

--- Ubuntu_16.04_LTS/i386/2016/USN-3041-1.sh
+++ Ubuntu_16.04_LTS/i386/2016/USN-3041-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-3041-1
 #
 # Security announcement date: 2016-08-05 00:00:00 UTC
-# Script generation date:     2016-08-07 21:08:28 UTC
+# Script generation date:     2016-09-14 21:05:26 UTC
 #
 # Operating System: Ubuntu 16.04 LTS
 # Architecture: i386
@@ -18,12 +18,12 @@
 #   - oxideqt-codecs:1.16.5-0ubuntu0.16.04.1
 #
 # Last versions recommanded by security team:
-#   - liboxideqtcore0:1.16.5-0ubuntu0.16.04.1
-#   - liboxideqtcore-dev:1.16.5-0ubuntu0.16.04.1
-#   - liboxideqtquick0:1.16.5-0ubuntu0.16.04.1
-#   - liboxideqtquick-dev:1.16.5-0ubuntu0.16.04.1
-#   - liboxideqt-qmlplugin:1.16.5-0ubuntu0.16.04.1
-#   - oxideqt-codecs:1.16.5-0ubuntu0.16.04.1
+#   - liboxideqtcore0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtcore-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqt-qmlplugin:1.17.7-0ubuntu0.16.04.1
+#   - oxideqt-codecs:1.17.7-0ubuntu0.16.04.1
 #
 # CVE List:
 #   - CVE-2016-1705
@@ -45,9 +45,9 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade liboxideqtcore0=1.16.5-0ubuntu0.16.04.1 -y
-sudo apt-get install --only-upgrade liboxideqtcore-dev=1.16.5-0ubuntu0.16.04.1 -y
-sudo apt-get install --only-upgrade liboxideqtquick0=1.16.5-0ubuntu0.16.04.1 -y
-sudo apt-get install --only-upgrade liboxideqtquick-dev=1.16.5-0ubuntu0.16.04.1 -y
-sudo apt-get install --only-upgrade liboxideqt-qmlplugin=1.16.5-0ubuntu0.16.04.1 -y
-sudo apt-get install --only-upgrade oxideqt-codecs=1.16.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtcore0=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtcore-dev=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtquick0=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtquick-dev=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqt-qmlplugin=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade oxideqt-codecs=1.17.7-0ubuntu0.16.04.1 -y
