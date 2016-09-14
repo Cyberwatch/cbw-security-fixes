a4ff8985fb0972255be0a2171bad35c5710939d5
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-14 21:15:07 +0000

    DSA: update 10 fixes

--- Debian_8_(Jessie)/i386/2015/DSA-3377-1.sh
+++ Debian_8_(Jessie)/i386/2015/DSA-3377-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3377-1
 #
 # Security announcement date: 2015-10-24 00:00:00 UTC
-# Script generation date:     2016-07-28 21:10:48 UTC
+# Script generation date:     2016-09-14 21:11:19 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
@@ -27,19 +27,19 @@
 #
 # Last versions recommanded by security team:
 #   - mysql-5.5:5.5.46-0+deb8u1
-#   - libmysqlclient18:5.5.50-0+deb8u1
-#   - libmysqld-pic:5.5.50-0+deb8u1
-#   - libmysqld-dev:5.5.50-0+deb8u1
-#   - libmysqlclient-dev:5.5.50-0+deb8u1
-#   - mysql-common:5.5.50-0+deb8u1
-#   - mysql-client-5.5:5.5.50-0+deb8u1
-#   - mysql-server-core-5.5:5.5.50-0+deb8u1
-#   - mysql-server-5.5:5.5.50-0+deb8u1
-#   - mysql-server:5.5.50-0+deb8u1
-#   - mysql-client:5.5.50-0+deb8u1
-#   - mysql-testsuite:5.5.50-0+deb8u1
-#   - mysql-testsuite-5.5:5.5.50-0+deb8u1
-#   - mysql-source-5.5:5.5.50-0+deb8u1
+#   - libmysqlclient18:5.5.52-0+deb8u1
+#   - libmysqld-pic:5.5.52-0+deb8u1
+#   - libmysqld-dev:5.5.52-0+deb8u1
+#   - libmysqlclient-dev:5.5.52-0+deb8u1
+#   - mysql-common:5.5.52-0+deb8u1
+#   - mysql-client-5.5:5.5.52-0+deb8u1
+#   - mysql-server-core-5.5:5.5.52-0+deb8u1
+#   - mysql-server-5.5:5.5.52-0+deb8u1
+#   - mysql-server:5.5.52-0+deb8u1
+#   - mysql-client:5.5.52-0+deb8u1
+#   - mysql-testsuite:5.5.52-0+deb8u1
+#   - mysql-testsuite-5.5:5.5.52-0+deb8u1
+#   - mysql-source-5.5:5.5.52-0+deb8u1
 #
 # CVE List:
 #   - CVE-2015-4792
@@ -61,16 +61,16 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade mysql-5.5=5.5.46-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqlclient18=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqld-pic=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqld-dev=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqlclient-dev=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-common=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-client-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server-core-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-client=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-testsuite=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-testsuite-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-source-5.5=5.5.50-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqlclient18=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqld-pic=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqld-dev=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqlclient-dev=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-common=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-client-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server-core-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-client=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-testsuite=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-testsuite-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-source-5.5=5.5.52-0+deb8u1 -y
--- Debian_8_(Jessie)/x86_64/2016/DSA-3624-1.sh
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3624-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3624-1
 #
 # Security announcement date: 2016-07-21 00:00:00 UTC
-# Script generation date:     2016-07-28 21:11:32 UTC
+# Script generation date:     2016-09-14 21:12:15 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: x86_64
@@ -27,19 +27,19 @@
 #
 # Last versions recommanded by security team:
 #   - mysql-5.5:5.5.50-0+deb8u1
-#   - libmysqlclient18:5.5.50-0+deb8u1
-#   - libmysqld-pic:5.5.50-0+deb8u1
-#   - libmysqld-dev:5.5.50-0+deb8u1
-#   - libmysqlclient-dev:5.5.50-0+deb8u1
-#   - mysql-common:5.5.50-0+deb8u1
-#   - mysql-client-5.5:5.5.50-0+deb8u1
-#   - mysql-server-core-5.5:5.5.50-0+deb8u1
-#   - mysql-server-5.5:5.5.50-0+deb8u1
-#   - mysql-server:5.5.50-0+deb8u1
-#   - mysql-client:5.5.50-0+deb8u1
-#   - mysql-testsuite:5.5.50-0+deb8u1
-#   - mysql-testsuite-5.5:5.5.50-0+deb8u1
-#   - mysql-source-5.5:5.5.50-0+deb8u1
+#   - libmysqlclient18:5.5.52-0+deb8u1
+#   - libmysqld-pic:5.5.52-0+deb8u1
+#   - libmysqld-dev:5.5.52-0+deb8u1
+#   - libmysqlclient-dev:5.5.52-0+deb8u1
+#   - mysql-common:5.5.52-0+deb8u1
+#   - mysql-client-5.5:5.5.52-0+deb8u1
+#   - mysql-server-core-5.5:5.5.52-0+deb8u1
+#   - mysql-server-5.5:5.5.52-0+deb8u1
+#   - mysql-server:5.5.52-0+deb8u1
+#   - mysql-client:5.5.52-0+deb8u1
+#   - mysql-testsuite:5.5.52-0+deb8u1
+#   - mysql-testsuite-5.5:5.5.52-0+deb8u1
+#   - mysql-source-5.5:5.5.52-0+deb8u1
 #
 # CVE List:
 #   - CVE-2016-3477
@@ -52,16 +52,16 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade mysql-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqlclient18=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqld-pic=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqld-dev=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmysqlclient-dev=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-common=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-client-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server-core-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-server=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-client=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-testsuite=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-testsuite-5.5=5.5.50-0+deb8u1 -y
-sudo apt-get install --only-upgrade mysql-source-5.5=5.5.50-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqlclient18=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqld-pic=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqld-dev=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmysqlclient-dev=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-common=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-client-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server-core-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-server=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-client=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-testsuite=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-testsuite-5.5=5.5.52-0+deb8u1 -y
+sudo apt-get install --only-upgrade mysql-source-5.5=5.5.52-0+deb8u1 -y
