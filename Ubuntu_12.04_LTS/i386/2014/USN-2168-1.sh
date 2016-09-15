bf05e8f1a3590d8b7f98eec646339bf4955124c4
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-15 21:07:12 +0000

    USN: add 4 fixes and update 2 fixes

--- Ubuntu_12.04_LTS/i386/2014/USN-2168-1.sh
+++ Ubuntu_12.04_LTS/i386/2014/USN-2168-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2168-1
 #
 # Security announcement date: 2014-04-15 00:00:00 UTC
-# Script generation date:     2016-06-20 13:26:32 UTC
+# Script generation date:     2016-09-15 21:01:00 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -19,13 +19,13 @@
 #   - python-imaging-doc:1.1.7-4ubuntu0.12.04.1
 #
 # Last versions recommanded by security team:
-#   - python-imaging:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-dbg:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-tk:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-tk-dbg:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-sane:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-sane-dbg:1.1.7-4ubuntu0.12.04.1
-#   - python-imaging-doc:1.1.7-4ubuntu0.12.04.1
+#   - python-imaging:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-dbg:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-tk:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-tk-dbg:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-sane:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-sane-dbg:1.1.7-4ubuntu0.12.04.2
+#   - python-imaging-doc:1.1.7-4ubuntu0.12.04.2
 #
 # CVE List:
 #   - CVE-2014-1932
@@ -35,10 +35,10 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade python-imaging=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-dbg=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-tk=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-tk-dbg=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-sane=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-sane-dbg=1.1.7-4ubuntu0.12.04.1 -y
-sudo apt-get install --only-upgrade python-imaging-doc=1.1.7-4ubuntu0.12.04.1 -y
+sudo apt-get install --only-upgrade python-imaging=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-dbg=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-tk=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-tk-dbg=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-sane=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-sane-dbg=1.1.7-4ubuntu0.12.04.2 -y
+sudo apt-get install --only-upgrade python-imaging-doc=1.1.7-4ubuntu0.12.04.2 -y
---
+++ Ubuntu_16.04_LTS/x86_64/2016/USN-3078-1.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3078-1
+#
+# Security announcement date: 2016-09-13 00:00:00 UTC
+# Script generation date:     2016-09-15 21:04:17 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - mysql-server-5.7:5.7.15-0ubuntu0.16.04.1
+#   - libmysqlclient20:5.7.15-0ubuntu0.16.04.1
+#   - libmysqld-dev:5.7.15-0ubuntu0.16.04.1
+#   - libmysqlclient-dev:5.7.15-0ubuntu0.16.04.1
+#   - mysql-common:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client-core-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-server-core-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-server:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client:5.7.15-0ubuntu0.16.04.1
+#
+# Last versions recommanded by security team:
+#   - mysql-server-5.7:5.7.15-0ubuntu0.16.04.1
+#   - libmysqlclient20:5.7.15-0ubuntu0.16.04.1
+#   - libmysqld-dev:5.7.15-0ubuntu0.16.04.1
+#   - libmysqlclient-dev:5.7.15-0ubuntu0.16.04.1
+#   - mysql-common:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client-core-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-server-core-5.7:5.7.15-0ubuntu0.16.04.1
+#   - mysql-server:5.7.15-0ubuntu0.16.04.1
+#   - mysql-client:5.7.15-0ubuntu0.16.04.1
+#
+# CVE List:
+#   - CVE-2016-6662
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade mysql-server-5.7=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libmysqlclient20=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libmysqld-dev=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libmysqlclient-dev=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-common=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-client-core-5.7=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-client-5.7=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-server-core-5.7=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-server=5.7.15-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade mysql-client=5.7.15-0ubuntu0.16.04.1 -y
