30a69ecd2a870d9434011011275b7ee6e7a49122
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-15 21:14:22 +0000

    DLA: add 3 fixes and update 3 fixes

--- Debian_7_(Wheezy)/armv7l/2016/DLA-620-1.sh
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-620-1.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 # CYBERWATCH SAS - 2016
 #
-# Security fix for DLA-529-1
+# Security fix for DLA-620-1
 #
-# Security announcement date: 2016-06-26 00:00:00 UTC
-# Script generation date:     2016-09-15 21:13:23 UTC
+# Security announcement date: 2016-09-13 00:00:00 UTC
+# Script generation date:     2016-09-15 21:13:28 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: armv7l
 #
 # Vulnerable packages fix on version:
-#   - tomcat7:7.0.28-4+deb7u5
+#   - libphp-adodb:5.15-1+deb7u1
 #
 # Last versions recommanded by security team:
-#   - tomcat7:7.0.28-4+deb7u6
+#   - libphp-adodb:5.15-1+deb7u1
 #
 # CVE List:
-#   - CVE-2016-3092
+#   - CVE-2016-4855
+#   - CVE-2016-7405
 #
 # More details:
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tomcat7=7.0.28-4+deb7u6 -y
+sudo apt-get install --only-upgrade libphp-adodb=5.15-1+deb7u1 -y
