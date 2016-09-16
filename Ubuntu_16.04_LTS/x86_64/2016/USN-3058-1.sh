0c83f574991ac7ee1ae2b389cb79ea5387768034
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-16 21:04:01 +0000

    CBW: add 4 fixes

---
+++ Ubuntu_16.04_LTS/x86_64/2016/USN-3058-1.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3058-1
+#
+# Security announcement date: 2016-09-14 00:00:00 UTC
+# Script generation date:     2016-09-16 21:03:55 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - liboxideqtcore0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtcore-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqt-qmlplugin:1.17.7-0ubuntu0.16.04.1
+#   - oxideqt-codecs:1.17.7-0ubuntu0.16.04.1
+#
+# Last versions recommanded by security team:
+#   - liboxideqtcore0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtcore-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick0:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqtquick-dev:1.17.7-0ubuntu0.16.04.1
+#   - liboxideqt-qmlplugin:1.17.7-0ubuntu0.16.04.1
+#   - oxideqt-codecs:1.17.7-0ubuntu0.16.04.1
+#
+# CVE List:
+#   - CVE-2016-5141
+#   - CVE-2016-5142
+#   - CVE-2016-5143
+#   - CVE-2016-5144
+#   - CVE-2016-5145
+#   - CVE-2016-5146
+#   - CVE-2016-5167
+#   - CVE-2016-5147
+#   - CVE-2016-5148
+#   - CVE-2016-5150
+#   - CVE-2016-5153
+#   - CVE-2016-5155
+#   - CVE-2016-5156
+#   - CVE-2016-5161
+#   - CVE-2016-5164
+#   - CVE-2016-5165
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade liboxideqtcore0=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtcore-dev=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtquick0=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqtquick-dev=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade liboxideqt-qmlplugin=1.17.7-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade oxideqt-codecs=1.17.7-0ubuntu0.16.04.1 -y
---
+++ Ubuntu_16.04_LTS/x86_64/2016/USN-3079-1.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3079-1
+#
+# Security announcement date: 2016-09-14 00:00:00 UTC
+# Script generation date:     2016-09-16 21:03:55 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libwebkit2gtk-4.0-37:2.12.5-0ubuntu0.16.04.1
+#   - libjavascriptcoregtk-4.0-18:2.12.5-0ubuntu0.16.04.1
+#   - libjavascriptcoregtk-4.0-dev:2.12.5-0ubuntu0.16.04.1
+#   - gir1.2-javascriptcoregtk-4.0:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-dev:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-doc:2.12.5-0ubuntu0.16.04.1
+#   - gir1.2-webkit2-4.0:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-37-gtk2:2.12.5-0ubuntu0.16.04.1
+#
+# Last versions recommanded by security team:
+#   - libwebkit2gtk-4.0-37:2.12.5-0ubuntu0.16.04.1
+#   - libjavascriptcoregtk-4.0-18:2.12.5-0ubuntu0.16.04.1
+#   - libjavascriptcoregtk-4.0-dev:2.12.5-0ubuntu0.16.04.1
+#   - gir1.2-javascriptcoregtk-4.0:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-dev:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-doc:2.12.5-0ubuntu0.16.04.1
+#   - gir1.2-webkit2-4.0:2.12.5-0ubuntu0.16.04.1
+#   - libwebkit2gtk-4.0-37-gtk2:2.12.5-0ubuntu0.16.04.1
+#
+# CVE List:
+#   - CVE-2016-1854
+#   - CVE-2016-1856
+#   - CVE-2016-1857
+#   - CVE-2016-1858
+#   - CVE-2016-1859
+#   - CVE-2016-4583
+#   - CVE-2016-4585
+#   - CVE-2016-4586
+#   - CVE-2016-4588
+#   - CVE-2016-4589
+#   - CVE-2016-4590
+#   - CVE-2016-4591
+#   - CVE-2016-4622
+#   - CVE-2016-4623
+#   - CVE-2016-4624
+#   - CVE-2016-4651
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libwebkit2gtk-4.0-37=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libjavascriptcoregtk-4.0-18=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libjavascriptcoregtk-4.0-dev=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade gir1.2-javascriptcoregtk-4.0=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libwebkit2gtk-4.0-dev=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libwebkit2gtk-4.0-doc=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade gir1.2-webkit2-4.0=2.12.5-0ubuntu0.16.04.1 -y
+sudo apt-get install --only-upgrade libwebkit2gtk-4.0-37-gtk2=2.12.5-0ubuntu0.16.04.1 -y
