02db169e5a8541411fdd1d3a08ce09892671727c
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-18 21:18:15 +0000

    CBW: add one fix

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:1905.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:1905
+#
+# Security announcement date: 2016-09-16 07:34:22 UTC
+# Script generation date:     2016-09-18 21:18:11 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser.x86_64:53.0.2785.113-1.el6
+#   - chromium-browser-debuginfo.x86_64:53.0.2785.113-1.el6
+#
+# Last versions recommanded by security team:
+#   - chromium-browser.x86_64:53.0.2785.113-1.el6
+#   - chromium-browser-debuginfo.x86_64:53.0.2785.113-1.el6
+#
+# CVE List:
+#   - CVE-2016-5170
+#   - CVE-2016-5171
+#   - CVE-2016-5172
+#   - CVE-2016-5173
+#   - CVE-2016-5174
+#   - CVE-2016-5175
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install chromium-browser.x86_64-53.0.2785.113 -y 
+sudo yum install chromium-browser-debuginfo.x86_64-53.0.2785.113 -y 
