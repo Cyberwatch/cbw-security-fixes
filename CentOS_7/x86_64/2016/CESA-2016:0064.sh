7267ebf42b17338ee6e167a9fdc03b26da48e235
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-20 21:13:32 +0000

    CBW: update 22 fixes

--- CentOS_7/x86_64/2016/CESA-2016:0064.sh
+++ CentOS_7/x86_64/2016/CESA-2016:0064.sh
@@ -4,7 +4,7 @@
 # Security fix for CESA-2016:0064
 #
 # Security announcement date: 2016-01-26 02:08:06 UTC
-# Script generation date:     2016-08-20 21:22:02 UTC
+# Script generation date:     2016-09-20 21:12:19 UTC
 #
 # Operating System: CentOS 7
 # Architecture: x86_64
@@ -24,18 +24,18 @@
 #   - python-perf.x86_64:3.10.0-327.4.5.el7
 #
 # Last versions recommanded by security team:
-#   - kernel.x86_64:3.10.0-327.28.3.el7
-#   - kernel-abi-whitelists.noarch:3.10.0-327.28.3.el7
-#   - kernel-debug.x86_64:3.10.0-327.28.3.el7
-#   - kernel-debug-devel.x86_64:3.10.0-327.28.3.el7
-#   - kernel-devel.x86_64:3.10.0-327.28.3.el7
-#   - kernel-doc.noarch:3.10.0-327.28.3.el7
-#   - kernel-headers.x86_64:3.10.0-327.28.3.el7
-#   - kernel-tools.x86_64:3.10.0-327.28.3.el7
-#   - kernel-tools-libs.x86_64:3.10.0-327.28.3.el7
-#   - kernel-tools-libs-devel.x86_64:3.10.0-327.28.3.el7
-#   - perf.x86_64:3.10.0-327.28.3.el7
-#   - python-perf.x86_64:3.10.0-327.28.3.el7
+#   - kernel.x86_64:3.10.0-327.36.1.el7
+#   - kernel-abi-whitelists.noarch:3.10.0-327.36.1.el7
+#   - kernel-debug.x86_64:3.10.0-327.36.1.el7
+#   - kernel-debug-devel.x86_64:3.10.0-327.36.1.el7
+#   - kernel-devel.x86_64:3.10.0-327.36.1.el7
+#   - kernel-doc.noarch:3.10.0-327.36.1.el7
+#   - kernel-headers.x86_64:3.10.0-327.36.1.el7
+#   - kernel-tools.x86_64:3.10.0-327.36.1.el7
+#   - kernel-tools-libs.x86_64:3.10.0-327.36.1.el7
+#   - kernel-tools-libs-devel.x86_64:3.10.0-327.36.1.el7
+#   - perf.x86_64:3.10.0-327.36.1.el7
+#   - python-perf.x86_64:3.10.0-327.36.1.el7
 #
 # CVE List:
 #   - CVE-2016-0728
