b740e1c2edddd3407f4c50fb5556807d04b9c54f
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-24 21:12:55 +0000

    CBW: add 3 fixes

---
+++ CentOS_5/x86_64/2016/CESA-2016:1912.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1912
+#
+# Security announcement date: 2016-09-22 15:31:34 UTC
+# Script generation date:     2016-09-24 21:12:52 UTC
+#
+# Operating System: CentOS 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - firefox.i386:45.4.0-1.el5.centos
+#   - firefox.x86_64:45.4.0-1.el5.centos
+#
+# Last versions recommanded by security team:
+#   - firefox.i386:45.4.0-1.el5.centos
+#   - firefox.x86_64:45.4.0-1.el5.centos
+#
+# CVE List:
+#   - CVE-2016-5250
+#   - CVE-2016-5257
+#   - CVE-2016-5261
+#   - CVE-2016-5270
+#   - CVE-2016-5272
+#   - CVE-2016-5274
+#   - CVE-2016-5276
+#   - CVE-2016-5277
+#   - CVE-2016-5278
+#   - CVE-2016-5280
+#   - CVE-2016-5281
+#   - CVE-2016-5284
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install firefox.i386-45.4.0 -y 
+sudo yum install firefox.x86_64-45.4.0 -y 
