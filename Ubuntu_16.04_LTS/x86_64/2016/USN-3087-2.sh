fdc6887c4afd527d56f8e4d5d6271accad77a96e
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-25 21:04:36 +0000

    CBW: add 4 fixes

---
+++ Ubuntu_16.04_LTS/x86_64/2016/USN-3087-2.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3087-2
+#
+# Security announcement date: 2016-09-23 00:00:00 UTC
+# Script generation date:     2016-09-25 21:04:27 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libssl1.0.0:1.0.2g-1ubuntu4.5
+#   - openssl:1.0.2g-1ubuntu4.5
+#   - libssl-dev:1.0.2g-1ubuntu4.5
+#   - libssl-doc:1.0.2g-1ubuntu4.5
+#   - libssl1.0.0-dbg:1.0.2g-1ubuntu4.5
+#
+# Last versions recommanded by security team:
+#   - libssl1.0.0:1.0.2g-1ubuntu4.5
+#   - openssl:1.0.2g-1ubuntu4.5
+#   - libssl-dev:1.0.2g-1ubuntu4.5
+#   - libssl-doc:1.0.2g-1ubuntu4.5
+#   - libssl1.0.0-dbg:1.0.2g-1ubuntu4.5
+#
+# CVE List:
+#   - CVE-2016-2182
+#   - CVE-2016-6304
+#   - CVE-2016-2177
+#   - CVE-2016-2178
+#   - CVE-2016-2179
+#   - CVE-2016-2180
+#   - CVE-2016-2181
+#   - CVE-2016-2183
+#   - CVE-2016-6302
+#   - CVE-2016-6303
+#   - CVE-2016-6306
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libssl1.0.0=1.0.2g-1ubuntu4.5 -y
+sudo apt-get install --only-upgrade openssl=1.0.2g-1ubuntu4.5 -y
+sudo apt-get install --only-upgrade libssl-dev=1.0.2g-1ubuntu4.5 -y
+sudo apt-get install --only-upgrade libssl-doc=1.0.2g-1ubuntu4.5 -y
+sudo apt-get install --only-upgrade libssl1.0.0-dbg=1.0.2g-1ubuntu4.5 -y
