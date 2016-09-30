0979aecf366cf4a9b232b79d13b083deedb97859
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-09-30 21:11:28 +0000

    CBW: update 18 fixes

--- Debian_7_(Wheezy)/x86_64/2016/DLA-603-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-603-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-603-1
 #
 # Security announcement date: 2016-08-27 00:00:00 UTC
-# Script generation date:     2016-08-29 21:11:11 UTC
+# Script generation date:     2016-09-30 21:10:30 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - ruby-activesupport-3.2:3.2.6-6+deb7u2
 #
 # Last versions recommanded by security team:
-#   - ruby-activesupport-3.2:3.2.6-6+deb7u2
+#   - ruby-activesupport-3.2:3.2.6-6+deb7u3
 #
 # CVE List:
 #   - CVE-2015-3227
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade ruby-activesupport-3.2=3.2.6-6+deb7u2 -y
+sudo apt-get install --only-upgrade ruby-activesupport-3.2=3.2.6-6+deb7u3 -y
