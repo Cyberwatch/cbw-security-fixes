dbdd1a9e0f58b081b3bf0be16aebb7bccb682a42
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-01 21:12:56 +0000

    CBW: add 3 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:1978.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1978
+#
+# Security announcement date: 2016-09-29 18:58:54 UTC
+# Script generation date:     2016-10-01 21:12:53 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - python-twisted-web.x86_64:8.2.0-5.el6_8
+#
+# Last versions recommanded by security team:
+#   - python-twisted-web.x86_64:8.2.0-5.el6_8
+#
+# CVE List:
+#   - CVE-2016-1000111
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install python-twisted-web.x86_64-8.2.0 -y 
