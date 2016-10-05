ae2bc447227e2c03ff52a2612ee9475a0b311b7e
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-05 21:16:31 +0000

    CBW: add 3 fixes

---
+++ CentOS_7/x86_64/2016/CESA-2016:1985.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:1985
+#
+# Security announcement date: 2016-10-03 20:23:48 UTC
+# Script generation date:     2016-10-05 21:16:28 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - thunderbird.x86_64:45.4.0-1.el7.centos
+#
+# Last versions recommanded by security team:
+#   - thunderbird.x86_64:45.4.0-1.el7.centos
+#
+# CVE List:
+#   - CVE-2016-5257
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird.x86_64-45.4.0 -y 
