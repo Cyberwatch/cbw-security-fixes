004cf6c971374f72b145421c9cd771daf541cf8a
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-06 21:14:36 +0000

    CBW: update 49 fixes

--- CentOS_6/x86_64/2013/CESA-2013:1051.sh
+++ CentOS_6/x86_64/2013/CESA-2013:1051.sh
@@ -4,7 +4,7 @@
 # Security fix for CESA-2013:1051
 #
 # Security announcement date: 2013-07-17 04:56:29 UTC
-# Script generation date:     2016-08-25 21:08:43 UTC
+# Script generation date:     2016-10-06 21:12:30 UTC
 #
 # Operating System: CentOS 6
 # Architecture: x86_64
@@ -21,15 +21,15 @@
 #   - python-perf.x86_64:2.6.32-358.14.1.el6
 #
 # Last versions recommanded by security team:
-#   - kernel-doc.noarch:2.6.32-642.4.2.el6
-#   - kernel-firmware.noarch:2.6.32-642.4.2.el6
-#   - kernel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-debug.x86_64:2.6.32-642.4.2.el6
-#   - kernel-debug-devel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-devel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-headers.x86_64:2.6.32-642.4.2.el6
-#   - perf.x86_64:2.6.32-642.4.2.el6
-#   - python-perf.x86_64:2.6.32-642.4.2.el6
+#   - kernel-doc.noarch:2.6.32-642.6.1.el6
+#   - kernel-firmware.noarch:2.6.32-642.6.1.el6
+#   - kernel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-debug.x86_64:2.6.32-642.6.1.el6
+#   - kernel-debug-devel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-devel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-headers.x86_64:2.6.32-642.6.1.el6
+#   - perf.x86_64:2.6.32-642.6.1.el6
+#   - python-perf.x86_64:2.6.32-642.6.1.el6
 #
 # CVE List:
 #   - CVE-2013-2128
--- CentOS_6/x86_64/2016/CESA-2016:0715.sh
+++ CentOS_6/x86_64/2016/CESA-2016:0715.sh
@@ -4,7 +4,7 @@
 # Security fix for CESA-2016:0715
 #
 # Security announcement date: 2016-05-04 03:07:19 UTC
-# Script generation date:     2016-08-25 21:09:36 UTC
+# Script generation date:     2016-10-06 21:13:22 UTC
 #
 # Operating System: CentOS 6
 # Architecture: x86_64
@@ -23,17 +23,17 @@
 #   - python-perf.x86_64:2.6.32-573.26.1.el6
 #
 # Last versions recommanded by security team:
-#   - kernel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-abi-whitelists.noarch:2.6.32-642.4.2.el6
-#   - kernel-debug.x86_64:2.6.32-642.4.2.el6
-#   - kernel-debug-devel.i686:2.6.32-642.4.2.el6
-#   - kernel-debug-devel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-devel.x86_64:2.6.32-642.4.2.el6
-#   - kernel-doc.noarch:2.6.32-642.4.2.el6
-#   - kernel-firmware.noarch:2.6.32-642.4.2.el6
-#   - kernel-headers.x86_64:2.6.32-642.4.2.el6
-#   - perf.x86_64:2.6.32-642.4.2.el6
-#   - python-perf.x86_64:2.6.32-642.4.2.el6
+#   - kernel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-abi-whitelists.noarch:2.6.32-642.6.1.el6
+#   - kernel-debug.x86_64:2.6.32-642.6.1.el6
+#   - kernel-debug-devel.i686:2.6.32-642.6.1.el6
+#   - kernel-debug-devel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-devel.x86_64:2.6.32-642.6.1.el6
+#   - kernel-doc.noarch:2.6.32-642.6.1.el6
+#   - kernel-firmware.noarch:2.6.32-642.6.1.el6
+#   - kernel-headers.x86_64:2.6.32-642.6.1.el6
+#   - perf.x86_64:2.6.32-642.6.1.el6
+#   - python-perf.x86_64:2.6.32-642.6.1.el6
 #
 # CVE List:
 #   - CVE-2015-5157
