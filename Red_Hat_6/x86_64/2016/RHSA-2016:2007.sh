4b5d059e6778ae6d6a1ded554e505ceeec979a77
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-07 21:22:45 +0000

    CBW: add 3 fixes

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:2007.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:2007
+#
+# Security announcement date: 2016-10-05 13:09:12 UTC
+# Script generation date:     2016-10-07 21:22:41 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - chromium-browser.x86_64:53.0.2785.143-1.el6
+#   - chromium-browser-debuginfo.x86_64:53.0.2785.143-1.el6
+#
+# Last versions recommanded by security team:
+#   - chromium-browser.x86_64:53.0.2785.143-1.el6
+#   - chromium-browser-debuginfo.x86_64:53.0.2785.143-1.el6
+#
+# CVE List:
+#   - CVE-2016-5177
+#   - CVE-2016-5178
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install chromium-browser.x86_64-53.0.2785.143 -y 
+sudo yum install chromium-browser-debuginfo.x86_64-53.0.2785.143 -y 
---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:2008.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:2008
+#
+# Security announcement date: 2016-10-05 13:25:34 UTC
+# Script generation date:     2016-10-07 21:22:41 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - redhat-release-server.x86_64:7.1-1.el7_1.4
+#
+# Last versions recommanded by security team:
+#   - redhat-release-server.x86_64:7.1-1.el7_1.4
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install redhat-release-server.x86_64-7.1 -y 
