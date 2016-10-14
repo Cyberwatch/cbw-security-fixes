e4a3c9f75bbc5aff9b1699dc1ac8abd2b0676ca0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-14 21:04:29 +0000

    CBW: add 4 fixes

---
+++ Ubuntu_12.04_LTS/x86_64/2016/USN-3100-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3100-1
+#
+# Security announcement date: 2016-10-12 00:00:00 UTC
+# Script generation date:     2016-10-14 21:04:21 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libkpimutils4:4:4.8.5-0ubuntu0.3
+#
+# Last versions recommanded by security team:
+#   - libkpimutils4:4:4.8.5-0ubuntu0.3
+#
+# CVE List:
+#   - CVE-2016-7966
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libkpimutils4=4:4.8.5-0ubuntu0.3 -y
---
+++ Ubuntu_16.04_LTS/i386/2016/USN-3101-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3101-1
+#
+# Security announcement date: 2016-10-12 00:00:00 UTC
+# Script generation date:     2016-10-14 21:04:21 UTC
+#
+# Operating System: Ubuntu 16.04 LTS
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - tracker-extract:1.6.2-0ubuntu1.1
+#
+# Last versions recommanded by security team:
+#   - tracker-extract:1.6.2-0ubuntu1.1
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tracker-extract=1.6.2-0ubuntu1.1 -y
