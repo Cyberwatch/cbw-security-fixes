718cc99fe2229fc388fc77d9533e88ad18980be9
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-14 21:10:11 +0000

    CBW: add 2 fixes and update 3 fixes

--- Debian_7_(Wheezy)/i386/2013/DSA-2676-1.sh
+++ Debian_7_(Wheezy)/i386/2013/DSA-2676-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-2676-1
 #
 # Security announcement date: 2013-05-23 00:00:00 UTC
-# Script generation date:     2016-07-28 21:09:11 UTC
+# Script generation date:     2016-10-14 21:04:54 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -16,10 +16,10 @@
 #   - libxfixes-dev:1:5.0-4+deb7u1
 #
 # Last versions recommanded by security team:
-#   - libxfixes:1:5.0-4+deb7u1
-#   - libxfixes3:1:5.0-4+deb7u1
-#   - libxfixes3-dbg:1:5.0-4+deb7u1
-#   - libxfixes-dev:1:5.0-4+deb7u1
+#   - libxfixes:1:5.0-4+deb7u2
+#   - libxfixes3:1:5.0-4+deb7u2
+#   - libxfixes3-dbg:1:5.0-4+deb7u2
+#   - libxfixes-dev:1:5.0-4+deb7u2
 #
 # CVE List:
 #   - CVE-2013-1983
@@ -28,7 +28,7 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libxfixes=1:5.0-4+deb7u1 -y
-sudo apt-get install --only-upgrade libxfixes3=1:5.0-4+deb7u1 -y
-sudo apt-get install --only-upgrade libxfixes3-dbg=1:5.0-4+deb7u1 -y
-sudo apt-get install --only-upgrade libxfixes-dev=1:5.0-4+deb7u1 -y
+sudo apt-get install --only-upgrade libxfixes=1:5.0-4+deb7u2 -y
+sudo apt-get install --only-upgrade libxfixes3=1:5.0-4+deb7u2 -y
+sudo apt-get install --only-upgrade libxfixes3-dbg=1:5.0-4+deb7u2 -y
+sudo apt-get install --only-upgrade libxfixes-dev=1:5.0-4+deb7u2 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3691-1.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3691-1
+#
+# Security announcement date: 2016-10-12 00:00:00 UTC
+# Script generation date:     2016-10-14 21:07:24 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - ghostscript:9.06~dfsg-2+deb8u3
+#   - ghostscript-x:9.06~dfsg-2+deb8u3
+#   - ghostscript-doc:9.06~dfsg-2+deb8u3
+#   - libgs9:9.06~dfsg-2+deb8u3
+#   - libgs9-common:9.06~dfsg-2+deb8u3
+#   - libgs-dev:9.06~dfsg-2+deb8u3
+#   - ghostscript-dbg:9.06~dfsg-2+deb8u3
+#
+# Last versions recommanded by security team:
+#   - ghostscript:9.06~dfsg-2+deb8u3
+#   - ghostscript-x:9.06~dfsg-2+deb8u3
+#   - ghostscript-doc:9.06~dfsg-2+deb8u3
+#   - libgs9:9.06~dfsg-2+deb8u3
+#   - libgs9-common:9.06~dfsg-2+deb8u3
+#   - libgs-dev:9.06~dfsg-2+deb8u3
+#   - ghostscript-dbg:9.06~dfsg-2+deb8u3
+#
+# CVE List:
+#   - CVE-2013-5653
+#   - CVE-2016-7976
+#   - CVE-2016-7977
+#   - CVE-2016-7978
+#   - CVE-2016-7979
+#   - CVE-2016-8602
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade ghostscript=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade ghostscript-x=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade ghostscript-doc=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade libgs9=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade libgs9-common=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade libgs-dev=9.06~dfsg-2+deb8u3 -y
+sudo apt-get install --only-upgrade ghostscript-dbg=9.06~dfsg-2+deb8u3 -y
