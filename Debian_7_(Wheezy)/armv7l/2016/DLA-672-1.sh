3904cf6cfe6c776250515faa5c66105f480d3842
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-22 21:10:10 +0000

    CBW: add 3 fixes

---
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-672-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-672-1
+#
+# Security announcement date: 2016-10-20 00:00:00 UTC
+# Script generation date:     2016-10-22 21:10:07 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: armv7l
+#
+# Vulnerable packages fix on version:
+#   - bind9:1:9.8.4.dfsg.P1-6+nmu2+deb7u12
+#
+# Last versions recommanded by security team:
+#   - bind9:1:9.8.4.dfsg.P1-6+nmu2+deb7u12
+#
+# CVE List:
+#   - CVE-2016-2848
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade bind9=1:9.8.4.dfsg.P1-6+nmu2+deb7u12 -y
