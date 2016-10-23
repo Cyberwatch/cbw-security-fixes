db767d4a51c756a4cec61283d50555b9b35480d5
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-23 21:04:26 +0000

    CBW: add 2 fixes

---
+++ Ubuntu_12.04_LTS/x86_64/2016/USN-3108-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for USN-3108-1
+#
+# Security announcement date: 2016-10-21 00:00:00 UTC
+# Script generation date:     2016-10-23 21:04:20 UTC
+#
+# Operating System: Ubuntu 12.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - bind9:1:9.8.1.dfsg.P1-4ubuntu0.18
+#
+# Last versions recommanded by security team:
+#   - bind9:1:9.8.1.dfsg.P1-4ubuntu0.18
+#
+# CVE List:
+#   - CVE-2016-2848
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade bind9=1:9.8.1.dfsg.P1-4ubuntu0.18 -y
