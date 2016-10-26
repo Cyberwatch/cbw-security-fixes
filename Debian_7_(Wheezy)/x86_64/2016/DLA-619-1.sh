27ba9493955ceb8ae5056b2dbece4c6863eabdb7
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-10-26 21:12:27 +0000

    CBW: add 3 fixes and update 24 fixes

--- Debian_7_(Wheezy)/x86_64/2016/DLA-619-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-619-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-619-1
 #
 # Security announcement date: 2016-09-11 00:00:00 UTC
-# Script generation date:     2016-10-11 21:11:16 UTC
+# Script generation date:     2016-10-26 21:11:24 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - qemu-kvm:1.1.2+dfsg-6+deb7u15
 #
 # Last versions recommanded by security team:
-#   - qemu-kvm:1.1.2+dfsg-6+deb7u16
+#   - qemu-kvm:1.1.2+dfsg-6+deb7u17
 #
 # CVE List:
 #   - CVE-2016-7116
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade qemu-kvm=1.1.2+dfsg-6+deb7u16 -y
+sudo apt-get install --only-upgrade qemu-kvm=1.1.2+dfsg-6+deb7u17 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-674-1.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-674-1
+#
+# Security announcement date: 2016-10-24 00:00:00 UTC
+# Script generation date:     2016-10-26 21:11:28 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - ghostscript:9.05~dfsg-6.3+deb7u3
+#
+# Last versions recommanded by security team:
+#   - ghostscript:9.05~dfsg-6.3+deb7u3
+#
+# CVE List:
+#   - CVE-2013-5653
+#   - CVE-2016-7976
+#   - CVE-2016-7977
+#   - CVE-2016-7978
+#   - CVE-2016-7979
+#   - CVE-2016-8602
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade ghostscript=9.05~dfsg-6.3+deb7u3 -y
