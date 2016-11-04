457eabdb58945419a4eaa501b415b431171441cc
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-04 21:18:12 +0000

    CBW: add 3 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:2141.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:2141
+#
+# Security announcement date: 2016-11-02 15:59:55 UTC
+# Script generation date:     2016-11-04 21:18:09 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - bind.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-chroot.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-devel.i686:9.8.2-0.47.rc1.el6_8.3
+#   - bind-devel.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-libs.i686:9.8.2-0.47.rc1.el6_8.3
+#   - bind-libs.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-sdb.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-utils.x86_64:9.8.2-0.47.rc1.el6_8.3
+#
+# Last versions recommanded by security team:
+#   - bind.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-chroot.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-devel.i686:9.8.2-0.47.rc1.el6_8.3
+#   - bind-devel.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-libs.i686:9.8.2-0.47.rc1.el6_8.3
+#   - bind-libs.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-sdb.x86_64:9.8.2-0.47.rc1.el6_8.3
+#   - bind-utils.x86_64:9.8.2-0.47.rc1.el6_8.3
+#
+# CVE List:
+#   - CVE-2016-8864
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install bind.x86_64-9.8.2 -y 
+sudo yum install bind-chroot.x86_64-9.8.2 -y 
+sudo yum install bind-devel.i686-9.8.2 -y 
+sudo yum install bind-devel.x86_64-9.8.2 -y 
+sudo yum install bind-libs.i686-9.8.2 -y 
+sudo yum install bind-libs.x86_64-9.8.2 -y 
+sudo yum install bind-sdb.x86_64-9.8.2 -y 
+sudo yum install bind-utils.x86_64-9.8.2 -y 
