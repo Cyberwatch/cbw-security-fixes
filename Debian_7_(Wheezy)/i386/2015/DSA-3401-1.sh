64ba981c5648bc2b065151a80c3bdc909e25432d
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-06 21:13:04 +0000

    CBW: update 7 fixes

--- Debian_7_(Wheezy)/i386/2015/DSA-3401-1.sh
+++ Debian_7_(Wheezy)/i386/2015/DSA-3401-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3401-1
 #
 # Security announcement date: 2015-11-22 00:00:00 UTC
-# Script generation date:     2016-08-05 21:12:42 UTC
+# Script generation date:     2016-11-06 21:09:27 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -25,17 +25,17 @@
 #
 # Last versions recommanded by security team:
 #   - openjdk-7:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-jdk:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-jre-headless:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-jre:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-jre-lib:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-demo:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-source:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-doc:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-dbg:7u111-2.6.7-1~deb7u1
-#   - icedtea-7-jre-cacao:7u111-2.6.7-1~deb7u1
-#   - icedtea-7-jre-jamvm:7u111-2.6.7-1~deb7u1
-#   - openjdk-7-jre-zero:7u111-2.6.7-1~deb7u1
+#   - openjdk-7-jdk:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-jre-headless:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-jre:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-jre-lib:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-demo:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-source:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-doc:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-dbg:7u111-2.6.7-2~deb7u1
+#   - icedtea-7-jre-cacao:7u111-2.6.7-2~deb7u1
+#   - icedtea-7-jre-jamvm:7u111-2.6.7-2~deb7u1
+#   - openjdk-7-jre-zero:7u111-2.6.7-2~deb7u1
 #
 # CVE List:
 #   - CVE-2015-4871
@@ -45,14 +45,14 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade openjdk-7=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-jdk=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-jre-headless=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-jre=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-jre-lib=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-demo=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-source=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-doc=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-dbg=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade icedtea-7-jre-cacao=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade icedtea-7-jre-jamvm=7u111-2.6.7-1~deb7u1 -y
-sudo apt-get install --only-upgrade openjdk-7-jre-zero=7u111-2.6.7-1~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jdk=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-headless=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-lib=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-demo=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-source=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-doc=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-dbg=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade icedtea-7-jre-cacao=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade icedtea-7-jre-jamvm=7u111-2.6.7-2~deb7u1 -y
+sudo apt-get install --only-upgrade openjdk-7-jre-zero=7u111-2.6.7-2~deb7u1 -y
