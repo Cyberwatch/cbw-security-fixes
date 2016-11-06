7f498c56451ae4bae2d7ffc1b73a537d1ea20ea0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-06 21:14:41 +0000

    CBW: update 6 fixes

--- Debian_7_(Wheezy)/armv7l/2016/DLA-681-1.sh
+++ Debian_7_(Wheezy)/armv7l/2016/DLA-681-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-681-1
 #
 # Security announcement date: 2016-10-26 00:00:00 UTC
-# Script generation date:     2016-10-28 21:16:13 UTC
+# Script generation date:     2016-11-06 21:13:40 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: armv7l
@@ -13,7 +13,7 @@
 #   - tzdata:2016h-0+deb7u1
 #
 # Last versions recommanded by security team:
-#   - tzdata:2016h-0+deb7u1
+#   - tzdata:2016i-0+deb7u1
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tzdata=2016h-0+deb7u1 -y
+sudo apt-get install --only-upgrade tzdata=2016i-0+deb7u1 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-682-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-682-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-682-1
 #
 # Security announcement date: 2016-10-26 00:00:00 UTC
-# Script generation date:     2016-10-28 21:16:13 UTC
+# Script generation date:     2016-11-06 21:13:40 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - libdatetime-timezone-perl:1:1.58-1+2016h
 #
 # Last versions recommanded by security team:
-#   - libdatetime-timezone-perl:1:1.58-1+2016h
+#   - libdatetime-timezone-perl:1:1.58-1+2016i
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libdatetime-timezone-perl=1:1.58-1+2016h -y
+sudo apt-get install --only-upgrade libdatetime-timezone-perl=1:1.58-1+2016i -y
