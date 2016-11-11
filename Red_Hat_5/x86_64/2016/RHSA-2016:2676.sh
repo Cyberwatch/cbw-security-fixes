9958ef05721c3aef1e87a5c68d9b82f45310031a
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-11 21:21:13 +0000

    CBW: add 4 fixes

---
+++ Red_Hat_5/x86_64/2016/RHSA-2016:2676.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:2676
+#
+# Security announcement date: 2016-11-09 08:16:41 UTC
+# Script generation date:     2016-11-11 21:21:09 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - flash-plugin.i386:11.2.202.644-1.el5_11
+#
+# Last versions recommanded by security team:
+#   - flash-plugin.i386:11.2.202.644-1.el5_11
+#
+# CVE List:
+#   - CVE-2016-7857
+#   - CVE-2016-7858
+#   - CVE-2016-7859
+#   - CVE-2016-7860
+#   - CVE-2016-7861
+#   - CVE-2016-7862
+#   - CVE-2016-7863
+#   - CVE-2016-7864
+#   - CVE-2016-7865
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install flash-plugin.i386-11.2.202.644 -y 
---
+++ Red_Hat_7/x86_64/2016/RHSA-2016:2694.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:2694
+#
+# Security announcement date: 2016-11-09 18:48:11 UTC
+# Script generation date:     2016-11-11 21:21:09 UTC
+#
+# Operating System: Red Hat 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libgudev1.i686:219-19.el7_2.18
+#   - libgudev1.x86_64:219-19.el7_2.18
+#   - systemd.x86_64:219-19.el7_2.18
+#   - systemd-debuginfo.i686:219-19.el7_2.18
+#   - systemd-debuginfo.x86_64:219-19.el7_2.18
+#   - systemd-libs.i686:219-19.el7_2.18
+#   - systemd-libs.x86_64:219-19.el7_2.18
+#   - systemd-python.x86_64:219-19.el7_2.18
+#   - systemd-sysv.x86_64:219-19.el7_2.18
+#   - libgudev1-devel.i686:219-19.el7_2.18
+#   - libgudev1-devel.x86_64:219-19.el7_2.18
+#   - systemd-devel.i686:219-19.el7_2.18
+#   - systemd-devel.x86_64:219-19.el7_2.18
+#   - systemd-journal-gateway.x86_64:219-19.el7_2.18
+#   - systemd-networkd.x86_64:219-19.el7_2.18
+#   - systemd-resolved.i686:219-19.el7_2.18
+#   - systemd-resolved.x86_64:219-19.el7_2.18
+#
+# Last versions recommanded by security team:
+#   - libgudev1.i686:219-19.el7_2.18
+#   - libgudev1.x86_64:219-19.el7_2.18
+#   - systemd.x86_64:219-19.el7_2.18
+#   - systemd-debuginfo.i686:219-19.el7_2.18
+#   - systemd-debuginfo.x86_64:219-19.el7_2.18
+#   - systemd-libs.i686:219-19.el7_2.18
+#   - systemd-libs.x86_64:219-19.el7_2.18
+#   - systemd-python.x86_64:219-19.el7_2.18
+#   - systemd-sysv.x86_64:219-19.el7_2.18
+#   - libgudev1-devel.i686:219-19.el7_2.18
+#   - libgudev1-devel.x86_64:219-19.el7_2.18
+#   - systemd-devel.i686:219-19.el7_2.18
+#   - systemd-devel.x86_64:219-19.el7_2.18
+#   - systemd-journal-gateway.x86_64:219-19.el7_2.18
+#   - systemd-networkd.x86_64:219-19.el7_2.18
+#   - systemd-resolved.i686:219-19.el7_2.18
+#   - systemd-resolved.x86_64:219-19.el7_2.18
+#
+# CVE List:
+#   - CVE-2016-7795
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install libgudev1.i686-219 -y 
+sudo yum install libgudev1.x86_64-219 -y 
+sudo yum install systemd.x86_64-219 -y 
+sudo yum install systemd-debuginfo.i686-219 -y 
+sudo yum install systemd-debuginfo.x86_64-219 -y 
+sudo yum install systemd-libs.i686-219 -y 
+sudo yum install systemd-libs.x86_64-219 -y 
+sudo yum install systemd-python.x86_64-219 -y 
+sudo yum install systemd-sysv.x86_64-219 -y 
+sudo yum install libgudev1-devel.i686-219 -y 
+sudo yum install libgudev1-devel.x86_64-219 -y 
+sudo yum install systemd-devel.i686-219 -y 
+sudo yum install systemd-devel.x86_64-219 -y 
+sudo yum install systemd-journal-gateway.x86_64-219 -y 
+sudo yum install systemd-networkd.x86_64-219 -y 
+sudo yum install systemd-resolved.i686-219 -y 
+sudo yum install systemd-resolved.x86_64-219 -y 
