bfaaca1398a1dfec680b7a0b9a52ea47ff476106
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-11 21:13:19 +0000

    CBW: update 2 fixes

--- Debian_8_(Jessie)/i386/2016/DSA-3499-1.sh
+++ Debian_8_(Jessie)/i386/2016/DSA-3499-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3499-1
 #
 # Security announcement date: 2016-02-28 00:00:00 UTC
-# Script generation date:     2016-07-28 21:11:12 UTC
+# Script generation date:     2016-11-11 21:10:00 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
@@ -29,21 +29,21 @@
 #
 # Last versions recommanded by security team:
 #   - pillow:2.6.1-2+deb8u2
-#   - python-pil:2.6.1-2+deb8u2
-#   - python-pil-dbg:2.6.1-2+deb8u2
-#   - python-pil.imagetk:2.6.1-2+deb8u2
-#   - python-pil.imagetk-dbg:2.6.1-2+deb8u2
-#   - python-imaging-tk:2.6.1-2+deb8u2
-#   - python-sane:2.6.1-2+deb8u2
-#   - python-sane-dbg:2.6.1-2+deb8u2
-#   - python3-pil:2.6.1-2+deb8u2
-#   - python3-pil-dbg:2.6.1-2+deb8u2
-#   - python3-pil.imagetk:2.6.1-2+deb8u2
-#   - python3-pil.imagetk-dbg:2.6.1-2+deb8u2
-#   - python3-sane:2.6.1-2+deb8u2
-#   - python3-sane-dbg:2.6.1-2+deb8u2
-#   - python-pil-doc:2.6.1-2+deb8u2
-#   - python-imaging:2.6.1-2+deb8u2
+#   - python-pil:2.6.1-2+deb8u3
+#   - python-pil-dbg:2.6.1-2+deb8u3
+#   - python-pil.imagetk:2.6.1-2+deb8u3
+#   - python-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python-imaging-tk:2.6.1-2+deb8u3
+#   - python-sane:2.6.1-2+deb8u3
+#   - python-sane-dbg:2.6.1-2+deb8u3
+#   - python3-pil:2.6.1-2+deb8u3
+#   - python3-pil-dbg:2.6.1-2+deb8u3
+#   - python3-pil.imagetk:2.6.1-2+deb8u3
+#   - python3-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python3-sane:2.6.1-2+deb8u3
+#   - python3-sane-dbg:2.6.1-2+deb8u3
+#   - python-pil-doc:2.6.1-2+deb8u3
+#   - python-imaging:2.6.1-2+deb8u3
 #
 # CVE List:
 #   - CVE-2016-0740
@@ -55,18 +55,18 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade pillow=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-pil=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-pil-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-pil.imagetk=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-pil.imagetk-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-imaging-tk=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-sane=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-sane-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-pil=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-pil-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-pil.imagetk=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-pil.imagetk-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-sane=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python3-sane-dbg=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-pil-doc=2.6.1-2+deb8u2 -y
-sudo apt-get install --only-upgrade python-imaging=2.6.1-2+deb8u2 -y
+sudo apt-get install --only-upgrade python-pil=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil.imagetk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil.imagetk-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-imaging-tk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-sane=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-sane-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil.imagetk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil.imagetk-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-sane=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-sane-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil-doc=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-imaging=2.6.1-2+deb8u3 -y
