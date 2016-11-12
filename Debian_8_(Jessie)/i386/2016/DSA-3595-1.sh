2c818fd4c1bba19f65388883ed4ee7436941b67f
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-12 21:13:45 +0000

    CBW: add 2 fixes and update 10 fixes

--- Debian_8_(Jessie)/i386/2016/DSA-3595-1.sh
+++ Debian_8_(Jessie)/i386/2016/DSA-3595-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3595-1
 #
 # Security announcement date: 2016-06-05 00:00:00 UTC
-# Script generation date:     2016-07-28 21:11:28 UTC
+# Script generation date:     2016-11-12 21:10:36 UTC
 #
 # Operating System: Debian 8 (Jessie)
 # Architecture: i386
@@ -26,18 +26,18 @@
 #
 # Last versions recommanded by security team:
 #   - mariadb-10.0:10.0.25-0+deb8u1
-#   - libmariadbd-dev:10.0.26-0+deb8u1
-#   - mariadb-common:10.0.26-0+deb8u1
-#   - mariadb-client-core-10.0:10.0.26-0+deb8u1
-#   - mariadb-client-10.0:10.0.26-0+deb8u1
-#   - mariadb-server-core-10.0:10.0.26-0+deb8u1
-#   - mariadb-test-10.0:10.0.26-0+deb8u1
-#   - mariadb-server-10.0:10.0.26-0+deb8u1
-#   - mariadb-server:10.0.26-0+deb8u1
-#   - mariadb-client:10.0.26-0+deb8u1
-#   - mariadb-test:10.0.26-0+deb8u1
-#   - mariadb-connect-engine-10.0:10.0.26-0+deb8u1
-#   - mariadb-oqgraph-engine-10.0:10.0.26-0+deb8u1
+#   - libmariadbd-dev:10.0.28-0+deb8u1
+#   - mariadb-common:10.0.28-0+deb8u1
+#   - mariadb-client-core-10.0:10.0.28-0+deb8u1
+#   - mariadb-client-10.0:10.0.28-0+deb8u1
+#   - mariadb-server-core-10.0:10.0.28-0+deb8u1
+#   - mariadb-test-10.0:10.0.28-0+deb8u1
+#   - mariadb-server-10.0:10.0.28-0+deb8u1
+#   - mariadb-server:10.0.28-0+deb8u1
+#   - mariadb-client:10.0.28-0+deb8u1
+#   - mariadb-test:10.0.28-0+deb8u1
+#   - mariadb-connect-engine-10.0:10.0.28-0+deb8u1
+#   - mariadb-oqgraph-engine-10.0:10.0.28-0+deb8u1
 #
 # CVE List:
 #   - CVE-2016-0640
@@ -58,15 +58,15 @@
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
 sudo apt-get install --only-upgrade mariadb-10.0=10.0.25-0+deb8u1 -y
-sudo apt-get install --only-upgrade libmariadbd-dev=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-common=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-client-core-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-client-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-server-core-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-test-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-server-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-server=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-client=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-test=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-connect-engine-10.0=10.0.26-0+deb8u1 -y
-sudo apt-get install --only-upgrade mariadb-oqgraph-engine-10.0=10.0.26-0+deb8u1 -y
+sudo apt-get install --only-upgrade libmariadbd-dev=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-common=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-client-core-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-client-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-server-core-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-test-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-server-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-server=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-client=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-test=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-connect-engine-10.0=10.0.28-0+deb8u1 -y
+sudo apt-get install --only-upgrade mariadb-oqgraph-engine-10.0=10.0.28-0+deb8u1 -y
---
+++ Debian_8_(Jessie)/i386/2016/DSA-3710-1.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3710-1
+#
+# Security announcement date: 2016-11-10 00:00:00 UTC
+# Script generation date:     2016-11-12 21:10:50 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - pillow:2.6.1-2+deb8u3
+#   - python-pil:2.6.1-2+deb8u3
+#   - python-pil-dbg:2.6.1-2+deb8u3
+#   - python-pil.imagetk:2.6.1-2+deb8u3
+#   - python-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python-imaging-tk:2.6.1-2+deb8u3
+#   - python-sane:2.6.1-2+deb8u3
+#   - python-sane-dbg:2.6.1-2+deb8u3
+#   - python3-pil:2.6.1-2+deb8u3
+#   - python3-pil-dbg:2.6.1-2+deb8u3
+#   - python3-pil.imagetk:2.6.1-2+deb8u3
+#   - python3-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python3-sane:2.6.1-2+deb8u3
+#   - python3-sane-dbg:2.6.1-2+deb8u3
+#   - python-pil-doc:2.6.1-2+deb8u3
+#   - python-imaging:2.6.1-2+deb8u3
+#
+# Last versions recommanded by security team:
+#   - pillow:2.6.1-2+deb8u3
+#   - python-pil:2.6.1-2+deb8u3
+#   - python-pil-dbg:2.6.1-2+deb8u3
+#   - python-pil.imagetk:2.6.1-2+deb8u3
+#   - python-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python-imaging-tk:2.6.1-2+deb8u3
+#   - python-sane:2.6.1-2+deb8u3
+#   - python-sane-dbg:2.6.1-2+deb8u3
+#   - python3-pil:2.6.1-2+deb8u3
+#   - python3-pil-dbg:2.6.1-2+deb8u3
+#   - python3-pil.imagetk:2.6.1-2+deb8u3
+#   - python3-pil.imagetk-dbg:2.6.1-2+deb8u3
+#   - python3-sane:2.6.1-2+deb8u3
+#   - python3-sane-dbg:2.6.1-2+deb8u3
+#   - python-pil-doc:2.6.1-2+deb8u3
+#   - python-imaging:2.6.1-2+deb8u3
+#
+# CVE List:
+#   - CVE-2016-9189
+#   - CVE-2016-9190
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade pillow=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil.imagetk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil.imagetk-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-imaging-tk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-sane=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-sane-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil.imagetk=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-pil.imagetk-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-sane=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python3-sane-dbg=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-pil-doc=2.6.1-2+deb8u3 -y
+sudo apt-get install --only-upgrade python-imaging=2.6.1-2+deb8u3 -y
