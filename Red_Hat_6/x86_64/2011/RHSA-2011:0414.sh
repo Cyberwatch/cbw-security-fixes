08c560754be47c99ac646779b43974ce8b6490e7
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-14 21:21:22 +0000

    CBW: update one fix

--- Red_Hat_6/x86_64/2011/RHSA-2011:0414.sh
+++ Red_Hat_6/x86_64/2011/RHSA-2011:0414.sh
@@ -4,7 +4,7 @@
 # Security fix for RHSA-2011:0414
 #
 # Security announcement date: 2011-04-04 21:13:58 UTC
-# Script generation date:     2016-05-12 18:10:12 UTC
+# Script generation date:     2016-11-14 21:14:58 UTC
 #
 # Operating System: Red Hat 6
 # Architecture: x86_64
@@ -27,12 +27,12 @@
 #   - selinux-policy-minimum.noarch:3.7.19-54.el6_0.5
 #   - selinux-policy-mls.noarch:3.7.19-54.el6_0.5
 #   - selinux-policy-targeted.noarch:3.7.19-244.el6
-#   - policycoreutils.x86_64:2.0.83-19.8.el6_0
-#   - policycoreutils-debuginfo.x86_64:2.0.83-19.8.el6_0
-#   - policycoreutils-gui.x86_64:2.0.83-19.8.el6_0
-#   - policycoreutils-newrole.x86_64:2.0.83-19.8.el6_0
-#   - policycoreutils-python.x86_64:2.0.83-19.8.el6_0
-#   - policycoreutils-sandbox.x86_64:2.0.83-19.8.el6_0
+#   - policycoreutils.x86_64:2.0.83-30.1.el6_8
+#   - policycoreutils-debuginfo.x86_64:2.0.83-30.1.el6_8
+#   - policycoreutils-gui.x86_64:2.0.83-30.1.el6_8
+#   - policycoreutils-newrole.x86_64:2.0.83-30.1.el6_8
+#   - policycoreutils-python.x86_64:2.0.83-30.1.el6_8
+#   - policycoreutils-sandbox.x86_64:2.0.83-30.1.el6_8
 #   - selinux-policy-doc.noarch:3.7.19-54.el6_0.5
 #
 # CVE List:
