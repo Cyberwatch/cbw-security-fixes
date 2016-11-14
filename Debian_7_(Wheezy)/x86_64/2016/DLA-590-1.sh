c43c714fdd80b2a319b3575f089b796556826935
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-14 21:11:49 +0000

    CBW: update 9 fixes

--- Debian_7_(Wheezy)/x86_64/2016/DLA-590-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-590-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-590-1
 #
 # Security announcement date: 2016-08-09 00:00:00 UTC
-# Script generation date:     2016-10-07 21:13:55 UTC
+# Script generation date:     2016-11-14 21:10:38 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - python-django:1.4.22-1
 #
 # Last versions recommanded by security team:
-#   - python-django:1.4.22-1+deb7u1
+#   - python-django:1.4.22-1+deb7u2
 #
 # CVE List:
 #
@@ -21,4 +21,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade python-django=1.4.22-1+deb7u1 -y
+sudo apt-get install --only-upgrade python-django=1.4.22-1+deb7u2 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-649-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-649-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-649-1
 #
 # Security announcement date: 2016-10-06 00:00:00 UTC
-# Script generation date:     2016-10-08 21:11:30 UTC
+# Script generation date:     2016-11-14 21:10:42 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - python-django:1.4.22-1+deb7u1
 #
 # Last versions recommanded by security team:
-#   - python-django:1.4.22-1+deb7u1
+#   - python-django:1.4.22-1+deb7u2
 #
 # CVE List:
 #   - CVE-2016-7401
@@ -22,4 +22,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade python-django=1.4.22-1+deb7u1 -y
+sudo apt-get install --only-upgrade python-django=1.4.22-1+deb7u2 -y
