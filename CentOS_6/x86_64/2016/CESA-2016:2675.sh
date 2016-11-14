eb1aea12629d61fb0b939b16e9b3d706fa3f239c
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-14 21:13:58 +0000

    CBW: add 3 fixes

---
+++ CentOS_6/x86_64/2016/CESA-2016:2675.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for CESA-2016:2675
+#
+# Security announcement date: 2016-11-12 06:32:37 UTC
+# Script generation date:     2016-11-14 21:13:56 UTC
+#
+# Operating System: CentOS 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - pacemaker.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cli.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cluster-libs.i686:1.1.14-8.el6_8.2
+#   - pacemaker-cluster-libs.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cts.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-doc.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-libs.i686:1.1.14-8.el6_8.2
+#   - pacemaker-libs.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-libs-devel.i686:1.1.14-8.el6_8.2
+#   - pacemaker-libs-devel.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-remote.x86_64:1.1.14-8.el6_8.2
+#
+# Last versions recommanded by security team:
+#   - pacemaker.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cli.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cluster-libs.i686:1.1.14-8.el6_8.2
+#   - pacemaker-cluster-libs.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-cts.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-doc.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-libs.i686:1.1.14-8.el6_8.2
+#   - pacemaker-libs.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-libs-devel.i686:1.1.14-8.el6_8.2
+#   - pacemaker-libs-devel.x86_64:1.1.14-8.el6_8.2
+#   - pacemaker-remote.x86_64:1.1.14-8.el6_8.2
+#
+# CVE List:
+#   - CVE-2016-7035
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install pacemaker.x86_64-1.1.14 -y 
+sudo yum install pacemaker-cli.x86_64-1.1.14 -y 
+sudo yum install pacemaker-cluster-libs.i686-1.1.14 -y 
+sudo yum install pacemaker-cluster-libs.x86_64-1.1.14 -y 
+sudo yum install pacemaker-cts.x86_64-1.1.14 -y 
+sudo yum install pacemaker-doc.x86_64-1.1.14 -y 
+sudo yum install pacemaker-libs.i686-1.1.14 -y 
+sudo yum install pacemaker-libs.x86_64-1.1.14 -y 
+sudo yum install pacemaker-libs-devel.i686-1.1.14 -y 
+sudo yum install pacemaker-libs-devel.x86_64-1.1.14 -y 
+sudo yum install pacemaker-remote.x86_64-1.1.14 -y 
