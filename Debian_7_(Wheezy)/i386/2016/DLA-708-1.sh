5d6f56a46dbb7b8eab3f924565dbdc9a03d23773
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-18 21:11:32 +0000

    CBW: add 6 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-708-1.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-708-1
+#
+# Security announcement date: 2016-11-16 00:00:00 UTC
+# Script generation date:     2016-11-18 21:11:30 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - mysql-5.5:5.5.53-0+deb7u1
+#
+# Last versions recommanded by security team:
+#   - mysql-5.5:5.5.53-0+deb7u1
+#
+# CVE List:
+#   - CVE-2016-5584
+#   - CVE-2016-7440
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade mysql-5.5=5.5.53-0+deb7u1 -y
---
+++ Debian_7_(Wheezy)/i386/2016/DLA-709-1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-709-1
+#
+# Security announcement date: 2016-11-16 00:00:00 UTC
+# Script generation date:     2016-11-18 21:11:30 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - postgresql-9.1:9.1.24-0+deb7u1
+#
+# Last versions recommanded by security team:
+#   - postgresql-9.1:9.1.24-0+deb7u1
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade postgresql-9.1=9.1.24-0+deb7u1 -y
