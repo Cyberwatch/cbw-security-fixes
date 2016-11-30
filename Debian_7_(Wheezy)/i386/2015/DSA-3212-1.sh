349e88f6a23322f0190e373d6945955e797d9cb4
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-11-30 21:13:44 +0000

    CBW: update 46 fixes

--- Debian_7_(Wheezy)/i386/2015/DSA-3212-1.sh
+++ Debian_7_(Wheezy)/i386/2015/DSA-3212-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3212-1
 #
 # Security announcement date: 2015-04-02 00:00:00 UTC
-# Script generation date:     2016-11-28 21:08:20 UTC
+# Script generation date:     2016-11-30 21:09:13 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - icedove:31.6.0-1~deb7u1
 #
 # Last versions recommanded by security team:
-#   - icedove:1:45.4.0-1~deb7u1
+#   - icedove:45.4.0-1~deb7u1
 #
 # CVE List:
 #   - CVE-2015-0801
@@ -26,4 +26,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=1:45.4.0-1~deb7u1 -y
+sudo apt-get install --only-upgrade icedove=45.4.0-1~deb7u1 -y
