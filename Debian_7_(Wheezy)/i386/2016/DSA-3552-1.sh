58322b85d6cbce6438fa04d24af7cb9f2bf579c7
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-01 21:13:14 +0000

    CBW: update 14 fixes

--- Debian_7_(Wheezy)/i386/2016/DSA-3552-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DSA-3552-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3552-1
 #
 # Security announcement date: 2016-04-17 00:00:00 UTC
-# Script generation date:     2016-11-24 21:06:53 UTC
+# Script generation date:     2016-12-01 21:09:43 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - tomcat7:7.0.28-4+deb7u4
 #
 # Last versions recommanded by security team:
-#   - tomcat7:7.0.28-4+deb7u6
+#   - tomcat7:7.0.28-4+deb7u7
 #
 # CVE List:
 #   - CVE-2015-5174
@@ -28,4 +28,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tomcat7=7.0.28-4+deb7u6 -y
+sudo apt-get install --only-upgrade tomcat7=7.0.28-4+deb7u7 -y
--- Debian_7_(Wheezy)/x86_64/2016/DSA-3530-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DSA-3530-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3530-1
 #
 # Security announcement date: 2016-03-25 00:00:00 UTC
-# Script generation date:     2016-11-24 21:06:50 UTC
+# Script generation date:     2016-12-01 21:09:40 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - tomcat6:6.0.45+dfsg-1~deb7u1
 #
 # Last versions recommanded by security team:
-#   - tomcat6:6.0.45+dfsg-1~deb7u2
+#   - tomcat6:6.0.45+dfsg-1~deb7u3
 #
 # CVE List:
 #   - CVE-2013-4286
@@ -39,4 +39,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade tomcat6=6.0.45+dfsg-1~deb7u2 -y
+sudo apt-get install --only-upgrade tomcat6=6.0.45+dfsg-1~deb7u3 -y
