e093e4e90a3d47286ee6b61a7c17003d018ee068
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-02 21:09:43 +0000

    CBW: add one fix and update 6 fixes

---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3727-1.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3727-1
+#
+# Security announcement date: 2016-11-30 00:00:00 UTC
+# Script generation date:     2016-12-02 21:07:36 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libhdf5-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-cpp-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-cpp-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich2-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpi-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-doc:1.8.13+docs-15+deb8u1
+#   - hdf5-helpers:1.8.13+docs-15+deb8u1
+#   - hdf5-tools:1.8.13+docs-15+deb8u1
+#   - libhdf5-serial-dev:1.8.13+docs-15+deb8u1
+#
+# Last versions recommanded by security team:
+#   - libhdf5-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-cpp-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-cpp-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-openmpi-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-8:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich2-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpich-8-dbg:1.8.13+docs-15+deb8u1
+#   - libhdf5-mpi-dev:1.8.13+docs-15+deb8u1
+#   - libhdf5-doc:1.8.13+docs-15+deb8u1
+#   - hdf5-helpers:1.8.13+docs-15+deb8u1
+#   - hdf5-tools:1.8.13+docs-15+deb8u1
+#   - libhdf5-serial-dev:1.8.13+docs-15+deb8u1
+#
+# CVE List:
+#   - CVE-2016-4330
+#   - CVE-2016-4331
+#   - CVE-2016-4332
+#   - CVE-2016-4333
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libhdf5-8=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-8-dbg=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-cpp-8=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-cpp-8-dbg=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-dev=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-openmpi-8=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-openmpi-dev=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-openmpi-8-dbg=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-mpich-8=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-mpich-dev=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-mpich2-dev=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-mpich-8-dbg=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-mpi-dev=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-doc=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade hdf5-helpers=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade hdf5-tools=1.8.13+docs-15+deb8u1 -y
+sudo apt-get install --only-upgrade libhdf5-serial-dev=1.8.13+docs-15+deb8u1 -y
