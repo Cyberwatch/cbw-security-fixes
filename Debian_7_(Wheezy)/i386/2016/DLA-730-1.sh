6bca07b0c2b1f69c40ec07097f1dd55f66debc99
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-03 21:09:24 +0000

    CBW: add 6 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-730-1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-730-1
+#
+# Security announcement date: 2016-12-01 00:00:00 UTC
+# Script generation date:     2016-12-03 21:09:21 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - firefox-esr:45.5.1esr-1~deb7u1
+#
+# Last versions recommanded by security team:
+#   - firefox-esr:45.5.1esr-1~deb7u1
+#
+# CVE List:
+#   - CVE-2016-5290
+#   - CVE-2016-5291
+#   - CVE-2016-5296
+#   - CVE-2016-5297
+#   - CVE-2016-9064
+#   - CVE-2016-9066
+#   - CVE-2016-9079
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade firefox-esr=45.5.1esr-1~deb7u1 -y
---
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-729-1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-729-1
+#
+# Security announcement date: 2016-12-01 00:00:00 UTC
+# Script generation date:     2016-12-03 21:09:21 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - tomcat7:7.0.28-4+deb7u7
+#
+# Last versions recommanded by security team:
+#   - tomcat7:7.0.28-4+deb7u7
+#
+# CVE List:
+#   - CVE-2016-0762
+#   - CVE-2016-5018
+#   - CVE-2016-6794
+#   - CVE-2016-6796
+#   - CVE-2016-6797
+#   - CVE-2016-6816
+#   - CVE-2016-8735
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tomcat7=7.0.28-4+deb7u7 -y
