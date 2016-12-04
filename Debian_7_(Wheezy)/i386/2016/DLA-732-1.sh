6416736a9039bef64e720e0ec1d4fbf9fe6de145
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-04 21:14:43 +0000

    CBW: add 4 fixes and update 2 fixes

---
+++ Debian_7_(Wheezy)/i386/2016/DLA-732-1.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DLA-732-1
+#
+# Security announcement date: 2016-12-02 00:00:00 UTC
+# Script generation date:     2016-12-04 21:13:47 UTC
+#
+# Operating System: Debian 7 (Wheezy)
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - monit:5.4-2+deb7u1
+#   - monit:1:5.4-2+deb7u1
+#
+# Last versions recommanded by security team:
+#   - monit:1:5.4-2+deb7u1
+#   - monit:1:5.4-2+deb7u1
+#
+# CVE List:
+#   - CVE-2016-7067
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u1 -y
+sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u1 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-493-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-493-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-493-1
 #
 # Security announcement date: 2016-05-29 00:00:00 UTC
-# Script generation date:     2016-06-22 12:20:34 UTC
+# Script generation date:     2016-12-04 21:13:33 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -13,7 +13,7 @@
 #   - openafs:1.6.1-3+deb7u6
 #
 # Last versions recommanded by security team:
-#   - openafs:1.6.1-3+deb7u6
+#   - openafs:1.6.1-3+deb7u7
 #
 # CVE List:
 #   - CVE-2015-8312
@@ -24,4 +24,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade openafs=1.6.1-3+deb7u6 -y
+sudo apt-get install --only-upgrade openafs=1.6.1-3+deb7u7 -y
