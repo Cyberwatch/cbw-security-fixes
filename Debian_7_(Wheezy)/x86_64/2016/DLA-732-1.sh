cbb3520b33d703ec148254421f776e1310dae7fa
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-06 21:12:13 +0000

    CBW: update 2 fixes

--- Debian_7_(Wheezy)/x86_64/2016/DLA-732-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-732-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-732-1
 #
 # Security announcement date: 2016-12-02 00:00:00 UTC
-# Script generation date:     2016-12-04 21:13:47 UTC
+# Script generation date:     2016-12-06 21:11:22 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -14,8 +14,8 @@
 #   - monit:1:5.4-2+deb7u1
 #
 # Last versions recommanded by security team:
-#   - monit:1:5.4-2+deb7u1
-#   - monit:1:5.4-2+deb7u1
+#   - monit:1:5.4-2+deb7u2
+#   - monit:1:5.4-2+deb7u2
 #
 # CVE List:
 #   - CVE-2016-7067
@@ -24,5 +24,5 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u1 -y
-sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u1 -y
+sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u2 -y
+sudo apt-get install --only-upgrade monit=1:5.4-2+deb7u2 -y
