dfe3517ce47262387b149e6ac0f3f15d0acf2ff0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-15 21:14:05 +0000

    CBW: add 2 fixes and update 46 fixes

---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3733-1.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3733-1
+#
+# Security announcement date: 2016-12-13 00:00:00 UTC
+# Script generation date:     2016-12-15 21:10:46 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - apt:1.0.9.8.4
+#   - libapt-pkg4.12:1.0.9.8.4
+#   - libapt-inst1.5:1.0.9.8.4
+#   - apt-doc:1.0.9.8.4
+#   - libapt-pkg-dev:1.0.9.8.4
+#   - libapt-pkg-doc:1.0.9.8.4
+#   - apt-utils:1.0.9.8.4
+#   - apt-transport-https:1.0.9.8.4
+#
+# Last versions recommanded by security team:
+#   - apt:1.0.9.8.4
+#   - libapt-pkg4.12:1.0.9.8.4
+#   - libapt-inst1.5:1.0.9.8.4
+#   - apt-doc:1.0.9.8.4
+#   - libapt-pkg-dev:1.0.9.8.4
+#   - libapt-pkg-doc:1.0.9.8.4
+#   - apt-utils:1.0.9.8.4
+#   - apt-transport-https:1.0.9.8.4
+#
+# CVE List:
+#   - CVE-2016-1252
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade apt=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade libapt-pkg4.12=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade libapt-inst1.5=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade apt-doc=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade libapt-pkg-dev=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade libapt-pkg-doc=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade apt-utils=1.0.9.8.4 -y
+sudo apt-get install --only-upgrade apt-transport-https=1.0.9.8.4 -y
