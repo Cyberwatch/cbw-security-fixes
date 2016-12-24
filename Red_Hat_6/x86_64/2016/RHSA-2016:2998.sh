ac8e2323318ad6f3aea3fbe039de7c084ebb94af
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-24 21:23:32 +0000

    CBW: add one fix

---
+++ Red_Hat_6/x86_64/2016/RHSA-2016:2998.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for RHSA-2016:2998
+#
+# Security announcement date: 2016-12-22 04:22:35 UTC
+# Script generation date:     2016-12-24 21:23:25 UTC
+#
+# Operating System: Red Hat 6
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - redhat-release-server.x86_64:6Server-6.2.0.7.el6_2
+#
+# Last versions recommanded by security team:
+#   - redhat-release-server.x86_64:6Server-6.2.0.7.el6_2
+#
+# CVE List:
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install redhat-release-server.x86_64-6Server -y 
