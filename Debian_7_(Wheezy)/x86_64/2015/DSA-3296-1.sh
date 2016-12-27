6693d283088c89e9f797b1129987a978ff27ecc0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2016-12-27 21:14:14 +0000

    CBW: add one fix and update 2 fixes

--- Debian_7_(Wheezy)/x86_64/2015/DSA-3296-1.sh
+++ Debian_7_(Wheezy)/x86_64/2015/DSA-3296-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DSA-3296-1
 #
 # Security announcement date: 2015-06-29 00:00:00 UTC
-# Script generation date:     2016-07-28 21:10:33 UTC
+# Script generation date:     2016-12-27 21:09:53 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
@@ -18,12 +18,12 @@
 #   - libcrypto++-doc:5.6.1-6+deb7u1
 #
 # Last versions recommanded by security team:
-#   - libcrypto++:5.6.1-6+deb7u1
-#   - libcrypto++9:5.6.1-6+deb7u1
-#   - libcrypto++9-dbg:5.6.1-6+deb7u1
-#   - libcrypto++-dev:5.6.1-6+deb7u1
-#   - libcrypto++-utils:5.6.1-6+deb7u1
-#   - libcrypto++-doc:5.6.1-6+deb7u1
+#   - libcrypto++:5.6.1-6+deb7u3
+#   - libcrypto++9:5.6.1-6+deb7u3
+#   - libcrypto++9-dbg:5.6.1-6+deb7u3
+#   - libcrypto++-dev:5.6.1-6+deb7u3
+#   - libcrypto++-utils:5.6.1-6+deb7u3
+#   - libcrypto++-doc:5.6.1-6+deb7u3
 #
 # CVE List:
 #   - CVE-2015-2141
@@ -32,9 +32,9 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade libcrypto++=5.6.1-6+deb7u1 -y
-sudo apt-get install --only-upgrade libcrypto++9=5.6.1-6+deb7u1 -y
-sudo apt-get install --only-upgrade libcrypto++9-dbg=5.6.1-6+deb7u1 -y
-sudo apt-get install --only-upgrade libcrypto++-dev=5.6.1-6+deb7u1 -y
-sudo apt-get install --only-upgrade libcrypto++-utils=5.6.1-6+deb7u1 -y
-sudo apt-get install --only-upgrade libcrypto++-doc=5.6.1-6+deb7u1 -y
+sudo apt-get install --only-upgrade libcrypto++=5.6.1-6+deb7u3 -y
+sudo apt-get install --only-upgrade libcrypto++9=5.6.1-6+deb7u3 -y
+sudo apt-get install --only-upgrade libcrypto++9-dbg=5.6.1-6+deb7u3 -y
+sudo apt-get install --only-upgrade libcrypto++-dev=5.6.1-6+deb7u3 -y
+sudo apt-get install --only-upgrade libcrypto++-utils=5.6.1-6+deb7u3 -y
+sudo apt-get install --only-upgrade libcrypto++-doc=5.6.1-6+deb7u3 -y
---
+++ Debian_8_(Jessie)/x86_64/2016/DSA-3747-1.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2016
+#
+# Security fix for DSA-3747-1
+#
+# Security announcement date: 2016-12-25 00:00:00 UTC
+# Script generation date:     2016-12-27 21:10:54 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - exim4:4.84.2-2+deb8u2
+#   - exim4-base:4.84.2-2+deb8u2
+#   - exim4-config:4.84.2-2+deb8u2
+#   - exim4-daemon-light:4.84.2-2+deb8u2
+#   - exim4-daemon-heavy:4.84.2-2+deb8u2
+#   - eximon4:4.84.2-2+deb8u2
+#   - exim4-dbg:4.84.2-2+deb8u2
+#   - exim4-daemon-light-dbg:4.84.2-2+deb8u2
+#   - exim4-daemon-heavy-dbg:4.84.2-2+deb8u2
+#   - exim4-dev:4.84.2-2+deb8u2
+#
+# Last versions recommanded by security team:
+#   - exim4:4.84.2-2+deb8u2
+#   - exim4-base:4.84.2-2+deb8u2
+#   - exim4-config:4.84.2-2+deb8u2
+#   - exim4-daemon-light:4.84.2-2+deb8u2
+#   - exim4-daemon-heavy:4.84.2-2+deb8u2
+#   - eximon4:4.84.2-2+deb8u2
+#   - exim4-dbg:4.84.2-2+deb8u2
+#   - exim4-daemon-light-dbg:4.84.2-2+deb8u2
+#   - exim4-daemon-heavy-dbg:4.84.2-2+deb8u2
+#   - exim4-dev:4.84.2-2+deb8u2
+#
+# CVE List:
+#   - CVE-2016-9963
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade exim4=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-base=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-config=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-daemon-heavy=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade eximon4=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-dbg=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-daemon-light-dbg=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-daemon-heavy-dbg=4.84.2-2+deb8u2 -y
+sudo apt-get install --only-upgrade exim4-dev=4.84.2-2+deb8u2 -y
