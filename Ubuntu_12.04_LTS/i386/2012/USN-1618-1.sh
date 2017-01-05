255cdfb8d0b004ee7ad7e23c8d4ce74fc9ce2643
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-01-05 21:11:06 +0000

    CBW: update 5 fixes

--- Ubuntu_12.04_LTS/i386/2012/USN-1618-1.sh
+++ Ubuntu_12.04_LTS/i386/2012/USN-1618-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-1618-1
 #
 # Security announcement date: 2012-10-26 00:00:00 UTC
-# Script generation date:     2017-01-01 21:03:01 UTC
+# Script generation date:     2017-01-05 21:04:38 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: i386
@@ -17,11 +17,11 @@
 #   - exim4-daemon-light:4.76-3ubuntu3.1
 #
 # Last versions recommanded by security team:
-#   - exim4-daemon-heavy:4.76-3ubuntu3.3
-#   - exim4-daemon-light:4.76-3ubuntu3.3
+#   - exim4-daemon-heavy:4.76-3ubuntu3.4
+#   - exim4-daemon-light:4.76-3ubuntu3.4
 #   - exim4-daemon-custom:4.76-3ubuntu3.1
 #   - exim4-daemon-custom:4.76-3ubuntu3.1
-#   - exim4-daemon-light:4.76-3ubuntu3.3
+#   - exim4-daemon-light:4.76-3ubuntu3.4
 #
 # CVE List:
 #   - CVE-2012-5671
@@ -30,8 +30,8 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.3 -y
-sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.3 -y
+sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.4 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.4 -y
 sudo apt-get install --only-upgrade exim4-daemon-custom=4.76-3ubuntu3.1 -y
 sudo apt-get install --only-upgrade exim4-daemon-custom=4.76-3ubuntu3.1 -y
-sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.3 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.4 -y
--- Ubuntu_12.04_LTS/x86_64/2016/USN-2933-1.sh
+++ Ubuntu_12.04_LTS/x86_64/2016/USN-2933-1.sh
@@ -4,7 +4,7 @@
 # Security fix for USN-2933-1
 #
 # Security announcement date: 2016-03-15 00:00:00 UTC
-# Script generation date:     2017-01-01 21:05:17 UTC
+# Script generation date:     2017-01-05 21:07:01 UTC
 #
 # Operating System: Ubuntu 12.04 LTS
 # Architecture: x86_64
@@ -17,11 +17,11 @@
 #   - exim4-daemon-light:4.76-3ubuntu3.3
 #
 # Last versions recommanded by security team:
-#   - exim4-daemon-heavy:4.76-3ubuntu3.3
-#   - exim4-daemon-light:4.76-3ubuntu3.3
+#   - exim4-daemon-heavy:4.76-3ubuntu3.4
+#   - exim4-daemon-light:4.76-3ubuntu3.4
 #   - exim4-daemon-custom:4.76-3ubuntu3.3
 #   - exim4-daemon-custom:4.76-3ubuntu3.3
-#   - exim4-daemon-light:4.76-3ubuntu3.3
+#   - exim4-daemon-light:4.76-3ubuntu3.4
 #
 # CVE List:
 #   - CVE-2016-1531
@@ -31,8 +31,8 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.3 -y
-sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.3 -y
+sudo apt-get install --only-upgrade exim4-daemon-heavy=4.76-3ubuntu3.4 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.4 -y
 sudo apt-get install --only-upgrade exim4-daemon-custom=4.76-3ubuntu3.3 -y
 sudo apt-get install --only-upgrade exim4-daemon-custom=4.76-3ubuntu3.3 -y
-sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.3 -y
+sudo apt-get install --only-upgrade exim4-daemon-light=4.76-3ubuntu3.4 -y
