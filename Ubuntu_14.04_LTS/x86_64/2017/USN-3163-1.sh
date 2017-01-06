2be865f8ff76bc017bed4e2cc445096e28ea5fee
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-01-06 21:05:01 +0000

    CBW: add 7 fixes

---
+++ Ubuntu_14.04_LTS/x86_64/2017/USN-3163-1.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2017
+#
+# Security fix for USN-3163-1
+#
+# Security announcement date: 2017-01-04 00:00:00 UTC
+# Script generation date:     2017-01-06 21:04:56 UTC
+#
+# Operating System: Ubuntu 14.04 LTS
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - libnss3:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-1d:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-nssdb:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-tools:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-dev:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-dbg:2:3.26.2-0ubuntu0.14.04.3
+#
+# Last versions recommanded by security team:
+#   - libnss3:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-1d:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-nssdb:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-tools:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-dev:2:3.26.2-0ubuntu0.14.04.3
+#   - libnss3-dbg:2:3.26.2-0ubuntu0.14.04.3
+#
+# CVE List:
+#   - CVE-2016-5285
+#   - CVE-2016-8635
+#   - CVE-2016-9074
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade libnss3=2:3.26.2-0ubuntu0.14.04.3 -y
+sudo apt-get install --only-upgrade libnss3-1d=2:3.26.2-0ubuntu0.14.04.3 -y
+sudo apt-get install --only-upgrade libnss3-nssdb=2:3.26.2-0ubuntu0.14.04.3 -y
+sudo apt-get install --only-upgrade libnss3-tools=2:3.26.2-0ubuntu0.14.04.3 -y
+sudo apt-get install --only-upgrade libnss3-dev=2:3.26.2-0ubuntu0.14.04.3 -y
+sudo apt-get install --only-upgrade libnss3-dbg=2:3.26.2-0ubuntu0.14.04.3 -y
