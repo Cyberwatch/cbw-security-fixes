46b84ee23dfbbe1a50d1eec8c8ec426b8804a1e0
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-01-10 21:15:20 +0000

    CBW: add 2 fixes and update 2 fixes

---
+++ Debian_8_(Jessie)/x86_64/2017/DSA-3754-1.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2017
+#
+# Security fix for DSA-3754-1
+#
+# Security announcement date: 2017-01-08 00:00:00 UTC
+# Script generation date:     2017-01-10 21:12:11 UTC
+#
+# Operating System: Debian 8 (Jessie)
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - tomcat7:7.0.56-3+deb8u7
+#
+# Last versions recommanded by security team:
+#   - tomcat7:7.0.56-3+deb8u7
+#
+# CVE List:
+#   - CVE-2016-8745
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade tomcat7=7.0.56-3+deb8u7 -y
