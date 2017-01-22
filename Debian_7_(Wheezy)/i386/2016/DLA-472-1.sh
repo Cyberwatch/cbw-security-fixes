cac71aa02c612551d1a119b3ffce05b6fbb0c997
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-01-22 21:14:06 +0000

    CBW: update 14 fixes

--- Debian_7_(Wheezy)/i386/2016/DLA-472-1.sh
+++ Debian_7_(Wheezy)/i386/2016/DLA-472-1.sh
@@ -4,7 +4,7 @@
 # Security fix for DLA-472-1
 #
 # Security announcement date: 2016-05-14 00:00:00 UTC
-# Script generation date:     2017-01-21 21:13:58 UTC
+# Script generation date:     2017-01-22 21:12:37 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: i386
@@ -13,7 +13,7 @@
 #   - icedove:38.8.0-1~deb7u1
 #
 # Last versions recommanded by security team:
-#   - icedove:1:45.6.0-2~deb7u1
+#   - icedove:45.6.0-2~deb7u1
 #
 # CVE List:
 #   - CVE-2016-1979
@@ -24,4 +24,4 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
-sudo apt-get install --only-upgrade icedove=1:45.6.0-2~deb7u1 -y
+sudo apt-get install --only-upgrade icedove=45.6.0-2~deb7u1 -y
--- Debian_7_(Wheezy)/x86_64/2016/DLA-666-1.sh
+++ Debian_7_(Wheezy)/x86_64/2016/DLA-666-1.sh
@@ -4,12 +4,14 @@
 # Security fix for DLA-666-1
 #
 # Security announcement date: 2016-10-18 00:00:00 UTC
-# Script generation date:     2017-01-21 21:14:13 UTC
+# Script generation date:     2017-01-22 21:12:51 UTC
 #
 # Operating System: Debian 7 (Wheezy)
 # Architecture: x86_64
 #
 # Vulnerable packages fix on version:
+#   - guile-2.0:2.0.5+1-3+deb7u1
+#   - guile-2.0:2.0.5+1-3+deb7u1
 #   - guile-2.0:2.0.5+1-3+deb7u1
 #   - guile-2.0:2.0.5+1-3+deb7u1
 #   - guile-2.0:2.0.5+1-3+deb7u1
@@ -128,6 +130,8 @@
 #   - guile-2.0:2.0.5+1-3+deb7u1
 #   - guile-2.0:2.0.5+1-3+deb7u1
 #   - guile-2.0:2.0.5+1-3+deb7u1
+#   - guile-2.0:2.0.5+1-3+deb7u1
+#   - guile-2.0:2.0.5+1-3+deb7u1
 #
 # CVE List:
 #   - CVE-2016-8605
@@ -137,6 +141,8 @@
 #   - https://www.cyberwatch.fr/vulnerabilites
 #
 # Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo apt-get install --only-upgrade guile-2.0=2.0.5+1-3+deb7u1 -y
+sudo apt-get install --only-upgrade guile-2.0=2.0.5+1-3+deb7u1 -y
 sudo apt-get install --only-upgrade guile-2.0=2.0.5+1-3+deb7u1 -y
 sudo apt-get install --only-upgrade guile-2.0=2.0.5+1-3+deb7u1 -y
 sudo apt-get install --only-upgrade guile-2.0=2.0.5+1-3+deb7u1 -y
