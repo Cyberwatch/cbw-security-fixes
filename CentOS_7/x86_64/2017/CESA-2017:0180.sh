469517fdc399cfe303dfc1b8220881d0895d99b1
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-01-23 21:16:06 +0000

    CBW: add 3 fixes

---
+++ CentOS_7/x86_64/2017/CESA-2017:0180.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2017
+#
+# Security fix for CESA-2017:0180
+#
+# Security announcement date: 2017-01-21 15:55:44 UTC
+# Script generation date:     2017-01-23 21:16:01 UTC
+#
+# Operating System: CentOS 7
+# Architecture: x86_64
+#
+# Vulnerable packages fix on version:
+#   - java-1.8.0-openjdk.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-accessibility.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-accessibility-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-zip.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-zip-debug.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.121-0.b13.el7_3
+#
+# Last versions recommanded by security team:
+#   - java-1.8.0-openjdk.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-accessibility.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-accessibility-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-demo.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-demo-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-devel-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless-debug.i686:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-headless-debug.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-debug.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-zip.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-javadoc-zip-debug.noarch:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-src.x86_64:1.8.0.121-0.b13.el7_3
+#   - java-1.8.0-openjdk-src-debug.x86_64:1.8.0.121-0.b13.el7_3
+#
+# CVE List:
+#   - CVE-2016-5546
+#   - CVE-2016-5547
+#   - CVE-2016-5548
+#   - CVE-2016-5552
+#   - CVE-2017-3231
+#   - CVE-2017-3241
+#   - CVE-2017-3252
+#   - CVE-2017-3253
+#   - CVE-2017-3261
+#   - CVE-2017-3272
+#   - CVE-2017-3289
+#   - CVE-2016-2183
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install java-1.8.0-openjdk.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-accessibility.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-accessibility-debug.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-debug.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-debug.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-demo.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-demo-debug.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-devel.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-devel.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-devel-debug.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-headless.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-headless.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug.i686-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-headless-debug.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-javadoc.noarch-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-debug.noarch-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-zip.noarch-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-javadoc-zip-debug.noarch-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-src.x86_64-1.8.0.121 -y 
+sudo yum install java-1.8.0-openjdk-src-debug.x86_64-1.8.0.121 -y 
