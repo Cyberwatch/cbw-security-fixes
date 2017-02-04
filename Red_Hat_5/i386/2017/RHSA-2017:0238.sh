589a267fcc86481bbe6997eae6bda7727473dbad
Cyberwatch Security Team <support-it@cyberwatch.fr>
2017-02-04 21:16:43 +0000

    CBW: add 5 fixes

---
+++ Red_Hat_5/i386/2017/RHSA-2017:0238.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# CYBERWATCH SAS - 2017
+#
+# Security fix for RHSA-2017:0238
+#
+# Security announcement date: 2017-02-02 05:43:12 UTC
+# Script generation date:     2017-02-04 21:16:38 UTC
+#
+# Operating System: Red Hat 5
+# Architecture: i386
+#
+# Vulnerable packages fix on version:
+#   - thunderbird.i386:45.7.0-1.el5_11
+#   - thunderbird-debuginfo.i386:45.7.0-1.el5_11
+#
+# Last versions recommanded by security team:
+#   - thunderbird.i386:45.7.0-1.el5_11
+#   - thunderbird-debuginfo.i386:45.7.0-1.el5_11
+#
+# CVE List:
+#   - CVE-2017-5373
+#   - CVE-2017-5375
+#   - CVE-2017-5376
+#   - CVE-2017-5378
+#   - CVE-2017-5380
+#   - CVE-2017-5383
+#   - CVE-2017-5390
+#   - CVE-2017-5396
+#
+# More details:
+#   - https://www.cyberwatch.fr/vulnerabilites
+#
+# Licence: Released under The MIT License (MIT), See LICENSE FILE
+sudo yum install thunderbird.i386-45.7.0 -y 
+sudo yum install thunderbird-debuginfo.i386-45.7.0 -y 
